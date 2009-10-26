a8902ee4fc820f4f5c5ae9e60b08d0c286b9bf6e
Neil Bartlett <njbartlett@gmail.com>
2009-10-26 00:29:12 +0000

    Added EPL licence headers

--- name.neilbartlett.eclipse.jareditor/src/name/neilbartlett/eclipse/jareditor/internal/JAREditorContributor.java
+++ name.neilbartlett.eclipse.jareditor/src/name/neilbartlett/eclipse/jareditor/internal/JAREditorContributor.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Neil Bartlett.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Neil Bartlett - initial API and implementation
+ ******************************************************************************/
 package name.neilbartlett.eclipse.jareditor.internal;
 
 import org.eclipse.jface.action.*;
