f42c965cddb85e1b3145f14c8a5657c0d1e0a95e
Neil Bartlett <njbartlett@gmail.com>
2010-04-14 01:19:39 +0100

    Fixed bugs in LaunchDelegate and EclipseRepo Signed-off-by: Neil Bartlett <njbartlett@gmail.com>

--- bndtools.core/src/aQute/bnd/launch/LaunchDelegate.java
+++ bndtools.core/src/aQute/bnd/launch/LaunchDelegate.java
@@ -1,39 +1,45 @@
 package aQute.bnd.launch;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.eclipse.core.runtime.*;
-import org.eclipse.debug.core.*;
-import org.eclipse.jdt.core.*;
-import org.eclipse.jdt.launching.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
-import aQute.bnd.build.*;
-import aQute.bnd.plugin.*;
-import aQute.bnd.test.*;
-import aQute.lib.osgi.*;
+import aQute.bnd.build.Project;
+import aQute.bnd.plugin.Activator;
+import aQute.bnd.test.ProjectLauncher;
+import aQute.lib.osgi.Processor;
+import bndtools.Plugin;
 
 public class LaunchDelegate extends JavaLaunchDelegate {
     String   programArguments;
     String   vmArguments;
     String[] classpath;
 
-    public void launch(ILaunchConfiguration configuration, String mode,
-            ILaunch launch, IProgressMonitor monitor) throws CoreException {
+    public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
         IJavaProject javaProject = getJavaProject(configuration);
         Project model = Activator.getDefault().getCentral().getModel(javaProject);
         try {
-            ProjectLauncher launcher = new ProjectLauncher(model);
-            List<String> vmArguments = new ArrayList<String>();
-            List<String> programArguments = new ArrayList<String>();
-            launcher.getArguments(vmArguments, programArguments, false);
-            programArguments.add("-keepalive");
-            this.classpath = launcher.getClasspath();
-            this.vmArguments = Processor.join(vmArguments, " ");
-            this.programArguments = Processor.join(programArguments, " ");
+            synchronized (model) {
+                ProjectLauncher launcher = new ProjectLauncher(model);
+                List<String> vmArguments = new ArrayList<String>();
+                List<String> programArguments = new ArrayList<String>();
+                launcher.getArguments(vmArguments, programArguments, false);
+                programArguments.add("-keepalive");
+                this.classpath = launcher.getClasspath();
+                this.vmArguments = Processor.join(vmArguments, " ");
+                this.programArguments = Processor.join(programArguments, " ");
+            }
             super.launch(configuration, mode, launch, monitor);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new CoreException(new Status(IStatus.ERROR, Plugin.PLUGIN_ID, 0, "Launch error", e));
         }
     }
 
