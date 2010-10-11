71d5ac3c5bb92ed9813d9a1799582bec563d1a97
Peter Kriens <peter.kriens@aqute.biz>
2010-10-11 19:05:28 +0200

    New utils

---
+++ aQute.libg/src/aQute/lib/collections/LineCollection.java
@@ -0,0 +1,50 @@
+package aQute.lib.collections;
+
+import java.io.*;
+import java.util.*;
+
+public class LineCollection implements Iterator<String>, Closeable {
+	final BufferedReader	reader;
+	String					next;
+
+	public LineCollection(InputStream in) throws IOException {
+		this(new InputStreamReader(in));
+	}
+
+	public LineCollection(Reader reader) throws IOException {
+		this(new BufferedReader(reader));
+	}
+
+	public LineCollection(BufferedReader reader) throws IOException {
+		this.reader = reader;
+		next = reader.readLine();
+	}
+
+	public boolean hasNext() {
+		return next != null;
+	}
+
+	public String next() {
+		if (next == null)
+			throw new IllegalStateException("Iterator has finished");
+		try {
+			String result = next;
+			next = reader.readLine();
+			if (next == null)
+				reader.close();
+			return result;
+		} catch (Exception e) {
+			// ignore
+			return null;
+		}
+	}
+
+	public void remove() {
+		if (next == null)
+			throw new UnsupportedOperationException("Cannot remove");
+	}
+
+	public void close() throws IOException {
+		reader.close();
+	}
+}
---
+++ aQute.libg/src/test/TestDirectoryInputStream.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.*;
+import java.util.jar.*;
+
+import junit.framework.*;
+
+public class TestDirectoryInputStream extends TestCase {
+
+    public void testComplete() throws IOException {
+//        DirectoryInputStream din2 = new DirectoryInputStream(new File("dir"));
+//        JarInputStream in = new JarInputStream(din2);
+//        assertNotNull(in.getManifest());
+//        JarEntry entry = in.getNextJarEntry();
+//        while (entry != null) {
+//            System.out.println(entry.getName());
+//            entry = in.getNextJarEntry();
+//        }
+//
+//        in.close();
+    }
+}
