4bac24df5ce63dc15eefe0da14de83f612f803e4
Neil Bartlett <njbartlett@gmail.com>
2010-10-13 01:27:43 +0100

    Improved project template selection

---
+++ bndtools.core/src/bndtools/api/EmptyTemplate.java
@@ -0,0 +1,9 @@
+package bndtools.api;
+
+public class EmptyTemplate implements IProjectTemplate {
+
+    public void modifyInitialBndModel(IBndModel model) {
+        // noop
+    }
+
+}
--- bndtools.core/src/bndtools/wizards/project/TemplateSelectionWizardPage.java
+++ bndtools.core/src/bndtools/wizards/project/TemplateSelectionWizardPage.java
@@ -1,10 +1,13 @@
 package bndtools.wizards.project;
 
+import java.util.Arrays;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
@@ -14,20 +17,18 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import bndtools.Plugin;
 import bndtools.api.IProjectTemplate;
+import bndtools.utils.PriorityConfigurationElementCompator;
 
 public class TemplateSelectionWizardPage extends WizardPage {
 
@@ -36,10 +37,7 @@ public class TemplateSelectionWizardPage extends WizardPage {
 
     private IConfigurationElement[] elements;
 
-    private boolean useTemplate = false;
     private IProjectTemplate selectedTemplate = null;
-    private Button btnEmptyProject;
-    private Button btnUseTemplate;
     private boolean shown = false;
 
     /**
@@ -61,32 +59,10 @@ public class TemplateSelectionWizardPage extends WizardPage {
         setControl(container);
         container.setLayout(new GridLayout(1, false));
 
-        btnEmptyProject = new Button(container, SWT.RADIO);
-        btnEmptyProject.setText("Empty Project");
-        btnEmptyProject.setSelection(!useTemplate);
-
-        btnUseTemplate = new Button(container, SWT.RADIO);
-        btnUseTemplate.setText("Use a Template:");
-        btnUseTemplate.setSelection(useTemplate);
+        new Label(container, SWT.NONE).setText("Select Template:");
 
         table = new Table(container, SWT.BORDER | SWT.FULL_SELECTION);
         table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-        table.setEnabled(false);
-
-        SelectionListener radioListener = new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                useTemplate = btnUseTemplate.getSelection();
-                table.setEnabled(useTemplate);
-                if (useTemplate && elements != null && elements.length > 0 && viewer.getSelection().isEmpty()) {
-                    viewer.setSelection(new StructuredSelection(elements[0]));
-                    setSelectionFromConfigElement(elements[0]);
-                }
-                updateUI();
-            }
-        };
-        btnEmptyProject.addSelectionListener(radioListener);
-        btnUseTemplate.addSelectionListener(radioListener);
 
         viewer = new TableViewer(table);
         viewer.setContentProvider(new ArrayContentProvider());
@@ -101,10 +77,11 @@ public class TemplateSelectionWizardPage extends WizardPage {
         });
 
         loadData();
+        updateUI();
     }
 
     private void updateUI() {
-        if (useTemplate && selectedTemplate == null) {
+        if (selectedTemplate == null) {
             setPageComplete(false);
             setMessage("Select a template", IMessageProvider.INFORMATION);
         } else {
@@ -115,11 +92,20 @@ public class TemplateSelectionWizardPage extends WizardPage {
 
     private void loadData() {
         elements = Platform.getExtensionRegistry().getConfigurationElementsFor(Plugin.PLUGIN_ID, "projectTemplates");
+        Arrays.sort(elements, new PriorityConfigurationElementCompator(false));
         viewer.setInput(elements);
+
+        if (elements.length > 0) {
+            setSelectionFromConfigElement(elements[0]);
+
+            ISelection selection = new StructuredSelection(elements[0]);
+            viewer.setSelection(selection);
+        }
+
     }
 
     public IProjectTemplate getTemplate() {
-        return useTemplate ? selectedTemplate : null;
+        return selectedTemplate;
     }
 
     void setSelectionFromConfigElement(IConfigurationElement element) {
