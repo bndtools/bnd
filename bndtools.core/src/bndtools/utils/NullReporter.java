ed654fe6b814fd9ca41e2205e795a5b729bb20c7
Neil Bartlett <njbartlett@gmail.com>
2010-10-28 23:52:32 +0100

    Updated repository & fixed NPE resulting from bndlib update

---
+++ bndtools.core/src/bndtools/utils/NullReporter.java
@@ -0,0 +1,34 @@
+package bndtools.utils;
+
+import java.util.Collections;
+import java.util.List;
+
+import aQute.libg.reporter.Reporter;
+
+public class NullReporter implements Reporter {
+
+    public void error(String s, Object... args) {
+    }
+
+    public void warning(String s, Object... args) {
+    }
+
+    public void progress(String s, Object... args) {
+    }
+
+    public void trace(String s, Object... args) {
+    }
+
+    public List<String> getWarnings() {
+        return Collections.emptyList();
+    }
+
+    public List<String> getErrors() {
+        return Collections.emptyList();
+    }
+
+    public boolean isPedantic() {
+        return false;
+    }
+
+}
