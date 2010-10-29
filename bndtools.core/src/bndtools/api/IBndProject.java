dd2e5db95fd3d59aa4aaab7cac34c5ae1aa82422
Neil Bartlett <njbartlett@gmail.com>
2010-10-30 06:48:50 +0800

    Give access to IProject name in project templates.

--- bndtools.core/src/bndtools/api/IBndProject.java
+++ bndtools.core/src/bndtools/api/IBndProject.java
@@ -3,6 +3,7 @@ package bndtools.api;
 import java.net.URL;
 
 public interface IBndProject {
+    String getProjectName();
     void addResource(String path, String name, URL url);
     void addResource(String fullPath, URL url);
 }
