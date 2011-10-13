2403c3eaf7aa80bdc59c74818fbf9a7c7160b4eb
Peter Kriens <peter.kriens@aqute.biz>
2011-10-13 15:25:09 +0200

    Forker framework

--- aQute.libg/src/aQute/libg/forker/Forker.java
+++ aQute.libg/src/aQute/libg/forker/Forker.java
@@ -4,61 +4,181 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
 
+/**
+ * A Forker is good in parallel scheduling tasks with dependencies. You can add
+ * tasks with {@link #doWhen(Collection, Object, Runnable)}. The collection is
+ * the list of dependencies, the object is the target, and the runnable is run
+ * to update the target. The runnable will only run when all its dependencies
+ * have ran their associated runnable.
+ * 
+ * @author aqute
+ * 
+ * @param <T>
+ */
 public class Forker<T> {
-	final Executor executor;
-	final Set<T>	done	= new HashSet<T>();
-	final List<Job>	waiting	= new ArrayList<Job>();
-	final Semaphore semaphore = new Semaphore(0);
-	final AtomicInteger outstanding = new AtomicInteger();
-	
-	class Job {
-		Set<T>		dependencies;
-		Runnable	runnable;
+	final Executor		executor;
+	final Set<T>		done		= new HashSet<T>();
+	final List<Job>		waiting		= new ArrayList<Job>();
+	final Semaphore		semaphore	= new Semaphore(0);
+	final AtomicInteger	outstanding	= new AtomicInteger();
+	final AtomicBoolean	canceled	= new AtomicBoolean();
+
+	/**
+	 * Helper class to model a Job
+	 */
+	class Job implements Runnable {
+		T						target;
+		Set<T>					dependencies;
+		Runnable				runnable;
+		Throwable				exception;
+		volatile Thread			t;
+		volatile AtomicBoolean	canceled	= new AtomicBoolean(false);
+
+		/**
+		 * Run when the job's dependencies are done.
+		 */
+		public void run() {
+			Thread.interrupted(); // clear the interrupt flag
+
+			try {
+				synchronized (this) {
+					// Check if we go canceled
+					if (canceled.get())
+						return;
+
+					t = Thread.currentThread();
+				}
+				runnable.run();
+			} catch (Exception e) {
+				exception = e;
+			} finally {
+				synchronized (this) {
+					t = null;
+				}
+				Thread.interrupted(); // clear the interrupt flag
+				done(target);
+			}
+		}
+
+		/**
+		 * Cancel this job
+		 */
+		private void cancel() {
+			if (!canceled.getAndSet(true)) {
+				synchronized (this) {
+					if (t != null)
+						t.interrupt();
+				}
+			}
+		}
 	}
 
-
-	
+	/**
+	 * Constructor
+	 * 
+	 * @param executor
+	 */
 	public Forker(Executor executor) {
 		this.executor = executor;
 	}
-	
+
+	/**
+	 * Constructor
+	 * 
+	 */
 	public Forker() {
-		this.executor = Executors.newFixedThreadPool( 4);
+		this.executor = Executors.newFixedThreadPool(4);
 	}
-	
-	
-	
-	
-	public synchronized void doWhen(Collection<? extends T> dependencies, Runnable runnable) {
-		System.out.println("doWhen " + dependencies );
+
+	/**
+	 * Schedule a job for execution when the dependencies are done of target are
+	 * done.
+	 * 
+	 * @param dependencies the dependencies that must have run
+	 * @param target the target, is removed from all the dependencies when it ran
+	 * @param runnable the runnable to run
+	 */
+	public synchronized void doWhen(Collection<? extends T> dependencies, T target,
+			Runnable runnable) {
+		System.out.println("doWhen " + dependencies);
 		outstanding.incrementAndGet();
 		Job job = new Job();
 		job.dependencies = new HashSet<T>(dependencies);
 		job.dependencies.removeAll(done);
-		if ( job.dependencies.isEmpty())
-			executor.execute(runnable);
-		else {
-			job.runnable = runnable;
+		job.target = target;
+
+		job.runnable = runnable;
+		if (job.dependencies.isEmpty()) {
+			executor.execute(job);
+		} else {
 			waiting.add(job);
 		}
 	}
 
-	public synchronized void done( T done ) {
-		System.out.println("done " + done);
-		semaphore.release();
-		for ( Job job : waiting ) {
-			if ( job.dependencies.remove(done) && job.dependencies.isEmpty() ) {
-				System.out.println("scheduling " + job.dependencies);
-				executor.execute(job.runnable);
+	/**
+	 * Called when the target has ran by the Job.
+	 * 
+	 * @param done
+	 */
+	private void done(T done) {
+		List<Runnable> torun = new ArrayList<Runnable>();
+		synchronized (this) {
+			System.out.println("done " + done);
+			semaphore.release();
+
+			for (Iterator<Job> e = waiting.iterator(); e.hasNext();) {
+				Job job = e.next();
+				if (job.dependencies.remove(done) && job.dependencies.isEmpty()) {
+					System.out.println("scheduling " + job.target);
+					torun.add(job);
+					e.remove();
+				}
 			}
 		}
+		for (Runnable r : torun)
+			executor.execute(r);
 	}
-	
 
-
+	/**
+	 * Wait until all jobs have run.
+	 * 
+	 * @throws InterruptedException
+	 */
 	public void join() throws InterruptedException {
 		System.out.println("join " + outstanding + " " + semaphore);
+		check();
 		semaphore.acquire(outstanding.getAndSet(0));
 	}
 
+	/**
+	 * Check that we have no jobs that can never be satisfied. I.e. if 
+	 * the dependencies contain a target that is not listed.
+	 */
+	private void check() {
+		// TODO
+	}
+
+	/**
+	 * Return the number of outstanding jobs
+	 * @return outstanding jobs
+	 */
+	public int getOutstanding() {
+		return semaphore.availablePermits();
+	}
+
+	/**
+	 * Cancel the forker.
+	 * 
+	 * @throws InterruptedException
+	 */
+	public void cancel() throws InterruptedException {
+		System.out.println("canceled " + outstanding + " " + semaphore);
+
+		if (!canceled.getAndSet(true)) {
+			for (Job job : waiting) {
+				job.cancel();
+			}
+		}
+		join();
+	}
 }
--- aQute.libg/src/test/TestForker.java
+++ aQute.libg/src/test/TestForker.java
@@ -8,20 +8,18 @@ import aQute.libg.forker.*;
 public class TestForker extends TestCase {
 
 	final Collection<Integer> EMPTY = Collections.emptyList();
+	
 	static class R implements Runnable {
 		final Collection<Integer> result;
 		final int n;
-		final Forker<Integer> forker;
 		
-		R(Collection<Integer> result, int n, Forker<Integer> forker ) {
+		R(Collection<Integer> result, int n) {
 			this.result = result;
 			this.n=n;
-			this.forker = forker;
 		}
 		
 		public void run() {
 			result.add(n);
-			forker.done(n);
 		}
 	}
 	
@@ -29,9 +27,9 @@ public class TestForker extends TestCase {
 		final Forker<Integer> forker = new Forker<Integer>();
 		final Collection<Integer> result = Collections.synchronizedList( new ArrayList<Integer>());
 		
-		forker.doWhen( Arrays.asList(3), new R(result, 2,forker));
-		forker.doWhen( Arrays.asList(2), new R(result,1,forker));		
-		forker.doWhen( EMPTY, new R(result,3,forker));		
+		forker.doWhen( Arrays.asList(3), 2, new R(result, 2));
+		forker.doWhen( Arrays.asList(2), 1, new R(result,1));		
+		forker.doWhen( EMPTY, 3, new R(result,3));		
 		forker.join();
 		assertEquals( Arrays.asList(3,2,1), result);		
 	}
@@ -40,11 +38,40 @@ public class TestForker extends TestCase {
 		final Forker<Integer> forker = new Forker<Integer>();
 		final Collection<Integer> result = Collections.synchronizedList( new ArrayList<Integer>());
 		
-		forker.doWhen( Arrays.asList(1,2,3), new R(result,4,forker));
-		forker.doWhen( EMPTY, new R(result,1,forker));
-		forker.doWhen( EMPTY, new R(result,2,forker));
-		forker.doWhen( EMPTY, new R(result,3,forker));
+		forker.doWhen( Arrays.asList(1,2,3), 4, new R(result,4));
+		forker.doWhen( EMPTY, 1, new R(result,1));
+		forker.doWhen( EMPTY, 2, new R(result,2));
+		forker.doWhen( EMPTY, 3, new R(result,3));
 		forker.join();
 		assertEquals( Arrays.asList(1,2,3,4), result);		
+	}
+	
+	
+	public void testCancel() throws InterruptedException {
+		final Forker<Integer> forker = new Forker<Integer>();
+		final Collection<Integer> result = Collections.synchronizedList( new ArrayList<Integer>());
+		
+		forker.doWhen( EMPTY, 4, new Runnable() {
+
+			public void run() {
+				synchronized(result) {
+					try {
+						System.out.println("starting to wait");
+						wait();
+						System.out.println("finished wait");
+					} catch (InterruptedException e) {
+						System.out.println("exception");
+						e.printStackTrace();
+					} finally {
+						System.out.println("leaving task");						
+					}
+				}
+			}
+			
+		});
+		Thread.sleep(100);
+		assertEquals(1, forker.getOutstanding());
+		forker.cancel();
+		assertEquals(0, forker.getOutstanding());
 	}
 }
