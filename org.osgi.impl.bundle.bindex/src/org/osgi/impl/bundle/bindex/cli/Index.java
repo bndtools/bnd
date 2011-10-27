988dcab6e1808321020f7114163b1ed8861ed4b2
Ferry Huberts <ferry.huberts@pelagic.nl>
2011-10-27 16:00:04 +0200

    move Index into cli package
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- org.osgi.impl.bundle.bindex/src/org/osgi/impl/bundle/bindex/cli/Index.java
+++ org.osgi.impl.bundle.bindex/src/org/osgi/impl/bundle/bindex/cli/Index.java
@@ -13,12 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.osgi.impl.bundle.bindex;
+package org.osgi.impl.bundle.bindex.cli;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.osgi.impl.bundle.bindex.Indexer;
 import org.osgi.impl.bundle.obr.resource.RepositoryImpl;
 
 public class Index {
