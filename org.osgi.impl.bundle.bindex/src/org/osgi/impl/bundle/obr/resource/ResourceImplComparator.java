23105bca1e7026b59ba306d13dabba15f83b08ac
Ferry Huberts <ferry.huberts@pelagic.nl>
2011-10-27 22:09:15 +0200

    improve the comparator
    It would only compare the name, thereby deeming
    biz.aQute.bndlib v1.45.0 and biz.aQute.bndlib v1.46.0 equal,
    since these both have the name biz.aQute.bndlib. we now account
    for the version as well, since that is in the toString.
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- org.osgi.impl.bundle.bindex/src/org/osgi/impl/bundle/obr/resource/ResourceImplComparator.java
+++ org.osgi.impl.bundle.bindex/src/org/osgi/impl/bundle/obr/resource/ResourceImplComparator.java
@@ -3,18 +3,14 @@ package org.osgi.impl.bundle.obr.resource;
 import java.util.Comparator;
 
 public class ResourceImplComparator implements Comparator<ResourceImpl> {
-	private String getName(ResourceImpl impl) {
-		String s = impl.getSymbolicName();
-		if (s != null) {
-			return s;
-		} else {
-			return "no-symbolic-name";
-		}
-	}
-
 	public int compare(ResourceImpl r1, ResourceImpl r2) {
-		String s1 = getName(r1);
-		String s2 = getName(r2);
-		return s1.compareTo(s2);
+		if (r2 == null) {
+			return -1;
+		}
+		if (r1 == null) {
+			return 1;
+		}
+
+		return r1.toString().compareTo(r2.toString());
 	}
 }
