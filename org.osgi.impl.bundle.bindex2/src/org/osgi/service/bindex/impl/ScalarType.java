f0d350bd428e36b530a6ba038918e3ea20d89ecf
Neil Bartlett <njbartlett@gmail.com>
2012-02-12 12:11:00 +0000

    [rfc112]: REFACTORED org.osgi.impl.bundle.bindex2 => bindex2

--- org.osgi.impl.bundle.bindex2/src/org/osgi/service/bindex/impl/ScalarType.java
+++
@@ -1,5 +0,0 @@
-package org.osgi.service.bindex.impl;
-
-public enum ScalarType {
-	String, Version, Long, Double;
-}
--- org.osgi.impl.bundle.bindex2/src/org/osgi/service/bindex/impl/Yield.java
+++
@@ -1,7 +0,0 @@
-package org.osgi.service.bindex.impl;
-
-public interface Yield<T> {
-
-	void yield(T obj);
-
-}
