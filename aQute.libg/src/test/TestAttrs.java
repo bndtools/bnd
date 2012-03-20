7e3e1380606ee5aa3c6ef6ae8026a5a32ddbc092
Peter Kriens <peter.kriens@aqute.biz>
2012-03-20 16:06:26 +0100

    Testcase for type attrs

---
+++ aQute.libg/src/test/TestAttrs.java
@@ -0,0 +1,21 @@
+package test;
+
+import junit.framework.*;
+import aQute.libg.header.*;
+
+public class TestAttrs extends TestCase {
+
+	
+	public void testAttrs() {
+		Attrs attrs = new Attrs();
+		attrs.put("a", "aa");
+		attrs.put("a:", "ab");
+		attrs.put("along:Long", "123");
+		attrs.put("aversion:Version", "1.2.3");
+		
+		assertEquals( "aa", attrs.get("a"));
+		assertEquals( "ab", attrs.get("a:"));
+		assertEquals( "123", attrs.get("along"));
+		assertEquals( "1.2.3", attrs.get("aversion"));
+	}
+}
