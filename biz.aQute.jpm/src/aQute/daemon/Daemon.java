632da1da579c887695fc512c2c0af5626b822b91
Peter Kriens <peter.kriens@aqute.biz>
2012-04-10 11:46:33 +0200

    Added parameters to the test daemon to see if they are passed

--- biz.aQute.jpm/src/aQute/daemon/Daemon.java
+++ biz.aQute.jpm/src/aQute/daemon/Daemon.java
@@ -4,8 +4,15 @@ public class Daemon {
 
 	public static void  main(String args[]) throws Exception {
 		int n = 1;
+		int inc = 1;
+		if ( args.length > 0)
+			inc = Integer.parseInt(args[0]);
+		if ( args.length > 1)
+			n = Integer.parseInt(args[1]);
+		
 		while ( true ) {
-			System.out.println("count " + n++);
+			System.out.println("count " + n);
+			n += inc;
 			Thread.sleep(3000);
 		}
 	}
