0a6a22fd3be3e3c77a99e5f0b9acfe2bb9898894
Peter Kriens <peter.kriens@aqute.biz>
2012-04-10 10:31:28 +0200

    Testing

---
+++ biz.aQute.jpm/test/test/JPMTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import static org.mockito.Mockito.*;
+
+import java.io.*;
+
+import junit.framework.*;
+import aQute.jpm.lib.*;
+import aQute.jpm.platform.*;
+import aQute.libg.reporter.*;
+
+public class JPMTest extends TestCase {
+	File	cwd	= new File(System.getProperty("user.dir")).getAbsoluteFile();
+
+	class PLF extends Unix {
+
+		@Override public void shell(String initial) throws Exception {
+		}
+
+		@Override public String getName() {
+			return "Test Platform";
+		}
+
+		@Override public void uninstall() {
+		}
+
+		@Override public File getGlobal() {
+			return new File(cwd, "global").getAbsoluteFile();
+		}
+
+		@Override public File getLocal() {
+			return new File(cwd, "local").getAbsoluteFile();
+		}
+
+		@Override public String createCommand(CommandData data) throws Exception {
+			return null;
+		}
+
+		@Override public String createService(ServiceData data) throws Exception {
+			return null;
+		}
+	};
+
+	public void testSimple() {
+		Reporter r = new ReporterAdapter();
+		JustAnotherPackageManager jpm = new JustAnotherPackageManager(r);
+		Platform plf = new PLF();
+		jpm.setPlatform(plf);
+
+	}
+}
