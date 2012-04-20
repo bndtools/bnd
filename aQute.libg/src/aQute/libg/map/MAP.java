429680ed8a00de36bab89e2ab19950ad5b89b4eb
Peter Kriens <peter.kriens@aqute.biz>
2012-04-20 15:53:38 +0200

    Added dictionary converter

--- aQute.libg/src/aQute/libg/map/MAP.java
+++ aQute.libg/src/aQute/libg/map/MAP.java
@@ -2,6 +2,12 @@ package aQute.libg.map;
 
 import java.util.*;
 
+/**
+ * Easy way to build a map:
+ * 
+ * 	Map<String,Integer> s = MAP.$("a",2).$("b",3);
+ *
+ */
 public class MAP {
 
 	static public class MAPX<K, V> extends LinkedHashMap<K, V> {
@@ -15,6 +21,17 @@ public class MAP {
 	public static <Kx, Vx> MAPX<Kx, Vx> $(Kx key, Vx value) {
 		MAPX<Kx, Vx> map = new MAPX<Kx, Vx>();
 		map.put(key, value);
+		return map;
+	}
+	
+	
+	public <K,V> Map<K,V> dictionary(Dictionary<K,V> dict ) {
+		Map<K,V> map = new LinkedHashMap<K, V>();
+		for ( Enumeration<K> e = dict.keys(); e.hasMoreElements(); ) {
+			K k = e.nextElement();
+			V v = dict.get(k);
+			map.put(k,v);
+		}
 		return map;
 	}
 }
