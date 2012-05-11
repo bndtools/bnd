eb9974cb94ad18ec75528482d3725fe19acb5d99
Neil Bartlett <njbartlett@gmail.com>
2012-05-11 10:08:58 +0100

    Made GitOBRRepo tests network- and location-independent

--- bndtools.bndplugins/test/test/bndtools/bndplugins/repo/git/TestGitOBRRepo.java
+++ bndtools.bndplugins/test/test/bndtools/bndplugins/repo/git/TestGitOBRRepo.java
@@ -7,24 +7,39 @@ import java.util.Map;
 
 import junit.framework.TestCase;
 import aQute.bnd.service.RepositoryPlugin.Strategy;
+import aQute.lib.io.IO;
 import bndtools.bndplugins.repo.git.GitOBRRepo;
 
 public class TestGitOBRRepo extends TestCase {
+	
+	private File checkoutDir = new File("generated/test-gitcheckout-tmp");
+	
+	@Override
+	protected void setUp() throws Exception {
+		IO.delete(checkoutDir);
+		checkoutDir.mkdirs();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		IO.delete(checkoutDir);
+	}
 
 	public void testGitRepo() throws Exception {
-
 		GitOBRRepo repo = getOBRRepo();
 		File bundleFile = repo.get("osgi.core", "[4.2, 4.3)", Strategy.HIGHEST, null);
 		assertNotNull("Repository returned null", bundleFile);
-
+		assertEquals(new File(checkoutDir, "jars/osgi.core/osgi.core-4.2.0.jar").getAbsoluteFile(), bundleFile);
 	}
 
-	private static GitOBRRepo getOBRRepo() throws IOException {
-
+	private GitOBRRepo getOBRRepo() throws IOException {
+		String repoUri = new File("testdata/testrepo.git").getAbsoluteFile().toURI().toString();
+		
 		Map<String, String> properties = new HashMap<String, String>();
-		properties.put(GitOBRRepo.PROP_GIT_URI, "git://github.com/bndtools/repo.git");
-		properties.put(GitOBRRepo.PROP_GIT_PUSH_URI, "git@github.com:bndtools/repo.git");
+		properties.put(GitOBRRepo.PROP_GIT_URI, repoUri);
+		properties.put(GitOBRRepo.PROP_GIT_PUSH_URI, repoUri);
 		properties.put(GitOBRRepo.PROP_GIT_BRANCH, "master");
+		properties.put(GitOBRRepo.PROP_LOCAL_DIR, checkoutDir.getAbsolutePath());
 		properties.put(GitOBRRepo.PROP_LOCAL_SUB_DIR, "jars");
 		properties.put(GitOBRRepo.PROP_REPO_TYPE, GitOBRRepo.REPO_TYPE_OBR);
 
