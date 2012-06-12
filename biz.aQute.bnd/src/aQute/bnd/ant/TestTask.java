2707c4a8b546bf3441a8bf9dd4da7483825af397
Neil Bartlett <njbartlett@gmail.com>
2012-06-12 10:45:29 +0100

    Make bndtest task run a series of bndrun files

--- biz.aQute.bnd/src/aQute/bnd/ant/TestTask.java
+++ biz.aQute.bnd/src/aQute/bnd/ant/TestTask.java
@@ -1,5 +1,11 @@
 package aQute.bnd.ant;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.apache.tools.ant.BuildException;
 
 import aQute.bnd.build.Project;
@@ -9,38 +15,75 @@ import aQute.bnd.build.Workspace;
 public class TestTask extends BaseTask {
 	
 	private boolean continuous = false;
+	private String runFiles = null;
 	
 	@Override
 	public void execute() throws BuildException {
+		
 		try {
-			Project project = Workspace.getProject(getProject().getBaseDir());
-			project.clear();
-			
-			ProjectTester tester = project.getProjectTester();
-			tester.setContinuous(continuous);
-			tester.prepare();
-
-			if (report(project))
-				throw new BuildException("Failed to initialise for testing.");
-			
-			int errors = tester.test();
-			if (errors == 0) {
-				System.err.println("All tests passed");
+			// Prepare list of projects...
+			List<Project> projects;
+			File baseDir = getProject().getBaseDir();
+			Project baseProject = Workspace.getProject(baseDir);
+			if (runFiles == null) {
+				projects = Collections.singletonList(baseProject);
 			} else {
-				if (errors > 0) {
-					System.err.println(errors + " Error(s)");
-				} else
-					System.err.println("Error " + errors);
-				throw new BuildException("Tests failed");
+				StringTokenizer tokenizer = new StringTokenizer(runFiles, ",");
+				projects = new LinkedList<Project>();
+				while (tokenizer.hasMoreTokens()) {
+					String runFilePath = tokenizer.nextToken().trim();
+					Project runProject;
+					if (".".equals(runFilePath)) {
+						runProject = baseProject;
+					} else {
+						File runFile = new File(baseDir, runFilePath);
+						if (!runFile.isFile())
+							throw new BuildException(String.format("Run file %s does not exist (or is not a file).", runFile.getAbsolutePath()));
+						runProject = new Project(baseProject.getWorkspace(), baseDir, runFile);
+						runProject.setParent(baseProject);
+					}
+					projects.add(runProject);
+				}
 			}
 			
-			if (report(project))
-				throw new BuildException("Tests failed");
-			
+			// Test them
+			for (Project project : projects) {
+				executeProject(project);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new BuildException(e);
 		}
+	}
+	
+	private void executeProject(Project project) throws Exception {
+		System.out.println("Testing " + project.getPropertiesFile());
+		project.clear();
+		
+		ProjectTester tester = project.getProjectTester();
+		tester.setContinuous(continuous);
+		tester.prepare();
+
+		if (report(project))
+			throw new BuildException("Failed to initialise for testing.");
+		
+		int errors = tester.test();
+		if (errors == 0) {
+			System.err.println("All tests passed");
+		} else {
+			if (errors > 0) {
+				System.err.println(errors + " Error(s)");
+			} else
+				System.err.println("Error " + errors);
+			throw new BuildException("Tests failed");
+		}
+		
+		if (report(project))
+			throw new BuildException("Tests failed");
+	}
+	
+	public void setRunfiles(String runFiles) {
+		this.runFiles = runFiles;
 	}
 	
 }
