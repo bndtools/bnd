64ad5cac5ac2d47b4cff733357d3001b23a00053
Peter Kriens <peter.kriens@aqute.biz>
2012-06-14 10:42:40 +0200

    Added mockito for testing

---
+++ biz.aQute.bndlib.tests/src/test/RepoTreeTest.java
@@ -0,0 +1,58 @@
+
+package test;
+
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+
+import junit.framework.*;
+import aQute.bnd.differ.*;
+import aQute.bnd.service.*;
+import aQute.bnd.service.diff.*;
+import aQute.libg.version.*;
+
+public class RepoTreeTest extends TestCase {
+	public void testSimple() throws Exception {
+		RepositoryPlugin a = mock(RepositoryPlugin.class);
+		RepositoryPlugin b = mock(RepositoryPlugin.class);
+		
+		when(a.getName()).thenReturn("a");
+		when(a.list(null)).thenReturn(Arrays.asList("a","b"));
+		when(a.versions("a")).thenReturn(Arrays.asList(new Version("1"), new Version("2")));
+		when(a.versions("b")).thenReturn(Arrays.asList(new Version("2"), new Version("3")));
+
+		when(b.getName()).thenReturn("b");
+		when(b.list(null)).thenReturn(Arrays.asList("b","c"));
+		when(b.versions("b")).thenReturn(Arrays.asList(new Version("1"), new Version("2")));
+		when(b.versions("c")).thenReturn(Arrays.asList(new Version("2"), new Version("3")));
+		
+		Tree ta = RepositoryElement.getTree(a);
+		Tree tb = RepositoryElement.getTree(b);
+		
+		Diff diff = new DiffImpl(ta, tb);
+		print(diff,0);
+		
+		assertEquals(Delta.MAJOR, diff.getDelta());
+		assertEquals(Type.PROGRAM, diff.get("a").getType());
+		assertEquals(Type.VERSION, diff.get("a").get("1.0.0").getType());
+		assertEquals(Delta.ADDED, diff.get("a").get("1.0.0").getDelta());
+		assertEquals(Delta.ADDED, diff.get("a").get("2.0.0").getDelta());
+		
+		assertEquals(Delta.REMOVED, diff.get("b").get("1.0.0").getDelta());
+		assertEquals(Delta.UNCHANGED, diff.get("b").get("2.0.0").getDelta());
+		assertEquals(Delta.ADDED, diff.get("b").get("3.0.0").getDelta());
+		
+		assertEquals(Delta.REMOVED, diff.get("c").getDelta());
+		assertEquals(Delta.REMOVED, diff.get("c").get("2.0.0").getDelta());
+		assertEquals(Delta.REMOVED, diff.get("c").get("3.0.0").getDelta());
+		
+	}
+	
+	void print(Diff diff , int n) {
+		System.out.println("                            ".substring(0,n) + diff.getName() + " " + diff.getType() + "  " + diff.getDelta());
+		for ( Diff c : diff.getChildren()) {
+			print(c, n+1);
+		}
+		
+	}
+}
