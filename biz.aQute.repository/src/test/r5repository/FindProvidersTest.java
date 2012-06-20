10f9b9947e5ab71351ac40fdf41ad03fdceb1718
Neil Bartlett <njbartlett@gmail.com>
2012-06-21 00:05:51 +0100

    Additional findProviders tests (but no code changes... it just works!)

--- biz.aQute.repository/src/test/r5repository/FindProvidersTest.java
+++ biz.aQute.repository/src/test/r5repository/FindProvidersTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import junit.framework.TestCase;
@@ -34,6 +35,49 @@ public class FindProvidersTest extends TestCase {
 		Capability identityCap = capsArray[0].getResource().getCapabilities("osgi.identity").get(0);
 		Object identityAttrValue = identityCap.getAttributes().get("osgi.identity");
 		assertEquals("dummybundle", identityAttrValue);
+	}
+
+	public void testReadGZippedStream() throws Exception {
+		FixedIndexedRepo repo = new FixedIndexedRepo();
+		Map<String,String> props = new HashMap<String,String>();
+		props.put("locations", new File("testdata/big_index.xml.gz").toURI().toString());
+		repo.setProperties(props);
+		
+		Requirement req = new CapReqBuilder("osgi.identity").addDirective("filter", "(&(osgi.identity=osgi.cmpn)(version>=4.2.0)(!(version>=4.2.1)))").buildSyntheticRequirement();
+		Map<Requirement,Collection<Capability>> result = repo.findProviders(Collections.singleton(req));
+		
+		assertNotNull(result);
+		assertTrue(result.containsKey(req));
+		
+		Collection<Capability> caps = result.get(req);
+		assertEquals(1, caps.size());
+		Capability identityCap = caps.iterator().next();
+		
+		List<Capability> contentCaps = identityCap.getResource().getCapabilities("osgi.content");
+		assertNotNull(contentCaps);
+		assertEquals(1, contentCaps.size());
+		Capability contentCap = contentCaps.iterator().next();
+		assertEquals(new File("testdata/osgi.cmpn/osgi.cmpn-4.2.0.jar").getAbsoluteFile().toURI(), contentCap.getAttributes().get("url"));
+	}
+
+	public void testMultipleMatches() throws Exception {
+		FixedIndexedRepo repo = new FixedIndexedRepo();
+		Map<String,String> props = new HashMap<String,String>();
+		props.put("locations", new File("testdata/big_index.xml").toURI().toString());
+		repo.setProperties(props);
+		
+		Requirement req = CapReqBuilder.createPackageRequirement("aQute.bnd.annotation", new VersionRange("[1.43,2)")).buildSyntheticRequirement();
+		Map<Requirement,Collection<Capability>> result = repo.findProviders(Collections.singleton(req));
+		Collection<Capability> matchingCaps = result.get(req);
+		
+		// 16 versions of biz.aQute.bndlib plus 8 versions of biz.aQute.bnd.annotation!
+		assertEquals(24, matchingCaps.size());
+		
+		for (Capability cap : matchingCaps) {
+			Capability identityCap = cap.getResource().getCapabilities("osgi.identity").iterator().next();
+			String bsn = (String) identityCap.getAttributes().get("osgi.identity");
+			assertTrue("biz.aQute.bndlib".equals(bsn) || "biz.aQute.bnd.annotation".equals(bsn));
+		}
 	}
 
 }
