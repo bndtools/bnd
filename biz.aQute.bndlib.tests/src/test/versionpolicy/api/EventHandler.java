9a3a583a716e46952f72c568d058531cb1eb594c
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-06-21 16:01:14 +0200

    remove deprecated UsePolicy annotation
    supplanted by ConsumerType annotation
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.bndlib.tests/src/test/versionpolicy/api/EventHandler.java
+++ biz.aQute.bndlib.tests/src/test/versionpolicy/api/EventHandler.java
@@ -2,8 +2,6 @@ package test.versionpolicy.api;
 
 import aQute.bnd.annotation.*;
 
-@SuppressWarnings("deprecation")
-@UsePolicy
 @ConsumerType
 public interface EventHandler {
 	void listen(Object o);
