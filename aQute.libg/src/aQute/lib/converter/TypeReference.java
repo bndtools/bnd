a384ac0c80ea6f9c424beb695b7844ea93b31215
Peter Kriens <peter.kriens@aqute.biz>
2012-06-28 11:22:21 +0200

    Ensured that the TypeReference class cannot be used as is, it is always necessary to subclass it.

--- aQute.libg/src/aQute/lib/converter/TypeReference.java
+++ aQute.libg/src/aQute/lib/converter/TypeReference.java
@@ -4,6 +4,9 @@ import java.lang.reflect.*;
 
 public class TypeReference<T> implements Type {
 
+	protected TypeReference() {
+		// Make sure it cannot be directly instantiated
+	}
 	public Type getType() {
 		return ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 	}
