c31aff89ab5f991249a396fea707ea4362733367
Neil Bartlett <njbartlett@gmail.com>
2012-07-03 15:56:21 +0100

    Constructor args of Region flipped!

--- aQute.libg/src/aQute/lib/properties/Region.java
+++ aQute.libg/src/aQute/lib/properties/Region.java
@@ -2,19 +2,21 @@ package aQute.lib.properties;
 
 public class Region implements IRegion {
 
-	private final int	length;
 	private final int	offset;
+	private final int	length;
 
-	public Region(int length, int offset) {
-		this.length = length;
+	public Region(int offset, int length) {
 		this.offset = offset;
+		this.length = length;
+	}
+
+	public int getOffset() {
+		return offset;
+
 	}
 
 	public int getLength() {
 		return length;
 	}
 
-	public int getOffset() {
-		return offset;
-	}
 }
