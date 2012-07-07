eedf78b6889930a6745b0e610a69f741c02acb30
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-07-07 11:55:13 +0200

    remove unread fields
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- bndtools.release/src/bndtools/release/ReleaseAction.java
+++ bndtools.release/src/bndtools/release/ReleaseAction.java
@@ -30,9 +30,8 @@ import aQute.bnd.build.Project;
 import aQute.bnd.build.Workspace;
 
 public class ReleaseAction implements IObjectActionDelegate {
-
-	@SuppressWarnings("unused")
-	private IWorkbenchPart targetPart;
+//	@SuppressWarnings("unused")
+//	private IWorkbenchPart targetPart;
 
 	private Map<Project, List<File>> bndFiles;
 	
@@ -109,7 +108,7 @@ public class ReleaseAction implements IObjectActionDelegate {
 	}
 
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		this.targetPart = targetPart;
+		// this.targetPart = targetPart;
 	}
 
 }
