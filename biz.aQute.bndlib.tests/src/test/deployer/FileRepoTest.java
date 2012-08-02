9d4e1dce28147aa9fbac833d3a20b3ba71aa8d17
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-08-02 19:05:20 +0200

    FileRepoTest: add testBundleNotModifiedOnPut
    This is to catch issue #470: Cksum Of Jar Files Changed After
    Import Into Local OBR repository
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.bndlib.tests/src/test/deployer/FileRepoTest.java
+++ biz.aQute.bndlib.tests/src/test/deployer/FileRepoTest.java
@@ -1,16 +1,32 @@
 package test.deployer;
 
 import java.io.*;
+import java.security.*;
 import java.util.*;
 
 import junit.framework.*;
 import aQute.bnd.osgi.*;
 import aQute.lib.deployer.*;
+import aQute.lib.io.*;
 
 public class FileRepoTest extends TestCase {
 	
 	private FileRepo	testRepo;
 	private FileRepo	nonExistentRepo;
+
+	private static String hashToString(byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		return formatter.toString();
+	}
+
+	private static byte[] calculateHash(MessageDigest algorithm, File file) throws Exception {
+		algorithm.reset();
+		IO.copy(file, algorithm);
+		return algorithm.digest();
+	}
 
 	@Override
 	protected void setUp() throws Exception {
@@ -51,6 +67,31 @@ public class FileRepoTest extends TestCase {
 		assertEquals(0, list.size());
 	}
 	
+	public void testBundleNotModifiedOnPut() throws Exception {
+		MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+		Jar srcJar = null;
+		File dstBundle = null;
+		try {
+			File srcBundle = new File("test/test.jar");
+			byte[] srcSha = calculateHash(sha1, srcBundle);
+
+			srcJar = new Jar(srcBundle);
+
+			dstBundle = testRepo.put(srcJar);
+			byte[] dstSha = calculateHash(sha1, dstBundle);
+
+			assertEquals(hashToString(srcSha), hashToString(dstSha));
+			assertTrue(MessageDigest.isEqual(srcSha, dstSha));
+		}
+		finally {
+			if (srcJar != null)
+				srcJar.close();
+			if (dstBundle != null) {
+				IO.delete(dstBundle.getParentFile());
+			}
+		}
+	}
+
 	//	public void testDeployToNonexistentRepoFails() throws Exception {
 	//		Jar bundleJar = new Jar(new File("test/test.jar"));
 	//		try {
