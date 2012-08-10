d8fc1f39405b0785b3183d801d6220b3958acbf0
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-08-10 15:35:10 +0200

    ProjectBuilder: refactor doBaseline
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.bndlib/src/aQute/bnd/build/ProjectBuilder.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/ProjectBuilder.java
@@ -2,20 +2,16 @@ package aQute.bnd.build;
 
 import java.io.*;
 import java.util.*;
-import java.util.Map.Entry;
 
 import aQute.bnd.differ.*;
 import aQute.bnd.differ.Baseline.Info;
-import aQute.bnd.header.Attrs;
-import aQute.bnd.header.Parameters;
 import aQute.bnd.osgi.*;
-import aQute.bnd.service.RepositoryPlugin;
 import aQute.bnd.service.RepositoryPlugin.Strategy;
 
 public class ProjectBuilder extends Builder {
 	private final DiffPluginImpl	differ	= new DiffPluginImpl();
-	Project	project;
-	boolean	initialized;
+	Project							project;
+	boolean							initialized;
 
 	public ProjectBuilder(Project project) {
 		super(project);
@@ -97,77 +93,39 @@ public class ProjectBuilder extends Builder {
 	 */
 	@Override
 	protected void doBaseline(Jar dot) throws Exception {
-		Parameters diffs = parseHeader(getProperty(Constants.BASELINE));
-		if (diffs.isEmpty())
+
+		String s = getProperty(Constants.BASELINE);
+		if (s == null || s.trim().length() == 0)
 			return;
 
-		trace("baseline %s", diffs);
+		trace("baseline %s", s);
 
-		Jar other = getBaselineJar();
-		if (other == null) {
-			return;
-		}
 		Baseline baseline = new Baseline(this, differ);
-		Set<Info> infos = baseline.baseline(dot, other, null);
-		for (Info info : infos) {
-			if (info.mismatch) {
-				error("%s %-50s %-10s %-10s %-10s %-10s %-10s\n", info.mismatch ? '*' : ' ', info.packageName,
-						info.packageDiff.getDelta(), info.newerVersion, info.olderVersion, info.suggestedVersion,
-						info.suggestedIfProviders == null ? "-" : info.suggestedIfProviders);
+
+		Collection<Container> bundles = project.getBundles(Strategy.LOWEST, s);
+		for (Container c : bundles) {
+			
+			if (c.getError() != null || c.getFile() == null) {
+				error("Erroneous baseline bundle %s", c);
+				continue;
+			}
+
+			Jar jar = new Jar(c.getFile());
+			try {
+				Set<Info> infos = baseline.baseline(dot, jar, null);
+				for (Info info : infos) {
+					if (info.mismatch) {
+						error("%s %-50s %-10s %-10s %-10s %-10s %-10s\n", info.mismatch ? '*' : ' ', info.packageName,
+								info.packageDiff.getDelta(), info.newerVersion, info.olderVersion, info.suggestedVersion,
+								info.suggestedIfProviders == null ? "-" : info.suggestedIfProviders);
+					}
+				}				
+			}
+			finally {
+				jar.close();
 			}
 		}
+
 	}
 
-	public Jar getBaselineJar() throws Exception {
-
-		List<RepositoryPlugin> repos = getPlugins(RepositoryPlugin.class);
-
-		String baseline = getProperty(Constants.BASELINE);
-		Parameters params = parseHeader(baseline);
-		File baselineFile = null;
-		if (baseline == null) {
-			String repoName = getProperty(Constants.BASELINEREPO);
-			if (repoName == null) {
-				repoName = getProperty(Constants.RELEASEREPO);
-				if (repoName == null) {
-					return null;
-				}
-			}
-			for (RepositoryPlugin repo : repos) {
-				if (repoName.equals(repo.getName())) {
-					baselineFile = repo.get(getBsn(), null, Strategy.HIGHEST, null);
-					break;
-				}
-			}
-		} else {
-
-			String bsn = null;
-			String version = null;
-			for (Entry<String,Attrs> entry : params.entrySet()) {
-				bsn = entry.getKey();
-				if ("@".equals(bsn)) {
-					bsn = getBsn();
-				}
-				version = entry.getValue().get(Constants.VERSION_ATTRIBUTE);
-				break;
-			}
-			if ("latest".equals(version)) {
-				version = null;
-			}
-			for (RepositoryPlugin repo : repos) {
-				if (version == null) {
-					baselineFile = repo.get(bsn, null, Strategy.HIGHEST, null);
-				} else {
-					baselineFile = repo.get(bsn, version, Strategy.EXACT, null);
-				}
-				if (baselineFile != null) {
-					break;
-				}
-			}
-		}
-		if (baselineFile == null) {
-			return new Jar(".");
-		}
-		return new Jar(baselineFile);
-	}
 }
