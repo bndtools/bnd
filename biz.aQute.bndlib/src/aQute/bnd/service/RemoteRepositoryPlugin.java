e13a5979e3635afd6e1de37dd43e93f4dd59bf0c
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-08-10 18:06:20 +0200

    RemoteRepositoryPlugin: add some comments
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.bndlib/src/aQute/bnd/service/RemoteRepositoryPlugin.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/RemoteRepositoryPlugin.java
@@ -7,17 +7,16 @@ public interface RemoteRepositoryPlugin extends RepositoryPlugin {
 	/**
 	 * Retrieve a resource handle from the repository. For all implementations
 	 * of this interface, calling {@code getFile(bsn, range, strategy, props)}
-	 * should always return the same result as
-	 * {@code getResource(bsn, range, strategy, props).request()}.
+	 * should always return the same result as {@link RepositoryPlugin#get(String, aQute.bnd.version.Version, Map)}
 	 * 
-	 * @param bsn
-	 * @param range
-	 * @param strategy
-	 * @param properties
+	 * @param bsn the bsn of the revision
+	 * @param version the version of the revision
+	 * @param strategy strategy
+	 * @param properties any properties
 	 * @return
 	 * @throws Exception
 	 */
-	ResourceHandle getHandle(String bsn, String range, Strategy strategy, Map<String,String> properties)
+	ResourceHandle getHandle(String bsn, String version, Strategy strategy, Map<String,String> properties)
 			throws Exception;
 
 	File getCacheDirectory();
