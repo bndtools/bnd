ed3ca60eae4dacd96f5b9b1627f440c88ee3de09
PK Söreide <per.kristian.soreide@gmail.com>
2012-11-06 10:38:24 +0100

    Fixed -baselinerepo + added tests

--- biz.aQute.bndlib.tests/src/test/baseline/BaselineTest.java
+++ biz.aQute.bndlib.tests/src/test/baseline/BaselineTest.java
@@ -6,15 +6,51 @@ import junit.framework.*;
 import aQute.bnd.build.*;
 import aQute.bnd.osgi.*;
 
-// TODO needs work because it does not test anything
 public class BaselineTest extends TestCase {
 
 	public static void testBaslineJar() throws Exception {
 		Workspace ws = new Workspace(new File("test/ws"));
-		Project top = ws.getProject("p3");
-		
-		Builder builder = top.getBuilder(null).getSubBuilder();
-		builder.set("-baseline", "org.apache.felix.configadmin;version=1.2.0");
-		
+
+		Project p3 = ws.getProject("p3");
+
+		ProjectBuilder builder = (ProjectBuilder) p3.getBuilder(null).getSubBuilder();
+		builder.setBundleSymbolicName("p3");
+
+		// Nothing specified
+		Jar jar = builder.getBaselineJar(false);
+		assertNull(jar);
+
+		jar = builder.getBaselineJar(true);
+		assertEquals(".", jar.getName());
+
+		// Fallback to release repo
+		builder.set("-releaserepo", "Repo");
+		jar = builder.getBaselineJar(false);
+		assertNull(jar);
+
+		jar = builder.getBaselineJar(true);
+		assertEquals("p3", jar.getBsn());
+		assertEquals("1.0.1", jar.getVersion());
+
+		// -baselinerepo specified
+		builder.set("-baselinerepo", "Release");
+		jar = builder.getBaselineJar(false);
+		assertEquals("p3", jar.getBsn());
+		assertEquals("1.2.0", jar.getVersion());
+
+		jar = builder.getBaselineJar(true);
+		assertEquals("p3", jar.getBsn());
+		assertEquals("1.2.0", jar.getVersion());
+
+		// -baseline specified
+		builder.set("-baseline", "p3;version=1.1.0");
+		jar = builder.getBaselineJar(false);
+		assertEquals("p3", jar.getBsn());
+		assertEquals("1.1.0", jar.getVersion());
+
+		jar = builder.getBaselineJar(true);
+		assertEquals("p3", jar.getBsn());
+		assertEquals("1.1.0", jar.getVersion());
+
 	}
 }
