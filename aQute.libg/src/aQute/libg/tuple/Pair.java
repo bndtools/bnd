11b03a25094abb9b31d387a634b1ed16611be171
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-11-06 12:11:15 +0100

    Pair: implement Clonable
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- aQute.libg/src/aQute/libg/tuple/Pair.java
+++ aQute.libg/src/aQute/libg/tuple/Pair.java
@@ -2,7 +2,7 @@ package aQute.libg.tuple;
 
 import java.io.Serializable;
 
-public class Pair<A, B> implements Serializable {
+public class Pair<A, B> implements Serializable, Cloneable {
 
 	private static final long	serialVersionUID	= 1L;
 
