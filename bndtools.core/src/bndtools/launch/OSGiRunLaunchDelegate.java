c95fb4f9b89b78c2eeb1ae9b1f395f284b8801e3
Carter Smithhart <carter.smithhart@gmail.com>
2012-11-08 23:44:09 -0800

    Add quotes around runVM arguments that have spaces. This allows eclipse to launch run and debug launchers if a workspace has a space in it.

--- bndtools.core/src/bndtools/launch/OSGiRunLaunchDelegate.java
+++ bndtools.core/src/bndtools/launch/OSGiRunLaunchDelegate.java
@@ -44,6 +44,17 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
     protected void initialiseBndLauncher(ILaunchConfiguration configuration, Project model) throws Exception {
         synchronized (model) {
             bndLauncher = model.getProjectLauncher();
+
+            Collection<String> runvmArgs = bndLauncher.getRunVM();
+
+            // this loop keeps the same order and adds quotes when there's a space.
+            for (String str : runvmArgs) {
+                runvmArgs.remove(str);
+                if (str.contains(" ")) {
+                    str = "\"" + str + "\"";
+                }
+                runvmArgs.add(str);
+            }
         }
         configureLauncher(configuration);
         bndLauncher.prepare();
