a296ffb8e9d638d813560a3350c1fe83f105d9ab
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-02-07 21:21:43 +0100

    core: add versioncontrol packages
    It's split into 2 packages to prevent a class cycle once
    the preferences are hooked up.
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

---
+++ bndtools.core/src/bndtools/versioncontrol/VersionControlSystem.java
@@ -0,0 +1,153 @@
+package bndtools.versioncontrol;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Supported version control systems
+ */
+public enum VersionControlSystem {
+    GIT("Git");
+
+    /**
+     * Constructor.
+     * 
+     * @param name
+     *            The friendly name of the version control system
+     */
+    private VersionControlSystem(String name) {
+        this.name = name;
+    }
+
+    /** The friendly name of the version control system */
+    private String name;
+
+    /**
+     * @return The friendly name of the version control system
+     */
+    public String getName() {
+        return name;
+    }
+
+    /** a map for the reverseOrdinal function */
+    private static final Map<Integer,VersionControlSystem> reverseOrdinalMap;
+
+    /** initialise the reverse ordinal map */
+    static {
+        reverseOrdinalMap = new TreeMap<Integer,VersionControlSystem>();
+        VersionControlSystem[] vcsEntries = VersionControlSystem.values();
+        for (VersionControlSystem vcsEntry : vcsEntries) {
+            reverseOrdinalMap.put(Integer.valueOf(vcsEntry.ordinal()), vcsEntry);
+        }
+    }
+
+    /**
+     * Performs a reverse ordinal lookup.
+     * 
+     * @param index
+     *            the ordinal of the enum
+     * @return the corresponding enum, or null when not found
+     */
+    public static VersionControlSystem reverseOrdinal(int index) {
+        return reverseOrdinalMap.get(Integer.valueOf(index));
+    }
+
+    /**
+     * @return True when the version control system can store empty directories, false otherwise
+     * @throws IllegalArgumentException
+     *             When the version control system is unknown
+     */
+    public boolean canStoreEmptyDirectories() throws IllegalArgumentException {
+        switch (this) {
+        case GIT :
+            return false;
+
+        default :
+            throw new IllegalArgumentException("Unknown version control system " + this.getName());
+        }
+    }
+
+    /**
+     * @return The (simple) filename to use for an ignore file
+     * @throws IllegalArgumentException
+     *             When the version control system is unknown
+     */
+    public String getIgnoreFilename() throws IllegalArgumentException {
+        switch (this) {
+        case GIT :
+            return ".gitignore";
+
+        default :
+            throw new IllegalArgumentException("Unknown version control system " + this.getName());
+        }
+    }
+
+    /**
+     * Convert a Git ignore entry to the format required by the version control system.
+     * 
+     * @param gitIgnoreEntry
+     *            The ignore entry in Git format
+     * @return The ignore entry in the format required by the target version control system
+     * @throws IllegalArgumentException
+     *             When the version control system is unknown
+     */
+    public String getIgnoreEntryFromGitIgnoreEntry(String gitIgnoreEntry) throws IllegalArgumentException {
+        switch (this) {
+        case GIT :
+            return gitIgnoreEntry;
+
+        default :
+            throw new IllegalArgumentException("Unknown version control system " + this.getName());
+        }
+    }
+
+    /**
+     * Convert an ignore entry from the format required by the version control system to the Git format.
+     * 
+     * @param ignoreEntry
+     *            The ignore entry in the format required by the version control system
+     * @return The ignore entry in Git format
+     * @throws IllegalArgumentException
+     *             When the version control system is unknown
+     */
+    public String getGitIgnoreEntryFromIgnoreEntry(String ignoreEntry) throws IllegalArgumentException {
+        switch (this) {
+        case GIT :
+            return ignoreEntry;
+
+        default :
+            throw new IllegalArgumentException("Unknown version control system " + this.getName());
+        }
+    }
+
+    /**
+     * Sanitise a Git ignore entry (a path) so that it is a properly formatted ignore entry in Git format.
+     * 
+     * @param ignoreEntry
+     *            The ignore entry (the path)
+     * @param directory
+     *            True when the path denotes a directory
+     * @param rooted
+     *            True when the ignore entry must be rooted in the directory of the ignore file
+     * @return The (sanitised) ignore in Git format
+     */
+    public static String sanitiseGitIgnoreEntry(String ignoreEntry, boolean directory, boolean rooted) {
+        /* replace all consecutive slashes with a single slash */
+        String newPath = ignoreEntry.replaceAll("/+", "/");
+
+        /* remove all leading slashes (with optional leading whitespace) */
+        newPath = newPath.replaceAll("^\\s*/+", "");
+
+        /* remove all trailing slashes (with optional trailing whitespace) */
+        newPath = newPath.replaceAll("/+\\s*$", "");
+
+        if (rooted) {
+            newPath = "/" + newPath;
+        }
+        if (directory) {
+            newPath = newPath + "/";
+        }
+
+        return newPath;
+    }
+}
