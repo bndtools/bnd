4382697bd901cdd2f62af70b9caafa83aa7cfafe
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-02-07 16:33:49 +0100

    core: put default project paths into exported ProjectPaths
    The ignores functionality needs this information.
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

---
+++ bndtools.core/src/bndtools/api/ProjectPaths.java
@@ -0,0 +1,9 @@
+package bndtools.api;
+
+public class ProjectPaths {
+    public static final String PATH_SRC = "src";
+    public static final String PATH_SRC_BIN = "bin";
+
+    public static final String PATH_TEST_SRC = "test";
+    public static final String PATH_TEST_BIN = "bin_test";
+}
