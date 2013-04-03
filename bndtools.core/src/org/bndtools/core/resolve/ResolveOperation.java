e1a5e0f05b50f838df2522e1f47720867ca0dbec
Neil Bartlett <njbartlett@gmail.com>
2013-04-03 10:48:46 +0100

    Start coordination before resolving (see #631)

--- bndtools.core/src/org/bndtools/core/resolve/ResolveOperation.java
+++ bndtools.core/src/org/bndtools/core/resolve/ResolveOperation.java
@@ -6,6 +6,10 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.coordinator.Coordination;
+import org.osgi.service.coordinator.Coordinator;
 import org.osgi.service.resolver.ResolutionException;
 import aQute.bnd.build.model.BndEditModel;
 import aQute.bnd.deployer.repository.ReporterLogService;
@@ -26,6 +30,13 @@ public class ResolveOperation implements IRunnableWithProgress {
     public void run(IProgressMonitor monitor) {
         MultiStatus status = new MultiStatus(Plugin.PLUGIN_ID, 0, Messages.ResolveOperation_errorOverview, null);
 
+        // Start a coordination
+        BundleContext bc = Plugin.getDefault().getBundleContext();
+        ServiceReference coordSvcRef = bc.getServiceReference(Coordinator.class.getName());
+        Coordinator coordinator = coordSvcRef != null ? (Coordinator) bc.getService(coordSvcRef) : null;
+        Coordination coordination = coordinator != null ? coordinator.begin(ResolveOperation.class.getName(), 0) : null;
+
+        // Begin resolve
         ResolveProcess resolve = new ResolveProcess();
         ResolverLogger logger = new ResolverLogger();
         try {
@@ -44,9 +55,18 @@ public class ResolveOperation implements IRunnableWithProgress {
 
                 result = new ResolutionResult(ResolutionResult.Outcome.Unresolved, resolve, status, logger.getLog());
             }
+
+            if (coordination != null)
+                coordination.end();
         } catch (Exception e) {
             status.add(new Status(IStatus.ERROR, Plugin.PLUGIN_ID, 0, "Exception during resolution.", e));
             result = new ResolutionResult(ResolutionResult.Outcome.Error, resolve, status, logger.getLog());
+
+            if (coordination != null)
+                coordination.fail(e);
+        } finally {
+            if (coordinator != null)
+                bc.ungetService(coordSvcRef);
         }
     }
 
