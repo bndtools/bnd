b165eff2b4514892f844e13dd968fd0a778753ec
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-04-03 17:58:10 +0200

    ProjectLaunchImplTest: trying to stabalise the test
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.launcher/src/test/ProjectLaunchImplTest.java
+++ biz.aQute.launcher/src/test/ProjectLaunchImplTest.java
@@ -11,26 +11,43 @@ public class ProjectLaunchImplTest extends TestCase {
 	public static void testParseRunProperties() throws Exception {
 		Workspace ws = Workspace.getWorkspace(new File("src/test/ws"));
 		Project project = ws.getProject("p1");
+		String lb = null;
 
-		ProjectLauncherImpl launcher = new ProjectLauncherImpl(project);
-		launcher.prepare();
+		try {
+			ProjectLauncherImpl launcher = new ProjectLauncherImpl(project);
+			launcher.prepare();
 
-		String arg = launcher.getRunVM().iterator().next();
-		String propertiesPath = arg.substring("-Dlauncher.properties=".length());
+			String arg = launcher.getRunVM().iterator().next();
+			String propertiesPath = arg.substring("-Dlauncher.properties=".length());
 
-		Properties launchProps = new Properties();
-		launchProps.load(new FileInputStream(new File(propertiesPath)));
-		assertEquals(new File("src/test/ws/p1/generated/p1.jar").getAbsolutePath(), launchProps.get("launch.bundles"));
+			Properties launchProps = new Properties();
+			launchProps.load(new FileInputStream(new File(propertiesPath)));
+			lb = (String) launchProps.get("launch.bundles");
+		}
+		finally {
+			project.close();
+			ws.close();
+		}
+		assertEquals(new File("src/test/ws/p1/generated/p1.jar").getAbsolutePath(), lb);
 	}
-	
+
 	public static void testParseSystemCapabilities() throws Exception {
 		Workspace ws = Workspace.getWorkspace(new File("src/test/ws"));
 		Project project = ws.getProject("p1");
-
-		ProjectLauncherImpl launcher = new ProjectLauncherImpl(project);
-		launcher.prepare();
+		String systemCaps =null;
 		
-		String systemCaps = launcher.getSystemCapabilities();
-		assertEquals("osgi.native;osgi.native.osname:List<String>=\"Win7,Windows7,Windows 7\";osgi.native.osversion:Version=6.1", systemCaps);
+		try {
+			ProjectLauncherImpl launcher = new ProjectLauncherImpl(project);
+			launcher.prepare();
+
+			systemCaps = launcher.getSystemCapabilities();
+		}
+		finally {
+			project.close();
+			ws.close();
+		}
+		assertEquals(
+				"osgi.native;osgi.native.osname:List<String>=\"Win7,Windows7,Windows 7\";osgi.native.osversion:Version=6.1",
+				systemCaps);
 	}
 }
