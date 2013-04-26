eec64064290c062c9621ac6654534f5cbbddb271
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-04-26 16:39:49 +0200

    manual: add a safety check to the deploy script
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- bndtools.manual/deploy.sh
+++ bndtools.manual/deploy.sh
@@ -1,4 +1,10 @@
 # Copy generated site to deploy directory
+if [[ ! -d ../../bndtools.github.com ]]; then
+  echo "Could not find directory ../../bndtools.github.com"
+  exit 1
+fi
+
+
 rm -fr ../../bndtools.github.com/*
 for name in `ls out/site/_site`; do
 	echo copying out/site/_site/$name to ../../bndtools.github.com
