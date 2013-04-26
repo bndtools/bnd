040fd0b9d167187aa87b49cf1c3019e3e6f23c8e
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-04-26 15:53:42 +0200

    manual: first remove on deploy
    Otherwise obsolete files are not removed
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- bndtools.manual/deploy.sh
+++ bndtools.manual/deploy.sh
@@ -1,4 +1,5 @@
 # Copy generated site to deploy directory
+rm -fr ../../bndtools.github.com/*
 for name in `ls out/site/_site`; do
 	echo copying out/site/_site/$name to ../../bndtools.github.com
 	cp -R out/site/_site/${name} ../../bndtools.github.com
