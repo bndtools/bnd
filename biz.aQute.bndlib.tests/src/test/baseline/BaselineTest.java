28340b92bb9d154634516a085d86b506c92c5e73
Peter Kriens <peter.kriens@aqute.biz>
2013-05-31 15:31:12 +0200

    Changed the algorithm to find the baseline for baselining. This is a breaking change since I removed functionality. The old way had a boolean that selected a fallback or not. I think this was too confusing and should in general not affect anybody. However, need to talk to PK about the consequences because I think he was responsible.
    See #356

--- biz.aQute.bndlib.tests/src/test/baseline/BaselineTest.java
+++ biz.aQute.bndlib.tests/src/test/baseline/BaselineTest.java
@@ -3,60 +3,116 @@ package test.baseline;
 import java.io.*;
 import java.util.*;
 
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
 import junit.framework.*;
 import aQute.bnd.build.*;
 import aQute.bnd.differ.*;
 import aQute.bnd.differ.Baseline.Info;
+import aQute.bnd.header.*;
 import aQute.bnd.osgi.*;
-import aQute.service.reporter.*;
+import aQute.bnd.service.*;
+import aQute.bnd.version.*;
+import aQute.lib.collections.*;
+import aQute.lib.io.*;
 
 public class BaselineTest extends TestCase {
 
 	public static void testBaslineJar() throws Exception {
-		Workspace ws = new Workspace(new File("test/ws"));
-
-		Project p3 = ws.getProject("p3");
-
-		ProjectBuilder builder = (ProjectBuilder) p3.getBuilder(null).getSubBuilder();
-		builder.setBundleSymbolicName("p3");
-
-		// Nothing specified
-		Jar jar = builder.getBaselineJar(false);
-		assertNull(jar);
-
-		jar = builder.getBaselineJar(true);
-		assertEquals(".", jar.getName());
-
-		// Fallback to release repo
-		builder.set("-releaserepo", "Repo");
-		jar = builder.getBaselineJar(false);
-		assertNull(jar);
-
-		jar = builder.getBaselineJar(true);
-		assertEquals("p3", jar.getBsn());
-		assertEquals("1.0.1", jar.getVersion());
-
-		// -baselinerepo specified
-		builder.set("-baselinerepo", "Release");
-		jar = builder.getBaselineJar(false);
-		assertEquals("p3", jar.getBsn());
-		assertEquals("1.2.0", jar.getVersion());
-
-		jar = builder.getBaselineJar(true);
-		assertEquals("p3", jar.getBsn());
-		assertEquals("1.2.0", jar.getVersion());
-
-		// -baseline specified
-		builder.set("-baseline", "p3;version=1.1.0");
-		jar = builder.getBaselineJar(false);
-		assertEquals("p3", jar.getBsn());
-		assertEquals("1.1.0", jar.getVersion());
-
-		jar = builder.getBaselineJar(true);
-		assertEquals("p3", jar.getBsn());
-		assertEquals("1.1.0", jar.getVersion());
+//		Workspace ws = new Workspace(new File("test/ws"));
+//
+//		Project p3 = ws.getProject("p3");
+//
+//		ProjectBuilder builder = (ProjectBuilder) p3.getBuilder(null).getSubBuilder();
+//		builder.setBundleSymbolicName("p3");
+//
+//		// Nothing specified
+//		Jar jar = builder.getBaselineJar(false);
+//		assertNull(jar);
+//
+//		jar = builder.getBaselineJar(true);
+//		assertEquals(".", jar.getName());
+//
+//		// Fallback to release repo
+//		builder.set("-releaserepo", "Repo");
+//		jar = builder.getBaselineJar(false);
+//		assertNull(jar);
+//
+//		jar = builder.getBaselineJar(true);
+//		assertEquals("p3", jar.getBsn());
+//		assertEquals("1.0.1", jar.getVersion());
+//
+//		// -baselinerepo specified
+//		builder.set("-baselinerepo", "Release");
+//		jar = builder.getBaselineJar(false);
+//		assertEquals("p3", jar.getBsn());
+//		assertEquals("1.2.0", jar.getVersion());
+//
+//		jar = builder.getBaselineJar(true);
+//		assertEquals("p3", jar.getBsn());
+//		assertEquals("1.2.0", jar.getVersion());
+//
+//		// -baseline specified
+//		builder.set("-baseline", "p3;version=1.1.0");
+//		jar = builder.getBaselineJar(false);
+//		assertEquals("p3", jar.getBsn());
+//		assertEquals("1.1.0", jar.getVersion());
+//
+//		jar = builder.getBaselineJar(true);
+//		assertEquals("p3", jar.getBsn());
+//		assertEquals("1.1.0", jar.getVersion());
 
 	}
+	
+	/**
+	 * In repo:
+	 * <pre>
+	 * p3-1.1.0.jar
+	 * p3-1.2.0.jar
+	 * </pre>
+	 * @throws Exception
+	 */
+	public static void testRepository() throws Exception {
+		Workspace ws = new Workspace(new File("test/ws"));
+		
+		Jar v1_2_0_a = mock(Jar.class);
+		when(v1_2_0_a.getVersion()).thenReturn("1.2.0.b");
+		when(v1_2_0_a.getBsn()).thenReturn("p3");
+		
+		RepositoryPlugin repo = mock(RepositoryPlugin.class);
+		ws.addBasicPlugin(repo);
+		when(repo.get(anyString(), any(Version.class), any(Map.class))).thenReturn(IO.getFile("test/ws/cnf/releaserepo/p3/p3-1.2.0.jar"));
+		System.out.println(repo.get("p3",new Version("1.2.0.b"),new Attrs()));
+		
+		when(repo.canWrite()).thenReturn(true);
+		when(repo.getName()).thenReturn("Baseline");
+		when(repo.versions("p3")).thenReturn(new SortedList<Version>( new Version("1.1.0.a"),new Version("1.1.0.b"), new Version("1.2.0.a"), new Version("1.2.0.b")));
+		
+		Project p3 = ws.getProject("p3");
+		p3.setBundleVersion("1.3.0");
+		ProjectBuilder builder = (ProjectBuilder) p3.getBuilder(null).getSubBuilder();
+		builder.setProperty(Constants.BASELINE, "*");
+		builder.setProperty(Constants.BASELINEREPO, "Baseline");
+		
+		// Nothing specified
+		Jar jar = builder.getBaselineJar();
+		assertEquals( "1.2.0", new Version(jar.getVersion()).getWithoutQualifier().toString());
+		
+		if ( !builder.check())
+			fail();
+		
+		// check for error when repository contains later versions
+		builder = (ProjectBuilder) p3.getBuilder(null).getSubBuilder();
+		builder.setBundleVersion("1.1.3");
+		builder.setTrace(true);
+		jar = builder.getBaselineJar();
+		assertNull(jar);
+		
+		if ( !builder.check())
+			fail();
+	}
+	
 	
 	// Adding a method to a ProviderType produces a MINOR bump (1.0.0 -> 1.1.0)
 	public static void testProviderTypeBump() throws Exception {
--- biz.aQute.bndlib/src/aQute/bnd/build/ProjectBuilder.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/ProjectBuilder.java
@@ -2,12 +2,15 @@ package aQute.bnd.build;
 
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 import aQute.bnd.differ.*;
 import aQute.bnd.differ.Baseline.Info;
+import aQute.bnd.header.*;
 import aQute.bnd.osgi.*;
 import aQute.bnd.service.*;
 import aQute.bnd.version.*;
+import aQute.lib.collections.*;
 
 public class ProjectBuilder extends Builder {
 	private final DiffPluginImpl	differ	= new DiffPluginImpl();
@@ -102,29 +105,34 @@ public class ProjectBuilder extends Builder {
 		}
 		Version newer = new Version(getVersion());
 		Version older = new Version(jar.getVersion());
-		if ( newer.compareTo(older) <= 0) {
+		if (newer.compareTo(older) <= 0) {
 			error("The baseline version is %s, which is less or equal than our current version %s", older, newer);
 			return;
 		}
 
-		if ( !getBsn().equals(jar.getBsn())){
+		if (!getBsn().equals(jar.getBsn())) {
 			error("The symbolic name of this project (%s) is not the same as the baseline: %s", getBsn(), jar.getBsn());
 			return;
 		}
-		
+
 		trace("baseline %s-%s against: %s", getBsn(), getVersion(), jar.getName());
 		try {
 			Baseline baseline = new Baseline(this, differ);
 
 			Set<Info> infos = baseline.baseline(dot, jar, null);
-			if ( infos.isEmpty())
+			if (infos.isEmpty())
 				trace("no deltas");
-			
+
 			for (Info info : infos) {
 				if (info.mismatch) {
-					error("%s %-50s %-10s %-10s %-10s %-10s %-10s\n", info.mismatch ? '*' : ' ', info.packageName,
-							info.packageDiff.getDelta(), info.newerVersion, info.olderVersion, info.suggestedVersion,
+					SetLocation l = error("Baseline mismatch for package %s, %s change. Current is %s, repo is %s, suggest %s or %s\n", info.packageName, info.packageDiff.getDelta(),
+							info.newerVersion, info.olderVersion, info.suggestedVersion,
 							info.suggestedIfProviders == null ? "-" : info.suggestedIfProviders);
+					System.out.println(l);
+					l.header(Constants.BASELINE);
+					if (getPropertiesFile()!=null)
+						l.file(getPropertiesFile().getAbsolutePath());
+					l.details(info);
 				}
 			}
 		}
@@ -133,74 +141,214 @@ public class ProjectBuilder extends Builder {
 		}
 	}
 
-	public Jar getBaselineJar(boolean fallback) throws Exception {
-
-		String baseline = getProperty(Constants.BASELINE);
-		String baselineRepo = getProperty(Constants.BASELINEREPO);
-		if ((baseline == null || baseline.trim().length() == 0) 
-				&& (baselineRepo == null || baselineRepo.trim().length() == 0) && !fallback)
-			return null;
-
-		File baselineFile = null;
-		if ((baseline == null || baseline.trim().length() == 0)) {
-			baselineFile = getBaselineFromRepo(fallback);
-			if (baselineFile != null)
-				trace("baseline %s", baselineFile.getName());
-		} else {
-
-			trace("baseline %s", baseline);
-
-			Collection<Container> bundles = project.getBundles(Strategy.LOWEST, baseline);
-			for (Container c : bundles) {
-
-				if (c.getError() != null || c.getFile() == null) {
-					error("Erroneous baseline bundle %s", c);
-					continue;
-				}
-
-				baselineFile = c.getFile();
-				break;
-			}
-		}
-		if (fallback && baselineFile == null) {
-			return new Jar(".");
-		}
-		return new Jar(baselineFile);
+	public Jar getBaselineJar(boolean fallbackToReleaseRepo) throws Exception {
+		return getBaselineJar();
 	}
 
-	private File getBaselineFromRepo(boolean fallback) throws Exception {
-		String repoName = getProperty(Constants.BASELINEREPO);
-		if (repoName == null && !fallback)
+	/**
+	 * This method attempts to find the baseline jar for the current project. It
+	 * reads the -baseline property and treats it as instructions. These
+	 * instructions are matched against the bsns of the jars (think sub
+	 * builders!). If they match, the sub builder is selected.
+	 * <p>
+	 * The instruction can then specify the following options:
+	 * 
+	 * <pre>
+	 * 	version : baseline version from repository
+	 * 	file    : a file path
+	 * </pre>
+	 * 
+	 * If neither is specified, the current version is used to find the highest
+	 * version (without qualifier) that is below the current version. If a
+	 * version is specified, we take the highest version with the same base
+	 * version.
+	 * <p>
+	 * Since baselining is expensive and easily generates errors you must enable
+	 * it. The easiest solution is to {@code -baseline: *}. This will match all
+	 * sub builders and will calculate the version.
+	 * 
+	 * @return a Jar or null
+	 */
+	public Jar getBaselineJar() throws Exception {
+		String bl = getProperty(Constants.BASELINE);
+		if ( bl == null || Constants.NONE.equals(bl))
 			return null;
+		
+		Instructions baselines = new Instructions(getProperty(Constants.BASELINE));
+		if (baselines.isEmpty())
+			return null; // no baselining
 
-		if (repoName == null) {
-			repoName = getProperty(Constants.RELEASEREPO);
-			if (repoName == null) {
+		RepositoryPlugin repo = getReleaseRepo();
+		if (repo == null)
+			return null; // errors reported already
+
+		String bsn = getBsn();
+		Version version = new Version(getVersion());
+		SortedSet<Version> versions = removeStaged(repo.versions(bsn));
+
+		if (versions.isEmpty()) {
+			error("There are no versions for %s in the %s repo", bsn, repo);
+			return null;
+		}
+
+		//
+		// Get any versions in the repo that have the same major version or
+		// higher
+		//
+
+		SortedSet<Version> tail = versions.tailSet(version.getWithoutQualifier());
+		trace("version: %s all: %s tail: %s", version.getWithoutQualifier(), versions, tail);
+		if (!tail.isEmpty()) {
+			Version already = tail.first();
+			if (already.getMajor() == version.getMajor()) {
+				error("The repository %s already contains later or equal version(s) %s for %s-%s with the same major number",
+						repo.getName(), tail, bsn, version);
 				return null;
 			}
 		}
 
-		List<RepositoryPlugin> repos = getPlugins(RepositoryPlugin.class);
-		for (RepositoryPlugin repo : repos) {
-			if (repoName.equals(repo.getName())) {
-				SortedSet<Version> versions = repo.versions(getBsn());
-				if (!versions.isEmpty()) {
-					return repo.get(getBsn(), versions.last(), null);
+		//
+		// Loop over the instructions, first match commits.
+		//
+
+		for (Entry<Instruction,Attrs> e : baselines.entrySet()) {
+			if (e.getKey().matches(bsn)) {
+				Attrs attrs = e.getValue();
+				Version target;
+
+				if (attrs.containsKey("version")) {
+
+					// Specified version!
+
+					String v = attrs.get("version");
+					if (!Verifier.isVersion(v)) {
+						error("Not a valid version in %s %s", Constants.BASELINE, v);
+						return null;
+					}
+
+					Version base = new Version(v);
+
+					// Get the tail (includes the specified base if present)
+
+					SortedSet<Version> set = versions.tailSet(base);
+					if (set.isEmpty()) {
+						error("There is no higher or equal version for %s than the current version %s in %s", bsn,
+								version, repo);
+						return null;
+					}
+
+					// First element is equal or next to the base we desire
+
+					target = set.first();
+
+					// Now, we could end up with a higher version than our
+					// current
+					// project
+
+					if (target.compareTo(version) >= 0) {
+						error("The baseline version %s is higher or equal than the current version %s for %s in %s",
+								target, version, bsn, repo);
+						return null;
+					}
+				} else if (attrs.containsKey("file")) {
+
+					// Can be useful to specify a file
+					// for example when copying a bundle with a public api
+
+					File f = getProject().getFile(attrs.get("file"));
+					if (f != null && f.isFile()) {
+						Jar jar = new Jar(f);
+						addClose(jar);
+						return jar;
+					}
+					error("Specified file for baseline but could not find it %s", f);
+					return null;
+				} else {
+
+					// Ok, now we need to locate a version that is prior
+					// to the current version. The head is < then current.
+
+					SortedSet<Version> head = versions.headSet(version);
+					if (head.isEmpty()) {
+						error("There is no lower version for %s than the current version %s in %s", bsn, version, repo);
+						return null;
+					}
+
+					// The last in the head set must be the last valid
+					// version to compare against
+
+					target = head.last();
 				}
-				break;
+
+				// Fetch the revision
+
+				File file = repo.get(bsn, target, attrs);
+				if (file == null || !file.isFile()) {
+					error("Decided on version %s-%s but cannot get file from repo %s", bsn, version, repo);
+					return null;
+				}
+				Jar jar = new Jar(file);
+				addClose(jar);
+				return jar;
 			}
 		}
+
+		// Ignore, nothing matched
+
 		return null;
-
 	}
 
-	/** 
-	 * Gets the baseline Jar. 
+	/**
+	 * Remove any staging versions that have a variant with a higher qualifier.
 	 * 
-	 * @return the baseline jar
-	 * @throws Exception
+	 * @param versions
+	 * @return
 	 */
-	public Jar getBaselineJar() throws Exception {
-		return getBaselineJar(true);
+	private SortedSet<Version> removeStaged(SortedSet<Version> versions) {
+		List<Version> filtered = new ArrayList<Version>(versions);
+		Collections.reverse(filtered);
+
+		Version last = null;
+		for (Iterator<Version> i = filtered.iterator(); i.hasNext();) {
+			Version current = i.next().getWithoutQualifier();
+			if (last != null && current.equals(last))
+				i.remove();
+			else
+				last = current;
+		}
+		SortedList<Version> set = new SortedList<Version>(filtered);
+		trace("filtered for only latest staged: %s from %s", set, versions);
+		return set;
 	}
+
+	private RepositoryPlugin getReleaseRepo() {
+		RepositoryPlugin repo = null;
+		String repoName = getProperty(Constants.BASELINEREPO);
+		if (repoName == null)
+			repoName = getProperty(Constants.RELEASEREPO);
+
+		if (repoName != null && Constants.NONE.equals(repoName))
+			return null;
+
+		List<RepositoryPlugin> repos = getPlugins(RepositoryPlugin.class);
+		for (RepositoryPlugin r : repos) {
+			if (r.canWrite()) {
+				if (repoName == null || r.getName().equals(repoName)) {
+					repo = r;
+					break;
+				}
+			}
+		}
+
+		if (repo == null) {
+			if (repoName != null)
+				error("No writeable repo with name %s found", repoName);
+			else
+				error("No writeable repo found");
+		}
+
+		return repo;
+
+	}
+
 }
