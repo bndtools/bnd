bndtools/bnd

commit 3a216f64b9cadc8b9e0d4fe965fc5fc52425e9db

WIP on refreshing repos view when repos change

bndtools.core/src/bndtools/RepositoryListenerPluginTracker.java
@@ -9,7 +9,7 @@ import aQute.bnd.osgi.Jar;
 import aQute.bnd.service.RepositoryListenerPlugin;
 import aQute.bnd.service.RepositoryPlugin;
 
-public class RepositoryListenerPluginTracker extends ServiceTracker implements RepositoryListenerPlugin {
+public class RepositoryListenerPluginTracker extends ServiceTracker<RepositoryListenerPlugin,RepositoryListenerPlugin> implements RepositoryListenerPlugin {
 
     public RepositoryListenerPluginTracker(BundleContext context) {
         super(context, RepositoryListenerPlugin.class.getName(), null);
@@ -20,6 +20,22 @@ public class RepositoryListenerPluginTracker extends ServiceTracker implements R
         if (snapshot != null)
             for (Object l : snapshot) {
                 ((RepositoryListenerPlugin) l).bundleAdded(repository, jar, file);
+            }
+    }
+
+    public void bundleRemoved(RepositoryPlugin repository, Jar jar, File file) {
+        Object[] snapshot = getServices();
+        if (snapshot != null)
+            for (Object l : snapshot) {
+                ((RepositoryListenerPlugin) l).bundleRemoved(repository, jar, file);
+            }
+    }
+
+    public void repositoryRefreshed(RepositoryPlugin repository) {
+        Object[] snapshot = getServices();
+        if (snapshot != null)
+            for (Object l : snapshot) {
+                ((RepositoryListenerPlugin) l).repositoryRefreshed(repository);
             }
     }
 

bndtools.core/src/bndtools/builder/NewBuilder.java
@@ -47,6 +47,8 @@ import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import aQute.bnd.build.Project;
 import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Builder;
+import aQute.bnd.service.RepositoryListenerPlugin;
+import aQute.bnd.service.RepositoryPlugin;
 import aQute.lib.io.IO;
 import bndtools.Central;
 import bndtools.Logger;
@@ -126,6 +128,16 @@ public class NewBuilder extends IncrementalProjectBuilder {
                 } else {
                     log(LOG_FULL, "classpaths did not need to change");
                 }
+
+                // Notify the repository listeners that ALL repo contents may have changed.
+                List<RepositoryListenerPlugin> repoListeners = Central.getWorkspace().getPlugins(RepositoryListenerPlugin.class);
+                List<RepositoryPlugin> repos = Central.getWorkspace().getRepositories();
+                for (RepositoryListenerPlugin repoListener : repoListeners) {
+                    for (RepositoryPlugin repo : repos) {
+                        repoListener.repositoryRefreshed(repo);
+                    }
+                }
+
                 return dependsOn;
             }
 

bndtools.core/src/bndtools/views/RepositoriesView.java
@@ -486,6 +486,27 @@ public class RepositoriesView extends ViewPart implements RepositoryListenerPlug
     }
 
     public void bundleAdded(final RepositoryPlugin repository, Jar jar, File file) {
+        // TODO: make this smarter!
+        if (viewer != null)
+            SWTConcurrencyUtil.execForControl(viewer.getControl(), true, new Runnable() {
+                public void run() {
+                    viewer.refresh(repository);
+                }
+            });
+    }
+
+    public void bundleRemoved(final RepositoryPlugin repository, Jar jar, File file) {
+        // TODO: make this smarter!
+        if (viewer != null)
+            SWTConcurrencyUtil.execForControl(viewer.getControl(), true, new Runnable() {
+                public void run() {
+                    viewer.refresh(repository);
+                }
+            });
+    }
+
+    public void repositoryRefreshed(final RepositoryPlugin repository) {
+        // TODO: make this smarter!
         if (viewer != null)
             SWTConcurrencyUtil.execForControl(viewer.getControl(), true, new Runnable() {
                 public void run() {

commit 8dfb71a54dad96875e490f069f48c2b8033df31a

Fix the build on JDK7 after 3a216f6 broke it
Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

bndtools.core/src/bndtools/RepositoryListenerPluginTracker.java
@@ -9,7 +9,7 @@ import aQute.bnd.osgi.Jar;
 import aQute.bnd.service.RepositoryListenerPlugin;
 import aQute.bnd.service.RepositoryPlugin;
 
-public class RepositoryListenerPluginTracker extends ServiceTracker<RepositoryListenerPlugin,RepositoryListenerPlugin> implements RepositoryListenerPlugin {
+public class RepositoryListenerPluginTracker extends ServiceTracker implements RepositoryListenerPlugin {
 
     public RepositoryListenerPluginTracker(BundleContext context) {
         super(context, RepositoryListenerPlugin.class.getName(), null);