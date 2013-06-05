49e37bae8c5afe625aa780be9d1a7af7ffe6f5e6
Neil Bartlett <njbartlett@gmail.com>
2013-06-05 11:06:46 +0100

    Refresh repos menu entry

---
+++ bndtools.core/src/bndtools/RefreshReposHandler.java
@@ -0,0 +1,52 @@
+package bndtools;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import bndtools.api.ILogger;
+
+public class RefreshReposHandler extends AbstractHandler {
+
+    private static final ILogger logger = Logger.getLogger();
+
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+        try {
+            final IFile buildFile = Central.getWorkspaceBuildFile();
+            if (buildFile == null) {
+                MessageDialog.openError(window.getShell(), "Error", "Unable to refresh repositories: workspace build file is missing.");
+                return null;
+            }
+
+            window.run(true, false, new IRunnableWithProgress() {
+                public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                    try {
+                        buildFile.getWorkspace().run(new IWorkspaceRunnable() {
+                            public void run(IProgressMonitor monitor) throws CoreException {
+                                buildFile.touch(monitor);
+                            }
+                        }, monitor);
+                    } catch (CoreException e) {
+                        throw new InvocationTargetException(e);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            logger.logError("Error refreshing repositories.", e);
+        }
+
+        return null;
+    }
+
+}
