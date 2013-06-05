88334eb727fa3812fd307c8d0afa0279a3ec3e9e
Peter Kriens <peter.kriens@aqute.biz>
2013-06-05 11:59:31 +0200

    Added a new project

--- biz.aQute.bndlib.tests/src/test/ProjectTest.java
+++ biz.aQute.bndlib.tests/src/test/ProjectTest.java
@@ -523,12 +523,12 @@ public class ProjectTest extends TestCase {
 	}
 	
 	public static void testBuildAll() throws Exception {
-		assertTrue(testBuildAll("*", 14).check()); //there are 14 projects
-		assertTrue(testBuildAll("p*", 8).check()); //7 begin with p, plus build-all
+		assertTrue(testBuildAll("*", 15).check()); //there are 14 projects
+		assertTrue(testBuildAll("p*", 9).check()); //7 begin with p, plus build-all
 		assertTrue(testBuildAll("!p*, *", 6).check()); // negation: 6 don't begin with p, including build-all
 		assertTrue(testBuildAll("*-*", 7).check()); //more than one wildcard: 7 have a dash
 		assertTrue(testBuildAll("!p*, p1, *", 6).check("Missing dependson p1")); //check that an unused instruction is an error
-		assertTrue(testBuildAll("p*, !*-*, *", 11).check()); // check that negation works after some projects have been selected.
+		assertTrue(testBuildAll("p*, !*-*, *", 12).check()); // check that negation works after some projects have been selected.
 	}
 	
 	private static Project testBuildAll(String dependsOn, int count) throws Exception {
