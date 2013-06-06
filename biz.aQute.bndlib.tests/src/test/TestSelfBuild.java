45ed5f794ab7696fd1b462107afab7fdccc0babe
Peter Kriens <peter.kriens@aqute.biz>
2013-06-06 22:00:32 +0200

    Build test

--- biz.aQute.bndlib.tests/src/test/TestSelfBuild.java
+++ biz.aQute.bndlib.tests/src/test/TestSelfBuild.java
@@ -8,14 +8,14 @@ import aQute.bnd.build.*;
 public class TestSelfBuild extends TestCase {
 
 	public static void testSelfBuild() throws Throwable {
-//		Project project = Workspace.getWorkspace(new File("").getAbsoluteFile().getParentFile()).getProject(
-//				"biz.aQute.bndlib");
-//		project.setPedantic(true);
-//		project.action("build");
-//
-//		File files[] = project.build();
-//		assertTrue(project.check("Imports that lack version ranges", "for exported package aQute.bnd.service.resolve.hook"));
-//		assertNotNull(files);
-//		assertEquals(1, files.length);
+		Project project = Workspace.getWorkspace(new File("").getAbsoluteFile().getParentFile()).getProject(
+				"biz.aQute.bndlib");
+		project.setPedantic(true);
+		project.action("build");
+
+		File files[] = project.build();
+		assertTrue(project.check("Imports that lack version ranges", "for exported package aQute.bnd.service.resolve.hook"));
+		assertNotNull(files);
+		assertEquals(1, files.length);
 	}
 }
