8106fd594fb6bc1937c253251c47fe5ae1ad25e1
Peter Kriens <peter.kriens@aqute.biz>
2013-06-12 14:53:01 +0200

    Renamed and cleaned up

---
+++ bnd-maven-plugin/src/main/java/aQute/bnd/mavenplugin/ConfigureMavenProject.java
@@ -0,0 +1,215 @@
+package aQute.bnd.mavenplugin;
+
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.maven.artifact.*;
+import org.apache.maven.execution.*;
+import org.apache.maven.model.*;
+import org.apache.maven.plugin.*;
+import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.*;
+import org.codehaus.plexus.util.xml.*;
+
+import aQute.bnd.build.*;
+import aQute.bnd.osgi.*;
+import aQute.lib.io.*;
+
+/**
+ * This class is instantiated to setup the build path based on the bnd project
+ * information that resides in the same directory.
+ * 
+ */
+@Mojo(name = "prepare")
+public class ConfigureMavenProject extends AbstractMojo {
+	private static final String ORG_APACHE_MAVEN_PLUGINS_MAVEN_COMPILER_PLUGIN = "org.apache.maven.plugins:maven-compiler-plugin";
+	Set<Project> built = new HashSet<Project>();
+	@Component
+	private MavenSession session;
+
+	@Component
+	protected MavenProject project;
+
+	/**
+	 * Called to setup this project.
+	 */
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// Ensure this is an ordered set!
+		Set<Artifact> classpath = new LinkedHashSet<Artifact>();
+
+		try {
+			Workspace workspace = InitializeForBnd.getWorkspace(new File(
+					session.getExecutionRootDirectory()));
+
+			
+			Project bndProject = workspace.getProject(project.getArtifactId());
+			if ( bndProject == null) {
+				bndProject = Workspace.getProject(project.getBasedir());
+			}
+			if (bndProject == null || !bndProject.isValid())
+				throw new MojoExecutionException("Cannot find valid project "
+						+ project.getArtifactId() + " in "
+						+ workspace.getBase());
+
+			// The bnd project might have some different ideas about
+			// source and bin folders. So get them and set them.
+
+			Build build = project.getBuild();
+			build.setOutputDirectory(bndProject.getOutput().getAbsolutePath());
+			build.setTestOutputDirectory(bndProject.getOutput()
+					.getAbsolutePath());
+
+			// We might want to change this to methods in bnd's Project.
+
+			build.setTestSourceDirectory(bndProject.getProperty("src.test",
+					bndProject.getSrc().getAbsolutePath()));
+			build.setTestOutputDirectory(bndProject.getProperty("bin.test",
+					bndProject.getOutput().getAbsolutePath()));
+
+			build.setDirectory(bndProject.getTarget().getAbsolutePath());
+			build.setSourceDirectory(bndProject.getSrc().getAbsolutePath());
+
+			// Now we have to configure the compiler so that we have the same
+			// options in Eclipse and maven
+
+			Plugin plugin = project
+					.getPlugin(ORG_APACHE_MAVEN_PLUGINS_MAVEN_COMPILER_PLUGIN);
+			if (plugin == null) {
+				// TODO understand why we must list the compiler plugin
+				// in the project pom?
+				getLog().error(
+						"Cannot find compiler plugin "
+								+ ORG_APACHE_MAVEN_PLUGINS_MAVEN_COMPILER_PLUGIN);
+			} else {
+
+				// Configuration is per PluginExecution, so configure all the
+				// executions
+				// TODO understand the life cycle ... have no clue how the
+				// configuration
+				// works. Seems impossible to override the pom configuration :-(
+
+				for (PluginExecution pe : plugin.getExecutions()) {
+					Xpp3Dom config = (Xpp3Dom) pe.getConfiguration();
+					if (config == null)
+						config = new Xpp3Dom("configuration");
+					set(config, "source",
+							bndProject.getProperty("javac.source"));
+					set(config, "target",
+							bndProject.getProperty("javac.target"));
+					set(config, "optimize",
+							bndProject.getProperty("javac.optimize"));
+					set(config, "debugLevel",
+							bndProject.getProperty("javac.debugLevel"));
+					set(config, "encoding",
+							bndProject.getProperty("javac.encoding", "UTF-8"));
+
+					set(config,
+							"debug",
+							""
+									+ Processor.isTrue(bndProject.getProperty(
+											"javac.debug", "" + true)));
+					set(config, "compilerVersion",
+							bndProject.getProperty("javac.compilerVersion"));
+					set(config, "compilerArgument",
+							bndProject.getProperty("javac.compilerArgument"));
+					getLog().debug(
+							"[bnd] compiler configuration "
+									+ config.toString().replace('\n', ' '));
+					pe.setConfiguration(config);
+				}
+
+			}
+			// Hmm, the compiler seems special ...
+			for (File src : bndProject.getSourcePath())
+				project.addCompileSourceRoot(src.getAbsolutePath());
+
+			// Handle any source resources since the standard
+			// java compiler does not copy them
+			// TODO shoul do this in bnd some way since the ant
+			// and gradle build also suffer from this
+
+			copyResourcesFromSourcePath(bndProject);
+
+			// Setup the classpath
+
+			for (Container entry : bndProject.getBuildpath()) {
+				if (entry.getError() != null) {
+					getLog().error(
+							"Erroneous dependency "
+									+ entry.getBundleSymbolicName() + "-"
+									+ entry.getVersion() + " = "
+									+ entry.getError());
+				} else {
+					Artifact artifact = new BndArtifact(entry);
+					classpath.add(artifact);
+				}
+			}
+
+			getLog().info("[bnd] classpath " + classpath);
+			project.setResolvedArtifacts(classpath);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Utility to handle this XML tag thingy
+	 * 
+	 * @param config
+	 *            the xml object
+	 * @param key
+	 *            tkey we want to change
+	 * @param value
+	 *            the value
+	 * 
+	 *            TODO handle nested keys?
+	 */
+	private void set(Xpp3Dom config, String key, String value) {
+		if (config != null && value != null) {
+			Xpp3Dom child = config.getChild(key);
+			if (child == null) {
+				child = new Xpp3Dom(key);
+				config.addChild(child);
+			}
+			child.setValue(value);
+			project.getProperties().setProperty("maven.compiler." + key, value);
+		}
+	}
+
+	/**
+	 * The ecj compiler copies resources from source directories but the
+	 * standard Java compiler does not. So this method will copy the non-Java
+	 * files.
+	 * 
+	 * @param p the bnd project
+	 * @throws Exception
+	 */
+	private void copyResourcesFromSourcePath(Project p) throws Exception {
+		for (File src : p.getSourcePath())
+			copyResources(src, p.getOutput());
+	}
+
+	/**
+	 * Recursive routine to traverse the directory
+	 * 
+	 * @param src the src directory
+	 * @param output the output directory
+	 * @throws IOException
+	 */
+	private void copyResources(File src, File output) throws IOException {
+		if (src.isFile()) {
+			if (!src.getName().endsWith(".java"))
+				IO.copy(src, output);
+		} else if (src.isDirectory()) {
+			output.mkdirs();
+			for (String sub : src.list()) {
+				copyResources(new File(src, sub), new File(output, sub));
+			}
+		}
+	}
+}
---
+++ bnd-maven-plugin/src/main/java/aQute/bnd/mavenplugin/InitializeForBnd.java
@@ -0,0 +1,121 @@
+package aQute.bnd.mavenplugin;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.maven.*;
+import org.apache.maven.execution.*;
+import org.apache.maven.model.*;
+import org.apache.maven.plugin.*;
+import org.apache.maven.project.*;
+import org.codehaus.plexus.component.annotations.*;
+import org.codehaus.plexus.logging.*;
+
+import aQute.bnd.build.*;
+
+/**
+ * Since we have the project ordering information in bnd, we can reorder the
+ * list of projects. However, that requires us to start very early in the
+ * process. This is done through a AbstractMavenLifecycleParticipant. After the
+ * projects are read we reorder the list according to the bnd deps. We provide
+ * warnings for missing projects and of course cycles.
+ * 
+ * The example that helped me write this:
+ * 
+ * {@code http://brettporter.wordpress.com/2010/10/05/creating-a-custom-build-extension-for-maven-3-0/ }
+ * 
+ */
+
+@Component(role = AbstractMavenLifecycleParticipant.class)
+public class InitializeForBnd extends AbstractMavenLifecycleParticipant {
+
+	@Requirement
+	Logger logger;
+	static Workspace workspace;
+
+	@Override
+	public void afterProjectsRead(MavenSession session)
+			throws MavenExecutionException {
+		try {
+			Workspace workspace = InitializeForBnd.getWorkspace(new File(session
+					.getExecutionRootDirectory()));
+
+			// Build an index and an inverted list of the projects
+
+			Map<MavenProject, Project> index = new HashMap<MavenProject, Project>();
+			Map<Project, MavenProject> inverted = new HashMap<Project, MavenProject>();
+
+			for (MavenProject mp : session.getProjects()) {
+				logger.info("+ " + mp.getBasedir());
+				Project bp = workspace.getProject(mp.getArtifactId());
+				if (bp == null) {
+					logger.warn("[bnd] cannot find a bnd project for " + mp + " " + mp.isExecutionRoot());
+				} else {
+					index.put(mp, bp);
+					inverted.put(bp, mp);
+				}
+			}
+
+			// Go through the list of the maven projects and create
+			// a dependency on any project in the current build that we can find
+			// maven will then automatically order them correctly
+			// (the reactor order I think it is called)
+
+			for (Map.Entry<MavenProject, Project> e : index.entrySet()) {
+				for (Project bp : e.getValue().getDependson()) {
+					MavenProject mp = inverted.get(bp);
+					if (mp != null) {
+						Dependency dp = new Dependency();
+						dp.setArtifactId(mp.getArtifactId());
+						dp.setGroupId(mp.getGroupId());
+						dp.setVersion(mp.getVersion());
+						e.getKey().getModel().addDependency(dp);
+					} else {
+						logger.error("[bnd] dependency missing from "
+								+ e.getValue() + " to " + bp);
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Hmm, seems never to get called
+	 */
+	@Override
+	public void afterSessionStart(MavenSession session)
+			throws MavenExecutionException {
+	}
+
+	
+	/**
+	 * Get the workspace and keep a local copy as a static (YUCK!!!!!!!!!).
+	 * 
+	 * TODO figure out how to share an instance between phases and goals
+	 * 
+	 * @param executionRoot
+	 * @return
+	 * @throws Exception
+	 */
+	static Workspace getWorkspace(File executionRoot) throws Exception {
+	    if ( InitializeForBnd.workspace != null)
+	    	return InitializeForBnd.workspace;
+	    
+	    File currentFolder = new File("cnf").getCanonicalFile();
+	    if (currentFolder.exists() && currentFolder.isDirectory())
+	        return InitializeForBnd.workspace=new Workspace(currentFolder.getParentFile());
+	    
+	    File sessionRoot = new File(executionRoot,"/cnf").getCanonicalFile();
+	    if (sessionRoot.exists() && sessionRoot.isDirectory())
+	        return InitializeForBnd.workspace=new Workspace(sessionRoot.getParentFile());
+	    
+	    File upFolder = new File("../cnf").getCanonicalFile();
+	    if (upFolder.exists() && upFolder.isDirectory())
+	        return InitializeForBnd.workspace=new Workspace(upFolder.getParentFile());
+	
+	    throw new MojoFailureException("No workspace folder found!");
+	}
+
+}
