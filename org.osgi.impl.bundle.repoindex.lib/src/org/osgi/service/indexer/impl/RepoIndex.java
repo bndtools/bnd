b090cebeaa3271bd82f8ffab62d98a47d601318b
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-06-26 18:58:33 +0200

    indexer: add some comments
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- org.osgi.impl.bundle.repoindex.lib/src/org/osgi/service/indexer/impl/RepoIndex.java
+++ org.osgi.impl.bundle.repoindex.lib/src/org/osgi/service/indexer/impl/RepoIndex.java
@@ -30,23 +30,50 @@ import org.osgi.service.indexer.impl.util.Pair;
 import org.osgi.service.indexer.impl.util.Tag;
 import org.osgi.service.log.LogService;
 
+/**
+ * The repository indexer. See OSGi Enterprise Specification 5.0.0, chapter 132.
+ */
 public class RepoIndex implements ResourceIndexer {
-	
+	/**
+	 * Name of the configuration variable for the increment (if not set then
+	 * System.currentTimeMillis() is used)
+	 */
 	static final String REPOSITORY_INCREMENT_OVERRIDE = "-repository.increment.override";
-	
+
+	/** the generic bundle analyzer */
 	private final BundleAnalyzer bundleAnalyzer;
+
+	/** the OSGi Framework analyzer */
 	private final OSGiFrameworkAnalyzer frameworkAnalyzer;
+
+	/** the Declarative Services analyzer */
 	private final SCRAnalyzer scrAnalyzer;
+
+	/** the BluePrint analyzer */
 	private final BlueprintAnalyzer blueprintAnalyzer;
-	
+
+	/** the logger */
 	private final LogService log;
-	
-	private final List<Pair<ResourceAnalyzer, Filter>> analyzers = new LinkedList<Pair<ResourceAnalyzer,Filter>>();
-	
+
+	/**
+	 * the list of analyzer/filter pairs. The filter determines which resources
+	 * can be analyzed
+	 */
+	private final List<Pair<ResourceAnalyzer, Filter>> analyzers = new LinkedList<Pair<ResourceAnalyzer, Filter>>();
+
+	/**
+	 * Construct a default instance that uses a console logger.
+	 */
 	public RepoIndex() {
 		this(new ConsoleLogSvc());
 	}
-	
+
+	/**
+	 * Constructor
+	 * 
+	 * @param log
+	 *            the log service to use
+	 */
 	public RepoIndex(LogService log) {
 		this.log = log;
 
@@ -54,36 +81,50 @@ public class RepoIndex implements ResourceIndexer {
 		this.frameworkAnalyzer = new OSGiFrameworkAnalyzer(log);
 		this.scrAnalyzer = new SCRAnalyzer(log);
 		this.blueprintAnalyzer = new BlueprintAnalyzer(log);
-		
+
 		try {
 			Filter allFilter = createFilter("(name=*.jar)");
-			
+
 			addAnalyzer(bundleAnalyzer, allFilter);
 			addAnalyzer(frameworkAnalyzer, allFilter);
 			addAnalyzer(scrAnalyzer, allFilter);
 			addAnalyzer(blueprintAnalyzer, allFilter);
-
 		} catch (InvalidSyntaxException e) {
 			throw new ExceptionInInitializerError("Unexpected internal error compiling filter");
 		}
 	}
-	
+
+	/**
+	 * @param analyzer
+	 *            the analyzer to add
+	 * @param filter
+	 *            the filter that determines which resources can be analyzed
+	 */
 	public final void addAnalyzer(ResourceAnalyzer analyzer, Filter filter) {
 		synchronized (analyzers) {
 			analyzers.add(Pair.create(analyzer, filter));
 		}
 	}
-	
+
+	/**
+	 * @param analyzer
+	 *            the analyzer to add
+	 * @param filter
+	 *            the filter that determines which resources can be analyzed
+	 */
 	public final void removeAnalyzer(ResourceAnalyzer analyzer, Filter filter) {
 		synchronized (analyzers) {
 			analyzers.remove(Pair.create(analyzer, filter));
 		}
 	}
 
+	/*
+	 * See ResourceIndexer interface
+	 */
 	public void index(Set<File> files, OutputStream out, Map<String, String> config) throws Exception {
 		if (config == null)
 			config = new HashMap<String, String>(0);
-		
+
 		Indent indent;
 		PrintWriter pw;
 		if (config.get(ResourceIndexer.PRETTY) != null) {
@@ -93,22 +134,22 @@ public class RepoIndex implements ResourceIndexer {
 			indent = Indent.NONE;
 			pw = new PrintWriter(new GZIPOutputStream(out, Deflater.BEST_COMPRESSION));
 		}
-		
+
 		pw.print(Schema.XML_PROCESSING_INSTRUCTION);
 		Tag repoTag = new Tag(Schema.ELEM_REPOSITORY);
-		
+
 		String repoName = config.get(REPOSITORY_NAME);
 		if (repoName == null)
 			repoName = REPOSITORYNAME_DEFAULT;
 		repoTag.addAttribute(Schema.ATTR_NAME, repoName);
-		
+
 		String increment = config.get(REPOSITORY_INCREMENT_OVERRIDE);
 		if (increment == null)
 			increment = Long.toString(System.currentTimeMillis());
 		repoTag.addAttribute(Schema.ATTR_INCREMENT, increment);
-		
+
 		repoTag.addAttribute(Schema.ATTR_XML_NAMESPACE, Schema.NAMESPACE);
-		
+
 		repoTag.printOpen(indent, pw, false);
 		for (File file : files) {
 			Tag resourceTag = generateResource(file, config);
@@ -124,19 +165,19 @@ public class RepoIndex implements ResourceIndexer {
 			pw = (PrintWriter) out;
 		else
 			pw = new PrintWriter(out);
-		
+
 		for (File file : files) {
 			Tag resourceTag = generateResource(file, config);
 			resourceTag.print(Indent.PRETTY, pw);
 		}
 	}
-	
+
 	private Tag generateResource(File file, Map<String, String> config) throws Exception {
-		
+
 		JarResource resource = new JarResource(file);
 		List<Capability> caps = new AddOnlyList<Capability>(new LinkedList<Capability>());
 		List<Requirement> reqs = new AddOnlyList<Requirement>(new LinkedList<Requirement>());
-		
+
 		// Read config settings and save in thread local state
 		if (config != null) {
 			URL rootURL;
@@ -150,20 +191,20 @@ public class RepoIndex implements ResourceIndexer {
 			}
 			else
 				rootURL = new File(System.getProperty("user.dir")).toURI().toURL();
-			
+
 			String urlTemplate = config.get(ResourceIndexer.URL_TEMPLATE);
 			bundleAnalyzer.setStateLocal(new GeneratorState(rootURL, urlTemplate));
 		} else {
 			bundleAnalyzer.setStateLocal(null);
 		}
-		
+
 		// Iterate over the analyzers
 		try {
 			synchronized (analyzers) {
 				for (Pair<ResourceAnalyzer, Filter> entry : analyzers) {
 					ResourceAnalyzer analyzer = entry.getFirst();
 					Filter filter = entry.getSecond();
-					
+
 					if (filter == null || filter.match(resource.getProperties())) {
 						try {
 							analyzer.analyzeResource(resource, caps, reqs);
@@ -176,26 +217,26 @@ public class RepoIndex implements ResourceIndexer {
 		} finally {
 			bundleAnalyzer.setStateLocal(null);
 		}
-		
+
 		Tag resourceTag = new Tag(Schema.ELEM_RESOURCE);
 		for (Capability cap : caps) {
 			Tag capTag = new Tag(Schema.ELEM_CAPABILITY);
 			capTag.addAttribute(Schema.ATTR_NAMESPACE, cap.getNamespace());
-			
+
 			appendAttributeAndDirectiveTags(capTag, cap.getAttributes(), cap.getDirectives());
-			
+
 			resourceTag.addContent(capTag);
 		}
-		
+
 		for (Requirement req : reqs) {
 			Tag reqTag = new Tag(Schema.ELEM_REQUIREMENT);
 			reqTag.addAttribute(Schema.ATTR_NAMESPACE, req.getNamespace());
-			
+
 			appendAttributeAndDirectiveTags(reqTag, req.getAttributes(), req.getDirectives());
-			
+
 			resourceTag.addContent(reqTag);
 		}
-		
+
 		return resourceTag;
 	}
 
@@ -203,7 +244,7 @@ public class RepoIndex implements ResourceIndexer {
 		if (log != null)
 			log.log(level, message, t);
 	}
-	
+
 	private static void appendAttributeAndDirectiveTags(Tag parentTag, Map<String, Object> attribs, Map<String, String> directives) {
 		for (Entry<String, Object> attribEntry : attribs.entrySet()) {
 			Tag attribTag = new Tag(Schema.ELEM_ATTRIBUTE);
@@ -212,7 +253,7 @@ public class RepoIndex implements ResourceIndexer {
 			TypedAttribute typedAttrib = TypedAttribute.create(attribEntry.getKey(), attribEntry.getValue());
 			parentTag.addContent(typedAttrib.toXML());
 		}
-		
+
 		for (Entry<String, String> directiveEntry : directives.entrySet()) {
 			Tag directiveTag = new Tag(Schema.ELEM_DIRECTIVE);
 			directiveTag.addAttribute(Schema.ATTR_NAME, directiveEntry.getKey());
@@ -220,5 +261,5 @@ public class RepoIndex implements ResourceIndexer {
 			parentTag.addContent(directiveTag);
 		}
 	}
-	
+
 }
