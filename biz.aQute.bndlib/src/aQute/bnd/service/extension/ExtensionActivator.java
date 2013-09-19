1906ab34fde5745f10327e4d1c7ab56cf847ef5a
Peter Kriens <peter.kriens@aqute.biz>
2013-09-19 19:13:56 +0200

    Complained about missing package info

--- biz.aQute.bndlib/src/aQute/bnd/service/extension/ExtensionActivator.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/extension/ExtensionActivator.java
@@ -3,7 +3,6 @@ package aQute.bnd.service.extension;
 import java.util.*;
 
 import aQute.bnd.build.*;
-import aQute.bnd.header.*;
 
 public interface ExtensionActivator {
 	List< ? > activate(Workspace workspace, Map<String,String> attrs);
