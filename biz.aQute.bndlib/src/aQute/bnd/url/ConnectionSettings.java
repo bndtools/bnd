bb78da398764513fadeb6a74fad7e005611ffcd3
Peter Kriens <peter.kriens@aqute.biz>
2013-09-19 18:05:42 +0200

    Documenting the URL Connection Handlers

--- biz.aQute.bndlib/src/aQute/bnd/url/ConnectionSettings.java
+++ biz.aQute.bndlib/src/aQute/bnd/url/ConnectionSettings.java
@@ -4,58 +4,69 @@ import java.net.*;
 import java.util.*;
 import java.util.Map.Entry;
 
-import aQute.bnd.service.*;
 import aQute.lib.converter.*;
 
 /**
- * Will iterate over the current plugins to find a matching URLConnectionHandler
- * and in the end use the default connector if no alternative is found.
+ * Generic connection setter can set a number of basic properties if applicable
+ * and can add generic headers. See {@link #config} for the options. For the
+ * propeties any property key that starts with an upper case is considered to be
+ * a header.
  */
 public class ConnectionSettings extends DefaultURLConnectionHandler {
-	Config config;
+	final Map<String,String>	headers	= new HashMap<String,String>();
+	Config						config;
+
+	/**
+	 * Options to set.
+	 */
 	interface Config {
 		int connectTimeout();
+
 		int readTimeout();
+
 		boolean useCaches();
+
 		int chunk();
+
 		boolean noredirect();
 	}
-	final Map<String,String> headers = new HashMap<String,String>(); 
-	
-	public ConnectionSettings(Registry registry) {
-		this.registry = registry;
-	}
 
+	/**
+	 * Handle the connection
+	 */
 	public void handle(URLConnection connection) throws Exception {
-		if ( matches(connection)) {
-			if ( config.connectTimeout() != 0)
+		if (matches(connection)) {
+			if (config.connectTimeout() != 0)
 				connection.setConnectTimeout(config.connectTimeout());
-			if ( config.readTimeout() != 0)
+			if (config.readTimeout() != 0)
 				connection.setConnectTimeout(config.readTimeout());
 
-			for ( Entry<String,String> entry : headers.entrySet()) {
-				if ( Character.isUpperCase(entry.getKey().charAt(0)))
+			for (Entry<String,String> entry : headers.entrySet()) {
+				if (Character.isUpperCase(entry.getKey().charAt(0)))
 					connection.setRequestProperty(entry.getKey(), entry.getValue());
 			}
-			
-			if ( connection instanceof HttpURLConnection) {
+
+			if (connection instanceof HttpURLConnection) {
 				HttpURLConnection http = (HttpURLConnection) connection;
 				if (config.chunk() > 0)
 					http.setChunkedStreamingMode(config.chunk());
 
 				http.setInstanceFollowRedirects(!config.noredirect());
-				
+
 			}
- 		}
+		}
 	}
-	
+
+	/**
+	 * Set the properties.
+	 */
 	public void setProperties(Map<String,String> map) throws Exception {
-		for ( Entry<String,String> entry : headers.entrySet()) {
-			if ( Character.isUpperCase(entry.getKey().charAt(0)))
+		super.setProperties(map);
+		for (Entry<String,String> entry : headers.entrySet()) {
+			if (Character.isUpperCase(entry.getKey().charAt(0)))
 				headers.put(entry.getKey(), entry.getValue());
 		}
 		config = Converter.cnv(Config.class, map);
 	}
-
 
 }
