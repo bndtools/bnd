f25c5314b35502ca11a6aa8cc0e90aaabe1e3b3c
PK Söreide <per.kristian.soreide@gmail.com>
2013-10-08 16:16:38 +0200

    The call to Central.isWorkspaceReady() is not needed

--- bndtools.core/src/bndtools/central/WorkspaceListener.java
+++ bndtools.core/src/bndtools/central/WorkspaceListener.java
@@ -5,8 +5,6 @@ import java.io.File;
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
 import org.bndtools.utils.Function;
-import org.eclipse.core.runtime.NullProgressMonitor;
-
 import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Processor;
 import aQute.bnd.service.BndListener;
@@ -27,16 +25,12 @@ public final class WorkspaceListener extends BndListener {
         try {
             final RefreshFileJob job = new RefreshFileJob(file, true);
             if (job.needsToSchedule()) {
-                if (!Central.isWorkspaceReady()) {
-                    Central.onWorkspaceInit(new Function<Workspace,Void>() {
-                        public Void run(final Workspace ws) {
-                            job.run(new NullProgressMonitor());
-                            return null;
-                        }
-                    });
-                } else {
-                    job.schedule();
-                }
+                Central.onWorkspaceInit(new Function<Workspace,Void>() {
+                    public Void run(final Workspace ws) {
+                        job.schedule();
+                        return null;
+                    }
+                });
             }
         } catch (Exception e) {
             logger.logError("Error refreshing workspace", e);
