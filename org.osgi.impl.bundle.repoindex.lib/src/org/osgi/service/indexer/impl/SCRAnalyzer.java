8263f4b0ae749233283a323ed3e9b35d1f84fe11
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-10-20 12:34:31 +0200

    Fix #27: Add osgi.service caps/reqs for DS components
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- org.osgi.impl.bundle.repoindex.lib/src/org/osgi/service/indexer/impl/SCRAnalyzer.java
+++ org.osgi.impl.bundle.repoindex.lib/src/org/osgi/service/indexer/impl/SCRAnalyzer.java
@@ -28,6 +28,15 @@ public class SCRAnalyzer implements ResourceAnalyzer {
 	public static final String NS_1_1 = Namespaces.NS_OSGI + "/scr/v1.1.0";
 	public static final String NS_1_2 = Namespaces.NS_OSGI + "/scr/v1.2.0";
 
+	public static final String ELEMENT_COMPONENT = "component";
+	public static final String ELEMENT_SERVICE = "service";
+	public static final String ELEMENT_PROVIDE = "provide";
+	public static final String ELEMENT_REFERENCE = "reference";
+
+	public static final String ATTRIB_INTERFACE = "interface";
+
+	public static final String ATTRIB_CARDINALITY = "cardinality";
+	public static final String ATTRIB_CARDINALITY_DEFAULT = "1..1";
 
 	private LogService log;
 
@@ -47,7 +56,7 @@ public class SCRAnalyzer implements ResourceAnalyzer {
 			List<String> paths = Util.findMatchingPaths(resource, pattern);
 			if (paths != null)
 				for (String path : paths) {
-					Version version = processScrXml(resource, path);
+					Version version = processScrXml(resource, path, caps, reqs);
 					if (highest == null || (version.compareTo(highest) > 0))
 						highest = version;
 				}
@@ -61,7 +70,7 @@ public class SCRAnalyzer implements ResourceAnalyzer {
 		}
 	}
 
-	private Version processScrXml(Resource resource, String path) throws IOException {
+	private Version processScrXml(Resource resource, String path, List<Capability> caps, List<Requirement> reqs) throws IOException {
 		Resource childResource = resource.getChild(path);
 		if (childResource == null) {
 			if (log != null)
@@ -76,7 +85,7 @@ public class SCRAnalyzer implements ResourceAnalyzer {
 
 		try {
 			SAXParser parser = spf.newSAXParser();
-			SCRContentHandler handler = new SCRContentHandler();
+			SCRContentHandler handler = new SCRContentHandler(caps, reqs);
 			parser.parse(childResource.getStream(), handler);
 
 			return handler.highest;
@@ -103,31 +112,114 @@ public class SCRAnalyzer implements ResourceAnalyzer {
 	}
 
 	private static class SCRContentHandler extends DefaultHandler {
+		private List<Capability> caps;
+		private List<Requirement> reqs;
 
+		private boolean inComponent = false;
+		private boolean inService = false;
 		private Version highest = null;
-		private boolean beforeRoot = true;
+
+		public SCRContentHandler(List<Capability> caps, List<Requirement> reqs) {
+			super();
+			this.caps = caps;
+			this.reqs = reqs;
+		}
 
 		@Override
 		public void startElement(String uri, String localName, String qName, Attributes attribs) throws SAXException {
-			if (uri == null || "".equals(uri)) {
-				if (beforeRoot) {
-					beforeRoot = false;
-					setVersion(new Version(1, 0, 0));
-				}
-			} else {
-				if (NS_1_2.equals(uri))
-					setVersion(new Version(1, 2, 0));
-				else if (NS_1_1.equals(uri))
-					setVersion(new Version(1, 1, 0));
-				else if (NS_1_0.equals(uri))
-					setVersion(new Version(1, 0, 0));
+			super.startElement(uri, localName, qName, attribs);
+
+			String localNameLowerCase = localName.toLowerCase();
+			if (ELEMENT_COMPONENT.equals(localNameLowerCase)) {
+				inComponent = true;
 			}
+
+			if (inComponent) {
+				if (uri == null || "".equals(uri)) {
+					setVersion(new Version(1, 0, 0));
+				} else {
+					if (NS_1_2.equals(uri))
+						setVersion(new Version(1, 2, 0));
+					else if (NS_1_1.equals(uri))
+						setVersion(new Version(1, 1, 0));
+					else if (NS_1_0.equals(uri))
+						setVersion(new Version(1, 0, 0));
+					else
+						throw new SAXException("Unknown namespace " + uri);
+				}
+
+				if (ELEMENT_SERVICE.equals(localNameLowerCase)) {
+					inService = true;
+					return;
+				}
+
+				if (inService && ELEMENT_PROVIDE.equals(localNameLowerCase)) {
+					createServiceCapability(attribs);
+					return;
+				}
+
+				if (ELEMENT_REFERENCE.equals(localNameLowerCase)) {
+					createServiceRequirement(attribs);
+					return;
+				}
+			}
+		}
+
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			String localNameLowerCase = localName.toLowerCase();
+			if (ELEMENT_COMPONENT.equals(localNameLowerCase)) {
+				inComponent = false;
+			}
+
+			if (inComponent) {
+				if (ELEMENT_SERVICE.equals(localNameLowerCase)) {
+					inService = false;
+					return;
+				}
+			}
+
+			super.endElement(uri, localName, qName);
 		}
 
 		private void setVersion(Version version) {
 			if (highest == null || (version.compareTo(highest) > 0))
 				highest = version;
 		}
-	}
 
+		private void createServiceCapability(Attributes attribs) throws SAXException {
+			String interfaceClass = attribs.getValue(ATTRIB_INTERFACE);
+			if (interfaceClass == null || interfaceClass.length() == 0) {
+				throw new SAXException("Missing required " + ATTRIB_INTERFACE + " attribute");
+			}
+
+			Builder builder = new Builder().setNamespace(Namespaces.NS_SERVICE);
+			builder.addAttribute(Namespaces.NS_SERVICE, interfaceClass);
+			builder.addDirective(Namespaces.DIRECTIVE_EFFECTIVE, Namespaces.EFFECTIVE_ACTIVE);
+
+			caps.add(builder.buildCapability());
+		}
+
+		private void createServiceRequirement(Attributes attribs) throws SAXException {
+			String interfaceClass = attribs.getValue(ATTRIB_INTERFACE);
+			if (interfaceClass == null || interfaceClass.length() == 0) {
+				throw new SAXException("Missing required " + ATTRIB_INTERFACE + " attribute");
+			}
+
+			String cardinality = attribs.getValue(ATTRIB_CARDINALITY);
+			if (cardinality == null || cardinality.length() == 0) {
+				cardinality = ATTRIB_CARDINALITY_DEFAULT;
+			}
+
+			Builder builder = new Builder().setNamespace(Namespaces.NS_SERVICE);
+			builder.addAttribute(Namespaces.NS_SERVICE, interfaceClass);
+			builder.addDirective(ATTRIB_CARDINALITY, cardinality.toLowerCase());
+			if (cardinality.startsWith("0")) {
+				builder.addDirective(Namespaces.DIRECTIVE_RESOLUTION, Namespaces.RESOLUTION_OPTIONAL);
+			}
+			builder.addDirective(Namespaces.DIRECTIVE_EFFECTIVE, Namespaces.EFFECTIVE_ACTIVE);
+
+			reqs.add(builder.buildRequirement());
+		}
+	}
 }
