f5bef0775a4393faddc98942050fe466c2c0f387
Peter Kriens <peter.kriens@aqute.biz>
2013-11-04 16:13:05 +0100

    Added Java 1.7, 1.8 compiler versions We maintain a number of test classes compiled with the Eclipse Java compiler and the Sun (now Oracle) Java compiler. I added the 1.7 for both and 1.8 for javac. Changed the method. Added the class file to git because newer compilers might not be able to recompile for all the old and they may change settings (which I ran into, ecj now required a boot class path).
    So the class files are now under git control

---
+++ biz.aQute.bndlib.tests/compilerversions/src/eclipse_1_7/ClassRef.java
@@ -0,0 +1,13 @@
+package eclipse_1_7;
+
+public class ClassRef {
+	class Inner {
+	};
+	static {
+		System.out.println(Inner.class);
+	}
+	
+	public static void main() {
+		System.out.println(javax.swing.Box.class);
+	}
+}
