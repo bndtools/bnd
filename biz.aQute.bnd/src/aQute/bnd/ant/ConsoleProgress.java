9c62199535beb7e1f4efa1d8c84c21a5975a3f09
Ferry Huberts <ferry.huberts@pelagic.nl>
2013-12-19 11:08:26 +0100

    ant: make the ConsoleProgress a bit more civilised
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.bnd/src/aQute/bnd/ant/ConsoleProgress.java
+++ biz.aQute.bnd/src/aQute/bnd/ant/ConsoleProgress.java
@@ -5,13 +5,13 @@ import aQute.bnd.service.progress.*;
 public class ConsoleProgress implements ProgressPlugin {
 
 	public Task startTask(String name, int size) {
-		System.out.println(name);
+		System.out.printf(name);
 		return new Task() {
 			public void worked(int units) {
 				// TODO
 			}
 			public void done(String message, Throwable e) {
-				System.out.println("DONE: " + message);
+				System.out.println(": " + message);
 				if (e != null)
 					e.printStackTrace();
 			}
