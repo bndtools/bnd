2fb690a9206a67a4fa46dcd7575fef0fff7fdb23
Peter Kriens <peter.kriens@aqute.biz>
2013-12-20 10:06:11 +0100

    Ongoing

---
+++ biz.aQute.bndoc/test/biz/aQute/bndoc/lib/DocumentBuilderTest.java
@@ -0,0 +1,15 @@
+package biz.aQute.bndoc.lib;
+
+import junit.framework.*;
+import aQute.lib.env.*;
+
+public class DocumentBuilderTest extends TestCase {
+	public void testBlock() {
+		DocumentBuilder db = new DocumentBuilder(new Env());
+		
+		assertEquals( "A\n  A\n  ...\n", db._block( new String[]{"","1", "2","2", "ABCDEF\nAbcdef\nA\n"}));
+		assertEquals( "ABC\n  Abc\n  ...\n", db._block( new String[]{"","3", "2","2", "ABCDEF\nAbcdef\nA\n"}));
+		assertEquals( "A\nA\n...\n", db._block( new String[]{"","1", "2","0", "A\nA\nA\n"}));
+		assertEquals( "A\n  A\n  ...\n", db._block( new String[]{"","1", "2","2", "A\nA\nA\n"}));
+	}
+}
