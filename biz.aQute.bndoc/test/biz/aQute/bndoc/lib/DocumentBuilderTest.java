0abfd5ae0d5948cef2ed1c46874c64266f564cce
Peter Kriens <peter.kriens@aqute.biz>
2014-02-17 09:16:57 +0100

    Removed warning

--- biz.aQute.bndoc/test/biz/aQute/bndoc/lib/DocumentBuilderTest.java
+++ biz.aQute.bndoc/test/biz/aQute/bndoc/lib/DocumentBuilderTest.java
@@ -1,15 +1,18 @@
 package biz.aQute.bndoc.lib;
 
+import java.io.*;
+
 import junit.framework.*;
 import aQute.lib.env.*;
 
 public class DocumentBuilderTest extends TestCase {
-	public void testBlock() {
+	public void testBlock() throws IOException {
 		DocumentBuilder db = new DocumentBuilder(new Env());
 		
 		assertEquals( "A\n  A\n  ...\n", db._block( new String[]{"","1", "2","2", "ABCDEF\nAbcdef\nA\n"}));
 		assertEquals( "ABC\n  Abc\n  ...\n", db._block( new String[]{"","3", "2","2", "ABCDEF\nAbcdef\nA\n"}));
 		assertEquals( "A\nA\n...\n", db._block( new String[]{"","1", "2","0", "A\nA\nA\n"}));
 		assertEquals( "A\n  A\n  ...\n", db._block( new String[]{"","1", "2","2", "A\nA\nA\n"}));
+		db.close();
 	}
 }
