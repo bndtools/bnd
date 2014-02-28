652c9a5b6c2a589b1e421ba9346bcfceac61373b
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-02-28 02:05:15 +0100

    ProjectLaunchImplTest: cleanup after tests
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- biz.aQute.launcher/test/aQute/launcher/plugin/ProjectLaunchImplTest.java
+++ biz.aQute.launcher/test/aQute/launcher/plugin/ProjectLaunchImplTest.java
@@ -4,8 +4,29 @@ import java.io.*;
 
 import junit.framework.*;
 import aQute.bnd.build.*;
+import aQute.lib.io.*;
 
 public class ProjectLaunchImplTest extends TestCase {
+
+	private static void reallyClean(Workspace ws) throws Exception {
+		for (Project project : ws.getAllProjects()) {
+			project.clean();
+
+			File target = project.getTargetDir();
+			if (target.isDirectory() && target.getParentFile() != null) {
+				IO.delete(target);
+			}
+			File output = project.getSrcOutput().getAbsoluteFile();
+			if (output.isDirectory() && output.getParentFile() != null) {
+				IO.delete(output);
+			}
+		}
+		IO.delete(ws.getFile("cnf/cache"));
+	}
+
+	public void tearDown() throws Exception {
+		reallyClean(new Workspace(new File("test/ws")));
+	}
 
 	public static void testParseSystemCapabilities() throws Exception {
 		Workspace ws = Workspace.getWorkspace(new File("test/ws"));
