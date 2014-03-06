d6b83cbc02350eee99cc9842b50ee209f94a68b0
Peter Kriens <peter.kriens@aqute.biz>
2014-03-06 14:01:17 +0100

    Added Common Public License 1.0

---
+++ biz.aQute.bndlib/src/aQute/bnd/annotation/licenses/CPL_1_0.java
@@ -0,0 +1,12 @@
+package aQute.bnd.annotation.licenses;
+
+import aQute.bnd.annotation.headers.*;
+
+/**
+ * An annotation to indicate that the type depends on the Common Public
+ * License 1.0. Applying this annotation will add a Bundle-License clause.
+ */
+@BundleLicense(name = "http://opensource.org/licenses/cpl1.0.php", link = "http://www.ibm.com/developerworks/library/os-cpl.html", description = "aims of supporting and encouraging collaborative open-source development while still retaining the ability to use the CPL'd content with software licensed under other licenses, including many proprietary licenses.")
+public @interface CPL_1_0 {
+
+}
