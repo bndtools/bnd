3663fb68c950cc21b50ab56ecbf164e84b7ae24e
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-03-08 14:20:56 +0100

    ProjectPaths: use defaults from bnd
    The call to get the defaults MUST not access any remotes.
    I've checked and currently it doesn't.
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- bndtools.api/src/org/bndtools/api/ProjectPaths.java
+++ bndtools.api/src/org/bndtools/api/ProjectPaths.java
@@ -1,9 +1,25 @@
 package org.bndtools.api;
 
-public class ProjectPaths {
-    public static final String PATH_SRC = "src";
-    public static final String PATH_SRC_BIN = "bin";
+import aQute.bnd.build.Workspace;
+import aQute.bnd.osgi.Constants;
+import aQute.bnd.osgi.Processor;
 
-    public static final String PATH_TEST_SRC = "test";
-    public static final String PATH_TEST_BIN = "bin_test";
+public class ProjectPaths {
+	public static final String PATH_SRC;
+	public static final String PATH_SRC_BIN;
+
+	public static final String PATH_TEST_SRC;
+	public static final String PATH_TEST_BIN;
+
+	static {
+		/* This call MUST not access any remotes */
+		Processor defaults = Workspace.getDefaults();
+
+		PATH_SRC = defaults.getProperty(Constants.DEFAULT_PROP_SRC_DIR, "src");
+		PATH_SRC_BIN = defaults.getProperty(Constants.DEFAULT_PROP_BIN_DIR, "bin");
+		PATH_TEST_SRC = defaults
+				.getProperty(Constants.DEFAULT_PROP_TESTSRC_DIR, "test");
+		PATH_TEST_BIN = defaults
+				.getProperty(Constants.DEFAULT_PROP_TESTBIN_DIR, "bin_test");
+	}
 }
