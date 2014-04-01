d17ce7697efb6fbef7bebf3145e585b7bfba8377
Peter Kriens <peter.kriens@aqute.biz>
2014-04-01 18:09:40 +0200

    [repos] Improved comments

--- biz.aQute.bndlib/src/aQute/bnd/service/repository/RepositoryDigest.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/repository/RepositoryDigest.java
@@ -1,5 +1,14 @@
 package aQute.bnd.service.repository;
 
 public interface RepositoryDigest {
+	/**
+	 * Return a SHA-1 for the collection. This sha is calculated as follows:
+	 * 
+	 * 1) If there is a single file, return its sha
+	 * 
+	 * 2) Otherwise, create a set (skipping duplicates) of all shas, sort them, and then create
+	 * the sha of this file. 
+	 */
+	
 	byte[] getDigest();
 }
