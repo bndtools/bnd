68a2fe3a31b56fb1d57d2ff4322ec45f6bb872dd
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-05-02 00:06:51 +0200

    Fix a resource leak warning
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- bndtools.utils/src/org/bndtools/utils/jar/JarUtils.java
+++ bndtools.utils/src/org/bndtools/utils/jar/JarUtils.java
@@ -5,16 +5,21 @@ import java.io.InputStream;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
-import aQute.lib.io.IO;
-
 public class JarUtils {
 
     public static Manifest loadJarManifest(InputStream stream) throws IOException {
+        JarInputStream jarInputStream = null;
         try {
-            JarInputStream jarInputStream = new JarInputStream(stream);
+            jarInputStream = new JarInputStream(stream);
             return jarInputStream.getManifest();
         } finally {
-            IO.close(stream);
+            if (jarInputStream != null) {
+                try {
+                    jarInputStream.close();
+                } catch (IOException e) {
+                    /* swallow */
+                }
+            }
         }
     }
 }
