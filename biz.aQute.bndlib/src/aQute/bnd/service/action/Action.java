bcb61e0345815ebbf98fea72d5406fc8f7c0afa8
Peter Kriens <peter.kriens@aqute.biz>
2014-05-11 19:36:38 +0200

    Made action provider type

--- biz.aQute.bndlib/src/aQute/bnd/service/action/Action.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/action/Action.java
@@ -1,7 +1,9 @@
 package aQute.bnd.service.action;
 
+import aQute.bnd.annotation.*;
 import aQute.bnd.build.*;
 
+@ProviderType
 public interface Action {
 	void execute(Project project, String action) throws Exception;
 	void execute(Project project, Object ... args) throws Exception;
