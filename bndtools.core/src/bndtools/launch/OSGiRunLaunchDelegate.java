f4a63481f28a2473de033083c2bb562f83e937f4
tim.ward@paremus.com <timothyjward@apache.org>
2014-05-12 23:43:10 +0200

    Add a PopupDialog which displays error messages from the bnd launcher

--- bndtools.core/src/bndtools/launch/OSGiRunLaunchDelegate.java
+++ bndtools.core/src/bndtools/launch/OSGiRunLaunchDelegate.java
@@ -26,10 +26,22 @@ import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jface.dialogs.PopupDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.internal.Workbench;
 import org.osgi.framework.launch.FrameworkFactory;
 
 import aQute.bnd.build.Project;
 import aQute.bnd.build.ProjectLauncher;
+import aQute.bnd.build.ProjectLauncher.NotificationListener;
+import aQute.bnd.build.ProjectLauncher.NotificationType;
 import aQute.bnd.osgi.Jar;
 import aQute.lib.io.IO;
 import bndtools.Plugin;
@@ -41,12 +53,35 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
 
     private ProjectLauncher bndLauncher = null;
 
+    private Display display;
+
+    private PopupDialog dialog;
+
+    private Text textArea;
+
     @Override
     protected void initialiseBndLauncher(ILaunchConfiguration configuration, Project model) throws Exception {
         synchronized (model) {
             bndLauncher = model.getProjectLauncher();
         }
         configureLauncher(configuration);
+
+        bndLauncher.registerForNotifications(new NotificationListener() {
+            @Override
+            public void notify(NotificationType type, final String notification) {
+
+                if (type == NotificationType.ERROR) {
+                    display.syncExec(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.open();
+                            textArea.append(notification + "\n\n");
+                            dialog.getShell().redraw();
+                        }
+                    });
+                }
+            }
+        });
         bndLauncher.prepare();
     }
 
@@ -102,6 +137,42 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
             throw new CoreException(new Status(IStatus.ERROR, Plugin.PLUGIN_ID, 0, "Error obtaining OSGi project launcher.", e));
         }
 
+        display = Workbench.getInstance().getDisplay();
+        display.syncExec(new Runnable() {
+            @Override
+            public void run() {
+                dialog = new PopupDialog(new Shell(display), PopupDialog.INFOPOPUPRESIZE_SHELLSTYLE, false, true, true, true, false, "Errors in running OSGi Framework", "") {
+                    @Override
+                    protected Control createDialogArea(Composite parent) {
+                        textArea = new Text(parent, SWT.LEAD | SWT.READ_ONLY | SWT.WRAP);
+                        return textArea;
+                    }
+
+                    @Override
+                    protected Point getDefaultSize() {
+                        Point p = getShell().getSize();
+                        p.x = Math.max(400, p.x / 2);
+                        p.y = Math.max(200, p.y / 2);
+                        return p;
+                    }
+
+                    @Override
+                    protected Point getInitialLocation(Point initialSize) {
+                        Rectangle r = getShell().getBounds();
+                        return new Point(r.x + r.width - initialSize.x, r.y + r.height - initialSize.y);
+                    }
+
+                    @Override
+                    public boolean close() {
+                        if (textArea != null) {
+                            textArea.setText("");
+                        }
+                        return super.close();
+                    }
+                };
+            }
+        });
+
         super.launch(configuration, mode, launch, progress.newChild(1, SubMonitor.SUPPRESS_NONE));
     }
 
@@ -147,6 +218,7 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
             throw new CoreException(new Status(IStatus.ERROR, Plugin.PLUGIN_ID, 0, "Error querying project output folder", e));
         }
         final IResourceChangeListener resourceListener = new IResourceChangeListener() {
+            @Override
             public void resourceChanged(IResourceChangeEvent event) {
                 try {
                     final AtomicBoolean update = new AtomicBoolean(false);
@@ -170,6 +242,7 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
                             runBundleSet.add(new org.eclipse.core.runtime.Path(bundlePath).toPortableString());
                         }
                         event.getDelta().accept(new IResourceDeltaVisitor() {
+                            @Override
                             public boolean visit(IResourceDelta delta) throws CoreException {
                                 // Short circuit if we have already found a
                                 // match
@@ -209,8 +282,17 @@ public class OSGiRunLaunchDelegate extends AbstractOSGiLaunchDelegate {
 
         // Register a listener for termination of the launched process
         Runnable onTerminate = new Runnable() {
+            @Override
             public void run() {
                 ResourcesPlugin.getWorkspace().removeResourceChangeListener(resourceListener);
+                display.asyncExec(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (dialog != null) {
+                            dialog.getShell().dispose();
+                        }
+                    }
+                });
             }
         };
         DebugPlugin.getDefault().addDebugEventListener(new TerminationListener(launch, onTerminate));
