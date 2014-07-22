9bcb234bebbb4edf3753bc57495dd40f0d215cce
Peter Kriens <peter.kriens@aqute.biz>
2014-07-22 18:12:42 +0200

    #547 [configurable] Multi valued values ambiguous Multi valued values were using the | as the separator but this is different from the Metatype spec, which uses escaping. This becomes messy when you have defaults. So now the Configurables use the official way when they do not contain ‘|’, the old value to split. The ‘|’ way is kept for backward compatibility.
    Actually, the use of defaults another then an empty list for multi valued attributes sucks (this is the reason I never use them). Strings are just not  good place to support multiple values.

---
+++ aQute.libg/test/aQute/configurable/ConfigurableTest.java
@@ -0,0 +1,30 @@
+package aQute.configurable;
+
+import java.util.*;
+
+import junit.framework.*;
+
+public class ConfigurableTest extends TestCase {
+
+	interface C {
+		@Config(deflt="abc\\,def,ghi")
+		List<String> foo();
+	}
+	public void testDefault() {
+		C c = Configurable.createConfigurable(C.class, Collections.emptyMap());
+		assertEquals( Arrays.asList("abc,def", "ghi"), c.foo());
+		
+	}
+	
+	public void testSimple() {
+		assertEquals( Arrays.asList("abc","def"), Configurable.unescape("abc|def"));
+		assertEquals( Arrays.asList("abc|def"), Configurable.unescape("abc\\|def"));
+		assertEquals( Arrays.asList("abc","def"), Configurable.unescape("abc,def"));
+		assertEquals( Arrays.asList("abc,def"), Configurable.unescape("abc\\,def"));
+		assertEquals( Arrays.asList("abc","def"), Configurable.unescape("abc  ,   def"));
+		assertEquals( Arrays.asList("abc","def"), Configurable.unescape("   abc  ,   def    "));
+		assertEquals( Arrays.asList(" abc","def"), Configurable.unescape("  \\ abc  ,   def    "));
+		assertEquals( Arrays.asList("a b c","def"), Configurable.unescape("  a b c  ,   def    "));
+		assertEquals( Arrays.asList("\\ a b c","def"), Configurable.unescape(" \\\\ a b c  ,   def    "));
+	}
+}
