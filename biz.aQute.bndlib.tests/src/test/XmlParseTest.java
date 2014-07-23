460a674213f516a81e965835ccb5c0dca5692bc3
Peter Kriens <peter.kriens@aqute.biz>
2014-07-23 16:13:17 +0200

    #423 [xml] Inner class references from blueprint generate package references that include the parent class
    Now remove segments from the end that start with an upper case
    
    
    
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

---
+++ biz.aQute.bndlib.tests/src/test/XmlParseTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import aQute.bnd.osgi.*;
+import junit.framework.*;
+
+public class XmlParseTest extends TestCase {
+	public void testSimple() throws Exception {
+		Builder b = new Builder();
+		b.setProperty("-plugin", "aQute.lib.spring.SpringXMLType");
+		b.setProperty("-resourceonly", "true");
+		b.setProperty("-includeresource", "OSGI-INF/blueprint/x.xml=testresources/blueprint.xml");
+		Jar jar = b.build();
+		assertTrue(b.check());
+		assertTrue( b.getImports().containsFQN("a.b"));
+		assertTrue( b.getImports().containsFQN("d.e"));
+		
+		b.close();
+	}
+}
