a94f80303dea240f5b4bf1f3b96ae616f049bb1d
Bram Pouwelse <bram@pouwelse.com>
2014-08-23 20:48:51 +0200

    Added RetentionPolicy.RUNTIME
    The ReporterMessages tries to find a @Message annotation (line 71) at runtime but the annotation was not visible at runtime.

--- aQute.libg/src/aQute/libg/reporter/Message.java
+++ aQute.libg/src/aQute/libg/reporter/Message.java
@@ -1,5 +1,9 @@
 package aQute.libg.reporter;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Message {
 	String value();
 }
