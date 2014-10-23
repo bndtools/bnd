47b8df313e8d83189024a12dc61dd83f7b1c1086
Peter Kriens <peter.kriens@aqute.biz>
2014-10-23 18:23:21 +0200

    [enroute] Template update

--- bndtools.core/resources/enroute/osgi.enroute.template/_application_/src/_package_/_stem_Application.java
+++ bndtools.core/resources/enroute/osgi.enroute.template/_application_/src/_package_/_stem_Application.java
@@ -5,22 +5,17 @@ import org.osgi.service.component.annotations.Component;
 import osgi.enroute.capabilities.AngularWebResource;
 import osgi.enroute.capabilities.BootstrapWebResource;
 import osgi.enroute.capabilities.WebServerExtender;
-import osgi.enroute.debug.api.Debug;
 import osgi.enroute.rest.api.REST;
 import osgi.enroute.rest.api.RESTRequest;
 
 @AngularWebResource.Require
 @BootstrapWebResource.Require
 @WebServerExtender.Require
-@Component(property = { Debug.COMMAND_SCOPE + "=_cmd_",
-	Debug.COMMAND_FUNCTION + "=_cmd_" },name="_pid_")
+@Component(name="_pid_")
 public class _stem_Application implements REST {
 
 	public String getUpper(RESTRequest rq, String string) {
 		return string.toUpperCase();
 	}
 
-	public String _cmd_(String m) throws Exception {
-		return m;
-	}
 }
