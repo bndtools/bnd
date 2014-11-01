b60b7f05a2eb485be3bfa9f339cb12f786751f51
Neil Bartlett <njbartlett@gmail.com>
2014-11-01 23:14:22 +0000

    Test for CollectionFormatter existing behaviour
    Signed-off-by: Neil Bartlett <njbartlett@gmail.com>

---
+++ biz.aQute.bndlib.tests/src/test/FormattersTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.*;
+
+import junit.framework.*;
+import aQute.bnd.build.model.conversions.*;
+
+public class FormattersTest extends TestCase {
+
+	public void testCollectionFormatter() {
+		Converter<String,Collection<?>> formatter = new CollectionFormatter<Object>(",\\\n\t", (String) null);
+		String formatted = formatter.convert(Arrays.asList(new String[] {"a", "b", "c"}));
+		assertEquals(" \\\n\ta,\\\n\tb,\\\n\tc", formatted);
+	}
+
+	public void testCollectionFormatterSingleEntry() {
+		Converter<String,Collection<?>> formatter = new CollectionFormatter<Object>(",\\\n\t", (String) null);
+		String formatted = formatter.convert(Arrays.asList(new String[] {"a"}));
+		assertEquals(" \\\n\ta", formatted);
+	}
+
+}
