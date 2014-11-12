05dbce3d210ff6dd10bc7c31ff165ade40831211
Peter Kriens <peter.kriens@aqute.biz>
2014-11-12 03:50:47 +0100

    [macro] Added thisfile and stem macros
    - thisfile -> the properties file of the current processor
    - stem -> provides the part before the extension

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Macro.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Macro.java
@@ -1252,6 +1252,17 @@ public class Macro {
 		return name.substring(n + 1);
 	}
 
+	static String	_stem	= "${stem;<string>}";
+
+	public String _stem(String args[]) throws Exception {
+		verifyCommand(args, _stem, null, 2, 2);
+		String name = args[1];
+		int n = name.indexOf('.');
+		if (n < 0)
+			return name;
+		return name.substring(0,n);
+	}
+
 	static String	_substring	= "${substring;<string>;<start>[;<end>]}";
 
 	public String _substring(String args[]) throws Exception {
