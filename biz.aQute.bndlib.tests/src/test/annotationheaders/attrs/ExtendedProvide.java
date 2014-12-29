090fee88f747a89406881118fcf41baed03af515
Peter Kriens <peter.kriens@aqute.biz>
2014-12-29 11:53:53 +0100

    [annotationheaders] Versions were encoded as strings
    Versions should be encoded as :Version in the Req/Provide headers. Added test cases and fixed the code to properly encode strings/

--- biz.aQute.bndlib.tests/src/test/annotationheaders/attrs/ExtendedProvide.java
+++ biz.aQute.bndlib.tests/src/test/annotationheaders/attrs/ExtendedProvide.java
@@ -2,7 +2,9 @@ package test.annotationheaders.attrs;
 
 import aQute.bnd.annotation.headers.*;
 
-@ProvideCapability(ns="nsx", name="ExtendedProvide")
+@ProvideCapability(ns = "nsx", name = "ExtendedProvide", version = "1.2.3")
 public @interface ExtendedProvide {
 	byte foo();
+
+	double bar();
 }
