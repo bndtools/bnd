172648a5ac4a1e8f7d4f66e317fe2515449f1b83
BJ Hargrave <bj@bjhargrave.com>
2015-01-15 11:12:54 -0500

    cnf file redirect must not change workspace folder
    The cnf file redirect should just use the new cnf folder location with
    the specified workspace folder rather than using the workspace folder
    holding the redirected cnf folder.
    
    Fixes https://github.com/bndtools/bnd/issues/341
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/build/Workspace.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/Workspace.java
@@ -134,31 +134,28 @@ public class Workspace extends Processor {
 	public static Workspace getWorkspace(File parent, String bndDir) throws Exception {
 		File workspaceDir = parent.getAbsoluteFile();
 
-		// the cnf directory can actually be a
-		// file that redirects
-		while (workspaceDir.isDirectory()) {
-			File test = new File(workspaceDir, CNFDIR);
-
-			if (!test.exists())
-				test = new File(workspaceDir, bndDir);
-
-			if (test.isDirectory())
-				break;
-
-			if (test.isFile()) {
-				String redirect = IO.collect(test).trim();
-				test = getFile(test.getParentFile(), redirect).getAbsoluteFile();
-				workspaceDir = test;
-			}
-			if (!test.exists())
-				throw new IllegalArgumentException("No Workspace found from: " + parent);
-		}
-
 		synchronized (cache) {
 			WeakReference<Workspace> wsr = cache.get(workspaceDir);
 			Workspace ws;
 			if (wsr == null || (ws = wsr.get()) == null) {
-				ws = new Workspace(workspaceDir, bndDir);
+				/*
+				 * The cnf "directory" can actually be a file that redirects to
+				 * another workspace. We will look for the cnf folder there.
+				 */
+				File buildDir = null;
+				File cnfWorkspaceDir = workspaceDir;
+				while (cnfWorkspaceDir.isDirectory()) {
+					buildDir = new File(cnfWorkspaceDir, bndDir);
+					if (!buildDir.exists())
+						buildDir = new File(cnfWorkspaceDir, CNFDIR);
+					if (!buildDir.exists())
+						throw new IllegalArgumentException("No cnf directory found in: " + cnfWorkspaceDir);
+					if (buildDir.isDirectory())
+						break;
+					cnfWorkspaceDir = getFile(buildDir.getParentFile(), IO.collect(buildDir).trim());
+				}
+
+				ws = new Workspace(workspaceDir, buildDir);
 				cache.put(workspaceDir, new WeakReference<Workspace>(ws));
 			}
 			return ws;
@@ -170,6 +167,10 @@ public class Workspace extends Processor {
 	}
 
 	public Workspace(File dir, String bndDir) throws Exception {
+		this(dir, new File(dir, bndDir));
+	}
+
+	private Workspace(File dir, File buildDir) throws Exception {
 		super(getDefaults());
 		dir = dir.getAbsoluteFile();
 		if (!dir.exists() && !dir.mkdirs()) {
@@ -177,7 +178,7 @@ public class Workspace extends Processor {
 		}
 		assert dir.isDirectory();
 
-		File buildDir = new File(dir, bndDir).getAbsoluteFile();
+		buildDir = buildDir.getAbsoluteFile();
 		if (!buildDir.isDirectory())
 			buildDir = new File(dir, CNFDIR).getAbsoluteFile();
 
