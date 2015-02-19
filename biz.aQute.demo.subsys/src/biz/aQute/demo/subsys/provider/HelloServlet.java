ce549c6617e54e787e54c5a614eea502b1ef26b5
Peter Kriens <peter.kriens@aqute.biz>
2015-02-19 19:33:37 +0100

    [subsys] Test project for subsystems
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

---
+++ biz.aQute.demo.subsys/src/biz/aQute/demo/subsys/provider/HelloServlet.java
@@ -0,0 +1,16 @@
+package biz.aQute.demo.subsys.provider;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest rq, HttpServletResponse rsp) throws ServletException, IOException {
+		rsp.getWriter().println("Hello World");
+	}
+}
