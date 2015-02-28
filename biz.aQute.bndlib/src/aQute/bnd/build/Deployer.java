20ecace16ae076358744a774d7794416c366897d
Raymond Auge <raymond.auge@liferay.com>
2015-02-28 16:29:42 -0500

    issue #632 - move only, direct copy, no actual changes

--- biz.aQute.bndlib/src/aQute/bnd/build/Deployer.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/Deployer.java
@@ -1,6 +1,10 @@
 package aQute.bnd.build;
 
+import java.io.*;
+import java.util.*;
+
 import aQute.bnd.osgi.*;
+import aQute.bnd.service.*;
 import aQute.libg.reporter.*;
 
 public class Deployer {
@@ -11,6 +15,56 @@ public class Deployer {
 	public Deployer(Processor processor) {
 		this.processor = processor;
 		this.msgs = ReporterMessages.base(this.processor, ProjectMessages.class);
+	}
+
+	/**
+	 * Deploy the file (which must be a bundle) into the repository.
+	 *
+	 * @param name
+	 *            The repository name
+	 * @param file
+	 *            bundle
+	 */
+	public void deploy(String name, File file) throws Exception {
+		List<RepositoryPlugin> plugins = getPlugins(RepositoryPlugin.class);
+
+		RepositoryPlugin rp = null;
+		for (RepositoryPlugin plugin : plugins) {
+			if (!plugin.canWrite()) {
+				continue;
+			}
+			if (name == null) {
+				rp = plugin;
+				break;
+			} else if (name.equals(plugin.getName())) {
+				rp = plugin;
+				break;
+			}
+		}
+
+		if (rp != null) {
+			try {
+				rp.put(new BufferedInputStream(new FileInputStream(file)), new RepositoryPlugin.PutOptions());
+				return;
+			}
+			catch (Exception e) {
+				msgs.DeployingFile_On_Exception_(file, rp.getName(), e);
+			}
+			return;
+		}
+		trace("No repo found " + file);
+		throw new IllegalArgumentException("No repository found for " + file);
+	}
+
+	/**
+	 * Deploy the file (which must be a bundle) into the repository.
+	 *
+	 * @param file
+	 *            bundle
+	 */
+	public void deploy(File file) throws Exception {
+		String name = getProperty(Constants.DEPLOYREPO);
+		deploy(name, file);
 	}
 
 }
--- biz.aQute.bndlib/src/aQute/bnd/build/Project.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/Project.java
@@ -1131,54 +1131,10 @@ public class Project extends Processor {
 		}
 	}
 
-	/**
-	 * Deploy the file (which must be a bundle) into the repository.
-	 *
-	 * @param name
-	 *            The repository name
-	 * @param file
-	 *            bundle
-	 */
 	public void deploy(String name, File file) throws Exception {
-		List<RepositoryPlugin> plugins = getPlugins(RepositoryPlugin.class);
-
-		RepositoryPlugin rp = null;
-		for (RepositoryPlugin plugin : plugins) {
-			if (!plugin.canWrite()) {
-				continue;
-			}
-			if (name == null) {
-				rp = plugin;
-				break;
-			} else if (name.equals(plugin.getName())) {
-				rp = plugin;
-				break;
-			}
-		}
-
-		if (rp != null) {
-			try {
-				rp.put(new BufferedInputStream(new FileInputStream(file)), new RepositoryPlugin.PutOptions());
-				return;
-			}
-			catch (Exception e) {
-				msgs.DeployingFile_On_Exception_(file, rp.getName(), e);
-			}
-			return;
-		}
-		trace("No repo found " + file);
-		throw new IllegalArgumentException("No repository found for " + file);
 	}
 
-	/**
-	 * Deploy the file (which must be a bundle) into the repository.
-	 *
-	 * @param file
-	 *            bundle
-	 */
 	public void deploy(File file) throws Exception {
-		String name = getProperty(Constants.DEPLOYREPO);
-		deploy(name, file);
 	}
 
 	/**
