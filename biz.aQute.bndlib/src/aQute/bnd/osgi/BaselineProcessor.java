9fb482e3bf92885da96a9bf56c10e369a9aee1e5
Raymond Auge <raymond.auge@liferay.com>
2015-02-28 13:47:59 -0500

    issue #632 - new class to hold the baseline logic

---
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/BaselineProcessor.java
@@ -0,0 +1,11 @@
+package aQute.bnd.osgi;
+
+public class BaselineProcessor extends Analyzer {
+
+	public BaselineProcessor() {}
+
+	public BaselineProcessor(Analyzer child) {
+		super(child);
+	}
+
+}
