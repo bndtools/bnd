fc01c918e7b7ad5de09f87d3fe8cc407ac6b5490
Tim Ward <timothyjward@apache.org>
2015-09-13 12:48:59 +0200

    [indexer-maven-plugin] Add support for excluding transitive dependencies
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- maven/bnd-indexer-maven-plugin/src/test/resources/integration-test/test/verify.groovy
+++ maven/bnd-indexer-maven-plugin/src/test/resources/integration-test/test/verify.groovy
@@ -4,40 +4,45 @@ import java.util.*;
 import org.osgi.resource.*;
 import org.osgi.service.repository.*;
 
+public void check(String xmlFile, String gzipFile, int size) {
+	// Check the bundles exist!
+	File xml = new File(xmlFile);
+	assert xml.isFile();
+	File gzip = new File(gzipFile);
+	assert gzip.isFile();
+	
+	// Load repository
+	XMLResourceParser xrp = new XMLResourceParser(xml.toURI());
+	xrp.setTrace(true);
+	List<Resource> resources = xrp.parse();
+	assert xrp.check();
+	assert resources != null;
+	assert size == resources.size();
+	
+	ResourcesRepository repo = new ResourcesRepository(resources);
+	
+	Requirement requirement = new RequirementBuilder("osgi.extender")
+						.addDirective("filter", "(osgi.extender=osgi.component)")
+						.buildSyntheticRequirement();
+	
+	Map<Requirement,Collection<Capability>> caps = repo
+						.findProviders(Collections.singleton(requirement));
+	
+	assert 1 == caps.get(requirement).size();
+	
+	Resource res = caps.get(requirement).iterator().next().getResource();
+	
+	assert "org.apache.felix.scr" ==
+						ResourceUtils.getIdentityCapability(res).getAttributes().get("osgi.identity");
+	
+	String location = ResourceUtils.getContentCapability(res).getAttributes().get("url").toString();
+	assert !location.contains("file:");
+}
+
 println "TODO: Need to write some test code for the generated index!"
 println "basedir ${basedir}"
 println "localRepositoryPath ${localRepositoryPath}"
 println "mavenVersion ${mavenVersion}"
 
-// Check the bundles exist!
-File xml = new File("${basedir}/target/index.xml");
-assert xml.isFile();
-File gzip = new File("${basedir}/target/index.xml.gz");
-assert gzip.isFile();
-
-// Load repository
-XMLResourceParser xrp = new XMLResourceParser(xml.toURI());
-xrp.setTrace(true);
-List<Resource> resources = xrp.parse();
-assert xrp.check();
-assert resources != null;
-assert 21 == resources.size();
-
-ResourcesRepository repo = new ResourcesRepository(resources);
-
-Requirement requirement = new RequirementBuilder("osgi.extender")
-					.addDirective("filter", "(osgi.extender=osgi.component)")
-					.buildSyntheticRequirement();
-
-Map<Requirement,Collection<Capability>> caps = repo
-					.findProviders(Collections.singleton(requirement));
-
-assert 1 == caps.get(requirement).size();
-
-Resource res = caps.get(requirement).iterator().next().getResource();
-
-assert "org.apache.felix.scr" ==
-					ResourceUtils.getIdentityCapability(res).getAttributes().get("osgi.identity");
-
-String location = ResourceUtils.getContentCapability(res).getAttributes().get("url").toString();
-assert !location.contains("file:");
+check("${basedir}/transitive/target/index.xml", "${basedir}/transitive/target/index.xml.gz", 21)
+check("${basedir}/non-transitive/target/index.xml", "${basedir}/non-transitive/target/index.xml.gz", 3)
