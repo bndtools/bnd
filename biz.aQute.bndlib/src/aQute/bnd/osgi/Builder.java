1ee17e13742700d8e17161a61b5313bf190fe2c1
Peter Kriens <peter.kriens@aqute.biz>
2015-09-13 19:26:07 +0200

    Removed warning
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Builder.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Builder.java
@@ -3,7 +3,6 @@ package aQute.bnd.osgi;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
