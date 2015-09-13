f366622b82ff239cd93cd68ad246b4c27f92d61c
Tim Ward <timothyjward@apache.org>
2015-09-13 16:36:41 +0200

    [maven-plugins] Maven plugin enhancements
    * Move to a common build for maven plugins
    * Add the bnd-indexer-maven-plugin
    * Use consistent versions of dependencies
    
    This commit represents a squashed cherry-pick from master, with minor changes to ensure versioning consistency for bnd 3.0.1
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

---
+++ maven/bnd-indexer-maven-plugin/README.md
@@ -0,0 +1,103 @@
+# bnd-indexer-maven-plugin
+
+The `bnd-indexer-maven-plugin` is a bnd based plugin that creates OSGi 
+indexes of bundles.
+
+## What does the `bnd-indexer-maven-plugin` do?
+
+This plugin creates an OSGi repository index (suitable for use in resolving 
+and provisioning) from a set of maven project dependencies. 
+
+The project dependencies are referenced in the remote repositories where 
+they are deployed (i.e. they are not republished in a single artifact as 
+they would be when using the maven-assembly-plugin).
+
+The output is an XML file, and a parallel gzipped version of the XML. When
+deployed to a remote repository these can be used to efficiently host an
+OSGi repository system.
+
+## How do I use the `bnd-indexer-maven-plugin` in my project?
+
+Normally you will create a module specifically to build your index file.
+The easiest way do this is to create a simple module with a `pom` packaging
+type. Including the bnd-indexer-maven-plugin in your module is very easy:
+
+    <plugin>
+        <groupId>biz.aQute.bnd</groupId>
+        <artifactId>bnd-indexer-maven-plugin</artifactId>
+        <version>${bnd.version}</version>
+        <configuration>...</configuration>
+        <executions>
+            <execution>
+                <id>index</id>
+                <goals>
+                    <goal>index</goal>
+                </goals>
+            </execution>
+        </executions>
+    </plugin>
+    
+### Running the `bnd-indexer-maven-plugin`
+
+The only goal of the `bnd-indexer-maven-plugin` is `index` which generates the
+OSGi index output files. By default the `bnd-indexer-maven-plugin` binds to the 
+package phase of your build.
+
+The outputs of the `bnd-indexer-maven-plugin` are `target/index.xml` and
+`target/index.xml.gz`. 
+
+### Configuring the `bnd-indexer-maven-plugin`
+
+The `bnd-indexer-maven-plugin` may be configured in several ways
+
+#### Allowing local file locations
+
+By default the `bnd-indexer-maven-plugin` will fail the build if it cannot 
+determine a remote repository from which a dependency can be downloaded.
+Sometimes (for example during local development) some dependencies are not
+yet deployed to a remote repository, even as a snapshot, and this should
+not fail the build. It is also sometimes the case that local URLs are
+specifically required for local testing.
+
+The `bnd-indexer-maven-plugin` can be configured to use local URLs by 
+setting the localURLs property to one of three values:
+
+* FORBIDDEN - The default value. This will cause the build to fail if
+no remote URL can be found
+* ALLOWED - Where a remote URL can be found it will be use. If one cannot
+be found then the local URL will be used instead
+* REQUIRED - No remote URLs will be used. All URLs in the index will be
+local.
+
+Setting the localURLs property is very easy:
+
+    <configuration>
+        <localURLs>true</localURLs>
+    </configuration>
+
+N.B. If local URLs are used in the index then it *should not be deployed* 
+to a remote repository. The local URLs are only valid on the machine that
+created them. 
+
+#### Excluding transitive dependencies
+
+By default the `bnd-indexer-maven-plugin` will include the full transitive
+dependency tree of the module in the index that it generates. If this
+behaviour is not desired then it can be disabled as follows:
+
+    <configuration>
+        <includeTransitive>false</includeTransitive>
+    </configuration>
+    
+#### Restricting dependency scopes
+
+By default the `bnd-indexer-maven-plugin` will include dependencies with 
+`compile` and `runtime` scope. Different scopes may be selected as follows:
+
+    <configuration>
+        <scopes>
+            <scope>compile</scope>
+            <scope>provided</scope>
+            <scope>test</scope>
+        </scopes>
+    </configuration>
---
+++ maven/bnd-indexer-maven-plugin/src/main/java/aQute/bnd/maven/indexer/plugin/IndexerMojo.java
@@ -0,0 +1,238 @@
+package aQute.bnd.maven.indexer.plugin;
+
+import static aQute.bnd.maven.indexer.plugin.LocalURLs.ALLOWED;
+import static aQute.bnd.maven.indexer.plugin.LocalURLs.REQUIRED;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.PACKAGE;
+import static org.apache.maven.plugins.annotations.ResolutionScope.TEST;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.maven.RepositoryUtils;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.DefaultDependencyResolutionRequest;
+import org.apache.maven.project.DependencyResolutionException;
+import org.apache.maven.project.DependencyResolutionRequest;
+import org.apache.maven.project.DependencyResolutionResult;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.project.ProjectDependenciesResolver;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.graph.DependencyFilter;
+import org.eclipse.aether.graph.DependencyNode;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.osgi.framework.Filter;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.indexer.ResourceIndexer;
+import org.osgi.service.indexer.impl.KnownBundleAnalyzer;
+import org.osgi.service.indexer.impl.RepoIndex;
+import org.osgi.service.indexer.impl.URLResolver;
+
+/**
+ * Exports project dependencies to OSGi R5 index format.
+ */
+@Mojo(name = "index", defaultPhase = PACKAGE, requiresDependencyResolution = TEST)
+public class IndexerMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    @Parameter(defaultValue = "${repositorySystemSession}", readonly = true, required = true)
+    private RepositorySystemSession session;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true)
+	private File targetDir;
+
+    @Parameter( property = "bnd.indexer.localURLs", defaultValue = "FORBIDDEN", readonly = true )
+    private LocalURLs localURLs;
+    
+    @Parameter( property = "bnd.indexer.includeTransitive", defaultValue = "true", readonly = true )
+    private boolean includeTransitive;
+
+    @Parameter( property = "bnd.indexer.scopes", readonly = true, required=false )
+    private List<String> scopes;
+    
+    @Component
+    private RepositorySystem system;
+
+    @Component
+    private ProjectDependenciesResolver resolver;
+    
+    @Component
+    private MavenProjectHelper projectHelper;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    	if(scopes == null || scopes.isEmpty()) {
+    		scopes = Arrays.asList("compile", "runtime");
+    	}
+    	
+    	getLog().debug("Indexing dependencies with scopes: " + scopes);
+    	getLog().debug("Including Transitive dependencies: " + includeTransitive);
+    	getLog().debug("Local file URLs permitted: " + localURLs);
+    	
+        DependencyResolutionRequest request = new DefaultDependencyResolutionRequest(project, session);
+
+        request.setResolutionFilter(new DependencyFilter(){
+			@Override
+			public boolean accept(DependencyNode node, List<DependencyNode> parents) {
+				if(node.getDependency() != null) {
+					return scopes.contains(node.getDependency().getScope());
+				}
+				return false;
+			}
+        });
+        
+        DependencyResolutionResult result;
+        try {
+            result = resolver.resolve(request);
+        } catch (DependencyResolutionException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+        Map<File, ArtifactResult> dependencies = new HashMap<>();
+        
+        if (result.getDependencyGraph() != null && !result.getDependencyGraph().getChildren().isEmpty()) {
+            discoverArtifacts(dependencies, result.getDependencyGraph().getChildren(), project.getArtifact().getId());
+        }
+
+        Map<String, ArtifactRepository> repositories = new HashMap<>();
+       
+        for (ArtifactRepository artifactRepository : project.getRemoteArtifactRepositories()) {
+        	repositories.put(artifactRepository.getId(), artifactRepository);
+        }
+
+        RepoIndex indexer = new RepoIndex();
+        Filter filter;
+        try {
+            filter = FrameworkUtil.createFilter("(name=*.jar)");
+        } catch (InvalidSyntaxException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        indexer.addAnalyzer(new KnownBundleAnalyzer(), filter);
+        
+        URLResolver resolver = new MavenURLResolver(dependencies, repositories);
+        indexer.setURLResolver(resolver);
+        
+        Map<String, String> config = new HashMap<String, String>();
+        config.put(ResourceIndexer.PRETTY, "true");
+
+        File outputFile = new File(targetDir, "index.xml");
+        OutputStream output;
+        try {
+        	targetDir.mkdirs();
+			output = new FileOutputStream(outputFile);
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+        getLog().debug("Indexing artifacts: " + dependencies.keySet());
+        try {
+            indexer.index(dependencies.keySet(), output, config);
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        
+		File gzipOutputFile = new File(outputFile.getPath() + ".gz");
+		
+		try (InputStream is = new BufferedInputStream(new FileInputStream(outputFile));
+			 OutputStream gos = new GZIPOutputStream(new FileOutputStream(gzipOutputFile))) {
+			byte[] bytes = new byte[4096];
+			int read;
+			while((read = is.read(bytes)) != -1) {
+				gos.write(bytes, 0, read);
+			}
+		} catch (IOException ioe) {
+			throw new MojoExecutionException("Unable to create the gzipped output file");
+		}
+		
+		attach(outputFile, "xml", "xml");
+		attach(gzipOutputFile, "xml", "xml.gz");
+    }
+    
+    private void attach(File file, String type, String extension) {
+    	DefaultArtifact artifact = new DefaultArtifact(project.getGroupId(), 
+        		project.getArtifactId(), project.getVersion(), null, type, null, 
+        		new DefaultArtifactHandler(extension));
+        artifact.setFile(file);
+		project.addAttachedArtifact(artifact);
+    }
+
+    class MavenURLResolver implements URLResolver {
+
+        private final Map<File, ArtifactResult> dependencies;
+		private final Map<String, ArtifactRepository> repositories;
+
+		public MavenURLResolver(Map<File, ArtifactResult> dependencies, Map<String, ArtifactRepository> repositories) {
+			this.dependencies = dependencies;
+			this.repositories = repositories;
+        }
+
+		public URI resolver(File file) throws Exception {
+			ArtifactResult artifact = dependencies.get(file);
+			if(artifact == null) {
+				throw new FileNotFoundException("The file " + file.getCanonicalPath() + " is not known to this resolver");
+			}
+			
+			if(localURLs == REQUIRED) {
+				return file.toURI();
+			}
+			
+			ArtifactRepository repo = repositories.get(artifact.getRepository().getId());
+			if(repo == null) {
+				if(localURLs == ALLOWED) {
+					getLog().info("The Artifact " + artifact.getArtifact().toString() + 
+							" could not be found in any repository, returning the local location");
+					return file.toURI();
+				}
+				throw new FileNotFoundException("The repository " + artifact.getRepository().getId() + " is not known to this resolver");
+			}
+			
+			return URI.create(repo.getUrl() + "/" + 
+					repo.getLayout().pathOf(RepositoryUtils.toArtifact(artifact.getArtifact())));
+        }
+    }
+
+    private void discoverArtifacts(Map<File, ArtifactResult> files, List<DependencyNode> nodes, String parent) throws MojoExecutionException {
+        for ( DependencyNode node : nodes) {
+            //Ensure that the file is downloaded so we can index it
+            try {
+				ArtifactResult resolvedArtifact = system.resolveArtifact(session, new ArtifactRequest(node.getArtifact(), 
+						project.getRemoteProjectRepositories(), parent));
+				getLog().debug("Located file: " + resolvedArtifact.getArtifact().getFile() + " for artifact " + resolvedArtifact);
+				files.put(resolvedArtifact.getArtifact().getFile(), resolvedArtifact);
+			} catch (ArtifactResolutionException e) {
+				throw new MojoExecutionException("Failed to resolve the dependency " + node.getArtifact().toString(), e);
+			}
+            if(includeTransitive) {
+				discoverArtifacts(files, node.getChildren(), node.getRequestContext());
+			} else {
+				getLog().debug("Ignoring transitive dependencies of " + node.getDependency());
+			}
+        }
+    }
+
+}
