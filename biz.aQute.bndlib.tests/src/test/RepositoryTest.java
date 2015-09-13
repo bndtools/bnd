93e738a8cdd50d31950c5ce2d74dd584abcf0393
Tim Ward <timothyjward@apache.org>
2015-09-13 11:11:41 +0200

    [repo] Fix the repo parsing bug where attributes are missed
    Signed-off-by: Tim Ward <timothyjward@apache.org>
    (cherry picked from commit 2e37f1530043759aeb64beb585323d6e8b169e36)

--- biz.aQute.bndlib.tests/src/test/RepositoryTest.java
+++ biz.aQute.bndlib.tests/src/test/RepositoryTest.java
@@ -1,13 +1,19 @@
 package test;
 
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import org.osgi.resource.Capability;
+import org.osgi.resource.Requirement;
 import org.osgi.resource.Resource;
 
 import aQute.bnd.osgi.repository.ResourcesRepository;
 import aQute.bnd.osgi.repository.XMLResourceParser;
+import aQute.bnd.osgi.resource.RequirementBuilder;
+import aQute.bnd.osgi.resource.ResourceUtils;
 import junit.framework.TestCase;
 
 /**
@@ -15,7 +21,7 @@ import junit.framework.TestCase;
  */
 public class RepositoryTest extends TestCase {
 
-	public void testXMLParser() throws Exception {
+	public void testXMLParserLarge() throws Exception {
 		URL url = RepositoryTest.class.getResource("larger-repo.xml");
 		try (XMLResourceParser xrp = new XMLResourceParser(url.toURI());) {
 			xrp.setTrace(true);
@@ -23,16 +29,54 @@ public class RepositoryTest extends TestCase {
 			assertTrue(xrp.check());
 			assertNotNull(resources);
 			assertEquals(61, resources.size());
+
+			Requirement requirement = new RequirementBuilder("osgi.extender")
+					.addDirective("filter", "(osgi.extender=osgi.component)").buildSyntheticRequirement();
+
+			Map<Requirement,Collection<Capability>> caps = getResourcesRepository(resources)
+					.findProviders(Collections.singleton(requirement));
+
+			assertEquals(1, caps.get(requirement).size());
+
+			Resource res = caps.get(requirement).iterator().next().getResource();
+
+			assertEquals("org.apache.felix.scr",
+					ResourceUtils.getIdentityCapability(res).getAttributes().get("osgi.identity"));
+
+			String location = ResourceUtils.getContentCapability(res).getAttributes().get("url").toString();
+			assertFalse(location, location.contains("file:"));
 		}
 	}
 
-	public ResourcesRepository getResourcesRepository() throws Exception {
-		List<Resource> resources = getResources();
+	public void testXMLParserSmall() throws Exception {
+		URL url = RepositoryTest.class.getResource("repoindex-file.xml");
+		try (XMLResourceParser xrp = new XMLResourceParser(url.toURI());) {
+			xrp.setTrace(true);
+			List<Resource> resources = xrp.parse();
+			assertTrue(xrp.check());
+			assertNotNull(resources);
+			assertEquals(21, resources.size());
+
+			Requirement requirement = new RequirementBuilder("osgi.extender")
+					.addDirective("filter", "(osgi.extender=osgi.component)").buildSyntheticRequirement();
+
+			Map<Requirement,Collection<Capability>> caps = getResourcesRepository(resources)
+					.findProviders(Collections.singleton(requirement));
+
+			assertEquals(1, caps.get(requirement).size());
+
+			Resource res = caps.get(requirement).iterator().next().getResource();
+
+			assertEquals("org.apache.felix.scr",
+					ResourceUtils.getIdentityCapability(res).getAttributes().get("osgi.identity"));
+
+			String location = ResourceUtils.getContentCapability(res).getAttributes().get("url").toString();
+			assertFalse(location, location.contains("file:"));
+		}
+	}
+
+	public ResourcesRepository getResourcesRepository(List<Resource> resources) throws Exception {
 		return new ResourcesRepository(resources);
 	}
 
-	private List<Resource> getResources() {
-		List<Resource> l = new ArrayList<>();
-		return null;
-	}
 }
