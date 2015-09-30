ce3205639fc0783562a1fdc26d419c6639303836
Neil Bartlett <njbartlett@gmail.com>
2015-09-30 20:33:27 +0100

    Fix #1190: Can't Run from bnd.bnd file
    Signed-off-by: Neil Bartlett <njbartlett@gmail.com>

--- bndtools.core/src/bndtools/launch/util/LaunchUtils.java
+++ bndtools.core/src/bndtools/launch/util/LaunchUtils.java
@@ -1,6 +1,7 @@
 package bndtools.launch.util;
 
 import java.io.File;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -9,6 +10,7 @@ import org.eclipse.debug.core.ILaunchConfiguration;
 
 import aQute.bnd.build.Project;
 import aQute.bnd.build.Run;
+import aQute.bnd.build.Workspace;
 import bndtools.central.Central;
 import bndtools.launch.LaunchConstants;
 
@@ -53,7 +55,28 @@ public final class LaunchUtils {
     }
 
     public static Run getBndProject(IResource targetResource) throws Exception {
-        File targetFile = targetResource.getLocation().toFile();
-        return Run.createRun(Central.getWorkspaceIfPresent(), targetFile);
+        Run run;
+
+        Workspace ws = Central.getWorkspaceIfPresent();
+        if (targetResource.getType() == IResource.PROJECT) {
+            // This is a bnd project --> find the bnd.bnd file
+            IProject project = (IProject) targetResource;
+            if (ws == null)
+                throw new Exception(String.format("Cannot load bnd project for directory %s: no bnd workspace found", project.getLocation()));
+            File bndFile = project.getFile(Project.BNDFILE).getLocation().toFile();
+            if (bndFile == null || !bndFile.isFile())
+                throw new Exception(String.format("Failed to load bnd project for directory %s: %s does not exist or is not a file.", project.getLocation(), Project.BNDFILE));
+
+            run = Run.createRun(ws, bndFile);
+        } else if (targetResource.getType() == IResource.FILE) {
+            // This is file, use directly
+            File file = targetResource.getLocation().toFile();
+            if (file == null || !file.isFile())
+                throw new Exception(String.format("Failed to create bnd launch configuration: %s does not exist or is not a file.", file));
+            run = Run.createRun(ws, file);
+        } else {
+            throw new Exception(String.format("Cannot create a bnd launch configuration for %s: not a project or file resource.", targetResource.getLocation()));
+        }
+        return run;
     }
 }
