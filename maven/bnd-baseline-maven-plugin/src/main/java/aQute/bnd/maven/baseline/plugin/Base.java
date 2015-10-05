323d5372fb62f07c1ae5d777dc0011d8fa210e8e
Tim Ward <timothyjward@apache.org>
2015-10-05 17:47:46 +0100

    [baseline-maven-plugin] An initial version of a baselining plugin
    This plugin is very basic, it will locate a version to baseline against and run the baseline checks, failing if any fail. It does not produce a report.
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

---
+++ maven/bnd-baseline-maven-plugin/src/main/java/aQute/bnd/maven/baseline/plugin/Base.java
@@ -0,0 +1,63 @@
+package aQute.bnd.maven.baseline.plugin;
+
+public class Base {
+
+	private String groupId;
+	
+	private String artifactId;
+	
+	private String version;
+	
+	private String classifier;
+	
+	private String extension;
+	
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getClassifier() {
+		return classifier;
+	}
+
+	public void setClassifier(String classifier) {
+		this.classifier = classifier;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	@Override
+	public String toString() {
+		return "Base [groupId=" + groupId + 
+				", artifactId=" + artifactId + 
+				", version=" + version + 
+				", classifier=" + classifier +
+				", extension=" + extension + "]";
+	}
+}
