d356ccb6c98aae2df9301bbf63f997ce38e25821
Neil Bartlett <neil.bartlett@paremus.com>
2015-10-10 22:04:41 +0100

    Make “bnd run” command support -standalone
    Signed-off-by: Neil Bartlett <neil.bartlett@paremus.com>

--- biz.aQute.bnd/test/aQute/bnd/main/TestBndMain.java
+++ biz.aQute.bnd/test/aQute/bnd/main/TestBndMain.java
@@ -7,30 +7,47 @@ import junit.framework.TestCase;
 
 public class TestBndMain extends TestCase {
 
-	private final ByteArrayOutputStream	capturedStdout	= new ByteArrayOutputStream();
-
-	private PrintStream					originalStdout;
+	private final ByteArrayOutputStream	capturedStdOut	= new ByteArrayOutputStream();
+	private PrintStream					originalStdOut;
 
 	@Override
 	protected void setUp() throws Exception {
-		capturedStdout.reset();
-		originalStdout = System.out;
-		System.setOut(new PrintStream(capturedStdout));
+		super.setUp();
+		capturedStdOut.reset();
+		originalStdOut = System.out;
+		System.setOut(new PrintStream(capturedStdOut));
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		System.setOut(originalStdout);
+		System.setOut(originalStdOut);
+		super.tearDown();
 	}
 
 	public void testBndVersion() throws Exception {
 		bnd.mainNoExit(new String[] {
 				"version"
 		});
-		// bnd.main(new String[] {"-etb",
-		// "/Ws/osgi/master/osgi.ct/generated/osgi.ct.cmpn", "runtests",
-		// "org.osgi.test.cases.log.bnd", "org.osgi.test.cases.metatype.bnd"});
-
-		assertEquals("${Bundle-Version}", capturedStdout.toString().trim());
+		expectOutput("${Bundle-Version}");
 	}
+
+	public void testRunStandalone() throws Exception {
+		bnd.mainNoExit(new String[] {
+				"run", "testdata/standalone/standalone.bndrun"
+		});
+		expectOutput("Gesundheit!");
+	}
+
+	public void testRunWorkspace() throws Exception {
+		bnd.mainNoExit(new String[] {
+				"run", "testdata/workspace/p/workspace.bndrun"
+		});
+
+		expectOutput("Gesundheit!");
+	}
+
+	private void expectOutput(String expected) {
+		assertEquals("wrong output", expected, capturedStdOut.toString().trim());
+	}
+
 }
