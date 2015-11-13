adce2a34e6798671dd0dd34e929fd520e2140d6e
Raymond Auge <raymond.auge@liferay.com>
2015-11-13 11:01:53 -0500

    fixes #1194

---
+++ biz.aQute.bndlib.tests/src/test/LibDirectiveTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.util.jar.Manifest;
+
+import aQute.bnd.osgi.Builder;
+import aQute.bnd.osgi.Constants;
+import junit.framework.TestCase;
+
+public class LibDirectiveTest extends TestCase {
+
+	public void testLibDirective() throws Exception {
+		Builder b = new Builder();
+		try {
+			b.set("Bundle-ClassPath", ".");
+			b.set("-includeresource", "lib/=jar/asm.jar;lib:=true");
+			b.build();
+			assertTrue(b.check());
+			Manifest m = b.getJar().getManifest();
+
+			assertNotNull(m.getMainAttributes().getValue(Constants.BUNDLE_CLASSPATH));
+			assertEquals(".,lib/asm.jar", m.getMainAttributes().getValue(Constants.BUNDLE_CLASSPATH));
+		}
+		finally {
+			b.close();
+		}
+	}
+
+	public void testLibDirectiveWithDefaultedBundleClassPath() throws Exception {
+		Builder b = new Builder();
+		try {
+			b.set("-includeresource", "lib/=jar/asm.jar;lib:=true");
+			b.build();
+			assertTrue(b.check());
+			Manifest m = b.getJar().getManifest();
+
+			assertNotNull(m.getMainAttributes().getValue(Constants.BUNDLE_CLASSPATH));
+			assertEquals(".,lib/asm.jar", m.getMainAttributes().getValue(Constants.BUNDLE_CLASSPATH));
+		}
+		finally {
+			b.close();
+		}
+	}
+
+}
