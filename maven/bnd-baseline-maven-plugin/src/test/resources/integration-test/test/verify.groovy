b6bb46c1dff5b0841b0fb79c38deb662ba866ff8
Tim Ward <timothyjward@apache.org>
2015-11-20 15:56:23 -0600

    [baseline] Avoid incorrect suggesting bundle version increments
    The baselining code should only suggest bundle version increments if the symbolic names of the two bundles being compared are the same
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- maven/bnd-baseline-maven-plugin/src/test/resources/integration-test/test/verify.groovy
+++ maven/bnd-baseline-maven-plugin/src/test/resources/integration-test/test/verify.groovy
@@ -15,13 +15,11 @@ assert fileContents.contains("[INFO] Baselining check succeeded checking biz.aQu
 // With provider
 assert fileContents.contains("[ERROR] Baseline mismatch for package bnd.test, MINOR change. Current is 1.0.0, repo is 1.0.0, suggest 1.1.0 or -\n" +
 "\n" +
-"[ERROR] The bundle version change (0.0.1 to 0.0.2) is too low, the new version must be at least 0.1.0\n" +
 "[WARNING] The baselining check failed when checking biz.aQute.bnd-test:invalid-with-provider:jar:0.0.2 against biz.aQute.bnd-test:valid-no-previous:jar:0.0.1 but the bnd-baseline-maven-plugin is configured not to fail the build.")
 
 
 // With consumer
 assert fileContents.contains("[ERROR] Baseline mismatch for package bnd.test, MAJOR change. Current is 1.0.0, repo is 1.0.0, suggest 2.0.0 or 1.0.0\n" +
 "\n" +
-"[ERROR] The bundle version change (0.0.1 to 0.0.2) is too low, the new version must be at least 1.0.0\n" +
 "[WARNING] The baselining check failed when checking biz.aQute.bnd-test:invalid-with-consumer:jar:0.0.2 against biz.aQute.bnd-test:valid-no-previous:jar:0.0.1 but the bnd-baseline-maven-plugin is configured not to fail the build.")
 
