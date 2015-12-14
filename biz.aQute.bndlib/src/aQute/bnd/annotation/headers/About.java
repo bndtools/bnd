a44691210df31f6cf08aef71c00b4d4c70dfdc8c
Sean Bright <sean@transparentbpo.com>
2015-12-14 14:30:28 -0500

    About: Remove an import that snuck in and more cleanup

--- biz.aQute.bndlib/src/aQute/bnd/annotation/headers/About.java
+++ biz.aQute.bndlib/src/aQute/bnd/annotation/headers/About.java
@@ -1,7 +1,5 @@
 package aQute.bnd.annotation.headers;
 
-import aQute.bnd.annotation.licenses.ASL_2_0;
-
 /**
  * This package contains a number of annotations that create or append headers
  * in the manifest. These annotations simplify maintaining these headers:
@@ -10,11 +8,11 @@ import aQute.bnd.annotation.licenses.ASL_2_0;
  * need instead of maintaining them in the manifest
  * <li>Less errors because of the use of annotations
  * <li>Use of IDE features to track them or navigate
- * <ul>
+ * </ul>
  * The annotations provide the following features:
  * <ul>
- * <li>macros - use the bnd macro preprocessor for DNRY
- * <li>coloring - define custom annotations that encapsulate some headers
+ * <li>Macros - use the bnd macro preprocessor for DNRY
+ * <li>Coloring - define custom annotations that encapsulate some headers
  * <li>No runtime dependencies since all annotations are build time.
  * <li>No duplicates
  * </ul>
@@ -23,32 +21,35 @@ import aQute.bnd.annotation.licenses.ASL_2_0;
  * system commands, for obvious reason they are excluded). As a convenience, a
  * number of local macros are set from the context:
  * <ul>
- * <li>{@code ${@package}} – The package name
- * <li>{@code ${@class}} – The class name to which this macro is applied to
- * <li>{@code ${@class-short}} – The short class name to which this macro is
- * applied to
- * <li>{@code ${@version}} – The package version if set
- * <li>{@code ${@frange;version[;isProvider]}} – A macro to create a filter
+ * <li><code>${@package}</code> – The package name
+ * <li><code>${@class}</code> – The class name to which this macro is applied to
+ * <li><code>${@class-short}</code> – The short class name to which this macro
+ * is applied to
+ * <li><code>${@version}</code> – The package version if set
+ * <li><code>${@frange;version[;isProvider]}</code> – A macro to create a filter
  * expression on a version based on the semantic versioning rules. Default is
  * consumer, specify true for the isProvider to get provider semantics.
+ * </ul>
  * <h2>Coloring</h2> Annotations can only be applied once, making it impossible
  * to add for example two Provide-Capability headers on the same type. It also
  * would become unreadable quickly. The advised way to use most of these
  * annotation headers is therefore through 'annotation coloring'. These header
  * annotations should be applied to custom annotations that represents the
  * 'thing'. This is clearly represented in the BundleLicense custom annotations
- * like for example the {@link ASL_2_0} annotation. This annotation can be
- * applied to any type and will automatically then create the appropriate
- * clauses.
+ * like for example the {@link aQute.bnd.annotation.licenses.ASL_2_0 ASL_2_0}
+ * annotation. This annotation can be applied to any type and will automatically
+ * then create the appropriate clauses.
  * <p>
  * For example:
  * 
  * <pre>
- *  public class Webserver { @RequireCapability(ns="osgi.extender",
- * name="webserver", version="${@version}") @interface Require
- * {} @ProvideCapability(ns="osgi.extender",
- * filter="(&(osgi.extender=webserver)${frange;${@version}))") @interface
- * Provide {}
+ * public class Webserver {
+ *   &#64;RequireCapability(ns="osgi.extender", name="webserver", version="${@version}")
+ *   &#64;interface Require {}
+ *   &#64;ProvideCapability(ns="osgi.extender", filter="(&(osgi.extender=webserver)${frange;${@version}))")
+ *   &#64;interface Provide {}
+ *   ...
+ * }
  * </pre>
  * 
  * This resource can now be stored in a library to be used by others. If a
@@ -56,7 +57,9 @@ import aQute.bnd.annotation.licenses.ASL_2_0;
  * follows:
  * 
  * <pre>
- *  &#64;Webserver.Require public class MyResource { ...
+ * &#64;Webserver.Require
+ * public class MyResource {
+ *   ...
  * }
  * </pre>
  */
