3d3514addc8c15a66c448c51d095184380f29d16
BJ Hargrave <bj@bjhargrave.com>
2016-03-17 13:24:50 -0400

    bndlib: Add CHANGES_3_2 to CHANGES
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/About.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/About.java
@@ -217,6 +217,7 @@ public class About {
 	public static Map<Version,String[]>	CHANGES		= new TreeMap<Version,String[]>(Collections.reverseOrder());
 
 	static {
+		CHANGES.put(_3_2, CHANGES_3_2);
 		CHANGES.put(_3_1, CHANGES_3_1);
 		CHANGES.put(_3_0, CHANGES_3_0);
 		CHANGES.put(_2_4, CHANGES_2_4);
