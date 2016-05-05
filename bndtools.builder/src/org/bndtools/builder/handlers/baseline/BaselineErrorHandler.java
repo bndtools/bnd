bndtools/bnd

commit d6b6e2108104538281253f75a0af5c386008251b

Baseline annotations & fixes on package-info.java
Signed-off-by: Neil Bartlett <njbartlett@gmail.com>

bndtools.builder/src/org/bndtools/builder/handlers/baseline/BaselineErrorHandler.java
@@ -2,16 +2,17 @@ package org.bndtools.builder.handlers.baseline;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.bndtools.api.BndtoolsConstants;
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
 import org.bndtools.build.api.AbstractBuildErrorDetailsHandler;
 import org.bndtools.build.api.MarkerData;
+import org.bndtools.builder.utils.MemberValuePairLocationRetriever;
 import org.bndtools.utils.jdt.ASTUtil;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
@@ -21,10 +22,18 @@ import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaModelMarker;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageDeclaration;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ITypeBinding;
@@ -34,6 +43,7 @@ import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.ui.IMarkerResolution;
+import org.osgi.util.function.Function;
 
 import aQute.bnd.build.Project;
 import aQute.bnd.differ.Baseline.Info;
@@ -50,7 +60,14 @@ import aQute.service.reporter.Report.Location;
 public class BaselineErrorHandler extends AbstractBuildErrorDetailsHandler {
 
     private static final String PACKAGEINFO = "packageinfo";
+    private static final String PACKAGEINFOJAVA = "package-info.java";
     private static final String PROP_SUGGESTED_VERSION = "suggestedVersion";
+
+    private static final String ANNOTATION_VERSION_BND_PKG = aQute.bnd.annotation.Version.class.getPackage().getName();
+    private static final String ANNOTATION_VERSION_OSGI_PKG = "org.osgi.annotation.versioning";
+    private static final String ANNOTATION_VERSION_NO_PKG = "Version";
+    private static final String ANNOTATION_VERSION_BND = aQute.bnd.annotation.Version.class.getName();
+    private static final String ANNOTATION_VERSION_OSGI = ANNOTATION_VERSION_OSGI_PKG + "." + ANNOTATION_VERSION_NO_PKG;
 
     private static final ILogger logger = Logger.getLogger(BaselineErrorHandler.class);
 
@@ -68,13 +85,13 @@ public class BaselineErrorHandler extends AbstractBuildErrorDetailsHandler {
     }
 
     List<MarkerData> generatePackageInfoMarkers(Info baselineInfo, IJavaProject javaProject, String message) throws JavaModelException {
+        List<MarkerData> markers = new LinkedList<>();
         for (IClasspathEntry entry : javaProject.getRawClasspath()) {
             if (IClasspathEntry.CPE_SOURCE == entry.getEntryKind()) {
                 IPath entryPath = entry.getPath();
                 IPath pkgPath = entryPath.append(baselineInfo.packageName.replace('.', '/'));
 
-                // TODO: handle package-info.java
-
+                // Find in packageinfo file
                 IPath pkgInfoPath = pkgPath.append(PACKAGEINFO);
                 IFile pkgInfoFile = javaProject.getProject().getWorkspace().getRoot().getFile(pkgInfoPath);
                 if (pkgInfoFile != null && pkgInfoFile.exists()) {
@@ -89,12 +106,61 @@ public class BaselineErrorHandler extends AbstractBuildErrorDetailsHandler {
                         attribs.put(IMarker.CHAR_END, lineLoc.end);
                     }
 
-                    return Collections.singletonList(new MarkerData(pkgInfoFile, attribs, true));
+                    markers.add(new MarkerData(pkgInfoFile, attribs, true));
+                }
+
+                // Find in package-info.java
+                IPackageFragment pkg = javaProject.findPackageFragment(pkgPath);
+                ICompilationUnit pkgInfoJava = pkg.getCompilationUnit(PACKAGEINFOJAVA);
+                if (pkgInfoJava != null && pkgInfoJava.exists()) {
+                    ISourceRange range = findPackageInfoJavaVersionLocation(baselineInfo.packageName, pkgInfoJava);
+
+                    Map<String,Object> attribs = new HashMap<String,Object>();
+                    attribs.put(IMarker.MESSAGE, message.trim());
+                    attribs.put(IJavaModelMarker.ID, 8088);
+                    attribs.put(PROP_SUGGESTED_VERSION, baselineInfo.suggestedVersion.toString());
+                    if (range != null) {
+                        attribs.put(IMarker.CHAR_START, range.getOffset());
+                        attribs.put(IMarker.CHAR_END, range.getOffset() + range.getLength());
+                        markers.add(new MarkerData(pkgInfoJava.getResource(), attribs, true, BndtoolsConstants.MARKER_JAVA_BASELINE));
+                    }
                 }
             }
         }
+        return markers;
+    }
 
-        return Collections.emptyList();
+    ISourceRange findPackageInfoJavaVersionLocation(String packageName, ICompilationUnit compUnit) throws JavaModelException {
+        ISourceRange range = null;
+        IPackageDeclaration[] pkgDecls = compUnit.getPackageDeclarations();
+        if (pkgDecls != null) {
+            for (IPackageDeclaration pkgDecl : pkgDecls) {
+                if (packageName.equals(pkgDecl.getElementName())) {
+                    IAnnotation[] annots = pkgDecl.getAnnotations();
+                    for (IAnnotation annot : annots) {
+                        String name = annot.getElementName();
+                        if (ANNOTATION_VERSION_NO_PKG.equals(name) || ANNOTATION_VERSION_OSGI.equals(name) || ANNOTATION_VERSION_BND.equals(name)) {
+                            ASTParser parser = ASTParser.newParser(AST.JLS8);
+                            parser.setKind(ASTParser.K_COMPILATION_UNIT);
+                            parser.setSource(compUnit);
+                            parser.setResolveBindings(true);
+                            CompilationUnit ast = (CompilationUnit) parser.createAST(null);
+
+                            MemberValuePairLocationRetriever mvpRetriever = new MemberValuePairLocationRetriever(annot, new Function<String,Boolean>() {
+                                @Override
+                                public Boolean apply(String t) {
+                                    return ANNOTATION_VERSION_BND.equals(t) || ANNOTATION_VERSION_OSGI.equals(t);
+                                }
+                            }, "value");
+                            ast.accept(mvpRetriever);
+                            range = mvpRetriever.getMemberValuePairSourceRange();
+
+                        }
+                    }
+                }
+            }
+        }
+        return range;
     }
 
     List<MarkerData> generateStructuralChangeMarkers(Info baselineInfo, IJavaProject javaProject) throws JavaModelException {

bndtools.api/src/org/bndtools/api/BndtoolsConstants.java
@@ -17,6 +17,7 @@ public final class BndtoolsConstants {
     public static final String MARKER_BND_PATH_PROBLEM = "bndtools.builder.bndpathproblem";
     public static final String MARKER_BND_WORKSPACE_PROBLEM = "bndtools.builder.bndworkspaceproblem";
     public static final String MARKER_BND_MISSING_WORKSPACE = "bndtools.builder.missingworkspace";
+    public static final String MARKER_JAVA_BASELINE = "bndtools.builder.packageInfoBaseline";
 
     /**
      * Marker attribute name for the bnd {@link Location#context}

bndtools.builder/_plugin.xml
@@ -80,6 +80,17 @@
 		<super type="org.eclipse.core.resources.problemmarker"/>
 	</extension>
 	
+	<extension point="org.eclipse.core.resources.markers"
+		id="packageInfoBaseline"
+		name="Package Info Baseline Problem Marker"
+		>
+		<persistent value="true"></persistent>
+		<super type="bndtools.builder.bndproblem"/>
+		<super type="org.eclipse.jdt.core.problem"/>
+		<super type="org.eclipse.core.resources.textmarker"/>
+		<super type="org.eclipse.core.resources.problemmarker"/>
+	</extension>
+	
 	<extension point="org.eclipse.jdt.core.classpathContainerInitializer">
 		<classpathContainerInitializer
 			id="aQute.bnd.classpath.container"

bndtools.builder/src/org/bndtools/builder/MarkerSupport.java
@@ -68,6 +68,7 @@ class MarkerSupport {
             deleteMarkers(MARKER_BND_PATH_PROBLEM);
             deleteMarkers(MARKER_BND_WORKSPACE_PROBLEM);
             deleteMarkers(MARKER_BND_MISSING_WORKSPACE);
+            deleteMarkers(MARKER_JAVA_BASELINE);
         } else
             project.deleteMarkers(markerType, true, IResource.DEPTH_INFINITE);
     }
@@ -88,7 +89,8 @@ class MarkerSupport {
             for (MarkerData markerData : markers) {
                 IResource resource = markerData.getResource();
                 if (resource != null && resource.exists()) {
-                    IMarker marker = resource.createMarker(markerType);
+                    String typeOverride = markerData.getTypeOverride();
+                    IMarker marker = resource.createMarker(typeOverride != null ? typeOverride : markerType);
                     marker.setAttribute(IMarker.SEVERITY, severity);
                     marker.setAttribute("$bndType", type);
 

bndtools.builder/src/org/bndtools/builder/utils/MemberValuePairLocationRetriever.java
@@ -1,0 +1,140 @@
+package org.bndtools.builder.utils;
+
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.SourceRange;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
+import org.eclipse.jdt.core.dom.MemberValuePair;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.osgi.util.function.Function;
+
+/**
+ * Visitor that will "visit" an ASTNode and its children until it finds the expected MemberValue pair to retain its
+ * location in the compilation unit source code
+ */
+public class MemberValuePairLocationRetriever extends ASTVisitor {
+
+    private final IAnnotation javaAnnotation;
+    private final Function<String,Boolean> annotationNameMatch;
+    private final String memberName;
+
+    private ISourceRange locatedSourceRange = null;
+
+    /**
+     * Constructor
+     */
+    public MemberValuePairLocationRetriever(final IAnnotation javaAnnotation, final Function<String,Boolean> annotationNameMatch, final String memberName) {
+        this.javaAnnotation = javaAnnotation;
+        this.annotationNameMatch = annotationNameMatch;
+        this.memberName = memberName;
+    }
+
+    public ISourceRange getMemberValuePairSourceRange() {
+        return locatedSourceRange;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.TypeDeclaration)
+     */
+    @Override
+    public boolean visit(AnnotationTypeDeclaration node) {
+        final IJavaElement ancestor = javaAnnotation.getAncestor(IJavaElement.TYPE);
+        if (ancestor != null && ancestor.exists() && ancestor.getElementName().equals(node.getName().getFullyQualifiedName())) {
+            // keep searching
+            return true;
+        }
+        // wrong path, stop searching from this branch of the AST
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.TypeDeclaration)
+     */
+    @Override
+    public boolean visit(TypeDeclaration node) {
+        final IJavaElement ancestor = javaAnnotation.getAncestor(IJavaElement.TYPE);
+        if (ancestor != null && ancestor.exists() && ancestor.getElementName().equals(node.getName().getFullyQualifiedName())) {
+            // keep searching
+            return true;
+        }
+        // wrong path, stop searching from this branch of the AST
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.FieldDeclaration)
+     */
+    @Override
+    public boolean visit(VariableDeclarationFragment node) {
+        final IJavaElement ancestor = javaAnnotation.getAncestor(IJavaElement.FIELD);
+        if (ancestor != null && ancestor.exists() && ancestor.getElementName().equals(node.getName().getFullyQualifiedName())) {
+            // keep searching
+            return true;
+        }
+        // wrong path, stop searching from this branch of the AST
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.MethodInvocation)
+     */
+    @Override
+    public boolean visit(MethodDeclaration node) {
+        final IJavaElement ancestor = javaAnnotation.getAncestor(IJavaElement.METHOD);
+        if (ancestor != null && ancestor.exists() && ancestor.getElementName().equals(node.getName().getFullyQualifiedName())) {
+            // keep searching
+            return true;
+        }
+        // wrong path, stop searching from this branch of the AST
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.SingleMemberAnnotation)
+     */
+    @Override
+    public boolean visit(SingleMemberAnnotation node) {
+        final IAnnotationBinding annotationBinding = node.resolveAnnotationBinding();
+        if (annotationBinding != null) {
+            final String nodeName = annotationBinding.getAnnotationType().getQualifiedName();
+            if (this.annotationNameMatch.apply(nodeName)) {
+                this.locatedSourceRange = new SourceRange(node.getValue().getStartPosition(), node.getValue().getLength());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.NormalAnnotation)
+     */
+    @Override
+    public boolean visit(NormalAnnotation node) {
+        final IJavaElement ancestor = javaAnnotation.getAncestor(IJavaElement.ANNOTATION);
+        if (ancestor != null && ancestor.exists() && ancestor.getElementName().equals(node.getTypeName().getFullyQualifiedName())) {
+            // keep searching
+            return true;
+        }
+        // wrong path, stop searching from this branch of the AST
+        return false;
+    }
+
+    /**
+     * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.MemberValuePair)
+     */
+    @Override
+    public boolean visit(MemberValuePair node) {
+        if (node.getName().getFullyQualifiedName().equals(memberName)) {
+            this.locatedSourceRange = new SourceRange(node.getStartPosition(), node.getLength());
+        }
+        // no need to drill down from here anyway
+        return false;
+    }
+
+}

bndtools.core/_plugin.xml
@@ -185,6 +185,23 @@
 		/>
 	</extension>
 
+	<extension point="org.eclipse.jdt.ui.quickFixProcessors">
+		<quickFixProcessor id="PackageInfoBaselineQuickFixProcessor"
+			name="Package Info Baseline Quick Fix" requiredSourceLevel="1.5"
+			class="org.bndtools.core.editors.PackageInfoBaselineQuickFixProcessor">
+			<handledMarkerTypes>
+				<markerType id="bndtools.builder.packageInfoBaseline" />
+			</handledMarkerTypes>
+			<enablement>
+				<with variable="projectNatures">
+					<iterate operator="or">
+						<equals value="org.eclipse.jdt.core.javanature" />
+					</iterate>
+				</with>
+			</enablement>
+		</quickFixProcessor>
+	</extension>
+
    <extension
          point="org.eclipse.ui.views">
       <category

bndtools.core/src/org/bndtools/build/api/MarkerData.java
@@ -9,11 +9,17 @@ public class MarkerData {
     private final IResource resource;
     private final Map<String,Object> attribs;
     private final boolean hasResolutions;
+    private final String typeOverride;
 
     public MarkerData(IResource resource, Map<String,Object> attribs, boolean hasResolutions) {
+        this(resource, attribs, hasResolutions, null);
+    }
+
+    public MarkerData(IResource resource, Map<String,Object> attribs, boolean hasResolutions, String typeOverride) {
         this.resource = resource;
         this.attribs = attribs;
         this.hasResolutions = hasResolutions;
+        this.typeOverride = typeOverride;
     }
 
     public IResource getResource() {
@@ -26,6 +32,10 @@ public class MarkerData {
 
     public boolean hasResolutions() {
         return hasResolutions;
+    }
+
+    public String getTypeOverride() {
+        return typeOverride;
     }
 
 }

bndtools.core/src/org/bndtools/core/editors/PackageInfoBaselineQuickFixProcessor.java
@@ -1,0 +1,56 @@
+package org.bndtools.core.editors;
+
+import org.bndtools.api.BndtoolsConstants;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.refactoring.CompilationUnitChange;
+import org.eclipse.jdt.ui.text.java.IInvocationContext;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jdt.ui.text.java.IProblemLocation;
+import org.eclipse.jdt.ui.text.java.IQuickFixProcessor;
+import org.eclipse.jdt.ui.text.java.correction.ChangeCorrectionProposal;
+import org.eclipse.text.edits.ReplaceEdit;
+
+public class PackageInfoBaselineQuickFixProcessor implements IQuickFixProcessor {
+
+    @Override
+    public boolean hasCorrections(ICompilationUnit unit, int problemId) {
+        return "package-info.java".equals(unit.getElementName());
+    }
+
+    @Override
+    public IJavaCompletionProposal[] getCorrections(IInvocationContext context, IProblemLocation[] locations) throws CoreException {
+        ICompilationUnit compUnit = context.getCompilationUnit();
+        IResource resource = compUnit.getResource();
+        IMarker[] markers = resource.findMarkers(BndtoolsConstants.MARKER_JAVA_BASELINE, false, 1);
+
+        for (IProblemLocation location : locations) {
+            for (IMarker marker : markers) {
+                int markerStart = marker.getAttribute(IMarker.CHAR_START, -1);
+                int markerEnd = marker.getAttribute(IMarker.CHAR_END, -1);
+                int markerLength = markerEnd - markerStart;
+                if (location.getOffset() <= markerStart && markerStart < location.getOffset() + location.getLength()) {
+                    String newVersion = marker.getAttribute("suggestedVersion", null);
+                    if (newVersion != null) {
+                        StringBuilder quotedVersion = new StringBuilder(newVersion.trim());
+                        if (quotedVersion.charAt(0) != '"')
+                            quotedVersion.insert(0, '"');
+                        if (quotedVersion.charAt(quotedVersion.length() - 1) != '"')
+                            quotedVersion.append('"');
+
+                        CompilationUnitChange change = new CompilationUnitChange("Change package-info.java", compUnit);
+                        change.setEdit(new ReplaceEdit(markerStart, markerLength, quotedVersion.toString()));
+
+                        return new IJavaCompletionProposal[] {
+                                new ChangeCorrectionProposal("Change package version to " + newVersion, change, 1000)
+                        };
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}

commit dfb01fa9753a9f1cec53f612bb51ea39a8975b5f

#1310 A baseline error not reported in Eclipse
Caused by the assumption that there was always a package-info.java file. Check for the presence. Now. This bug was introduced recently in d6b6e21

Fixes #1310

Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

bndtools.builder/src/org/bndtools/builder/handlers/baseline/BaselineErrorHandler.java
@@ -111,18 +111,20 @@ public class BaselineErrorHandler extends AbstractBuildErrorDetailsHandler {
 
                 // Find in package-info.java
                 IPackageFragment pkg = javaProject.findPackageFragment(pkgPath);
-                ICompilationUnit pkgInfoJava = pkg.getCompilationUnit(PACKAGEINFOJAVA);
-                if (pkgInfoJava != null && pkgInfoJava.exists()) {
-                    ISourceRange range = findPackageInfoJavaVersionLocation(baselineInfo.packageName, pkgInfoJava);
+                if (pkg != null) {
+                    ICompilationUnit pkgInfoJava = pkg.getCompilationUnit(PACKAGEINFOJAVA);
+                    if (pkgInfoJava != null && pkgInfoJava.exists()) {
+                        ISourceRange range = findPackageInfoJavaVersionLocation(baselineInfo.packageName, pkgInfoJava);
 
-                    Map<String,Object> attribs = new HashMap<String,Object>();
-                    attribs.put(IMarker.MESSAGE, message.trim());
-                    attribs.put(IJavaModelMarker.ID, 8088);
-                    attribs.put(PROP_SUGGESTED_VERSION, baselineInfo.suggestedVersion.toString());
-                    if (range != null) {
-                        attribs.put(IMarker.CHAR_START, range.getOffset());
-                        attribs.put(IMarker.CHAR_END, range.getOffset() + range.getLength());
-                        markers.add(new MarkerData(pkgInfoJava.getResource(), attribs, true, BndtoolsConstants.MARKER_JAVA_BASELINE));
+                        Map<String,Object> attribs = new HashMap<String,Object>();
+                        attribs.put(IMarker.MESSAGE, message.trim());
+                        attribs.put(IJavaModelMarker.ID, 8088);
+                        attribs.put(PROP_SUGGESTED_VERSION, baselineInfo.suggestedVersion.toString());
+                        if (range != null) {
+                            attribs.put(IMarker.CHAR_START, range.getOffset());
+                            attribs.put(IMarker.CHAR_END, range.getOffset() + range.getLength());
+                            markers.add(new MarkerData(pkgInfoJava.getResource(), attribs, true, BndtoolsConstants.MARKER_JAVA_BASELINE));
+                        }
                     }
                 }
             }