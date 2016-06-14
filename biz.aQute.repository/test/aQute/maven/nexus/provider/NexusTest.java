21acc007a1a997d31cb4c60f6821a9f8fb07c94c
Peter Kriens <peter.kriens@aqute.biz>
2016-06-14 09:37:45 +0200

    inactivate test
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- biz.aQute.repository/test/aQute/maven/nexus/provider/NexusTest.java
+++ biz.aQute.repository/test/aQute/maven/nexus/provider/NexusTest.java
@@ -1,26 +1,22 @@
 package aQute.maven.nexus.provider;
 
 import java.io.File;
-import java.net.URI;
-import java.util.List;
 
-import aQute.bnd.http.HttpClient;
-import aQute.bnd.osgi.Processor;
 import aQute.lib.io.IO;
-import aQute.lib.strings.Strings;
 import junit.framework.TestCase;
 
 public class NexusTest extends TestCase {
 
 
 	public void testTravers() throws Exception {
-		HttpClient client = new HttpClient();
-		client.readSettings(new Processor());
-		Nexus nexus = new Nexus(new URI("https://oss.sonatype.org/service/local/repositories/orgosgi-1073"),
-				client);
-
-		List<URI> files = nexus.files();
-		System.out.println(Strings.join("\n", files));
+		// HttpClient client = new HttpClient();
+		// client.readSettings(new Processor());
+		// Nexus nexus = new Nexus(new
+		// URI("https://oss.sonatype.org/service/local/repositories/orgosgi-1073"),
+		// client);
+		//
+		// List<URI> files = nexus.files();
+		// System.out.println(Strings.join("\n", files));
 	}
 
 	public void testSign() throws Exception {
