ac1990f086e32ca7d021b7d8c49a0d731b9121b2
Peter Kriens <peter.kriens@aqute.biz>
2016-08-02 16:07:05 +0200

    [httpclient] If ETAG was not send then we would always download again
    If no ETAG is set, try to use ifModified
    
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- biz.aQute.repository/test/aQute/bnd/repository/osgi/OSGiRepositoryTest.java
+++ biz.aQute.repository/test/aQute/bnd/repository/osgi/OSGiRepositoryTest.java
@@ -2,7 +2,9 @@ package aQute.bnd.repository.osgi;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import aQute.bnd.build.Workspace;
@@ -89,6 +91,61 @@ public class OSGiRepositoryTest extends TestCase {
 		assertNotNull(file3);
 		// second one should not have downloaded
 		assertEquals(2, tasks.get());
+	}
+
+	public void testBndRepo() throws Exception {
+		OSGiRepository r = new OSGiRepository();
+		Map<String,String> map = new HashMap<>();
+		map.put("locations",
+				"https://bndtools.ci.cloudbees.com/job/bnd.master/lastSuccessfulBuild/artifact/dist/bundles/index.xml.gz");
+		map.put("cache", "generated/tmp/cache");
+		map.put("max.stale", "10000");
+		r.setProperties(map);
+		Processor p = new Processor();
+		final AtomicInteger tasks = new AtomicInteger();
+
+		p.addBasicPlugin(new ProgressPlugin() {
+
+			@Override
+			public Task startTask(final String name, int size) {
+				System.out.println("Starting " + name);
+				tasks.incrementAndGet();
+				return new Task() {
+
+					@Override
+					public void worked(int units) {
+						System.out.println("Worked " + name + " " + units);
+					}
+
+					@Override
+					public void done(String message, Throwable e) {
+						System.out.println("Done " + name + " " + message);
+					}
+
+					@Override
+					public boolean isCanceled() {
+						return false;
+					}
+				};
+			}
+		});
+		HttpClient httpClient = new HttpClient();
+		httpClient.setRegistry(p);
+		p.addBasicPlugin(httpClient);
+		p.setBase(ws);
+		p.addBasicPlugin(Workspace.createStandaloneWorkspace(p, ws.toURI()));
+		r.setRegistry(p);
+		r.setReporter(new Slf4jReporter());
+
+		assertEquals(0, tasks.get());
+		List<String> list = r.list(null);
+		assertFalse(list.isEmpty());
+
+		SortedSet<Version> versions = r.versions("aQute.libg");
+		assertFalse(versions.isEmpty());
+		File f1 = r.get("aQute.libg", versions.first(), null);
+		assertNotNull(f1);
+		File f2 = r.get("aQute.libg", versions.first(), null);
 	}
 
 }
