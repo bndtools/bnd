29ca1b9a936119e792b081268bef255c2bfdd3ce
BJ Hargrave <bj@bjhargrave.com>
2016-08-25 11:47:50 -0400

    executor: Don't let caller-run execution propagate exception
    We catch any exception and pass to the thread's uncaught exception
    handler.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Processor.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Processor.java
@@ -35,6 +35,7 @@ import java.util.TreeSet;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.SynchronousQueue;
@@ -93,7 +94,29 @@ public class Processor extends Domain implements Reporter, Registry, Constants, 
 	static {
 		ThreadFactory threadFactory = Executors.defaultThreadFactory();
 		executor = new ThreadPoolExecutor(0, 64, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(),
-				threadFactory, new ThreadPoolExecutor.CallerRunsPolicy());
+				threadFactory, new RejectedExecutionHandler() {
+					/*
+					 * We are stealing another's thread because we have hit max
+					 * pool size, so we cannot let the runnable's exception
+					 * propagate back up this thread.
+					 */
+					@Override
+					public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+						if (executor.isShutdown()) {
+							return;
+						}
+						try {
+							r.run();
+						} catch (Throwable t) {
+							try {
+								Thread thread = Thread.currentThread();
+								thread.getUncaughtExceptionHandler().uncaughtException(thread, t);
+							} catch (Throwable for_real) {
+								// we will ignore this
+							}
+						}
+					}
+				});
 		sheduledExecutor = new ScheduledThreadPoolExecutor(4, threadFactory);
 	}
 	static Random					random			= new Random();
