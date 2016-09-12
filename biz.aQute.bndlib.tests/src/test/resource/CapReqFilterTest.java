9277b4f309d0ba36513ab8fac120d822b7a0b39d
Sean Bright <sean@callshaper.com>
2016-09-12 13:10:02 -0400

    Unit tests for ResourceUtils.matches()
    Signed-off-by: Sean Bright <sean.bright@gmail.com>

---
+++ biz.aQute.bndlib.tests/src/test/resource/CapReqFilterTest.java
@@ -0,0 +1,52 @@
+package test.resource;
+
+import org.osgi.resource.Capability;
+import org.osgi.resource.Requirement;
+
+import aQute.bnd.osgi.resource.CapabilityBuilder;
+import aQute.bnd.osgi.resource.RequirementBuilder;
+import aQute.bnd.osgi.resource.ResourceUtils;
+import junit.framework.TestCase;
+
+public class CapReqFilterTest extends TestCase {
+
+	public void testReqNoFilterDifferentNamespace() throws Exception {
+		Capability cap = new CapabilityBuilder("run.amock")
+				.buildSyntheticCapability();
+		Requirement req = new RequirementBuilder("bamboozled")
+				.buildSyntheticRequirement();
+
+		assertFalse(ResourceUtils.matches(req, cap));
+	}
+
+	public void testReqNoFilterCapWithAttr() throws Exception {
+		Capability cap = new CapabilityBuilder("foobar")
+				.addAttribute("blah", "baz")
+				.addAttribute("version", "42")
+				.buildSyntheticCapability();
+		Requirement req = new RequirementBuilder("foobar")
+				.buildSyntheticRequirement();
+
+		assertTrue(ResourceUtils.matches(req, cap));
+	}
+
+	public void testReqNoFilterCapWithNoAttr() {
+		Capability cap = new CapabilityBuilder("foobar")
+				.buildSyntheticCapability();
+		Requirement req = new RequirementBuilder("foobar")
+				.buildSyntheticRequirement();
+
+		assertTrue(ResourceUtils.matches(req, cap));
+	}
+
+	public void testReqWithFilter() {
+		Capability cap = new CapabilityBuilder("foobar")
+				.buildSyntheticCapability();
+		Requirement req = new RequirementBuilder("foobar")
+				.addDirective("filter", "(something=true)")
+				.buildSyntheticRequirement();
+
+		assertFalse(ResourceUtils.matches(req, cap));
+	}
+
+}
