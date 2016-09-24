1b0b3614bafce20e35756c97cb778fdde28e7e0f
BJ Hargrave <bj@bjhargrave.com>
2016-09-24 11:50:14 -0400

    logging: Create type to hold Gradle unique log levels
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

---
+++ aQute.libg/src/aQute/libg/slf4j/GradleLogging.java
@@ -0,0 +1,32 @@
+package aQute.libg.slf4j;
+
+import org.slf4j.Marker;
+
+/**
+ * SLF4J Markers for special Gradle log levels. These markers are to be used
+ * with the info logging methods.
+ */
+public class GradleLogging {
+	/**
+	 * The Gradle LIFECYCLE marker.
+	 */
+	public final static Marker LIFECYCLE;
+	/**
+	 * The Gradle QUIET marker.
+	 */
+	public final static Marker	QUIET;
+
+	static {
+		Marker lifecycle = null;
+		Marker quiet = null;
+		try {
+			Class< ? > logging = Class.forName("org.gradle.api.logging.Logging");
+			lifecycle = (Marker) logging.getField("LIFECYCLE").get(null);
+			quiet = (Marker) logging.getField("QUIET").get(null);
+		} catch (Exception e) {}
+		LIFECYCLE = lifecycle;
+		QUIET = quiet;
+	}
+
+	private GradleLogging() {}
+}
