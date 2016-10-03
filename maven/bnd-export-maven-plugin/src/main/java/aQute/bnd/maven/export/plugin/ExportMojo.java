d6b690d73e627f2a1c7aaa63a6673e36a79eb881
Peter Kriens <peter.kriens@aqute.biz>
2016-10-03 16:27:52 +0200

    [maven export] Printing -runbundles if the current bundles do not match the resolved bundles.
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- maven/bnd-export-maven-plugin/src/main/java/aQute/bnd/maven/export/plugin/ExportMojo.java
+++ maven/bnd-export-maven-plugin/src/main/java/aQute/bnd/maven/export/plugin/ExportMojo.java
@@ -5,6 +5,8 @@ import static org.apache.maven.plugins.annotations.LifecyclePhase.PACKAGE;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -18,16 +20,21 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import aQute.bnd.build.Container;
 import aQute.bnd.build.Run;
 import aQute.bnd.osgi.Jar;
+import aQute.bnd.version.Version;
+import aQute.bnd.version.VersionRange;
 import biz.aQute.resolve.ProjectResolver;
 
 @Mojo(name = "export", defaultPhase = PACKAGE, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class ExportMojo extends AbstractMojo {
 
-	@Parameter(readonly = true, required = true)
+	@Parameter(readonly = true, required = false)
 	private List<File>	bndruns;
 
+	@Parameter(readonly = true, required = false)
+	private File		bndrun;
+
 	@Parameter(defaultValue = "${project.build.directory}", readonly = true)
-	private File targetDir;
+	private File		targetDir;
 
 	@Parameter(readonly = true, required = false)
 	private boolean		resolve			= false;
@@ -37,19 +44,27 @@ public class ExportMojo extends AbstractMojo {
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
-			for (File runFile : bndruns) {
-				if (!runFile.exists()) {
-					throw new MojoExecutionException("Could not find bnd run file " + runFile);
-				}
-				try (StandaloneRun run = new StandaloneRun(runFile)) {
-					if (resolve) {
-						resolve(run);
-					}
-					export(run);
+			if (bndrun != null) {
+				export(bndrun);
+			} else {
+				for (File runFile : bndruns) {
+					export(runFile);
 				}
 			}
 		} catch (Exception e) {
 			throw new MojoExecutionException(e.getMessage(), e);
+		}
+	}
+
+	private void export(File runFile) throws MojoExecutionException, Exception, IOException {
+		if (!runFile.exists()) {
+			throw new MojoExecutionException("Could not find bnd run file " + runFile);
+		}
+		try (StandaloneRun run = new StandaloneRun(runFile)) {
+			if (resolve) {
+				resolve(run);
+			}
+			export(run);
 		}
 	}
 
@@ -65,15 +80,48 @@ public class ExportMojo extends AbstractMojo {
 		try (ProjectResolver projectResolver = new ProjectResolver(run)) {
 			List<Container> runBundles = projectResolver.getRunBundles();
 			Collection<Container> currentRunBundles = run.getRunbundles();
-			if (!CollectionUtils.isEqualCollection(currentRunBundles, runBundles)) {
+			Collections.sort(runBundles, new Comparator<Container>() {
+
+				@Override
+				public int compare(Container o1, Container o2) {
+					return o1.getBundleSymbolicName().compareTo(o2.getBundleSymbolicName());
+				}
+			});
+
+			if (!CollectionUtils.isEqualCollection(runBundles, currentRunBundles)) {
+				printRunBundles(runBundles, System.out);
 				if (failOnChanges) {
 					throw new MojoExecutionException("The runbundles have changed. Failing the build");
 				} else {
-					getLog().warn("The runbundles have changed. Please check the results of this build.");
+					getLog().warn("The runbundles have changed:");
 					run.setRunBundles(runBundles);
 				}
 			}
 		}
+	}
+
+	private void printRunBundles(List<Container> runBundles, Appendable ps) throws IOException {
+		if (runBundles.isEmpty())
+			return;
+
+		ps.append("\n-runbundles: ");
+		String del = "";
+
+		for (Container c : runBundles) {
+			Version version = Version.parseVersion(c.getVersion()).getWithoutQualifier();
+			VersionRange versionRange = new VersionRange(true, version,
+					new Version(version.getMajor(), version.getMinor(), version.getMicro() + 1), false);
+
+			ps.append(del)
+					.append("\\\n\t")
+					.append(c.getBundleSymbolicName())
+					.append("; version='")
+					.append(versionRange.toString())
+					.append("'");
+
+			del = ",";
+		}
+		ps.append("\n\n");
 	}
 
 	private String getNamePart(File runFile) {
