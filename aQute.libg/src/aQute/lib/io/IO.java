900d758cc0d1d384fcd35b4ff04a49b0acb7a9df
Sean Bright <sean.bright@gmail.com>
2017-01-10 11:51:24 -0500

    Add IO.createSymlinkOrCopy() for Windows compatability.
    Signed-off-by: Sean Bright <sean.bright@gmail.com>

--- aQute.libg/src/aQute/lib/io/IO.java
+++ aQute.libg/src/aQute/lib/io/IO.java
@@ -636,6 +636,31 @@ public class IO {
 		return Files.isSymbolicLink(link.toPath());
 	}
 
+	/**
+	 * Creates a symbolic link from {@code link} to the {@code target}, or
+	 * copies {@code target} to {@code link} if running on Windows.
+	 * <p>
+	 * Creating symbolic links on Windows requires administrator permissions, so
+	 * copying is a safer fallback.
+	 *
+	 * @param link the location of the symbolic link, or destination of the
+	 *            copy.
+	 * @param target the target of the symbolic link, or source of the copy.
+	 * @return {@code true} if the operation succeeds, {@code false} otherwise.
+	 */
+	public static boolean createSymbolicLinkOrCopy(File link, File target) {
+		try {
+			if (isWindows()) {
+				IO.copy(target, link);
+				return true;
+			}
+			return createSymbolicLink(link, target);
+		} catch (Exception ignore) {
+			// ignore
+		}
+		return false;
+	}
+
 	static public OutputStream	nullStream	= new OutputStream() {
 
 												@Override
