170ef674faededba8df08ad2556a9e8d3ed0267b
Raymond Auge <raymond.auge@liferay.com>
2017-01-10 15:28:04 -0500

    [bnd-export-maven-plugin] fixes #1837 bnd-export-maven-plugin producing unstable outout
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- maven/bnd-export-maven-plugin/src/main/java/aQute/bnd/maven/export/plugin/ExportMojo.java
+++ maven/bnd-export-maven-plugin/src/main/java/aQute/bnd/maven/export/plugin/ExportMojo.java
@@ -9,8 +9,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -18,23 +19,24 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.osgi.resource.Resource;
+import org.osgi.resource.Wire;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import aQute.bnd.build.Container;
 import aQute.bnd.build.Run;
 import aQute.bnd.build.Workspace;
 import aQute.bnd.build.model.BndEditModel;
+import aQute.bnd.build.model.clauses.HeaderClause;
 import aQute.bnd.build.model.clauses.VersionedClause;
+import aQute.bnd.build.model.conversions.CollectionFormatter;
+import aQute.bnd.build.model.conversions.Converter;
+import aQute.bnd.build.model.conversions.HeaderClauseFormatter;
 import aQute.bnd.osgi.Constants;
-import aQute.bnd.osgi.Domain;
 import aQute.bnd.osgi.Jar;
-import aQute.bnd.osgi.resource.ResourceBuilder;
 import aQute.bnd.osgi.resource.ResourceUtils;
 import aQute.bnd.properties.Document;
 import aQute.bnd.service.RepositoryPlugin;
-import aQute.bnd.version.Version;
-import aQute.bnd.version.VersionRange;
 import aQute.lib.io.IO;
 import biz.aQute.resolve.ProjectResolver;
 
@@ -56,6 +58,9 @@ public class ExportMojo extends AbstractMojo {
 
 	@Parameter(defaultValue = "${session}", readonly = true)
 	private MavenSession session;
+
+	private Converter<String,Collection< ? extends HeaderClause>>	headerClauseListFormatter	= new CollectionFormatter<HeaderClause>(
+			",\\\n\t", new HeaderClauseFormatter(), null);
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
@@ -98,75 +103,56 @@ public class ExportMojo extends AbstractMojo {
 
 	private void resolve(StandaloneRun run) throws Exception, IOException {
 		try (ProjectResolver projectResolver = new ProjectResolver(run)) {
-			List<Container> runBundles = projectResolver.getRunBundles();
-			Collection<Container> currentRunBundles = run.getRunbundles();
-			Collections.sort(runBundles, new Comparator<Container>() {
-
+			Map<Resource,List<Wire>> resolution = projectResolver.resolve();
+			Set<Resource> resources = resolution.keySet();
+			List<VersionedClause> runBundles = new ArrayList<>();
+			for (Resource resource : resources) {
+				VersionedClause runBundle = ResourceUtils.toVersionClause(resource, "[===,==+)");
+				if (!runBundles.contains(runBundle)) {
+					runBundles.add(runBundle);
+				}
+			}
+			Collections.sort(runBundles, new Comparator<VersionedClause>() {
 				@Override
-				public int compare(Container o1, Container o2) {
-					return o1.getBundleSymbolicName().compareTo(o2.getBundleSymbolicName());
+				public int compare(VersionedClause a, VersionedClause b) {
+					int diff = a.getName().compareTo(b.getName());
+					return (diff != 0) ? diff : a.getVersionRange().compareTo(b.getVersionRange());
 				}
 			});
 
-			if (!CollectionUtils.isEqualCollection(runBundles, currentRunBundles)) {
-				printRunBundles(runBundles, System.out);
-				if (failOnChanges) {
-					throw new MojoExecutionException("The runbundles have changed. Failing the build");
-				} else {
-					logger.warn("The runbundles have changed");
-					run.setRunBundles(runBundles);
-					File runFile = run.getPropertiesFile();
-					BndEditModel bem = new BndEditModel(run.getWorkspace());
-					Document doc = new Document(IO.collect(runFile));
-					bem.loadFrom(doc);
+			File runFile = run.getPropertiesFile();
+			BndEditModel bem = new BndEditModel(run.getWorkspace());
+			Document doc = new Document(IO.collect(runFile));
+			bem.loadFrom(doc);
 
-					List<VersionedClause> bemRunBundles = bem.getRunBundles();
-					List<VersionedClause> deltaAdd = new ArrayList<>();
-					for (Container c : runBundles) {
-						ResourceBuilder rb = new ResourceBuilder();
-						rb.addManifest(Domain.domain(c.getFile()));
-						VersionedClause versionedClause = ResourceUtils.toVersionClause(rb.build(), "[===,==+)");
-						deltaAdd.add(versionedClause);
-					}
-					deltaAdd.removeAll(bemRunBundles);
-					List<VersionedClause> deltaRemove = new ArrayList<>(bemRunBundles);
-					deltaRemove.removeAll(runBundles);
-					boolean added = bemRunBundles.addAll(deltaAdd);
-					boolean removed = bemRunBundles.removeAll(deltaRemove);
-					if (added || removed) {
-						bem.setRunBundles(bemRunBundles);
-						logger.info("Writing changes to {}", runFile.getAbsolutePath());
-						logger.info("{}:{}", Constants.RUNBUNDLES, bem.getDocumentChanges().get(Constants.RUNBUNDLES));
-						bem.saveChangesTo(doc);
-						IO.store(doc.get(), runFile);
-					}
+			List<VersionedClause> bemRunBundles = bem.getRunBundles() != null ? bem.getRunBundles()
+					: new ArrayList<VersionedClause>();
+
+			String originalRunbundlesString = headerClauseListFormatter.convert(bemRunBundles);
+			logger.debug("Original -runbundles was:\n\t {}", originalRunbundlesString);
+			String runbundlesString = headerClauseListFormatter.convert(runBundles);
+			logger.debug("Resolved -runbundles is:\n\t {}", runbundlesString);
+
+			List<VersionedClause> deltaAdd = new ArrayList<>(runBundles);
+			deltaAdd.removeAll(bemRunBundles);
+			List<VersionedClause> deltaRemove = new ArrayList<>(bemRunBundles);
+			deltaRemove.removeAll(runBundles);
+			boolean added = bemRunBundles.addAll(deltaAdd);
+			boolean removed = bemRunBundles.removeAll(deltaRemove);
+			if (added || removed) {
+				if (failOnChanges && !bemRunBundles.isEmpty()) {
+					String message = String.format(
+							"The runbundles have changed. Failing the build!\nWas: {}\nIs: {}",
+							originalRunbundlesString, runbundlesString);
+					throw new MojoExecutionException(message);
 				}
+				bem.setRunBundles(bemRunBundles);
+				logger.info("Writing changes to {}", runFile.getAbsolutePath());
+				logger.info("{}:{}", Constants.RUNBUNDLES, bem.getDocumentChanges().get(Constants.RUNBUNDLES));
+				bem.saveChangesTo(doc);
+				IO.store(doc.get(), runFile);
 			}
 		}
-	}
-
-	private void printRunBundles(List<Container> runBundles, Appendable ps) throws IOException {
-		if (runBundles.isEmpty())
-			return;
-
-		ps.append("\n-runbundles: ");
-		String del = "";
-
-		for (Container c : runBundles) {
-			Version version = Version.parseVersion(c.getVersion()).getWithoutQualifier();
-			VersionRange versionRange = new VersionRange(true, version,
-					new Version(version.getMajor(), version.getMinor(), version.getMicro() + 1), false);
-
-			ps.append(del)
-					.append("\\\n\t")
-					.append(c.getBundleSymbolicName())
-					.append("; version='")
-					.append(versionRange.toString())
-					.append("'");
-
-			del = ",";
-		}
-		ps.append("\n\n");
 	}
 
 	private String getNamePart(File runFile) {
