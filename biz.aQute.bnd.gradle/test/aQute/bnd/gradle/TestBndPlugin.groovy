f224689d7402f0137bcc1f111254d63f6f42078b
BJ Hargrave <bj@bjhargrave.com>
2017-03-13 18:00:43 +0100

    gradle: New biz.aQute.bnd.workspace Gradle plugin
    This new plugin can be applied in settings.gradle to replace the rather
    lengthy code to figure out the initial set of gradle projects from the
    Bnd workspace information. It also configures the root project by
    configuration the buildscript classpath and applying the biz.aQute.bnd
    Gradle plugin to all the Bnd projects in the workspace.
    
    This enables a much simpler settings.gradle and root project
    build.gradle. For a basic Bnd workspace, settings.gradle only has to put
    the biz.aQute.bnd.gradle jar on the buildscript classpath and apply
    the biz.aQute.bnd.workspace plugin. A root project build.gradle is not
    necessary if not additional Gradle project customization is necessary.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bnd.gradle/test/aQute/bnd/gradle/TestBndPlugin.groovy
+++ biz.aQute.bnd.gradle/test/aQute/bnd/gradle/TestBndPlugin.groovy
@@ -15,7 +15,7 @@ class TestBndPlugin extends Specification {
     File buildDir = new File('generated')
     File testResources = new File(buildDir, 'testresources')
 
-    def "Simple Bnd Workspace Plugin Test"() {
+    def "Bnd Workspace Plugin"() {
         given:
           String testProject = 'workspaceplugin1'
           File testProjectDir = new File(testResources, testProject)
@@ -54,6 +54,9 @@ class TestBndPlugin extends Specification {
 
           File release_jar = new File(testProjectDir, 'cnf/releaserepo/test.simple/test.simple-0.0.0.jar')
           release_jar.isFile()
+
+          result.output =~ Pattern.quote('### Project workspaceplugin1 has BndWorkspacePlugin applied')
+          result.output =~ Pattern.quote('### Project test.simple has BndPlugin applied')
     }
 
     def "Bnd Workspace Plugin echo/bndproperties Test"() {
@@ -128,7 +131,7 @@ class TestBndPlugin extends Specification {
           props.getProperty('-runbundles') =~ /osgi\.enroute\.junit\.wrapper/
     }
 
-    def "Bnd Workspace Plugin extra properties/extentions Test"() {
+    def "Bnd Workspace Plugin extra properties/extensions Test"() {
         given:
           String testProject = 'workspaceplugin2'
           File testProjectDir = new File(testResources, testProject)
@@ -143,5 +146,87 @@ class TestBndPlugin extends Specification {
 
         then:
           result.task(":tasks").outcome == SUCCESS
+    }
+
+    def "Bnd Workspace Plugin Old-style settings.gradle"() {
+        given:
+          String testProject = 'workspaceplugin3'
+          File testProjectDir = new File(testResources, testProject)
+          assert testProjectDir.isDirectory()
+
+        when:
+          def result = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments('--stacktrace', '--debug', 'build', 'release')
+            .forwardOutput()
+            .build()
+
+        then:
+          result.task(":test.simple:test").outcome == SUCCESS
+          result.task(":test.simple:testOSGi").outcome == SUCCESS
+          result.task(":test.simple:check").outcome == SUCCESS
+          result.task(":test.simple:build").outcome == SUCCESS
+          result.task(":test.simple:release").outcome == SUCCESS
+
+          File simple_bundle = new File(testProjectDir, 'test.simple/generated/test.simple.jar')
+          simple_bundle.isFile()
+
+          JarFile simple_jar = new JarFile(simple_bundle)
+          Attributes simple_manifest = simple_jar.getManifest().getMainAttributes()
+          simple_manifest.getValue('Bundle-SymbolicName') == 'test.simple'
+          simple_manifest.getValue('Bundle-Version') =~ /0\.0\.0\./
+          simple_manifest.getValue('Foo') == 'foo'
+          simple_manifest.getValue('Bar') == 'bar'
+          simple_manifest.getValue('Import-Package') =~ /junit\.framework/
+          simple_jar.getEntry('test/simple/Test.class')
+          simple_jar.getEntry('OSGI-OPT/src/')
+          simple_jar.getEntry('test.txt')
+          simple_jar.getInputStream(simple_jar.getEntry('test.txt')).text =~ /This is a test resource/
+          simple_jar.getEntry('test/simple/test.txt')
+          simple_jar.getInputStream(simple_jar.getEntry('test/simple/test.txt')).text =~ /This is a test resource/
+
+          File release_jar = new File(testProjectDir, 'cnf/releaserepo/test.simple/test.simple-0.0.0.jar')
+          release_jar.isFile()
+    }
+
+    def "Bnd Workspace Plugin Simple settings.gradle"() {
+        given:
+          String testProject = 'workspaceplugin4'
+          File testProjectDir = new File(testResources, testProject)
+          assert testProjectDir.isDirectory()
+
+        when:
+          def result = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments('--stacktrace', '--debug', 'build', 'release')
+            .forwardOutput()
+            .build()
+
+        then:
+          result.task(":test.simple:test").outcome == SUCCESS
+          result.task(":test.simple:testOSGi").outcome == SUCCESS
+          result.task(":test.simple:check").outcome == SUCCESS
+          result.task(":test.simple:build").outcome == SUCCESS
+          result.task(":test.simple:release").outcome == SUCCESS
+
+          File simple_bundle = new File(testProjectDir, 'test.simple/generated/test.simple.jar')
+          simple_bundle.isFile()
+
+          JarFile simple_jar = new JarFile(simple_bundle)
+          Attributes simple_manifest = simple_jar.getManifest().getMainAttributes()
+          simple_manifest.getValue('Bundle-SymbolicName') == 'test.simple'
+          simple_manifest.getValue('Bundle-Version') =~ /0\.0\.0\./
+          simple_manifest.getValue('Foo') == 'foo'
+          simple_manifest.getValue('Bar') == 'bar'
+          simple_manifest.getValue('Import-Package') =~ /junit\.framework/
+          simple_jar.getEntry('test/simple/Test.class')
+          simple_jar.getEntry('OSGI-OPT/src/')
+          simple_jar.getEntry('test.txt')
+          simple_jar.getInputStream(simple_jar.getEntry('test.txt')).text =~ /This is a test resource/
+          simple_jar.getEntry('test/simple/test.txt')
+          simple_jar.getInputStream(simple_jar.getEntry('test/simple/test.txt')).text =~ /This is a test resource/
+
+          File release_jar = new File(testProjectDir, 'cnf/releaserepo/test.simple/test.simple-0.0.0.jar')
+          release_jar.isFile()
     }
 }
