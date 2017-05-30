3dbfc7546fdb526647463d7d97b7eacd04e71457
BJ Hargrave <bj@bjhargrave.com>
2017-05-30 17:15:45 -0400

    template: Use PromiseExecutor to read github repo details
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- org.bndtools.templating.gitrepo/src/org/bndtools/templating/jgit/GitHub.java
+++ org.bndtools.templating.gitrepo/src/org/bndtools/templating/jgit/GitHub.java
@@ -1,41 +1,35 @@
 package org.bndtools.templating.jgit;
 
 import java.net.URI;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
-import org.osgi.util.promise.Deferred;
 import org.osgi.util.promise.Promise;
 
 import aQute.lib.json.JSONCodec;
+import aQute.lib.promise.PromiseExecutor;
 
 public class GitHub {
 
     private static final String URL_PREFIX = "https://api.github.com/repos/";
 
     private final Cache cache;
-    private final ExecutorService executor;
+    private final PromiseExecutor executor;
 
     public GitHub(Cache cache, ExecutorService executor) {
         this.cache = cache;
-        this.executor = executor;
+        this.executor = new PromiseExecutor(executor);
     }
 
     // TODO: use the async download service when available.
     public Promise<GithubRepoDetailsDTO> loadRepoDetails(final String repository) throws Exception {
-        final Deferred<GithubRepoDetailsDTO> deferred = new Deferred<>();
-        executor.submit(new Runnable() {
+        return executor.submit(new Callable<GithubRepoDetailsDTO>() {
             @Override
-            public void run() {
-                try {
-                    byte[] detailsDtoData = cache.download(URI.create(URL_PREFIX + repository));
-                    GithubRepoDetailsDTO detailsDTO = new JSONCodec().dec().from(detailsDtoData).get(GithubRepoDetailsDTO.class);
-                    deferred.resolve(detailsDTO);
-                } catch (Exception e) {
-                    deferred.fail(e);
-                }
+            public GithubRepoDetailsDTO call() throws Exception {
+                byte[] detailsDtoData = cache.download(URI.create(URL_PREFIX + repository));
+                return new JSONCodec().dec().from(detailsDtoData).get(GithubRepoDetailsDTO.class);
             }
         });
-        return deferred.getPromise();
     }
 
 }
