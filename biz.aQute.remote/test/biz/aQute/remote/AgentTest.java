4be163db07e3759c9ddbe361bf0777e54b7055d0
Peter Kriens <peter.kriens@aqute.biz>
2017-08-18 16:41:24 +0200

    [remote] Remove gogo test from tally
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- biz.aQute.remote/test/biz/aQute/remote/AgentTest.java
+++ biz.aQute.remote/test/biz/aQute/remote/AgentTest.java
@@ -184,13 +184,13 @@ public class AgentTest extends TestCase {
 		assertTrue(previousModified != t2Bundle.lastModified);
 	}
 
-	public void testAgentShell() throws Exception {
-		// This test requires a gogo to be added to the framework
-		// String result = supervisor.getAgent().shell("lb");
-		// assertNotNull(result);
-		// assertTrue(result.contains("START"));
-		// assertTrue(result.contains("LEVEL"));
-	}
+	// public void testAgentShell() throws Exception {
+	// This test requires a gogo to be added to the framework
+	// String result = supervisor.getAgent().shell("lb");
+	// assertNotNull(result);
+	// assertTrue(result.contains("START"));
+	// assertTrue(result.contains("LEVEL"));
+	// }
 
 	public void testAgentSupervisorTimeout() throws Exception {
 		TestSupervisor testSupervisor = new TestSupervisor();
