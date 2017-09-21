fdf218663855939a1cc2d3934e562299df07c2d1
BJ Hargrave <bj@bjhargrave.com>
2017-09-21 17:25:09 -0400

    build: Refresh build files synchronously
    This is necessary since code later in the build depends upon the built
    artifacts having been refreshed into the eclipse workspace. Also,
    using jobs for this creates too many jobs when there are many projects
    and provides no predictability on when then built archives are ready
    in the eclipse workspace.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.core/src/bndtools/central/WorkspaceListener.java
+++ bndtools.core/src/bndtools/central/WorkspaceListener.java
@@ -4,12 +4,8 @@ import java.io.File;
 
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
-import org.osgi.util.promise.Promise;
-import org.osgi.util.promise.Success;
-
 import aQute.bnd.build.Workspace;
 import aQute.bnd.service.BndListener;
-import aQute.service.reporter.Reporter;
 
 public final class WorkspaceListener extends BndListener {
     private static final ILogger logger = Logger.getLogger(WorkspaceListener.class);
@@ -19,32 +15,9 @@ public final class WorkspaceListener extends BndListener {
     @Override
     public void changed(final File file) {
         try {
-            final RefreshFileJob job = new RefreshFileJob(file, true);
-            if (job.needsToSchedule()) {
-                Central.onWorkspaceInit(new Success<Workspace,Void>() {
-                    @Override
-                    public Promise<Void> call(Promise<Workspace> resolved) throws Exception {
-                        job.schedule();
-                        return null;
-                    }
-                });
-            }
+            Central.refreshFile(file, null, true);
         } catch (Exception e) {
-            logger.logError("Error refreshing workspace", e);
+            logger.logError("Error refreshing file " + file, e);
         }
     }
-
-    @Override
-    public void signal(Reporter reporter) {
-        //        errorProcessor.clear();
-        //        errorProcessor.getInfo(workspace);
-        //
-        //        for (String warning : errorProcessor.getWarnings()) {
-        //            logger.logWarning(warning, null);
-        //        }
-        //        for (String error : errorProcessor.getErrors()) {
-        //            logger.logError(error, null);
-        //        }
-    }
-
 }
