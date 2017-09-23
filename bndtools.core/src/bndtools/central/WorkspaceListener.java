785a10d49cf76165a0ad19b772e0e3b3f218e488
BJ Hargrave <bj@bjhargrave.com>
2017-09-23 06:20:26 -0400

    builder: Refresh when called from a resource delta event handler
    Sometimes the WorkspaceListener may be called from a resource delta
    event handler. This can happen when importing a new projects and a
    request is made to initialize the classpath container.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.core/src/bndtools/central/WorkspaceListener.java
+++ bndtools.core/src/bndtools/central/WorkspaceListener.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.osgi.util.promise.Promise;
+import org.osgi.util.promise.Success;
+
 import aQute.bnd.build.Workspace;
 import aQute.bnd.service.BndListener;
 
@@ -13,9 +17,24 @@ public final class WorkspaceListener extends BndListener {
     public WorkspaceListener(@SuppressWarnings("unused") Workspace workspace) {}
 
     @Override
-    public void changed(final File file) {
+    public void changed(File file) {
         try {
-            Central.refreshFile(file, null, true);
+            if (ResourcesPlugin.getWorkspace().isTreeLocked()) {
+                // Sometimes we may be called from a resource delta event handler
+                // so we use a job to refresh the file
+                final RefreshFileJob job = new RefreshFileJob(file, true);
+                if (job.needsToSchedule()) {
+                    Central.onWorkspaceInit(new Success<Workspace,Void>() {
+                        @Override
+                        public Promise<Void> call(Promise<Workspace> resolved) throws Exception {
+                            job.schedule();
+                            return null;
+                        }
+                    });
+                }
+            } else {
+                Central.refreshFile(file, null, true);
+            }
         } catch (Exception e) {
             logger.logError("Error refreshing file " + file, e);
         }
