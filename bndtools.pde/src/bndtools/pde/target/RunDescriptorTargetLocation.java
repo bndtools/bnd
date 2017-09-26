550a411442bbe05e03a7b403c3e1980743cd88e0
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-09-26 17:43:23 +0100

    Added bndtools.pde plugin and feature for plugin development using Bndtools
    The project contains two target location extensions for exposing Bnd
    workspaces to PDE tooling:
    
    - RepositoryTargetLocation allows adding the contents of Bnd
    repositories to target definitions
    
    - RunDescriptorTargetLocation allows adding -runbundles from a run
    descriptor to a target definition.
    
    Signed-off-by: Elias N Vasylenko <eliasvasylenko@gmail.com>

---
+++ bndtools.pde/src/bndtools/pde/target/RunDescriptorTargetLocation.java
@@ -0,0 +1,162 @@
+package bndtools.pde.target;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.core.target.ITargetLocation;
+import org.eclipse.pde.core.target.TargetBundle;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import aQute.bnd.build.Container;
+import aQute.bnd.build.Workspace;
+import biz.aQute.resolve.Bndrun;
+import bndtools.central.Central;
+
+public class RunDescriptorTargetLocation extends BndTargetLocation {
+    static final String TYPE = "BndRunDescriptorLocation";
+
+    static final String ELEMENT_RUN_DESCRIPTOR = "bndrun";
+    static final String ATTRIBUTE_RUN_DESCRIPTOR_FILE = "file";
+
+    private String bndrunFileName;
+    private IFile bndrunFile;
+
+    public RunDescriptorTargetLocation() {
+        super(TYPE, "bndrun.gif");
+    }
+
+    public RunDescriptorTargetLocation setRunDescriptor(IFile bndrunFile) {
+        this.bndrunFileName = bndrunFile.getFullPath().toString();
+        this.bndrunFile = bndrunFile;
+        clearResolutionStatus();
+        return this;
+
+    }
+
+    public RunDescriptorTargetLocation setRunDescriptor(String bndrunFileName) {
+        this.bndrunFileName = bndrunFileName;
+        this.bndrunFile = null;
+        clearResolutionStatus();
+        return this;
+    }
+
+    public IFile getFile() {
+        return bndrunFile;
+    }
+
+    @Override
+    public String getText(Object element) {
+        return bndrunFile != null ? bndrunFile.getFullPath().toString() : bndrunFileName;
+    }
+
+    @Override
+    public IWizard getEditWizard(ITargetDefinition target, ITargetLocation targetLocation) {
+        RunDescriptorTargetLocationWizard wizard = new RunDescriptorTargetLocationWizard();
+        wizard.setTarget(target);
+        wizard.setTargetLocation(this);
+        return wizard;
+    }
+
+    @Override
+    protected TargetBundle[] resolveBundles(ITargetDefinition definition, IProgressMonitor monitor) throws CoreException {
+        resolveBndrunFile();
+
+        Workspace workspace;
+        try {
+            workspace = Central.getWorkspace();
+        } catch (Exception e) {
+            throw new CoreException(new Status(IStatus.ERROR, PLUGIN_ID, MESSAGE_UNABLE_TO_LOCATE_WORKSPACE, e));
+        }
+
+        try (Bndrun bndRun = new Bndrun(workspace, bndrunFile.getRawLocation().makeAbsolute().toFile())) {
+            Collection<Container> containers = bndRun.getRunbundles();
+            List<TargetBundle> bundles = new ArrayList<>(containers.size());
+
+            monitor.beginTask("Resolving Bundles", containers.size());
+
+            int i = 0;
+            for (Container container : containers) {
+                try {
+                    bundles.add(new TargetBundle(container.getFile()));
+                } catch (Exception e) {
+                    throw new CoreException(new Status(IStatus.ERROR, PLUGIN_ID, "Invalid plugin in run descriptor: " + container.getBundleSymbolicName() + " @ " + getLocation(false), e));
+                }
+
+                if (monitor.isCanceled())
+                    return null;
+                monitor.worked(++i);
+            }
+
+            monitor.done();
+
+            return bundles.toArray(new TargetBundle[bundles.size()]);
+        } catch (CoreException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new CoreException(new Status(IStatus.ERROR, PLUGIN_ID, MESSAGE_UNABLE_TO_RESOLVE_BUNDLES, e));
+        }
+    }
+
+    private void resolveBndrunFile() throws CoreException {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IPath path = new Path(bndrunFileName);
+        IResource resource = workspace.getRoot().findMember(path);
+
+        if (resource instanceof IFile) {
+            bndrunFile = (IFile) resource;
+        } else {
+            throw new CoreException(new Status(IStatus.ERROR, PLUGIN_ID, "No file at path: " + bndrunFileName));
+        }
+    }
+
+    @Override
+    public String getLocation(boolean resolve) {
+        return bndrunFileName;
+    }
+
+    @Override
+    protected void serialize(Document document, Element locationElement) {
+        Element runDescriptorElement = document.createElement(ELEMENT_RUN_DESCRIPTOR);
+        runDescriptorElement.setAttribute(ATTRIBUTE_RUN_DESCRIPTOR_FILE, getLocation(true));
+        locationElement.appendChild(runDescriptorElement);
+    }
+
+    public static class Factory extends BndTargetLocationFactory {
+        public Factory() {
+            super(TYPE);
+        }
+
+        @Override
+        public ITargetLocation getTargetLocation(Element locationElement) throws CoreException {
+            NodeList children = locationElement.getChildNodes();
+
+            for (int i = 0; i < children.getLength(); ++i) {
+                Node node = children.item(i);
+
+                if (isElement(node, ELEMENT_RUN_DESCRIPTOR)) {
+                    String file = ((Element) node).getAttribute(ATTRIBUTE_RUN_DESCRIPTOR_FILE);
+
+                    return new RunDescriptorTargetLocation().setRunDescriptor(file);
+                }
+            }
+
+            throw new CoreException(new Status(IStatus.ERROR, PLUGIN_ID, "No run descriptor file specified"));
+        }
+    }
+}
---
+++ bndtools.pde/src/bndtools/pde/target/RunDescriptorTargetLocationPage.java
@@ -0,0 +1,187 @@
+package bndtools.pde.target;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.internal.ui.SWTFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.dialogs.PatternFilter;
+import org.eclipse.ui.model.WorkbenchContentProvider;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+import aQute.bnd.build.Container;
+import aQute.bnd.build.Workspace;
+import biz.aQute.resolve.Bndrun;
+import bndtools.central.Central;
+
+public class RunDescriptorTargetLocationPage extends BndTargetLocationPage {
+    private static final String FILE_EXTENSION = "bndrun";
+
+    private RunDescriptorTargetLocation targetLocation;
+
+    private IFile runDescriptorFile;
+    private TreeViewer bundleList;
+
+    public RunDescriptorTargetLocationPage(ITargetDefinition targetDefinition, RunDescriptorTargetLocation targetLocation) {
+        super("AddBndRunDescriptorContainer", "Add Bnd Run Descriptor", "Select a Bnd run descriptor to be added to your target", targetDefinition);
+
+        if (targetLocation != null) {
+            this.targetLocation = targetLocation;
+            this.runDescriptorFile = targetLocation.getFile();
+        }
+    }
+
+    @Override
+    public void createControl(Composite parent) {
+        Composite composite = SWTFactory.createComposite(parent, 1, 1, GridData.FILL_BOTH, 0, 0);
+
+        SWTFactory.createLabel(composite, "Run descriptor:", 1);
+        TreeViewer projectTree = createRunDescriptorSelectionArea(composite);
+        SWTFactory.createLabel(composite, "Run bundles:", 1);
+        bundleList = createBundleListArea(composite, 1);
+
+        setControl(composite);
+        setPageComplete(false);
+
+        updateTarget();
+        selectTargetInTree(projectTree);
+
+        if (projectTree.getTree().getItems().length == 0)
+            logError("No run descriptors found in workspace", null);
+    }
+
+    private TreeViewer createRunDescriptorSelectionArea(Composite composite) {
+        final TreeViewer projectTree = new TreeViewer(new Tree(composite, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER));
+        projectTree.getTree().setLayoutData(fillGridData(1));
+
+        projectTree.setContentProvider(new WorkbenchContentProvider());
+        projectTree.setLabelProvider(new WorkbenchLabelProvider());
+        projectTree.setInput(ResourcesPlugin.getWorkspace());
+        projectTree.setFilters(getProjectTreeFilters());
+        projectTree.addSelectionChangedListener(new ISelectionChangedListener() {
+            @Override
+            public void selectionChanged(SelectionChangedEvent event) {
+                Object selection = ((ITreeSelection) event.getSelection()).getFirstElement();
+
+                if (selection != null && selection instanceof IFile) {
+                    runDescriptorFile = (IFile) selection;
+                } else {
+                    runDescriptorFile = null;
+                }
+
+                updateTarget();
+            }
+        });
+        projectTree.expandAll();
+
+        return projectTree;
+    }
+
+    private ViewerFilter[] getProjectTreeFilters() {
+        PatternFilter bndrunFilter = new PatternFilter() {
+            @Override
+            protected boolean isLeafMatch(Viewer viewer, Object element) {
+                return element instanceof IFile && FILE_EXTENSION.equals(((IFile) element).getFileExtension());
+            }
+
+            @Override
+            public boolean isElementSelectable(Object element) {
+                return element instanceof IFile;
+            }
+        };
+        bndrunFilter.setPattern("*");
+        return new ViewerFilter[] {
+                bndrunFilter
+        };
+    }
+
+    private void updateTarget() {
+        if (runDescriptorFile == null) {
+            clearTarget();
+        } else {
+            try {
+                File file = runDescriptorFile.getRawLocation().makeAbsolute().toFile();
+                Workspace workspace = Central.getWorkspace();
+                List<String> bundles = new ArrayList<>();
+
+                try (Bndrun bndRun = new Bndrun(workspace, file)) {
+                    for (Container bundle : bndRun.getRunbundles()) {
+                        bundles.add(bundle.getBundleSymbolicName() + " - " + bundle.getVersion());
+                    }
+                    bundleList.setInput(bundles);
+                }
+
+                setPageComplete(true);
+
+                if (bundles.isEmpty())
+                    logWarning("Run descriptor is empty: " + runDescriptorFile.getFullPath(), null);
+                else
+                    resetMessage();
+            } catch (Exception e) {
+                clearTarget();
+                logError(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void clearTarget() {
+        runDescriptorFile = null;
+        bundleList.setInput(Collections.emptySet());
+        setPageComplete(false);
+        resetMessage();
+    }
+
+    private boolean selectTargetInTree(TreeViewer projectTree) {
+        if (runDescriptorFile == null)
+            return false;
+
+        for (TreeItem item : projectTree.getTree().getItems()) {
+            if (setSelectedFileInTree(projectTree, item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean setSelectedFileInTree(TreeViewer projectTree, TreeItem item) {
+        if (item.getData() instanceof IFile) {
+            IFile file = (IFile) item.getData();
+            if (file.equals(runDescriptorFile)) {
+                projectTree.getTree().select(item);
+                return true;
+            }
+        }
+
+        for (TreeItem child : item.getItems()) {
+            if (setSelectedFileInTree(projectTree, child)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public RunDescriptorTargetLocation getBundleContainer() {
+        if (targetLocation == null) {
+            targetLocation = new RunDescriptorTargetLocation();
+        }
+
+        return targetLocation.setRunDescriptor(runDescriptorFile);
+    }
+}
