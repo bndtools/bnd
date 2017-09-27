f2f83efab89fce16dbbf2040f3ad28b2cd6e076e
BJ Hargrave <bj@bjhargrave.com>
2017-09-27 19:07:13 -0400

    resolve: Change ResolveLogger to AutoClosable and remove finalize
    To properly close tmp file. Also reduce object creation.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.resolve/test/biz/aQute/resolve/ResolverLoggerTest.java
+++ biz.aQute.resolve/test/biz/aQute/resolve/ResolverLoggerTest.java
@@ -5,16 +5,16 @@ import junit.framework.TestCase;
 public class ResolverLoggerTest extends TestCase {
 
 	public void testSimple() throws Exception {
-		ResolverLogger rl = new ResolverLogger();
+		try (ResolverLogger rl = new ResolverLogger()) {
+			rl.log(ResolverLogger.LOG_ERROR, "test", null);
+			assertEquals(String.format("ERROR: test%n"), rl.getLog());
 
-		rl.log(ResolverLogger.LOG_ERROR, "test", null);
-		assertEquals(String.format("ERROR: test%n"), rl.getLog());
+			for (int i = 0; i < 100000; i++) {
+				rl.log(ResolverLogger.LOG_ERROR, "test " + i, null);
+			}
 
-		for (int i = 0; i < 100000; i++) {
-			rl.log(ResolverLogger.LOG_ERROR, "test " + i, null);
+			String s = rl.getLog();
+			assertTrue(s.endsWith(String.format("test 99999%n")));
 		}
-
-		String s = rl.getLog();
-		assertTrue(s.endsWith(String.format("test 99999%n")));
 	}
 }
