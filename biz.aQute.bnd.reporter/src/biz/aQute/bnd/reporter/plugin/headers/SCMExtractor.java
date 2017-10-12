6e0ed46c21a0d0e9278d69a15a26d7a2ffcebf3d
Clement Delgrange <cde@cdecode.org>
2017-10-12 12:40:37 +0200

    correct scm header extraction
    Signed-off-by: Clement Delgrange <cde@cdecode.org>

--- biz.aQute.bnd.reporter/src/biz/aQute/bnd/reporter/plugin/headers/SCMExtractor.java
+++ biz.aQute.bnd.reporter/src/biz/aQute/bnd/reporter/plugin/headers/SCMExtractor.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import aQute.bnd.header.Attrs;
+import aQute.bnd.header.OSGiHeader;
 import aQute.bnd.header.Parameters;
 import aQute.bnd.osgi.Constants;
 import aQute.bnd.osgi.Jar;
@@ -24,30 +25,30 @@ public class SCMExtractor extends HeaderExtractor {
 		final List<Tag> result = new LinkedList<>();
 		final Parameters header = manifest.getHeader(Constants.BUNDLE_SCM, false);
 
-		if (!header.values().isEmpty()) {
+		final String scm = header.toString();
+		if (scm.length() > 0) {
 			final Tag tag = new Tag(HEADER_TAG);
+			final Attrs attrs = OSGiHeader.parseProperties(scm);
 
-			for (final Attrs attrs : header.values()) {
-				if (attrs.containsKey("url")) {
-					tag.addContent(new Tag(URL_TAG, attrs.get("url")));
-				}
-
-				if (attrs.containsKey("connection")) {
-					tag.addContent(new Tag(CONNECTION_TAG, attrs.get("connection")));
-				}
-
-				if (attrs.containsKey("developerConnection")) {
-					tag.addContent(new Tag(DEVELOPER_CONNECTION_TAG, attrs.get("developerConnection")));
-				}
-
-				if (attrs.containsKey("tag")) {
-					tag.addContent(new Tag(TAG_TAG, attrs.get("tag")));
-				} else {
-					tag.addContent(new Tag(TAG_TAG, "HEAD"));
+			for (final String key : attrs.keySet()) {
+				if (key.equals("url")) {
+					tag.addContent(new Tag(URL_TAG, attrs.get(key)));
+				} else if (key.equals("connection")) {
+					tag.addContent(new Tag(CONNECTION_TAG, attrs.get(key)));
+				} else if (key.equals("developerConnection")) {
+					tag.addContent(new Tag(DEVELOPER_CONNECTION_TAG, attrs.get(key)));
+				} else if (key.equals("tag")) {
+					tag.addContent(new Tag(TAG_TAG, attrs.get(key)));
 				}
 			}
+
+			if (!attrs.containsKey("tag")) {
+				tag.addContent(new Tag(TAG_TAG, "HEAD"));
+			}
+
 			result.add(tag);
 		}
+
 		return result;
 	}
 }
