7dccff71592596d3d33dba910a7dd096ade35195
Tim Ward <timothyjward@apache.org>
2017-10-20 11:02:45 +0100

    Capability annotation versions should be of type Version not String
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- biz.aQute.bndlib.tests/src/test/annotationheaders/attrs/std/AnnotatedAnnotation.java
+++ biz.aQute.bndlib.tests/src/test/annotationheaders/attrs/std/AnnotatedAnnotation.java
@@ -5,7 +5,7 @@ import org.osgi.annotation.bundle.Header;
 import org.osgi.annotation.bundle.Requirement;
 
 @Capability(namespace = "provide", name = "Provided")
-@Capability(namespace = "provide", name = "Provided2")
+@Capability(namespace = "provide", name = "Provided2", version = "2")
 @Requirement(namespace = "require", name = "Required", version = "1", filter = "(a=b)")
 @Requirement(namespace = "require", name = "Required2", version = "2", filter = "(a=b)")
 @Header(name = "foo", value = "bar")
