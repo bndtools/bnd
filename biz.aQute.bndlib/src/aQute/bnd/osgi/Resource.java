feb8c7a76b804f34052ab03019533f9a235f4113
BJ Hargrave <bj@bjhargrave.com>
2017-10-31 16:27:18 -0400

    resource: Handle relative file URLs in Resource.fromURL
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Resource.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Resource.java
@@ -1,13 +1,13 @@
 package aQute.bnd.osgi;
 
 import java.io.Closeable;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import aQute.bnd.osgi.URLResource.JarURLUtil;
 
@@ -28,19 +28,19 @@ public interface Resource extends Closeable {
 
 	static Resource fromURL(URL url) throws IOException {
 		if (url.getProtocol().equalsIgnoreCase("file")) {
-			File file = new File(URI.create(url.toExternalForm()));
-			return new FileResource(file);
+			Path path = Paths.get(Paths.get("").toUri().resolve(url.getPath()));
+			return new FileResource(path);
 		}
 		if (url.getProtocol().equals("jar")) {
 			JarURLUtil util = new JarURLUtil(url);
 			URL jarFileURL = util.getJarFileURL();
 			if (jarFileURL.getProtocol().equalsIgnoreCase("file")) {
-				File file = new File(URI.create(jarFileURL.toExternalForm()));
+				Path path = Paths.get(Paths.get("").toUri().resolve(jarFileURL.getPath()));
 				String entryName = util.getEntryName();
 				if (entryName == null) {
-					return new FileResource(file);
+					return new FileResource(path);
 				}
-				return new ZipResource(file, entryName);
+				return new ZipResource(path, entryName);
 			}
 		}
 		return new URLResource(url);
