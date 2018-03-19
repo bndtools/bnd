50fd7ee4d3f61e11422d5d6278e3687fc4ece20b
BJ Hargrave <bj@bjhargrave.com>
2018-03-19 13:14:31 -0400

    gradle: Add Index task type which uses the new SimpleIndexer class
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

---
+++ biz.aQute.bnd.gradle/src/aQute/bnd/gradle/Index.groovy
@@ -0,0 +1,197 @@
+/**
+ * Index task type for Gradle.
+ *
+ * <p>
+ * This task type can be used to index a set of bundles.
+ *
+ * <p>
+ * Here is an example of using the Baseline task type:
+ * <pre>
+ * import aQute.bnd.gradle.Index
+ * task index(type: Index) {
+ *   destination = file('bundles')
+ *   gzip = true
+ *   bundles = fileTree(destination) {
+ *    include '**&#47;*.jar'
+ *    exclude '**&#47;*-latest.jar'
+ *    exclude '**&#47;*-sources.jar'
+ *    exclude '**&#47;*-javadoc.jar'
+ *  }
+ * }
+ * </pre>
+ *
+ * <p>
+ * Properties:
+ * <ul>
+ * <li>gzip - If <code>true</code>, then a gzip'd copy of the index will be made.
+ * Otherwise, only the uncompressed index will be made. The default is
+ * <code>false</code>.</li>
+ * <li>indexName - The name of the index file. The default is
+ * <code>index.xml</code>.</li>
+ * <li>repositoryName - The name attribute in the generated index. The default is
+ * the name of the task.</li>
+ * <li>destinationDir - The destination directory for the index.
+ * This is used as the URI base of the generated index.
+ * The default value is buildDir.</li>
+ * <li>bundles - This is the bundles to be indexed. This property
+ * must be set.</li>
+ * </ul>
+ */
+
+package aQute.bnd.gradle
+
+import aQute.bnd.osgi.repository.SimpleIndexer
+import aQute.lib.io.IO
+
+import java.util.zip.GZIPOutputStream
+
+import org.gradle.api.Buildable
+import org.gradle.api.DefaultTask
+import org.gradle.api.Task
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+public class Index extends DefaultTask {
+  private ConfigurableFileCollection bundleCollection
+  private File destinationDir
+
+  /**
+   * Whether a gzip'd index should be made.
+   *
+   * <p>
+   * If <code>true</code>, then a gzip'd copy of the index will be made.
+   * Otherwise, only the uncompressed index will be made. The default is
+   * <code>false</code>.
+   */
+  @Input
+  @Optional
+  boolean gzip
+
+  /**
+   * The name of the index file.
+   *
+   * <p>
+   * The default is <code>index.xml</code>.
+   */
+  @Input
+  @Optional
+  String indexName
+
+  /**
+   * The name attribute in the generated index.
+   *
+   * <p>
+   * The default is the name of the task.
+   */
+  @Input
+  @Optional
+  String repositoryName
+
+  /**
+   * Create an Index task.
+   *
+   */
+  public Index() {
+    super()
+    gzip = false
+    destinationDir = project.buildDir
+    indexName = 'index.xml'
+    repositoryName = name
+    bundleCollection = project.files()
+    dependsOn { getBundles() }
+  }
+
+  /**
+   * Set the destination directory for the index.
+   * This is used as the URI base of the generated index.
+   *
+   * <p>
+   * The argument will be handled using
+   * Project.file().
+   */
+  public void setDestinationDir(Object file) {
+    destinationDir = project.file(file)
+  }
+
+  /**
+   * Return the destination directory for the index.
+   * This is used as the URI base of the generated index.
+   *
+   * <p>
+   * The default value is buildDir.
+   */
+  @Input
+  @Optional
+  public File getDestinationDir() {
+    return destinationDir
+  }
+
+  /**
+   * Add files to the bundles to be indexed.
+   *
+   * <p>
+   * The arguments will be handled using
+   * Project.files().
+   */
+  public ConfigurableFileCollection bundles(Object... paths) {
+    return bundleCollection.from(paths).builtBy(paths.findAll { path ->
+      path instanceof Task || path instanceof Buildable
+    })
+  }
+
+  /**
+   * Get the bundles to be indexed.
+   */
+  @InputFiles
+  public ConfigurableFileCollection getBundles() {
+    return bundleCollection
+  }
+
+  /**
+   * Set the bundles to be indexed.
+   */
+  public void setBundles(Object path) {
+    bundleCollection.from = []
+    bundleCollection.builtBy = []
+    bundles(path)
+  }
+
+  /**
+   * Return the uncompressed index file.
+   */
+  @OutputFile
+  public File getIndexUncompressed() {
+    return new File(destinationDir, indexName)
+  }
+
+  /**
+   * Return the compressed index file.
+   */
+  @OutputFile
+  public File getIndexCompressed() {
+    return new File(destinationDir, indexName + '.gz')
+  }
+
+  /**
+   * Index the bundles.
+   *
+   */
+  @TaskAction
+  void indexer() {
+    indexUncompressed.withOutputStream { out ->
+      SimpleIndexer.index(bundles.sort(), out, project.uri(destinationDir), false, repositoryName);
+    }
+    logger.info 'Generated index {}.', indexUncompressed
+
+    if (gzip) {
+      indexCompressed.withOutputStream { out ->
+        IO.copy(indexUncompressed, new GZIPOutputStream(out)).close()
+      }
+      logger.info 'Generated index {}.', indexCompressed
+    }
+  }
+}
---
+++ biz.aQute.bnd.gradle/test/aQute/bnd/gradle/TestIndexTask.groovy
@@ -0,0 +1,153 @@
+package aQute.bnd.gradle
+
+import java.util.jar.*
+import java.util.zip.*
+
+
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+import spock.lang.Specification
+
+class TestIndexTask extends Specification {
+
+    File buildDir = new File('generated')
+    File testResources = new File(buildDir, 'testresources')
+    List<File> pluginClasspath
+
+    def setup() {
+      File plugin = new File(buildDir, 'biz.aQute.bnd.gradle.jar').getCanonicalFile()
+      assert plugin.isFile()
+      pluginClasspath = Collections.singletonList(plugin)
+    }
+
+    def "Simple Bnd Index Task Test"() {
+        given:
+          String testProject = 'indexplugin1'
+          File testProjectDir = new File(testResources, testProject).canonicalFile
+          assert testProjectDir.isDirectory()
+          File testProjectBuildDir = new File(testProjectDir, 'build').canonicalFile
+
+        when:
+          def result = TestHelper.getGradleRunner()
+            .withProjectDir(testProjectDir)
+            .withArguments('--stacktrace', '--debug', 'build', 'indexer', 'indexer2')
+            .withPluginClasspath(pluginClasspath)
+            .forwardOutput()
+            .build()
+
+        then:
+          result.task(':bundle').outcome == SUCCESS
+          result.task(':jar').outcome == SUCCESS
+          result.task(':indexer').outcome == SUCCESS
+          result.task(':indexer2').outcome == SUCCESS
+
+          testProjectBuildDir.isDirectory()
+
+          File jartask_bundle = new File(testProjectBuildDir, "libs/${testProject}-1.0.0.jar")
+          jartask_bundle.isFile()
+          JarFile jartask_jar = new JarFile(jartask_bundle)
+          Attributes jartask_manifest = jartask_jar.getManifest().getMainAttributes()
+          File bundletask_bundle = new File(testProjectBuildDir, "libs/${testProject}_bundle-1.1.0.jar")
+          bundletask_bundle.isFile()
+          JarFile bundletask_jar = new JarFile(bundletask_bundle)
+          Attributes bundletask_manifest = bundletask_jar.getManifest().getMainAttributes()
+
+          jartask_manifest.getValue('Bundle-SymbolicName') == "${testProject}"
+          jartask_manifest.getValue('Bundle-Version') == '1.0.0'
+          jartask_jar.close()
+
+          bundletask_manifest.getValue('Bundle-SymbolicName') == "${testProject}_bundle"
+          bundletask_manifest.getValue('Bundle-Version') == '1.1.0'
+          bundletask_jar.close()
+
+        when:
+          File xmlfile = new File(testProjectBuildDir, "index.xml")
+          assert xmlfile.isFile()
+          def repository = new XmlSlurper().parse(xmlfile)
+
+        then:
+          repository.@name == testProject
+          repository.lookupNamespace('') == 'http://www.osgi.org/xmlns/repository/v1.0.0'
+          repository.resource.size() == 2
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.identity'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'osgi.identity'
+          }.@value == testProject
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.content'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'url'
+          }.@value == "libs/${testProject}-1.0.0.jar"
+          repository.resource[1].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.identity'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'osgi.identity'
+          }.@value == "${testProject}_bundle"
+          repository.resource[1].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.content'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'url'
+          }.@value == "libs/${testProject}_bundle-1.1.0.jar"
+
+        when:
+          xmlfile = new File(testProjectBuildDir, "index.xml.gz")
+
+        then:
+          !xmlfile.isFile()
+
+        when:
+          xmlfile = new File(testProjectBuildDir, "index2.xml")
+          assert xmlfile.isFile()
+          repository = new XmlSlurper().parse(xmlfile)
+
+        then:
+          repository.@name == 'indexer2'
+          repository.lookupNamespace('') == 'http://www.osgi.org/xmlns/repository/v1.0.0'
+          repository.resource.size() == 1
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.identity'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'osgi.identity'
+          }.@value == testProject
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.content'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'url'
+          }.@value == "libs/${testProject}-1.0.0.jar"
+
+        when:
+          xmlfile = new File(testProjectBuildDir, "index2.xml.gz")
+          assert xmlfile.isFile()
+          repository = new XmlSlurper().parse(new GZIPInputStream(new FileInputStream(xmlfile)))
+
+        then:
+          repository.@name == 'indexer2'
+          repository.lookupNamespace('') == 'http://www.osgi.org/xmlns/repository/v1.0.0'
+          repository.resource.size() == 1
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.identity'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'osgi.identity'
+          }.@value == testProject
+          repository.resource[0].'*'.find { node ->
+            node.name() == 'capability' && node.@namespace == 'osgi.content'
+          }.'*'.find { node ->
+            node.name() == 'attribute' && node.@name == 'url'
+          }.@value == "libs/${testProject}-1.0.0.jar"
+
+        when:
+          result = TestHelper.getGradleRunner()
+            .withProjectDir(testProjectDir)
+            .withArguments('--stacktrace', '--debug', 'build', 'indexer', 'indexer2')
+            .withPluginClasspath(pluginClasspath)
+            .forwardOutput()
+            .build()
+
+        then:
+          result.task(':bundle').outcome == UP_TO_DATE
+          result.task(':jar').outcome == UP_TO_DATE
+          result.task(':indexer').outcome == UP_TO_DATE
+          result.task(':indexer2').outcome == UP_TO_DATE
+    }
+}
