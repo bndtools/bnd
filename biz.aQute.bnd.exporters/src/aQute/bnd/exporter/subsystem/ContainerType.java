bb7a3d7d21932178357d30b01657abdd62a68662
bischofs <bipolis@bipolis.org>
2018-04-12 01:20:10 +0200

    SubsystemExporter, handling imports,exports,provide-cap,require-cap ans some tests

---
+++ biz.aQute.bnd.exporters/src/aQute/bnd/exporter/subsystem/ContainerType.java
@@ -0,0 +1,122 @@
+package aQute.bnd.exporter.subsystem;
+
+import java.io.File;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.subsystem.SubsystemConstants;;
+
+public enum ContainerType {
+
+	BUNDLE("jar", Constants.BUNDLE_LOCALIZATION, Constants.BUNDLE_LOCALIZATION_DEFAULT_BASENAME,
+		"OSGI-INF/SUBSYSTEM.MF", Constants.BUNDLE_SYMBOLICNAME, Constants.BUNDLE_VERSION, Constants.BUNDLE_NAME,
+		Constants.BUNDLE_DESCRIPTION, Constants.BUNDLE_COPYRIGHT, Constants.BUNDLE_DOCURL, Constants.BUNDLE_VENDOR,
+		Constants.BUNDLE_CONTACTADDRESS, Constants.BUNDLE_CATEGORY),
+
+	SUBSYSTEM("eas", SubsystemConstants.SUBSYSTEM_LOCALIZATION,
+		SubsystemConstants.SUBSYSTEM_LOCALIZATION_DEFAULT_BASENAME, "OSGI-INF/SUBSYSTEM.MF",
+		SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, SubsystemConstants.SUBSYSTEM_VERSION,
+		SubsystemConstants.SUBSYSTEM_NAME, SubsystemConstants.SUBSYSTEM_DESCRIPTION,
+		SubsystemConstants.SUBSYSTEM_COPYRIGHT, SubsystemConstants.SUBSYSTEM_DOCURL,
+		SubsystemConstants.SUBSYSTEM_VENDOR, SubsystemConstants.SUBSYSTEM_CONTACTADDRESS,
+		SubsystemConstants.SUBSYSTEM_CATEGORY);
+
+	private final String	localizationDefaultBasenameKey;
+	private final String	categoryKey;
+	private final String	contactAddressKey;
+	private final String	copyrightKey;
+	private final String	descriptionKey;
+	private final String	docURLKey;
+	private final String	extension;
+	private final String	localizationKey;
+	private final String	manifestUri;
+	private final String	nameKey;
+	private final String	symbolicNameKey;
+	private final String	vendorKey;
+	private final String	versionKey;
+
+	ContainerType(String extension, String localizationKey, String localizationDefaultBasenameKey, String manifestUri,
+		String symbolicNameKey, String versionKey, String nameKey, String descriptionKey, String copyrightKey,
+		String docURLKey, String vendorKey, String contactAddressKey, String categoryKey) {
+		this.extension = extension;
+		this.manifestUri = manifestUri;
+		this.localizationKey = localizationKey;
+		this.localizationDefaultBasenameKey = localizationDefaultBasenameKey;
+		this.symbolicNameKey = symbolicNameKey;
+		this.versionKey = versionKey;
+		this.nameKey = nameKey;
+		this.descriptionKey = descriptionKey;
+		this.copyrightKey = copyrightKey;
+		this.docURLKey = docURLKey;
+		this.vendorKey = vendorKey;
+		this.contactAddressKey = contactAddressKey;
+		this.categoryKey = categoryKey;
+	}
+
+	public String getLocalizationDefaultBasenameKey() {
+		return localizationDefaultBasenameKey;
+	}
+
+	public String getCategoryKey() {
+		return categoryKey;
+	}
+
+	public String getContactAddressKey() {
+		return contactAddressKey;
+	}
+
+	public String getCopyrightKey() {
+		return copyrightKey;
+	}
+
+	public String getDescriptionKey() {
+		return descriptionKey;
+	}
+
+	public String getDocURLKey() {
+		return docURLKey;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public String getLocalizationKey() {
+		return localizationKey;
+	}
+
+	public String getManifestUri() {
+		return manifestUri;
+	}
+
+	public String getNameKey() {
+		return nameKey;
+	}
+
+	public String getSymbolicNameKey() {
+		return symbolicNameKey;
+	}
+
+	public String getVendorKey() {
+		return vendorKey;
+	}
+
+	public String getVersionKey() {
+		return versionKey;
+	}
+
+	public static ContainerType byFile(File file) {
+
+		if (file != null && file.isFile()) {
+			for (ContainerType containerType : ContainerType.values()) {
+				if (file.getName()
+					.toLowerCase()
+					.endsWith("." + containerType.getExtension())) {
+					return containerType;
+				}
+			}
+		}
+
+		return null;
+	}
+
+}
--- biz.aQute.bnd.exporters/src/aQute/bnd/exporter/subsystem/SubsystemExporter.java
+++ biz.aQute.bnd.exporters/src/aQute/bnd/exporter/subsystem/SubsystemExporter.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.jar.Attributes;
+import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
 import org.osgi.service.subsystem.SubsystemConstants;
@@ -33,11 +34,10 @@ import aQute.bnd.service.Strategy;
 import aQute.bnd.service.export.Exporter;
 import aQute.lib.collections.MultiMap;
 import aQute.lib.io.ByteBufferOutputStream;
+import aQute.lib.io.IO;
 
 @BndPlugin(name = "subsystem")
 public class SubsystemExporter implements Exporter {
-
-	private static final String	OSGI_INF_SUBSYSTEM_MF		= "OSGI-INF/SUBSYSTEM.MF";
 
 	@Override
 	public String[] getTypes() {
@@ -50,6 +50,7 @@ public class SubsystemExporter implements Exporter {
 	@Override
 	public Map.Entry<String, Resource> export(String type, final Project project, Map<String, String> options)
 		throws Exception {
+
 		Jar jar = new Jar(".");
 
 		project.addClose(jar);
@@ -60,21 +61,75 @@ public class SubsystemExporter implements Exporter {
 		manifest.getMainAttributes()
 			.putValue("Subsystem-ManifestVersion", "1");
 
-		List<Container> distro = project.getBundles(Strategy.LOWEST, project.getProperty(Constants.DISTRO),
-			Constants.DISTRO);
-		List<File> distroFiles = getBundles(distro, project);
-		List<File> files = getBundles(project.getRunbundles(), project);
+		EasArchivType esaArchivType = null;
+		Boolean storeBundles = null;
+		String archiveContent = project.get("Subsystem-ArchiveContent");
+
+		esaArchivType = EasArchivType.byParameter(archiveContent);
+		if (esaArchivType == null) {
+			esaArchivType = EasArchivType.CONTENT;
+		}
+
+		Collection<Container> requireBundles = getRequiredBundles(project);
+		Collection<Container> runBundles = project.getRunbundles();
+
+		Collection<Container> bundles = null;
+		switch (esaArchivType) {
+			case NONE :
+				bundles = requireBundles;
+				storeBundles = false;
+				break;
+
+			case CONTENT :
+
+				bundles = requireBundles;
+				storeBundles = true;
+				break;
+
+			case ALL :
+
+				bundles = runBundles;
+				storeBundles = true;
+				break;
+
+			default :
+
+				throw new IllegalArgumentException("Unknown EsaArchivType: " + esaArchivType);
+
+		}
+
+		List<File> files = getBundles(bundles, project);
 
 		MultiMap<String, Attrs> imports = new MultiMap<>();
 		MultiMap<String, Attrs> exports = new MultiMap<>();
-		Parameters requirements = new Parameters();
-		Parameters capabilities = new Parameters();
+		MultiMap<String, Attrs> requireCapability = new MultiMap<>();
+		MultiMap<String, Attrs> provideCapability = new MultiMap<>();
 
 		for (File file : files) {
-			Domain domain = Domain.domain(file);
-			String bsn = domain.getBundleSymbolicName()
-				.getKey();
-			String version = domain.getBundleVersion();
+			ContainerType containerType = ContainerType.byFile(file);
+
+			Domain domain;
+			// default & last. Assume JAR
+			try (JarInputStream jin = new JarInputStream(IO.stream(file))) {
+				Manifest m = jin.getManifest();
+				if (m != null) {
+					domain = Domain.domain(m);
+
+				} else {
+					throw new Exception("Container could not be read: " + file.getName());
+				}
+			}
+
+			String csn = null;
+			Parameters p = domain.getParameters(containerType.getSymbolicNameKey());
+			if (!p.isEmpty()) {
+				csn = p.entrySet()
+					.iterator()
+					.next()
+					.getKey();
+
+			}
+			String version = domain.get(containerType.getVersionKey());
 
 			for (Entry<String, Attrs> e : domain.getImportPackage()
 				.entrySet()) {
@@ -86,11 +141,48 @@ public class SubsystemExporter implements Exporter {
 				exports.add(e.getKey(), e.getValue());
 			}
 
-			String path = bsn + "-" + version + ".jar";
-			jar.putResource(path, new FileResource(file));
+			for (Entry<String, Attrs> e : domain.getRequireCapability()
+				.entrySet()) {
+				requireCapability.add(e.getKey(), e.getValue());
+			}
+
+			for (Entry<String, Attrs> e : domain.getProvideCapability()
+				.entrySet()) {
+				provideCapability.add(e.getKey(), e.getValue());
+			}
+
+			if (storeBundles) {
+
+				String path = csn + "-" + version + "." + containerType.getExtension();
+				jar.putResource(path, new FileResource(file));
+			}
 		}
 
+		// as overriding parameter from bndrun or bnd.bnd
+		Parameters requirementParameter = project.getRequireCapability();
+		Parameters capabilityParameter = project.getProvideCapability();
+
+		if (requirementParameter != null && !requirementParameter.isEmpty()) {
+			requireCapability.clear();
+			for (Entry<String, Attrs> e : requirementParameter.entrySet()) {
+				requireCapability.add(e.getKey(), e.getValue());
+			}
+		}
+
+		if (capabilityParameter != null && !capabilityParameter.isEmpty()) {
+			provideCapability.clear();
+			for (Entry<String, Attrs> e : capabilityParameter.entrySet()) {
+				provideCapability.add(e.getKey(), e.getValue());
+			}
+		}
 		headers(project, manifest.getMainAttributes());
+
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.REQUIRE_CAPABILITY, requireCapability);
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.PROVIDE_CAPABILITY, provideCapability);
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.IMPORT_PACKAGE, imports);
+		set(manifest.getMainAttributes(), org.osgi.framework.Constants.EXPORT_PACKAGE, exports);
+		set(manifest.getMainAttributes(), SubsystemConstants.SUBSYSTEM_VERSION,
+			project.get(SubsystemConstants.SUBSYSTEM_VERSION, "0.0.0"));
 
 		set(manifest.getMainAttributes(), SubsystemConstants.SUBSYSTEM_TYPE, type);
 
@@ -106,7 +198,7 @@ public class SubsystemExporter implements Exporter {
 		ByteBufferOutputStream bout = new ByteBufferOutputStream();
 		manifest.write(bout);
 
-		jar.putResource(OSGI_INF_SUBSYSTEM_MF, new EmbeddedResource(bout.toByteBuffer(), 0));
+		jar.putResource(ContainerType.SUBSYSTEM.getManifestUri(), new EmbeddedResource(bout.toByteBuffer(), 0));
 
 		final JarResource jarResource = new JarResource(jar, true);
 		final String name = ssn + ".esa";
@@ -114,11 +206,42 @@ public class SubsystemExporter implements Exporter {
 		return new SimpleEntry<>(name, jarResource);
 	}
 
-	private List<File> getBundles(Collection<Container> bundles, Processor reporter) throws Exception {
+	private void set(Attributes mainAttributes, String key, MultiMap<String, Attrs> multiMap) {
+
+		Parameters parameters = new Parameters();
+		List<Attrs> allattrs = new ArrayList<>();
+		for (Entry<String, List<Attrs>> entry : multiMap.entrySet()) {
+
+			for (Attrs attrs : entry.getValue()) {
+
+				parameters.add(entry.getKey(), attrs);
+			}
+		}
+		mainAttributes.putValue(key, parameters.toString());
+		// (key, allattrs);
+
+	}
+
+	private Collection<Container> getRequiredBundles(final Project project) throws Exception {
+		Collection<Attrs> attrs = project.getParameters(Constants.RUNREQUIRES, "bnd.identity")
+			.values();
+		String value = "";
+		for (Attrs attrs2 : attrs) {
+			value += attrs2.get("id");
+		}
+
+		// project.getBundles(Strategy.LOWEST,
+		// project.getProperty(Constants.RUNREQUIRES), Constants.RUNREQUIRES);
+
+		List<Container> containers = project.getBundles(Strategy.HIGHEST, value, Constants.RUNREQUIRES);
+		return containers;
+	}
+
+	private List<File> getBundles(Collection<Container> containers, Processor reporter) throws Exception {
 		List<File> files = new ArrayList<>();
 
-		for (Container c : bundles) {
-			switch (c.getType()) {
+		for (Container container : containers) {
+			switch (container.getType()) {
 				case ERROR :
 					// skip, already reported
 					break;
@@ -126,10 +249,10 @@ public class SubsystemExporter implements Exporter {
 				case PROJECT :
 				case EXTERNAL :
 				case REPO :
-					files.add(c.getFile());
+					files.add(container.getFile());
 					break;
 				case LIBRARY :
-					c.contributeFiles(files, reporter);
+					container.contributeFiles(files, reporter);
 					break;
 			}
 		}
@@ -173,7 +296,5 @@ public class SubsystemExporter implements Exporter {
 				return;
 			}
 		}
-
 	}
-
 }
