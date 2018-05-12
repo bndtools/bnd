31a8ac0d41a02fa8b17f919c5e1ebfca13ede3e8
BJ Hargrave <bj@bjhargrave.com>
2018-05-12 12:43:21 -0400

    concurrentinit: Change to non-locking implementation
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- aQute.libg/src/aQute/lib/concurrentinit/ConcurrentInitialize.java
+++ aQute.libg/src/aQute/lib/concurrentinit/ConcurrentInitialize.java
@@ -1,89 +1,48 @@
 package aQute.lib.concurrentinit;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Helper class to handle concurrent system where you need to initialize a
  * value. The first one should create the value but the others should block
- * until the value has been created. Since we do not want to hold a lock during
- * the creation this is kind of tricky. This class uses a single monitor
- * {@link #lock} that oeprates a state machine.
+ * until the value has been created.
  *
  * @param <T>
  */
 public abstract class ConcurrentInitialize<T> {
-	enum State {
-		/*
-		 * Initial state, the first one that detects the machine is in this
-		 * state must create the object
-		 */
-		INIT,
-		/*
-		 * The object is being created, block until errored or created
-		 */
-		CREATING,
-		/*
-		 * There is an object, just return it
-		 */
-		DONE,
-		/*
-		 * There was an error during creation, throw the error
-		 */
-		ERROR
-	};
-
-	private State		state	= State.INIT;
-	private T			value;
-	private Object		lock	= new Object();
-	private Thread		creatingThread;
-	private Exception	exception;
+	private final AtomicBoolean		initializer	= new AtomicBoolean(false);
+	private final CountDownLatch	resolved	= new CountDownLatch(1);
+	private Thread					creator;
+	private T						value;
+	private Exception				exception;
 
 	/**
 	 * Get the value or wait until it is created.
 	 */
 	public T get() throws Exception {
-		synchronized (lock) {
-			switch (state) {
-				case INIT :
-					state = State.CREATING;
-					creatingThread = Thread.currentThread();
-					break;
-
-				case CREATING : {
-					if (creatingThread == Thread.currentThread())
-						throw new IllegalStateException(
-							"Cycle:  ConcurrentInitialize's create returns to same instance");
-					do {
-						lock.wait();
-					} while (state == State.CREATING);
-
-					if (state == State.ERROR)
-						throw exception;
-					return value;
-				}
-
-				case ERROR :
-					throw exception;
-
-				case DONE :
-					return value;
+		if (initializer.compareAndSet(false, true)) {
+			creator = Thread.currentThread();
+			try {
+				value = create();
+			} catch (Exception e) {
+				exception = e;
+			} finally {
+				creator = null;
+				resolved.countDown();
 			}
+		} else {
+			Thread t = creator;
+			if ((t != null) && (t == Thread.currentThread())) {
+				throw new IllegalStateException("Cycle: ConcurrentInitialize's create returns to same instance");
+			}
+			resolved.await();
 		}
-		try {
-			set(create(), null, State.DONE);
-			return value;
-		} catch (Exception e) {
-			set(null, e, State.ERROR);
-			throw e;
-		}
-	}
 
-	private void set(T value, Exception e, State state) {
-		synchronized (lock) {
-			this.value = value;
-			this.state = state;
-			this.creatingThread = null;
-			this.lock.notifyAll();
-			this.exception = e;
+		if (exception == null) {
+			return value;
 		}
+		throw exception;
 	}
 
 	/**
