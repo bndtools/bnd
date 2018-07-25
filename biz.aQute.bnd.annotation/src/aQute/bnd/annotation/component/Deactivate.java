9fa77a2d96dca8b9598f85e03ed2d8c6011477aa
Peter Kriens <peter.kriens@aqute.biz>
2018-07-25 15:15:33 +0200

    [compatibility] Restore some of compatibility
    - Added FixedIndexedRepo that was removed in R4, now extends OSGiRepository which was designed plug compatible. Gives a warning
    – Deprecated bnd annotations
    – Better diagnostics for the Service-Component header
    
    
    Signed-off-by: Peter Kriens <peter.kriens@aqute.biz>

--- biz.aQute.bnd.annotation/src/aQute/bnd/annotation/component/Deactivate.java
+++ biz.aQute.bnd.annotation/src/aQute/bnd/annotation/component/Deactivate.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Deprecated
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.METHOD)
 public @interface Deactivate {
---
+++ biz.aQute.bndlib.tests/src/test/deprecatedbndann/component/AnnotatedComponent2.java
@@ -0,0 +1,9 @@
+package test.deprecatedbndann.component;
+
+import aQute.bnd.annotation.component.Component;
+
+@SuppressWarnings("deprecation")
+@Component
+public class AnnotatedComponent2 {
+
+}
