b42653b574a050c9ad9ca9b2c871a7fe5cd0f6a1
BJ Hargrave <bj@bjhargrave.com>
2018-08-15 17:26:15 -0400

    clazz: Fix list of QUERY types which expect an argument
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Clazz.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Clazz.java
@@ -213,17 +213,8 @@ public class Clazz {
 	}
 
 	public final static EnumSet<QUERY>	HAS_ARGUMENT	= EnumSet.of(QUERY.IMPLEMENTS, QUERY.EXTENDS, QUERY.IMPORTS,
-		QUERY.NAMED, QUERY.VERSION, QUERY.ANNOTATED, QUERY.INDIRECTLY_ANNOTATED);
-
-	/**
-	 * <pre>
-	 *  ACC_PUBLIC 0x0001 Declared public; may be accessed from outside its
-	 * package. ACC_FINAL 0x0010 Declared final; no subclasses allowed.
-	 * ACC_SUPER 0x0020 Treat superclass methods specially when invoked by the
-	 * invokespecial instruction. ACC_INTERFACE 0x0200 Is an interface, not a
-	 * class. ACC_ABSTRACT 0x0400 Declared abstract; may not be instantiated.
-	 * </pre>
-	 */
+		QUERY.NAMED, QUERY.VERSION, QUERY.ANNOTATED, QUERY.INDIRECTLY_ANNOTATED, QUERY.HIERARCHY_ANNOTATED,
+		QUERY.HIERARCHY_INDIRECTLY_ANNOTATED);
 
 	// Declared public; may be accessed from outside its package.
 	final static int					ACC_PUBLIC		= 0x0001;
