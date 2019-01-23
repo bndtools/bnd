b00438f5c261a13ae31f9d1d4ae5c5f37ea594d0
Raymond Auge <raymond.auge@liferay.com>
2019-01-23 09:20:27 -0500

    [maven] unify formatting with other bundles in workspace
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- biz.aQute.bnd.maven/src/aQute/bnd/maven/lib/resolve/Operation.java
+++ biz.aQute.bnd.maven/src/aQute/bnd/maven/lib/resolve/Operation.java
@@ -6,15 +6,15 @@ import biz.aQute.resolve.Bndrun;
 
 @FunctionalInterface
 public interface Operation {
-    /**
-     * Performs this operation on the given arguments.
-     *
-     * @param bndrunFile the bndrun file to operate on
-     * @param taskname name of the task performing the operation
-     * @param bndrun the {@code Bndrun} instance created for the operation
-     * @return the number of errors
-     * @throws Exception
-     */
-    int apply(File bndrunFile, String taskname, Bndrun bndrun) throws Exception;
+	/**
+	 * Performs this operation on the given arguments.
+	 *
+	 * @param bndrunFile the bndrun file to operate on
+	 * @param taskname name of the task performing the operation
+	 * @param bndrun the {@code Bndrun} instance created for the operation
+	 * @return the number of errors
+	 * @throws Exception
+	 */
+	int apply(File bndrunFile, String taskname, Bndrun bndrun) throws Exception;
 
 }
