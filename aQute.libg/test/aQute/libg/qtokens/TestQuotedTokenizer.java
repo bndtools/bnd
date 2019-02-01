3084cada5abd919eaa6b6130df399096fb2e5971
BJ Hargrave <bj@bjhargrave.com>
2019-02-01 17:03:12 -0500

    qtokens: Add option to retain all quotes
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- aQute.libg/test/aQute/libg/qtokens/TestQuotedTokenizer.java
+++ aQute.libg/test/aQute/libg/qtokens/TestQuotedTokenizer.java
@@ -90,6 +90,15 @@ public class TestQuotedTokenizer extends TestCase {
 		assertEquals("'y", s);
 	}
 
+	public void testRetainQuotes() {
+		QuotedTokenizer qt = new QuotedTokenizer(" \" foo \", 'bar' ", ",", false, true);
+		assertThat(qt.nextToken()).isEqualTo("\" foo \"");
+		assertThat(qt.nextToken()).isEqualTo("'bar'");
+		qt = new QuotedTokenizer(" \" foo \", 'bar' ", ",", false, false);
+		assertThat(qt.nextToken()).isEqualTo(" foo ");
+		assertThat(qt.nextToken()).isEqualTo("bar");
+	}
+
 	public void testLongEscapedQuote() {
 		QuotedTokenizer qt = new QuotedTokenizer(
 			"\"{\\\":configurator:resource-version\\\": 1, \\\":configurator:version\\\":\\\"1\\\", \\\":configurator:symbolic-name\\\":\\\"dummy\\\", \\\"org.apache.aries.rsa.discovery.zookeeper\\\" : {}, \\\"org.apache.aries.rsa.discovery.zookeeper.server\\\" : {}}\",'{\":configurator:resource-version\": 1, \":configurator:version\":\"1\", \":configurator:symbolic-name\":\"dummy\", \"org.apache.aries.rsa.discovery.zookeeper\" : {}, \"org.apache.aries.rsa.discovery.zookeeper.server\" : {}}'",
