d8a231c9dd979551d53bbc170bdc2ff699d6ffe2
BJ Hargrave <bj@bjhargrave.com>
2019-02-15 15:25:45 -0500

    exceptions: Add unrollCause methods
    These can be used as a consistent way of unrolling a cause chain to some
    root cause.
    
    Also, add tests for the package.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- aQute.libg/src/aQute/lib/exceptions/Exceptions.java
+++ aQute.libg/src/aQute/lib/exceptions/Exceptions.java
@@ -1,5 +1,7 @@
 package aQute.lib.exceptions;
 
+import static java.util.Objects.requireNonNull;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.StringJoiner;
@@ -21,6 +23,18 @@ public class Exceptions {
 		StringWriter sw = new StringWriter();
 		t.printStackTrace(new PrintWriter(sw));
 		return sw.toString();
+	}
+
+	public static Throwable unrollCause(Throwable t, Class<? extends Throwable> unrollType) {
+		requireNonNull(t);
+		for (Throwable cause; unrollType.isInstance(t) && ((cause = t.getCause()) != null);) {
+			t = cause;
+		}
+		return t;
+	}
+
+	public static Throwable unrollCause(Throwable t) {
+		return unrollCause(t, Throwable.class);
 	}
 
 	public static String causes(Throwable t) {
