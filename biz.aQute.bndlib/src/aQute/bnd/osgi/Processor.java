cff6682ecbe68188b7455b472dd7a28689509a49
BJ Hargrave <bj@bjhargrave.com>
2019-02-25 16:34:49 -0500

    processor: abstract Domain.set method delegates to setProperty
    This means set will normalize well known keys.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/Processor.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/Processor.java
@@ -2296,7 +2296,7 @@ public class Processor extends Domain implements Reporter, Registry, Constants, 
 
 	@Override
 	public void set(String key, String value) {
-		getProperties().setProperty(key, value);
+		setProperty(key, value);
 	}
 
 	Stream<String> stream() {
