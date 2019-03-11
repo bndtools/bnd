1b78ed6be28705062628da1804545c0309b7febf
BJ Hargrave <bj@bjhargrave.com>
2019-03-11 13:49:09 -0400

    baseline: Fix package versions for baselining against 4.2 release
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

---
+++ biz.aQute.bndlib/src/aQute/bnd/build/package-info.java
@@ -0,0 +1,6 @@
+/**
+ */
+@Version("3.2.0")
+package aQute.bnd.build;
+
+import org.osgi.annotation.versioning.Version;
---
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/repository/package-info.java
@@ -0,0 +1,6 @@
+/**
+ */
+@Version("1.8.0")
+package aQute.bnd.osgi.repository;
+
+import org.osgi.annotation.versioning.Version;
