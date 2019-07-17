7c6d067a7d082569f04d3b1d39741342eb6060a8
Stefan Bischof <stbischof@bipolis.org>
2019-07-17 10:10:30 +0200

    typo

--- biz.aQute.bnd.reporter/src/biz/aQute/bnd/reporter/gogo/dto/GogoFunctionDTO.java
+++ biz.aQute.bnd.reporter/src/biz/aQute/bnd/reporter/gogo/dto/GogoFunctionDTO.java
@@ -7,12 +7,12 @@ import org.osgi.dto.DTO;
 public class GogoFunctionDTO extends DTO {
 
 	/**
-	 * Title of this gogo funktion.
+	 * Title of this gogo function.
 	 */
 	public String		title;
 
 	/**
-	 * List of methods that are provided by this funktion.
+	 * List of methods that are provided by this function.
 	 */
 	public List<GogoMethodDTO>	methods;
 }
