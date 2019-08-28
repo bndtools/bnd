c9d14b26d87dea1396cc7508d670edf817ff1b56
BJ Hargrave <bj@bjhargrave.com>
2019-08-28 16:31:44 -0400

    EPL 2: Changes usage of EPL 1 to EPL 2.
    See https://github.com/bndtools/bnd/issues/3346.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.builder/src/org/bndtools/builder/classpath/ClasspathContainerSerializationHelper.java
+++ bndtools.builder/src/org/bndtools/builder/classpath/ClasspathContainerSerializationHelper.java
@@ -1,14 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2008-2010 Sonatype, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *      Sonatype, Inc. - initial API and implementation
- *******************************************************************************/
-
 package org.bndtools.builder.classpath;
 
 import java.io.File;
