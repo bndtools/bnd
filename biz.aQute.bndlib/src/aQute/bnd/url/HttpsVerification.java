3709ac64e50c1ba35352d69fd2104153f56004ca
BJ Hargrave <bj@bjhargrave.com>
2019-10-01 12:37:50 -0400

    build: Remove Eclipse compiler warning
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/url/HttpsVerification.java
+++ biz.aQute.bndlib/src/aQute/bnd/url/HttpsVerification.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
