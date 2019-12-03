883656742c968c64bf1a9184c62f266134df737c
Peter Kriens <Peter.Kriens@aqute.biz>
2019-12-03 16:19:22 +0100

    [view] Bndtools Explorer
    A replacement for the package explorer in the
    bndtools perspective. The bndtools explorer
    has a filter field for projects. Filtering
    is only done on project name (glob), all its
    resources are then visible.
    
    
    
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

---
+++ bndtools.core/src/bndtools/explorer/BndtoolsExplorer.java
@@ -0,0 +1,71 @@
+package bndtools.explorer;
+
+import java.beans.PropertyChangeListener;
+
+import org.bndtools.utils.swt.FilterPanelPart;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.internal.core.JavaProject;
+import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+import aQute.lib.strings.Strings;
+import aQute.libg.glob.Glob;
+import bndtools.Plugin;
+
+public class BndtoolsExplorer extends PackageExplorerPart {
+	private final FilterPanelPart	filterPart	= new FilterPanelPart(Plugin.getDefault()
+		.getScheduler());
+	private PropertyChangeListener	listener;
+
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite c = new Composite(parent, SWT.NONE);
+		GridLayout sl = new GridLayout(1, true);
+		c.setLayout(sl);
+		filterPart.createControl(c);
+
+		super.createPartControl(c);
+		Control[] children = c.getChildren();
+		if (children.length > 1) {
+			children[1].setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		}
+		c.layout();
+
+		filterPart.setHint("Filter for projects (glob)");
+		listener = e -> {
+			String value = (String) e.getNewValue();
+
+			if (Strings.nonNullOrEmpty(value)) {
+				Glob glob = new Glob(value);
+				getTreeViewer().setFilters(new ViewerFilter() {
+
+					@Override
+					public boolean select(Viewer viewer, Object parentElement, Object element) {
+						if (element instanceof JavaProject) {
+							IJavaProject project = (JavaProject) element;
+							String name = project.getElementName();
+							return glob.finds(name) >= 0;
+
+						} else
+							return true;
+					}
+				});
+			} else {
+				getTreeViewer().setFilters();
+			}
+		};
+		filterPart.addPropertyChangeListener(listener);
+	}
+
+	@Override
+	public void dispose() {
+		filterPart.removePropertyChangeListener(listener);
+		super.dispose();
+	}
+}
