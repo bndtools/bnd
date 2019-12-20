674e5b43269605baa36e9a00327cbcc0a62f6b96
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2019-12-20 18:53:41 +1030

    [junit-platform tester] Check for mixed JUnit 3 and 4 tests
    Will check classes for mixed JUnit 3 and 4 tests and generate a test error for each one it finds.
    
    Implements #3671.
    
    Signed-off-by: Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

---
+++ biz.aQute.tester.test/src/org/junit/venus/Test.java
@@ -0,0 +1,13 @@
+package org.junit.venus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+	ElementType.METHOD
+})
+public @interface Test {
+}
