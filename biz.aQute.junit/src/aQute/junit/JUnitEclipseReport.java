8749531a5cd168abd4f269ffebd695f439086d7c
BJ Hargrave <bj@bjhargrave.com>
2019-12-24 19:15:23 -0500

    junit: Updates to the Eclipse reporting
    Some updates I made while reviewing/learning the code trying to see if
    I could figure out https://github.com/bndtools/bnd/issues/3559. These
    changes are improvements, but they did not appear to solve the issue.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.junit/src/aQute/junit/JUnitEclipseReport.java
+++ biz.aQute.junit/src/aQute/junit/JUnitEclipseReport.java
@@ -3,9 +3,12 @@ package aQute.junit;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -19,31 +22,65 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class JUnitEclipseReport implements TestReporter {
-	private final BufferedReader	in;
-	private final PrintWriter		out;
-	private long					startTime;
-	private List<Test>				tests;
-	private boolean					verbose	= false;
+	private long							startTime;
+	private List<Test>						tests;
+	private final boolean					verbose	= false;
+	private Connection<Reader, PrintWriter>	junit;
+
+	private static final class Connection<IN extends Closeable, OUT extends Closeable> implements Closeable {
+		final Socket	sock;
+		final IN		in;
+		final OUT		out;
+
+		Connection(Socket sock, IN in, OUT out) {
+			this.sock = sock;
+			this.in = in;
+			this.out = out;
+		}
+
+		@Override
+		public void close() {
+			safeClose(out);
+			safeClose(in);
+			safeClose(sock);
+		}
+	}
+
+	static void safeClose(Closeable closeable) {
+		if (closeable == null) {
+			return;
+		}
+		try {
+			closeable.close();
+		} catch (IOException e) {}
+	}
 
 	public JUnitEclipseReport(int port) throws Exception {
-		Socket socket = null;
+		try {
+			Socket socket = connectRetry(port);
+			junit = new Connection<>(socket, new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8)),
+				new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8), true));
+		} catch (ConnectException e) {
+			System.err.println("Cannot open the JUnit Port: " + port + " " + e);
+			System.exit(254);
+			throw new AssertionError("unreachable");
+		}
+	}
+
+	private Socket connectRetry(int port) throws Exception {
 		ConnectException e = null;
-		for (int i = 0; socket == null && i < 30; i++) {
+		for (int i = 0; i < 30; i++) {
 			try {
-				socket = new Socket(InetAddress.getByName(null), port);
+				return new Socket(InetAddress.getByName(null), port);
 			} catch (ConnectException ce) {
 				e = ce;
 				Thread.sleep(i * 100);
 			}
 		}
-		if (socket == null) {
-			System.err.println("Cannot open the JUnit Port: " + port + " " + e);
-			System.exit(254);
-			throw new AssertionError("unreachable");
+		if (e != null) {
+			throw e;
 		}
-
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
-		out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8));
+		return null;
 	}
 
 	@Override
@@ -52,16 +89,17 @@ public class JUnitEclipseReport implements TestReporter {
 	@Override
 	public void begin(List<Test> tests, int realcount) {
 		this.tests = tests;
-		message("%TESTC  ", realcount + " v2");
+		message("%TESTC  ", Integer.toString(realcount)
+			.concat(" v2"));
 		report(tests);
 		startTime = System.currentTimeMillis();
 	}
 
 	@Override
 	public void end() {
-		message("%RUNTIME", "" + (System.currentTimeMillis() - startTime));
-		out.flush();
-		out.close();
+		message("%RUNTIME", Long.toString(System.currentTimeMillis() - startTime));
+		junit.out.flush();
+		safeClose(junit);
 	}
 
 	@Override
@@ -78,8 +116,11 @@ public class JUnitEclipseReport implements TestReporter {
 
 	void trace(Throwable t) {
 		message("%TRACES ", "");
-		t.printStackTrace(out);
-		out.println();
+		t.printStackTrace(junit.out);
+		if (verbose) {
+			t.printStackTrace(System.err);
+		}
+		junit.out.println();
 		message("%TRACEE ", "");
 	}
 
@@ -97,37 +138,39 @@ public class JUnitEclipseReport implements TestReporter {
 		if (key.length() != 8)
 			throw new IllegalArgumentException(key + " is not 8 characters");
 
-		out.print(key);
-		out.println(payload);
-		out.flush();
+		String message = key.concat(payload);
+		junit.out.println(message);
 		if (verbose)
-			System.err.println(key + payload);
+			System.err.println(message);
+	}
+
+	private String getTestName(Test test) {
+		return String.valueOf(test);
 	}
 
 	private void message(String key, Test test) {
-		if (tests == null)
-			message(key, "?," + test);
-		else
-			message(key, (tests.indexOf(test) + 1) + "," + test);
+		StringBuilder sb = new StringBuilder();
+		if (tests == null) {
+			sb.append('?');
+		} else {
+			sb.append(tests.indexOf(test) + 1);
+		}
+		sb.append(',');
+		copyAndEscapeText(getTestName(test), sb);
+		String payload = sb.toString();
+		message(key, payload);
 	}
 
 	private void report(List<Test> flattened) {
 		for (int i = 0; i < flattened.size(); i++) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(i + 1);
-			sb.append(",");
 			Test test = flattened.get(i);
-			if (test instanceof TestSuite || test instanceof JUnit4TestAdapter) {
-				sb.append(test);
-				sb.append(",");
-				sb.append(true);
-			} else {
-				sb.append(test);
-				sb.append(",");
-				sb.append(false);
-
-			}
-			sb.append(",");
+			StringBuilder sb = new StringBuilder();
+			sb.append(i + 1)
+				.append(',');
+			copyAndEscapeText(getTestName(test), sb);
+			sb.append(',')
+				.append(test instanceof TestSuite || test instanceof JUnit4TestAdapter)
+				.append(',');
 			if (test instanceof JUnit4TestAdapter) {
 				sb.append(((JUnit4TestAdapter) test).getTests()
 					.size());
@@ -144,10 +187,39 @@ public class JUnitEclipseReport implements TestReporter {
 	}
 
 	public void close() {
-		try {
-			in.close();
-		} catch (Exception ioe) {
-			// ignore
+		safeClose(junit);
+	}
+
+	private static void copyAndEscapeText(String s, StringBuilder sb) {
+		if (s.isEmpty()) {
+			return;
+		}
+		if ((s.indexOf(',') < 0) && (s.indexOf('\\') < 0) && (s.indexOf('\r') < 0) && (s.indexOf('\n') < 0)) {
+			sb.append(s);
+			return;
+		}
+		for (int i = 0, len = s.length(); i < len; i++) {
+			char c = s.charAt(i);
+			switch (c) {
+				case '\r' :
+					int j;
+					if ((j = i + 1) < len && s.charAt(j) == '\n') {
+						i = j;
+					}
+					sb.append(' ');
+					break;
+				case '\n' :
+					sb.append(' ');
+					break;
+				case ',' :
+				case '\\' :
+					sb.append('\\');
+					sb.append(c);
+					break;
+				default :
+					sb.append(c);
+					break;
+			}
 		}
 	}
 
