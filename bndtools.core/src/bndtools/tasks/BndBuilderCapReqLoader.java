69a593f862fc050181b578ae5315163a12bbce09
BJ Hargrave <bj@bjhargrave.com>
2020-02-10 17:22:14 -0500

    resolution view: Change to use SyntheticBuilder to generate caps/reqs
    Replace code doing its own cap/req parsing with using the
    SyntheticBuilder. This centralizes cap/req parsing into a single place.
    
    Fixes https://github.com/bndtools/bnd/issues/3760
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.core/src/bndtools/tasks/BndBuilderCapReqLoader.java
+++ bndtools.core/src/bndtools/tasks/BndBuilderCapReqLoader.java
@@ -1,44 +1,35 @@
 package bndtools.tasks;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+
 import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
-import org.bndtools.utils.osgi.BundleUtils;
-import org.osgi.framework.Constants;
-import org.osgi.framework.namespace.AbstractWiringNamespace;
-import org.osgi.framework.namespace.BundleNamespace;
-import org.osgi.framework.namespace.HostNamespace;
-import org.osgi.framework.namespace.IdentityNamespace;
 import org.osgi.framework.namespace.PackageNamespace;
 import org.osgi.resource.Capability;
-import org.osgi.resource.Namespace;
+import org.osgi.resource.Requirement;
+import org.osgi.resource.Resource;
 
-import aQute.bnd.header.Attrs;
-import aQute.bnd.header.Parameters;
 import aQute.bnd.osgi.Builder;
 import aQute.bnd.osgi.Clazz;
+import aQute.bnd.osgi.Domain;
 import aQute.bnd.osgi.Jar;
-import aQute.bnd.osgi.Processor;
-import aQute.bnd.osgi.resource.CapReqBuilder;
-import aQute.bnd.version.VersionRange;
-import aQute.libg.filters.AndFilter;
-import aQute.libg.filters.Filter;
-import aQute.libg.filters.NotFilter;
-import aQute.libg.filters.Operator;
-import aQute.libg.filters.SimpleFilter;
+import aQute.bnd.osgi.resource.SyntheticBuilder;
+import aQute.lib.exceptions.Exceptions;
 import bndtools.model.resolution.RequirementWrapper;
 
 public abstract class BndBuilderCapReqLoader implements CapReqLoader {
 
-	protected final File file;
+	protected final File							file;
+	private Map<String, List<Capability>>			loadCapabilities;
+	private Map<String, List<RequirementWrapper>>	loadRequirements;
 
 	public BndBuilderCapReqLoader(File file) {
 		this.file = file;
@@ -57,215 +48,76 @@ public abstract class BndBuilderCapReqLoader implements CapReqLoader {
 
 	protected abstract Builder getBuilder() throws Exception;
 
-	@Override
-	public Map<String, List<Capability>> loadCapabilities() throws Exception {
+	private void load() throws Exception {
+		if ((loadCapabilities != null) && (loadRequirements != null)) {
+			return;
+		}
+
 		Builder builder = getBuilder();
-		if (builder == null)
-			return Collections.emptyMap();
+		if (builder == null) {
+			loadCapabilities = Collections.emptyMap();
+			loadRequirements = Collections.emptyMap();
+			return;
+		}
 
 		Jar jar = builder.getJar();
-		if (jar == null)
-			return Collections.emptyMap();
+		if (jar == null) {
+			loadCapabilities = Collections.emptyMap();
+			loadRequirements = Collections.emptyMap();
+			return;
+		}
 
 		Manifest manifest = jar.getManifest();
-		if (manifest == null)
-			return Collections.emptyMap();
-
-		Attributes attribs = manifest.getMainAttributes();
-		Map<String, List<Capability>> capMap = new HashMap<>();
-
-		// Load export packages
-		String exportsPkgStr = attribs.getValue(Constants.EXPORT_PACKAGE);
-		Parameters exportsMap = new Parameters(exportsPkgStr);
-		for (Entry<String, Attrs> entry : exportsMap.entrySet()) {
-			String pkg = Processor.removeDuplicateMarker(entry.getKey());
-			org.osgi.framework.Version version = org.osgi.framework.Version.parseVersion(entry.getValue()
-				.getVersion());
-			CapReqBuilder cb = new CapReqBuilder(PackageNamespace.PACKAGE_NAMESPACE)
-				.addAttribute(PackageNamespace.PACKAGE_NAMESPACE, pkg)
-				.addAttribute(PackageNamespace.CAPABILITY_VERSION_ATTRIBUTE, version);
-			// TODO attributes and directives
-			addCapability(capMap, cb.buildSyntheticCapability());
+		if (manifest == null) {
+			loadCapabilities = Collections.emptyMap();
+			loadRequirements = Collections.emptyMap();
+			return;
 		}
 
-		// Load identity/bundle/host
-		String bsn = BundleUtils.getBundleSymbolicName(attribs);
-		if (bsn != null) { // Ignore if not a bundle
-			org.osgi.framework.Version version = org.osgi.framework.Version
-				.parseVersion(attribs.getValue(Constants.BUNDLE_VERSION));
-			// TODO attributes and directives
-			addCapability(capMap,
-				new CapReqBuilder(IdentityNamespace.IDENTITY_NAMESPACE)
-					.addAttribute(IdentityNamespace.IDENTITY_NAMESPACE, bsn)
-					.addAttribute(IdentityNamespace.CAPABILITY_VERSION_ATTRIBUTE, version)
-					.buildSyntheticCapability());
-			addCapability(capMap,
-				new CapReqBuilder(BundleNamespace.BUNDLE_NAMESPACE).addAttribute(BundleNamespace.BUNDLE_NAMESPACE, bsn)
-					.addAttribute(AbstractWiringNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, version)
-					.buildSyntheticCapability());
-			addCapability(capMap,
-				new CapReqBuilder(HostNamespace.HOST_NAMESPACE).addAttribute(HostNamespace.HOST_NAMESPACE, bsn)
-					.addAttribute(AbstractWiringNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE, version)
-					.buildSyntheticCapability());
-		}
+		SyntheticBuilder rb = new SyntheticBuilder();
+		rb.addManifest(Domain.domain(manifest));
+		Resource resource = rb.build();
 
-		// Generic capabilities
-		String providesStr = attribs.getValue(Constants.PROVIDE_CAPABILITY);
-		Parameters provides = new Parameters(providesStr);
-		for (Entry<String, Attrs> entry : provides.entrySet()) {
-			String ns = Processor.removeDuplicateMarker(entry.getKey());
-			Attrs attrs = entry.getValue();
+		List<Capability> capabilities = resource.getCapabilities(null);
+		loadCapabilities = capabilities.stream()
+			.collect(groupingBy(Capability::getNamespace, toList()));
 
-			CapReqBuilder cb = new CapReqBuilder(ns);
-			for (String key : attrs.keySet()) {
-				if (key.endsWith(":"))
-					cb.addDirective(key.substring(0, key.length() - 1), attrs.get(key));
-				else
-					cb.addAttribute(key, attrs.getTyped(key));
-			}
-			addCapability(capMap, cb.buildSyntheticCapability());
-		}
+		List<Requirement> requirements = resource.getRequirements(null);
+		loadRequirements = requirements.stream()
+			.collect(groupingBy(Requirement::getNamespace, mapping(this::toRequirementWrapper, toList())));
+	}
 
-		return capMap;
+	@Override
+	public Map<String, List<Capability>> loadCapabilities() throws Exception {
+		load();
+		return loadCapabilities;
 	}
 
 	@Override
 	public Map<String, List<RequirementWrapper>> loadRequirements() throws Exception {
-		Builder builder = getBuilder();
-		if (builder == null)
-			return Collections.emptyMap();
+		load();
+		return loadRequirements;
+	}
 
-		Jar jar = builder.getJar();
-		if (jar == null)
-			return Collections.emptyMap();
-		Manifest manifest = jar.getManifest();
-		if (manifest == null)
-			return Collections.emptyMap();
-
-		Attributes attribs = manifest.getMainAttributes();
-		Map<String, List<RequirementWrapper>> requirements = new HashMap<>();
-
-		// Process imports
-		String importPkgStr = attribs.getValue(Constants.IMPORT_PACKAGE);
-		Parameters importsMap = new Parameters(importPkgStr);
-		for (Entry<String, Attrs> entry : importsMap.entrySet()) {
-			String pkgName = Processor.removeDuplicateMarker(entry.getKey());
-			Attrs attrs = entry.getValue();
-
-			CapReqBuilder rb = new CapReqBuilder(PackageNamespace.PACKAGE_NAMESPACE);
-			String filter = createVersionFilter(PackageNamespace.PACKAGE_NAMESPACE, pkgName,
-				attrs.get(Constants.VERSION_ATTRIBUTE), PackageNamespace.CAPABILITY_VERSION_ATTRIBUTE);
-			rb.addDirective(Namespace.REQUIREMENT_FILTER_DIRECTIVE, filter);
-			if (Constants.RESOLUTION_OPTIONAL.equals(attrs.get(Constants.RESOLUTION_DIRECTIVE + ":")))
-				rb.addDirective(Namespace.REQUIREMENT_RESOLUTION_DIRECTIVE, Namespace.RESOLUTION_OPTIONAL);
-
-			Collection<Clazz> importers = findImportingClasses(pkgName, builder);
-
-			RequirementWrapper rw = new RequirementWrapper();
-			rw.requirement = rb.buildSyntheticRequirement();
-			rw.requirers = importers;
-
-			addRequirement(requirements, rw);
-		}
-
-		// Process require-bundle
-		String requireBundleStr = attribs.getValue(Constants.REQUIRE_BUNDLE);
-		Parameters requireBundles = new Parameters(requireBundleStr);
-		for (Entry<String, Attrs> entry : requireBundles.entrySet()) {
-			String bsn = Processor.removeDuplicateMarker(entry.getKey());
-			Attrs attrs = entry.getValue();
-
-			CapReqBuilder rb = new CapReqBuilder(BundleNamespace.BUNDLE_NAMESPACE);
-			String filter = createVersionFilter(BundleNamespace.BUNDLE_NAMESPACE, bsn,
-				attrs.get(Constants.BUNDLE_VERSION_ATTRIBUTE),
-				AbstractWiringNamespace.CAPABILITY_BUNDLE_VERSION_ATTRIBUTE);
-			rb.addDirective(Namespace.REQUIREMENT_FILTER_DIRECTIVE, filter);
-			if (Constants.RESOLUTION_OPTIONAL.equals(attrs.get(Constants.RESOLUTION_DIRECTIVE + ":")))
-				rb.addDirective(Namespace.REQUIREMENT_RESOLUTION_DIRECTIVE, Namespace.RESOLUTION_OPTIONAL);
-
-			RequirementWrapper rw = new RequirementWrapper();
-			rw.requirement = rb.buildSyntheticRequirement();
-			addRequirement(requirements, rw);
-		}
-
-		// Process generic requires
-		String requiresStr = attribs.getValue(Constants.REQUIRE_CAPABILITY);
-		Parameters requires = new Parameters(requiresStr);
-		for (Entry<String, Attrs> entry : requires.entrySet()) {
-			String ns = Processor.removeDuplicateMarker(entry.getKey());
-			Attrs attrs = entry.getValue();
-
-			CapReqBuilder rb = new CapReqBuilder(ns);
-			for (String key : attrs.keySet()) {
-				if (key.endsWith(":"))
-					rb.addDirective(key.substring(0, key.length() - 1), attrs.get(key));
-				else
-					rb.addAttribute(key, attrs.getTyped(key));
+	private RequirementWrapper toRequirementWrapper(Requirement req) {
+		RequirementWrapper rw = new RequirementWrapper();
+		rw.requirement = req;
+		if (req.getNamespace()
+			.equals(PackageNamespace.PACKAGE_NAMESPACE)) {
+			String pkgName = (String) req.getAttributes()
+				.get(PackageNamespace.PACKAGE_NAMESPACE);
+			try {
+				rw.requirers = findImportingClasses(pkgName);
+			} catch (Exception e) {
+				throw Exceptions.duck(e);
 			}
-
-			RequirementWrapper rw = new RequirementWrapper();
-			rw.requirement = rb.buildSyntheticRequirement();
-			addRequirement(requirements, rw);
 		}
-
-		return requirements;
+		return rw;
 	}
 
-	private static void addCapability(Map<String, List<Capability>> capMap, Capability cap) {
-		List<Capability> capsForNs = capMap.get(cap.getNamespace());
-		if (capsForNs == null) {
-			capsForNs = new LinkedList<>();
-			capMap.put(cap.getNamespace(), capsForNs);
-		}
-		capsForNs.add(cap);
-	}
-
-	private static void addRequirement(Map<String, List<RequirementWrapper>> requirements, RequirementWrapper req) {
-		List<RequirementWrapper> listForNs = requirements.get(req.requirement.getNamespace());
-		if (listForNs == null) {
-			listForNs = new LinkedList<>();
-			requirements.put(req.requirement.getNamespace(), listForNs);
-		}
-		listForNs.add(req);
-	}
-
-	private static final String createVersionFilter(String ns, String value, String rangeStr, String versionAttr) {
-		SimpleFilter pkgNameFilter = new SimpleFilter(ns, value);
-
-		Filter filter = pkgNameFilter;
-		if (rangeStr != null) {
-			VersionRange range = new VersionRange(rangeStr);
-
-			Filter left;
-			if (range.includeLow())
-				left = new SimpleFilter(versionAttr, Operator.GreaterThanOrEqual, range.getLow()
-					.toString());
-			else
-				left = new NotFilter(new SimpleFilter(versionAttr, Operator.LessThanOrEqual, range.getLow()
-					.toString()));
-
-			Filter right;
-			if (!range.isRange())
-				right = null;
-			else if (range.includeHigh())
-				right = new SimpleFilter(versionAttr, Operator.LessThanOrEqual, range.getHigh()
-					.toString());
-			else
-				right = new NotFilter(new SimpleFilter(versionAttr, Operator.GreaterThanOrEqual, range.getHigh()
-					.toString()));
-
-			AndFilter combined = new AndFilter().addChild(pkgNameFilter)
-				.addChild(left);
-			if (right != null)
-				combined.addChild(right);
-			filter = combined;
-		}
-		return filter.toString();
-	}
-
-	static List<Clazz> findImportingClasses(String pkgName, Builder builder) throws Exception {
+	private List<Clazz> findImportingClasses(String pkgName) throws Exception {
 		List<Clazz> classes = new LinkedList<>();
-		Collection<Clazz> importers = builder.getClasses("", "IMPORTING", pkgName);
+		Collection<Clazz> importers = getBuilder().getClasses("", "IMPORTING", pkgName);
 
 		// Remove *this* package
 		for (Clazz clazz : importers) {
