9367a4eeda9f618ab1338bcb280db0793bce14de
Peter Kriens <Peter.Kriens@aqute.biz>
2020-03-17 17:56:08 +0100

    [import quickfix] Replaced previous Quickfix
        I am greatly thankful for the previous version of
        the quick fix because I'd never figured the eclipse
        part out.
        
        I've taken this part and moved any part that did
        stuff that  should be in bnd to bnd. This way,
        others can leverage this code. Support was
        added in Workspace and the BndEditModel to
        do this kind of stuff easier.
        
        Therefore, the deletion of the old classes is
        no criticism. Very happy I had that. That said,
        Fr. Krieg needs to take a good look I got all the
        functionality. I could not find good Eclipse
        documentation what one could expect for each of the
        error codes.
        
        In the bug, it is indicated that there was also
        a completion list. This did not work for me, and
        I could also not really see why I never got triggered.
    
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

---
+++ bndtools.core/src/org/bndtools/core/editors/quickfix/AddBundleCompletionProposal.java
@@ -0,0 +1,142 @@
+package org.bndtools.core.editors.quickfix;
+
+import org.bndtools.core.ui.icons.Icons;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.ui.text.java.IInvocationContext;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+
+import aQute.bnd.build.Project;
+import aQute.bnd.build.model.BndEditModel;
+import aQute.bnd.osgi.BundleId;
+import aQute.bnd.osgi.Constants;
+import aQute.bnd.osgi.Descriptors;
+import aQute.lib.strings.Strings;
+import bndtools.Plugin;
+import bndtools.central.Central;
+
+class AddBundleCompletionProposal extends WorkspaceJob implements IJavaCompletionProposal {
+
+	final BundleId				bundle;
+	final String				displayString;
+	final int					relevance;
+	final IInvocationContext	context;
+	final Project				project;
+	final String				pathtype;
+	final String				fqn;
+
+	public AddBundleCompletionProposal(String fqn, BundleId bundle, int relevance, IInvocationContext context,
+		Project project, String pathtype) {
+		super("Adding '" + bundle + "' to " + project + " " + pathtype);
+		this.fqn = fqn;
+		this.bundle = bundle;
+		this.relevance = relevance;
+		this.context = context;
+		this.project = project;
+		this.pathtype = pathtype;
+		this.displayString = Strings.format("Add %s %s to %s (found %s)", bundle.getBsn(), bundle.getShortVersion(),
+			pathtype, fqn);
+	}
+
+	@Override
+	public void apply(org.eclipse.jface.text.IDocument document) {
+		schedule();
+	}
+
+	/**
+	 * @see org.eclipse.jface.text.contentassist.ICompletionProposal#getSelection(org.eclipse.jface.text.IDocument)
+	 */
+	@Override
+	public Point getSelection(org.eclipse.jface.text.IDocument document) {
+		return new Point(context.getSelectionOffset(), context.getSelectionLength());
+	}
+
+	@Override
+	public String getAdditionalProposalInfo() {
+		return displayString;
+	}
+
+	@Override
+	public String getDisplayString() {
+		return displayString;
+	}
+
+	@Override
+	public Image getImage() {
+		return Icons.image("bundle");
+	}
+
+	@Override
+	public IContextInformation getContextInformation() {
+		return new IContextInformation() {
+
+			@Override
+			public String getContextDisplayString() {
+				return "Added " + bundle + " to build path";
+			}
+
+			@Override
+			public Image getImage() {
+				return Icons.image("bundle");
+			}
+
+			@Override
+			public String getInformationDisplayString() {
+				return "Added " + bundle + " to build path - info";
+			}
+
+		};
+	}
+
+	@Override
+	public int getRelevance() {
+		return relevance;
+	}
+
+	@Override
+	public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+		try {
+			IStatus status = Central.bndCall(() -> {
+				BndEditModel model = new BndEditModel(project);
+				model.load();
+
+				switch (pathtype) {
+					case Constants.TESTPATH :
+						model.addPath(bundle, Constants.TESTPATH);
+						break;
+
+					case Constants.BUILDPATH :
+					default :
+						model.addPath(bundle, Constants.BUILDPATH);
+						break;
+				}
+				model.saveChanges();
+				Central.refreshFile(project.getPropertiesFile());
+				return Status.OK_STATUS;
+			});
+
+			String[] determine = Descriptors.determine(fqn)
+				.unwrap();
+
+			assert determine[0] != null : "We must have found a package";
+
+			if (determine[1] == null) {
+				context.getCompilationUnit()
+					.createImport(fqn + ".*", null, monitor);
+			} else {
+				context.getCompilationUnit()
+					.createImport(fqn, null, monitor);
+			}
+			return status;
+		} catch (Exception e) {
+			throw new CoreException(
+				new Status(IStatus.ERROR, Plugin.PLUGIN_ID, "Failed to add bundle to -buildpath", e));
+		}
+	}
+}
