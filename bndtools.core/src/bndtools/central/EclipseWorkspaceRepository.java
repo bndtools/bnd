6bc9efe286aa77aeac4ccd9c192e594cf74b2268
BJ Hargrave <bj@bjhargrave.com>
2020-04-21 15:05:40 -0400

    workspacerepo: Remove use of .index.xml files
    We now always index through building Resources from the built bundles.
    This avoids the need to cache and staleness of that cache.
    
    We now defer, through the use of memoized suppliers, creating Resources
    until they are actually necessary. So until you need to resolve, we can
    defer this Resource creation. So this can significantly reduce Bndtools
    startup time since we do not need to parse .index.xml files or bundles.
    If you never use the resolver, we never need to index the workspace
    bundles.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.core/src/bndtools/central/EclipseWorkspaceRepository.java
+++ bndtools.core/src/bndtools/central/EclipseWorkspaceRepository.java
@@ -1,14 +1,15 @@
 package bndtools.central;
 
 import static aQute.lib.exceptions.FunctionWithException.asFunction;
+import static aQute.lib.exceptions.SupplierWithException.asSupplierOrElse;
 import static java.util.stream.Collectors.toList;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.text.MessageFormat;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,10 +17,7 @@ import java.util.function.Supplier;
 
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.osgi.resource.Capability;
 import org.osgi.resource.Requirement;
@@ -30,8 +28,6 @@ import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Constants;
 import aQute.bnd.osgi.repository.BaseRepository;
 import aQute.bnd.osgi.repository.ResourcesRepository;
-import aQute.bnd.osgi.repository.XMLResourceGenerator;
-import aQute.bnd.osgi.repository.XMLResourceParser;
 import aQute.bnd.osgi.resource.ResourceBuilder;
 import aQute.bnd.stream.MapStream;
 import aQute.lib.io.IO;
@@ -39,18 +35,17 @@ import aQute.lib.memoize.Memoize;
 import biz.aQute.resolve.WorkspaceRepositoryMarker;
 
 public class EclipseWorkspaceRepository extends BaseRepository implements WorkspaceRepositoryMarker {
-	private final static ILogger						logger			= Logger
+	private final static ILogger											logger	= Logger
 		.getLogger(EclipseWorkspaceRepository.class);
-	private static final String							NAME			= "Workspace";
-	private static final String							INDEX_FILENAME	= ".index.xml";
-	private final Map<IProject, Collection<Resource>>	repositories;
-	private volatile Supplier<ResourcesRepository>		repository;
+	private static final String												NAME	= "Workspace";
+	private final Map<IProject, Supplier<? extends Collection<Resource>>>	resources;
+	private volatile Supplier<ResourcesRepository>							repository;
 
 	/**
 	 * Can only be instantiated within the package.
 	 */
 	EclipseWorkspaceRepository() {
-		repositories = new ConcurrentHashMap<>();
+		resources = new ConcurrentHashMap<>();
 		repository = Memoize.supplier(this::aggregate);
 		Central.onWorkspace(this::setupProjects);
 	}
@@ -63,25 +58,16 @@ public class EclipseWorkspaceRepository extends BaseRepository implements Worksp
 			Project model = Central.getProject(project);
 			if (model != null) {
 				File target = getTarget(model);
-				File indexFile = new File(target, INDEX_FILENAME);
-				if (indexFile.isFile()) {
-					URI base = project.getLocation()
-						.toFile()
-						.toURI();
-					List<Resource> resources = XMLResourceParser.getResources(indexFile, base);
-					update(project, resources);
-					continue;
-				}
 				File buildfiles = new File(target, Constants.BUILDFILES);
 				if (buildfiles.isFile()) {
-					List<File> files;
-					try (BufferedReader rdr = IO.reader(buildfiles)) {
-						files = rdr.lines()
-							.map(line -> IO.getFile(target, line.trim()))
-							.filter(File::isFile)
-							.collect(toList());
-					}
-					index(project, files);
+					index(project, asSupplierOrElse(() -> {
+						try (BufferedReader rdr = IO.reader(buildfiles)) {
+							return rdr.lines()
+								.map(line -> IO.getFile(target, line.trim()))
+								.filter(File::isFile)
+								.collect(toList());
+						}
+					}, Collections.emptyList()));
 					continue;
 				}
 			}
@@ -101,43 +87,38 @@ public class EclipseWorkspaceRepository extends BaseRepository implements Worksp
 	}
 
 	public void index(IProject project, Collection<File> files) {
+		index(project, () -> files);
+	}
+
+	public void index(IProject project, Supplier<? extends Collection<File>> files) {
 		try {
+			resources.keySet()
+				.removeIf(p -> !p.isOpen());
 			Project model = Central.getProject(project);
-			String name = model.getName();
-			List<Resource> resources = files.stream()
-				.map(asFunction(file -> {
-					ResourceBuilder rb = new ResourceBuilder();
-					rb.addFile(file, file.toURI());
-					// Add a capability specific to the workspace so that we can
-					// identify this fact later during resource processing.
-					rb.addWorkspaceNamespace(name);
-					return rb.build();
-				}))
-				.collect(toList());
-
-			update(project, resources);
-
-			File indexFile = new File(model.getTarget(), INDEX_FILENAME);
-			XMLResourceGenerator xmlResourceGenerator = new XMLResourceGenerator().name(name)
-				.base(project.getLocation()
-					.toFile()
-					.toURI())
-				.resources(resources);
-			xmlResourceGenerator.save(indexFile);
-			IWorkspaceRoot wsroot = ResourcesPlugin.getWorkspace()
-				.getRoot();
-			IFile indexPath = wsroot.getFile(Central.toPath(indexFile));
-			indexPath.refreshLocal(IResource.DEPTH_ZERO, null);
-			if (indexPath.exists())
-				indexPath.setDerived(true, null);
+			if (model != null) {
+				resources.put(project, Memoize.supplier(indexer(model.getName(), files)));
+			} else {
+				resources.remove(project);
+			}
+			repository = Memoize.supplier(this::aggregate);
 		} catch (Exception e) {
 			logger.logError(MessageFormat.format("Failed to index bundles in project {0}.", project.getName()), e);
 		}
 	}
 
-	private void update(IProject project, Collection<Resource> resources) {
-		repositories.put(project, resources);
-		repository = Memoize.supplier(this::aggregate);
+	private Supplier<List<Resource>> indexer(String name, Supplier<? extends Collection<File>> files) {
+		return () -> files.get()
+			.stream()
+			.filter(File::isFile)
+			.map(asFunction(file -> {
+				ResourceBuilder rb = new ResourceBuilder();
+				rb.addFile(file, file.toURI());
+				// Add a capability specific to the workspace so that we can
+				// identify this fact later during resource processing.
+				rb.addWorkspaceNamespace(name);
+				return rb.build();
+			}))
+			.collect(toList());
 	}
 
 	@Override
@@ -148,9 +129,10 @@ public class EclipseWorkspaceRepository extends BaseRepository implements Worksp
 	}
 
 	private ResourcesRepository aggregate() {
-		ResourcesRepository aggregate = MapStream.of(repositories)
+		ResourcesRepository aggregate = MapStream.of(resources)
 			.filterKey(IProject::isOpen)
 			.values()
+			.map(Supplier::get)
 			.flatMap(Collection::stream)
 			.collect(ResourcesRepository.toResourcesRepository());
 		return aggregate;
