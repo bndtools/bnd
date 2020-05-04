fe7f27b89a5adb3b484de7a4a770ef751722f880
BJ Hargrave <bj@bjhargrave.com>
2020-05-04 08:46:36 -0400

    bndlib: Relocate WorkspaceRepositoryMarker to bndlib and exploit
    With the type in bndlib, we can directly reference the type.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- bndtools.core/src/bndtools/central/EclipseWorkspaceRepository.java
+++ bndtools.core/src/bndtools/central/EclipseWorkspaceRepository.java
@@ -17,9 +17,9 @@ import aQute.bnd.build.Project;
 import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Constants;
 import aQute.bnd.osgi.repository.AbstractIndexingRepository;
+import aQute.bnd.osgi.repository.WorkspaceRepositoryMarker;
 import aQute.bnd.osgi.resource.ResourceBuilder;
 import aQute.lib.io.IO;
-import biz.aQute.resolve.WorkspaceRepositoryMarker;
 
 public class EclipseWorkspaceRepository extends AbstractIndexingRepository<IProject> implements WorkspaceRepositoryMarker {
 	EclipseWorkspaceRepository() {
