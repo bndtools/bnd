716f07f267e98afd5f62239885a7bb983804eb2e
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2020-05-06 21:52:38 +0930

    [bndtools debug] Download sources during debug launch
    This commit enhances BundleSourceContainer by searching the
    repositories for source bundles corresponding to each of the bundles
    that it contains.
    
    Closes #3899
    
    Signed-off-by: Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>

--- bndtools.core/src/bndtools/launch/sourcelookup/containers/BundleSourceContainer.java
+++ bndtools.core/src/bndtools/launch/sourcelookup/containers/BundleSourceContainer.java
@@ -1,9 +1,16 @@
 package bndtools.launch.sourcelookup.containers;
 
+import static aQute.bnd.osgi.Constants.BSN_SOURCE_SUFFIX;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.bndtools.api.ILogger;
 import org.bndtools.api.Logger;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.sourcelookup.ISourceContainer;
 import org.eclipse.debug.core.sourcelookup.ISourceContainerType;
 import org.eclipse.debug.core.sourcelookup.ISourceLookupDirector;
@@ -11,23 +18,28 @@ import org.eclipse.debug.core.sourcelookup.containers.AbstractSourceContainer;
 import org.eclipse.debug.core.sourcelookup.containers.ExternalArchiveSourceContainer;
 
 import aQute.bnd.build.Container;
+import aQute.bnd.build.WorkspaceRepository;
+import aQute.bnd.service.RepositoryPlugin;
+import aQute.bnd.service.RepositoryPlugin.DownloadListener;
+import aQute.bnd.version.Version;
+import bndtools.central.RepositoryUtils;
 
 public class BundleSourceContainer extends AbstractSourceContainer {
-	private static final ILogger	logger	= Logger.getLogger(BundleSourceContainer.class);
+	private static final ILogger				logger		= Logger.getLogger(BundleSourceContainer.class);
 
-	public static final String		TYPE_ID	= "org.bndtools.core.launch.sourceContainerTypes.bundle";
+	public static final String					TYPE_ID		= "org.bndtools.core.launch.sourceContainerTypes.bundle";
 
-	private final Container			bundle;
+	private final Container						bundle;
 
-	private final ISourceContainer	delegate;
+	private AtomicReference<ISourceContainer>	delegate	= new AtomicReference<>();
+	private AtomicReference<String>				error		= new AtomicReference<>();
+	private CountDownLatch						flag		= new CountDownLatch(1);
 
 	public BundleSourceContainer(Container bundle) {
 		this.bundle = bundle;
 		if (bundle == null) {
 			throw new NullPointerException("bundle shouldn't be null");
 		}
-		delegate = new ExternalArchiveSourceContainer(bundle.getFile()
-			.toString(), false);
 	}
 
 	public Container getBundle() {
@@ -45,17 +57,67 @@ public class BundleSourceContainer extends AbstractSourceContainer {
 		return bundle.hashCode();
 	}
 
-	protected ILaunchConfiguration getLaunchConfiguration() {
-		ISourceLookupDirector director = getDirector();
-		if (director != null) {
-			return director.getLaunchConfiguration();
+	@Override
+	public String getName() {
+		StringBuilder retval = new StringBuilder(100);
+		retval.append(bundle.getBundleSymbolicName())
+			.append(':')
+			.append(bundle.getVersion());
+		if (error.get() != null) {
+			retval.append(" <error downloading source: ")
+				.append(error.get())
+				.append('>');
 		}
-		return null;
+		return retval.toString();
 	}
 
 	@Override
-	public String getName() {
-		return bundle.getBundleSymbolicName() + ":" + bundle.getVersion();
+	public void init(ISourceLookupDirector director) {
+		super.init(director);
+		// Look for possible source bundle.
+		final String sourceBSN = bundle.getBundleSymbolicName() + BSN_SOURCE_SUFFIX;
+		final Version version = new Version(bundle.getVersion());
+		for (RepositoryPlugin repo : RepositoryUtils.listRepositories(true)) {
+			if (repo == null || repo instanceof WorkspaceRepository) {
+				continue;
+			}
+			try {
+				File sourceBundle = repo.get(sourceBSN, version, Collections.emptyMap(), new DownloadListener() {
+					@Override
+					public void success(File file) {
+						delegate.compareAndSet(null, new ExternalArchiveSourceContainer(file.toString(), false));
+						flag.countDown();
+					}
+
+					@Override
+					public void failure(File file, String reason) {
+						logger.logError("Error trying to download " + file + ": " + reason, null);
+						error.set(reason);
+						flag.countDown();
+					}
+
+					@Override
+					public boolean progress(File file, int percentage) {
+						return true;
+					}
+
+				});
+				if (sourceBundle != null) {
+					// Bundle may not yet have finished downloading; don't
+					// construct the delegate yet, the downloadlistener callback
+					// will handle that.
+					return;
+				}
+			} catch (Exception e) {
+				logger.logError("Error trying to fetch " + sourceBSN + ':' + version + " from " + repo.getName(), e);
+			}
+		}
+
+		// If we've gotten this far no separate source bundle was found, so use
+		// the bundle itself.
+		delegate.set(new ExternalArchiveSourceContainer(bundle.getFile()
+			.toString(), false));
+		flag.countDown();
 	}
 
 	@Override
@@ -63,8 +125,20 @@ public class BundleSourceContainer extends AbstractSourceContainer {
 		return getSourceContainerType(TYPE_ID);
 	}
 
+	final private static Object[] EMPTY = new Object[0];
+
 	@Override
 	public Object[] findSourceElements(String name) throws CoreException {
-		return delegate.findSourceElements(name);
+		try {
+			flag.await(1000, TimeUnit.MILLISECONDS);
+			if (delegate.get() == null) {
+				return EMPTY;
+			}
+			return delegate.get()
+				.findSourceElements(name);
+		} catch (InterruptedException e) {
+			logger.logInfo("Time out waiting for source to finish downloading: " + getName(), e);
+			return EMPTY;
+		}
 	}
 }
