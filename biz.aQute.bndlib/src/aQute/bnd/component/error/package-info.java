05674896aa786cd28809bcead378239c8cd01474
BJ Hargrave <bj@bjhargrave.com>
2020-07-10 18:18:28 -0400

    ds: Add details to error messages for DS annotations processing
    We now add the location information to the error message.
    
    Fixes https://github.com/bndtools/bnd/issues/4171
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- biz.aQute.bndlib/src/aQute/bnd/component/error/package-info.java
+++ biz.aQute.bndlib/src/aQute/bnd/component/error/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This package provides Declarative Service Annotation Error information.
  */
-@Version("1.6.0")
+@Version("1.7.0")
 package aQute.bnd.component.error;
 
 import org.osgi.annotation.versioning.Version;
