854887ec15fa453d92a346b91bb209b6299b2121
BJ Hargrave <bj@bjhargrave.com>
2020-08-14 11:44:17 -0400

    ci: Make sure to unset JAVA_TOOL_OPTIONS and _JAVA_OPTIONS
    Otherwise the build can fail because the JVM will emit a
    "Picked up JAVA_TOOL_OPTIONS" message to stdout which can break some
    tests.
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- .github/scripts/rebuild.sh
+++ .github/scripts/rebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+unset JAVA_TOOL_OPTIONS _JAVA_OPTIONS
 ./gradlew --no-daemon --version
 ./gradlew --no-daemon -Dmaven.repo.local=maven/target/m2 :biz.aQute.bnd.gradle:build :biz.aQute.bnd.gradle:releaseNeeded
 ./gradlew --no-daemon -Dmaven.repo.local=maven/target/m2 -Pbnd_repourl=./dist/bundles :buildscriptDependencies :build "$@"
