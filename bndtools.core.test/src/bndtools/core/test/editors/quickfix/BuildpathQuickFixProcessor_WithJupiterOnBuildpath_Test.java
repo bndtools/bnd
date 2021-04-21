b3356db5c431b9564289d7a544493de42af16d1f
Peter Kriens <Peter.Kriens@aqute.biz>
2021-04-21 17:50:26 +0200

    tmp
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

--- bndtools.core.test/src/bndtools/core/test/editors/quickfix/BuildpathQuickFixProcessor_WithJupiterOnBuildpath_Test.java
+++ bndtools.core.test/src/bndtools/core/test/editors/quickfix/BuildpathQuickFixProcessor_WithJupiterOnBuildpath_Test.java
@@ -1,5 +1,9 @@
 package bndtools.core.test.editors.quickfix;
 
+import java.util.Arrays;
+
+import org.eclipse.jdt.internal.ui.text.correction.ProblemLocation;
+import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -25,42 +29,36 @@ public class BuildpathQuickFixProcessor_WithJupiterOnBuildpath_Test extends Abst
 		// test makes sure that it tests the problem on the type literal by
 		// testing proposals at a point in the middle of the ".class" part
 		// of the literal.
-		// String source = "package test; " + "import
-		// org.junit.jupiter.api.extension.ExtendWith; " + "@ExtendWith(";
-		// int start = source.length();
-		// source += "SoftAssertionsExtension.cl";
-		// int middleOfClass = source.length();
-		// source += "ass)\n" + "class " + DEFAULT_CLASS_NAME + "{" + "}";
-		//
-		// assertThatProposals(proposalsFor(middleOfClass, 0,
-		// source)).as("middle of .class")
-		// .haveExactly(1,
-		// suggestsBundle("assertj-core", "3.16.1",
-		// "org.assertj.core.api.junit.jupiter.SoftAssertionsExtension"));
+//		String source = "package test; " + "import org.junit.jupiter.api.extension.ExtendWith; " + "@ExtendWith(";
+//		int start = source.length();
+//		source += "SoftAssertionsExtension.cl";
+//		int middleOfClass = source.length();
+//		source += "ass)\n" + "class " + DEFAULT_CLASS_NAME + "{" + "}";
+//
+//		assertThatProposals(proposalsFor(middleOfClass, 0, source)).as("middle of .class")
+//			.haveExactly(1,
+//				suggestsBundle("assertj-core", "3.16.1", "org.assertj.core.api.junit.jupiter.SoftAssertionsExtension"));
 	}
 
 	@Test
 	void removesDuplicateProposals() {
-		// String source = "package test; " + "import
-		// org.junit.jupiter.api.extension.ExtendWith; " + "@ExtendWith(";
-		// int start = source.length();
-		// source += "SoftAssertionsExtension.cl";
-		// int middleOfClass = source.length();
-		// source += "ass)\n" + "class " + DEFAULT_CLASS_NAME + "{" + "}";
-		//
-		// problemsFor(start, 0, DEFAULT_CLASS_NAME, source);
-		// if (problems.size() < 2) {
-		// throw new IllegalStateException(
-		// "This test requires that multiple problems be generated in order to
-		// work, but only got: "
-		// + Arrays.toString(locs));
-		// }
-		// locs = problems.stream()
-		// .map(ProblemLocation::new)
-		// .toArray(IProblemLocation[]::new);
-		// assertThatProposals(proposals()).haveExactly(1,
-		// suggestsBundle("assertj-core", "3.16.1",
-		// "org.assertj.core.api.junit.jupiter.SoftAssertionsExtension"));
+//		String source = "package test; " + "import org.junit.jupiter.api.extension.ExtendWith; " + "@ExtendWith(";
+//		int start = source.length();
+//		source += "SoftAssertionsExtension.cl";
+//		int middleOfClass = source.length();
+//		source += "ass)\n" + "class " + DEFAULT_CLASS_NAME + "{" + "}";
+//
+//		problemsFor(start, 0, DEFAULT_CLASS_NAME, source);
+//		if (problems.size() < 2) {
+//			throw new IllegalStateException(
+//				"This test requires that multiple problems be generated in order to work, but only got: "
+//					+ Arrays.toString(locs));
+//		}
+//		locs = problems.stream()
+//			.map(ProblemLocation::new)
+//			.toArray(IProblemLocation[]::new);
+//		assertThatProposals(proposals()).haveExactly(1,
+//			suggestsBundle("assertj-core", "3.16.1", "org.assertj.core.api.junit.jupiter.SoftAssertionsExtension"));
 	}
 
 }
