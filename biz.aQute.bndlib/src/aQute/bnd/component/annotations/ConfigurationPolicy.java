08c86f279bb9c03be5a5a8c6d6768fc7089c0501
BJ Hargrave <bj@hargrave.dev>
2021-05-25 14:49:39 -0400

    ds: Update component package to latest DS 1.5 from osgi/osgi
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- biz.aQute.bndlib/src/aQute/bnd/component/annotations/ConfigurationPolicy.java
+++ biz.aQute.bndlib/src/aQute/bnd/component/annotations/ConfigurationPolicy.java
@@ -1,29 +1,32 @@
-/*
- * Copyright (c) OSGi Alliance (2011, 2016). All Rights Reserved.
+/*******************************************************************************
+ * Copyright (c) Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
+ * SPDX-License-Identifier: Apache-2.0 
+ *******************************************************************************/
 
 package aQute.bnd.component.annotations;
 
 /**
  * Configuration Policy for the {@link Component} annotation.
+ * 
  * <p>
  * Controls whether component configurations must be satisfied depending on the
  * presence of a corresponding Configuration object in the OSGi Configuration
  * Admin service. A corresponding configuration is a Configuration object where
  * the PID is the name of the component.
- *
+ * 
  * @author $Id$
  * @since 1.1
  */
@@ -47,7 +50,7 @@ public enum ConfigurationPolicy {
 	 */
 	IGNORE("ignore");
 
-	private final String value;
+	private final String	value;
 
 	ConfigurationPolicy(String value) {
 		this.value = value;
--- biz.aQute.bndlib/src/aQute/bnd/component/annotations/ServiceScope.java
+++ biz.aQute.bndlib/src/aQute/bnd/component/annotations/ServiceScope.java
@@ -1,24 +1,26 @@
-/*
- * Copyright (c) OSGi Alliance (2011, 2016). All Rights Reserved.
+/*******************************************************************************
+ * Copyright (c) Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
+ * SPDX-License-Identifier: Apache-2.0 
+ *******************************************************************************/
 
 package aQute.bnd.component.annotations;
 
 /**
  * Service scope for the {@link Component} annotation.
- *
+ * 
  * @author $Id$
  * @since 1.3
  */
@@ -50,7 +52,7 @@ public enum ServiceScope {
 	 */
 	DEFAULT("<<default>>");
 
-	private final String value;
+	private final String	value;
 
 	ServiceScope(String value) {
 		this.value = value;
