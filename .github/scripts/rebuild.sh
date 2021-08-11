bade38f10dff4ebc315de7191698c34c6ae423e2
BJ Hargrave <bj@hargrave.dev>
2021-08-11 12:04:06 -0400

    gradle: Update plugin publish version
    Also, remove warning-mode for plugin build in rebuild.sh.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- .github/scripts/rebuild.sh
+++ .github/scripts/rebuild.sh
@@ -2,5 +2,5 @@
 unset JAVA_TOOL_OPTIONS _JAVA_OPTIONS
 ./gradlew --no-daemon --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :dist:releaseDependencies
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --warning-mode=fail :gradle-plugins:build :gradle-plugins:publish
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :gradle-plugins:build :gradle-plugins:publish
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_repourl=./dist/bundles --warning-mode=fail :buildscriptDependencies :build "$@"
