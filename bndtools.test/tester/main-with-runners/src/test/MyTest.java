ba7ed02fa205e08200927e9210d8c4b9a6ad9d55
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2021-09-08 15:26:25 +0930

    [tester] Tidy-up test workspace to make sure it compiles
    Also tweaked one of the tests to facilitate testing of re-run functionality.
    
    Signed-off-by: Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>

--- bndtools.test/tester/main-with-runners/src/test/MyTest.java
+++ bndtools.test/tester/main-with-runners/src/test/MyTest.java
@@ -8,40 +8,45 @@ import junit.framework.AssertionFailedError;
 
 public class MyTest {
 
+	static int runCount = 0;
+
 	@Test
 	public void test0() {
+		if (++runCount % 2 == 0) {
+			throw new AssertionError("Expecting the runcount to be odd");
+		}
 	}
-	
+
 	@Test
 	public void test1() {
 		throw new AssertionFailedError("Hi there");
 	}
-	
+
 	@Test
 	public void test2() {
 		assertThat("a").isEqualTo("x");
 	}
-	
+
 	@Test
 	public void test3() {
-		throw new org.opentest4j.AssertionFailedError("hi", "there", "folks");
+		throw new org.opentest4j.AssertionFailedError("hi", runCount++ + "", "folks");
 	}
-	
+
 	@Test
 	public void test4() {
 		throw new junit.framework.ComparisonFailure("by", "here", "oaks");
 	}
-	
+
 	@Test
 	public void test6() {
 		throw new RuntimeException();
 	}
-	
+
 	@org.junit.jupiter.api.Test
 	public void test7() {
 		throw new RuntimeException();
 	}
-	
+
 	@org.junit.jupiter.api.Test
 	public void testerer5() {
 		assertThat("actual").isEqualTo("expected");
