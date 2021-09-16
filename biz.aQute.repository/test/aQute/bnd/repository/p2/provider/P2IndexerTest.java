312d24008ddf045a8baf99d82e1732d28cb8ee9a
BJ Hargrave <bj@hargrave.dev>
2021-09-16 11:04:35 -0400

    test: Remove test method which accesses eclipse server
    The Eclipse servers are not super reliable. Whenever there is an
    Eclipse release, this test method fails with timeouts. We have other
    test methods in this test class which do basically the same test on
    a p2 repo hosted by jfrog.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- biz.aQute.repository/test/aQute/bnd/repository/p2/provider/P2IndexerTest.java
+++ biz.aQute.repository/test/aQute/bnd/repository/p2/provider/P2IndexerTest.java
@@ -17,12 +17,12 @@ import org.osgi.resource.Requirement;
 import org.osgi.resource.Resource;
 import org.osgi.service.repository.Repository;
 
+import aQute.bnd.exceptions.Exceptions;
 import aQute.bnd.http.HttpClient;
 import aQute.bnd.osgi.resource.RequirementBuilder;
 import aQute.bnd.osgi.resource.ResourceUtils;
 import aQute.bnd.service.RepositoryPlugin;
 import aQute.bnd.version.Version;
-import aQute.bnd.exceptions.Exceptions;
 import aQute.lib.io.IO;
 import aQute.libg.cryptography.SHA256;
 import aQute.libg.reporter.slf4j.Slf4jReporter;
@@ -37,28 +37,6 @@ public class P2IndexerTest extends TestCase {
 		tmp = IO.getFile("generated/tmp/test/" + getClass().getName() + "/" + getName());
 		IO.delete(tmp);
 		tmp.mkdirs();
-	}
-
-	public void testEclipseP2Repo() throws Exception {
-		try (HttpClient client = new HttpClient()) {
-			client.setCache(IO.getFile(tmp, "cache"));
-
-			try (P2Indexer p2 = new P2Indexer(new Unpack200(), new Slf4jReporter(P2IndexerTest.class), tmp, client,
-				new URI("https://archive.eclipse.org/egit/updates-4.7.1/"), getName())) {
-				List<String> bsns = p2.list(null);
-				System.out.println(bsns);
-				assertThat(bsns).contains("org.kohsuke.args4j", "org.slf4j.api", "org.apache.httpcomponents.httpclient",
-					"org.eclipse.egit.ui.source", "org.eclipse.jgit.archive", "org.eclipse.jgit.pgm.source",
-					"org.eclipse.egit", "org.eclipse.egit.ui", "org.eclipse.egit.mylyn.ui.source",
-					"org.eclipse.jgit.pgm", "org.apache.commons.compress", "org.eclipse.egit.gitflow.ui",
-					"org.slf4j.impl.log4j12", "org.eclipse.jgit.ui", "javaewah", "org.apache.httpcomponents.httpcore",
-					"org.eclipse.jgit.source", "org.eclipse.jgit.http.apache", "org.eclipse.egit.gitflow.source",
-					"org.eclipse.jgit", "org.eclipse.egit.core.source", "org.eclipse.egit.gitflow.ui.source",
-					"org.eclipse.egit.mylyn.ui", "org.eclipse.jgit.lfs", "org.eclipse.egit.gitflow",
-					"org.eclipse.egit.core", "org.eclipse.egit.ui.smartimport", "org.eclipse.jgit.lfs.server",
-					"org.eclipse.egit.doc", "org.apache.log4j", "com.google.gson", "com.jcraft.jsch");
-			}
-		}
 	}
 
 	public void testURI() throws Exception {
