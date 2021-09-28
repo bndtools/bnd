c174550dd04f613e7cb03dda5395e197d1afb69e
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2021-09-28 13:50:32 +0930

    [launch] Made NativeLaunchTabGroup a component
    In implementing #4703 I missed making the NativeLaunchTabGroup a
    component, which causes errors when you try and configure the
    launch group.
    
    Fixes #4863.
    
    Signed-off-by: Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>

--- org.bndtools.launch/src/bndtools/launch/bnd/NativeLaunchTabGroup.java
+++ org.bndtools.launch/src/bndtools/launch/bnd/NativeLaunchTabGroup.java
@@ -3,9 +3,13 @@ package bndtools.launch.bnd;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaConnectTab;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
 
+@Component(scope = ServiceScope.PROTOTYPE, service = ILaunchConfigurationTabGroup.class)
 public class NativeLaunchTabGroup extends AbstractLaunchConfigurationTabGroup {
 
 	@Override
