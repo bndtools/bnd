c865a8087a920e412213a86768543d39b5ea16bc
BJ Hargrave <bj@hargrave.dev>
2021-11-15 22:40:35 -0500

    ci: Add test gradle plugins with warning-mode=fail
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- .github/scripts/rebuild-build.sh
+++ .github/scripts/rebuild-build.sh
@@ -2,4 +2,5 @@
 set -ev
 ./gradlew --no-daemon --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :publish
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --warning-mode=fail :gradle-plugins:build
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :gradle-plugins:publish
