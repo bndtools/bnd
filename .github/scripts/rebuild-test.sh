d22b0f2dd647eb845847c56baa2083a7409aacb2
BJ Hargrave <bj@hargrave.dev>
2021-11-19 17:07:02 -0500

    build: Update build to support using RC builds in next branch
    Previously, the next branch just uses the final snapshot before RC
    builds start.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- .github/scripts/rebuild-test.sh
+++ .github/scripts/rebuild-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ev
 ./gradlew --no-daemon --version
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_repourl=./dist/bundles --warning-mode=fail :buildscriptDependencies :build :publish "$@"
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_snapshots=./dist/bundles --warning-mode=fail :buildscriptDependencies :build :publish "$@"
