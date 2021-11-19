9c2566b237e607ba572d7b1afb9ae809d2aa6335
Peter Kriens <Peter.Kriens@aqute.biz>
2021-11-19 12:27:07 +0100

    try out
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -2,6 +2,6 @@
 set -ev
 ./gradlew --no-daemon --version
 ./mvnw --version
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
+./gradlew --no-daemon --stacktrace -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
 ./mvnw -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress --file=maven install
