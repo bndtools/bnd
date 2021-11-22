ece69c91941236177e45b43bb2aefd044b0387b4
BJ Hargrave <bj@hargrave.dev>
2021-11-22 15:20:06 -0500

    getopt: Declare Target for annotations
    This is necessary to address a compiler difference between ecj and
    javac. Without the Target, javac would generate a
    RuntimeVisibleAnnotation but ecj would generate a
    RuntimeVisibleAnnotation and a RuntimeVisibleTypeAnnotation for the
    method's return type.
    
    This caused a baselining mismatch in Bndtools when it baselined the
    ecj generated classes against the javac generated classes in the
    baseline jar. So every Bnd release, we get a baselining error in
    Bndtools for all packages which used the getopt annotations.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- aQute.libg/src/aQute/lib/getopt/OptionArgument.java
+++ aQute.libg/src/aQute/lib/getopt/OptionArgument.java
@@ -1,9 +1,14 @@
 package aQute.lib.getopt;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({
+	ElementType.METHOD, ElementType.TYPE
+})
 public @interface OptionArgument {
 	String value();
 }
