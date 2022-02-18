64498ba73edc853177214b5220294523931aa4d3
Peter Kriens <Peter.Kriens@aqute.biz>
2022-02-18 20:07:08 +0100

    [libg] Restore old package
    When we moved to 6.1 (not a major release) we lost
    the package aQute.lib.exceptions because we also
    made it available via aQute.bnd.util.
    
    This would've been a nice use case for my shading
    code but until we've got something like that we cannot
    just remove a package in a minor release.
    
    This is causing so much pain in all my builds that
    I am restoring it, unfortunately now having a 
    duplicate. 
    
    I am ok to deprecate one of them but this is
    a giant pita.
    
    
    Signed-off-by: Peter Kriens <Peter.Kriens@aqute.biz>

---
+++ aQute.libg/src/aQute/lib/exceptions/BiFunctionWithException.java
@@ -0,0 +1,63 @@
+package aQute.lib.exceptions;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+/**
+ * BiFunction interface that allows exceptions.
+ *
+ * @param <T> the type 1 of the argument
+ * @param <U> the type 2 of the argument
+ * @param <R> the result type
+ */
+@Deprecated() // move to package aQute.bnd.exceptions 
+@FunctionalInterface
+public interface BiFunctionWithException<T, U, R> {
+	R apply(T t, U u) throws Exception;
+
+	default BiFunction<T, U, R> orElseThrow() {
+		return (t, u) -> {
+			try {
+				return apply(t, u);
+			} catch (Exception e) {
+				throw Exceptions.duck(e);
+			}
+		};
+	}
+
+	default BiFunction<T, U, R> orElse(R orElse) {
+		return (t, u) -> {
+			try {
+				return apply(t, u);
+			} catch (Exception e) {
+				return orElse;
+			}
+		};
+	}
+
+	default BiFunction<T, U, R> orElseGet(Supplier<? extends R> orElseGet) {
+		requireNonNull(orElseGet);
+		return (t, u) -> {
+			try {
+				return apply(t, u);
+			} catch (Exception e) {
+				return orElseGet.get();
+			}
+		};
+	}
+
+	static <T, U, R> BiFunction<T, U, R> asBiFunction(BiFunctionWithException<T, U, R> unchecked) {
+		return unchecked.orElseThrow();
+	}
+
+	static <T, U, R> BiFunction<T, U, R> asBiFunctionOrElse(BiFunctionWithException<T, U, R> unchecked, R orElse) {
+		return unchecked.orElse(orElse);
+	}
+
+	static <T, U, R> BiFunction<T, U, R> asBiFunctionOrElseGet(BiFunctionWithException<T, U, R> unchecked,
+		Supplier<? extends R> orElseGet) {
+		return unchecked.orElseGet(orElseGet);
+	}
+}
---
+++ aQute.libg/src/aQute/lib/exceptions/ConsumerWithException.java
@@ -0,0 +1,40 @@
+package aQute.lib.exceptions;
+
+import java.util.function.Consumer;
+
+/**
+ * Consumer interface that allows exceptions.
+ *
+ * @param <T> the type of the argument
+ */
+@Deprecated() // move to package aQute.bnd.exceptions
+@FunctionalInterface
+public interface ConsumerWithException<T> {
+	void accept(T t) throws Exception;
+
+	default Consumer<T> orElseThrow() {
+		return t -> {
+			try {
+				accept(t);
+			} catch (Exception e) {
+				throw Exceptions.duck(e);
+			}
+		};
+	}
+
+	default Consumer<T> ignoreException() {
+		return t -> {
+			try {
+				accept(t);
+			} catch (Exception e) {}
+		};
+	}
+
+	static <T> Consumer<T> asConsumer(ConsumerWithException<T> unchecked) {
+		return unchecked.orElseThrow();
+	}
+
+	static <T> Consumer<T> asConsumerIgnoreException(ConsumerWithException<T> unchecked) {
+		return unchecked.ignoreException();
+	}
+}
