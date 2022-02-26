dc0106cabe0719525cedb7459182f7962ee19f49
BJ Hargrave <bj@hargrave.dev>
2022-02-26 16:44:41 -0500

    baselining: Update package versions
    Changes were made after the start of the 6.3.0 stream but before we
    6.2.0 was released to maven central and available as a baseline.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- biz.aQute.bndlib/src/aQute/bnd/build/package-info.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/package-info.java
@@ -1,6 +1,6 @@
 /**
  */
-@Version("4.2.0")
+@Version("4.3.0")
 package aQute.bnd.build;
 
 import org.osgi.annotation.versioning.Version;
--- biz.aQute.bndlib/src/aQute/bnd/osgi/resource/package-info.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/resource/package-info.java
@@ -1,4 +1,4 @@
-@Version("4.1.0")
+@Version("4.2.0")
 package aQute.bnd.osgi.resource;
 
 import org.osgi.annotation.versioning.Version;
