35ffa59165c9bb2d122ad9b868ffa148200b8fbd
Juergen Albert <j.albert@data-in-motion.biz>
2022-04-08 13:17:30 +0200

    adds additional wrapper capability
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- biz.aQute.bnd.annotation/src/aQute/bnd/annotation/service/ServiceCapability.java
+++ biz.aQute.bnd.annotation/src/aQute/bnd/annotation/service/ServiceCapability.java
@@ -1,13 +1,16 @@
 package aQute.bnd.annotation.service;
 
+import static aQute.bnd.annotation.spi.Constants.ATTRIBUTE_MACRO;
+
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.osgi.annotation.bundle.Capability;
 import org.osgi.namespace.service.ServiceNamespace;
-import org.osgi.resource.Namespace;
+import org.osgi.resource.Namespace;;
 
 /**
  * Adds a ProvideCapability for a service. This is useful to the resolver, when
@@ -19,11 +22,44 @@ import org.osgi.resource.Namespace;
 @Target({
 	ElementType.TYPE, ElementType.PACKAGE
 })
+@Repeatable(ServiceCapabilities.class)
 @Capability(namespace = ServiceNamespace.SERVICE_NAMESPACE, //
 	attribute = {
 		ServiceNamespace.CAPABILITY_OBJECTCLASS_ATTRIBUTE + ":List<String>=\"${uniq;${#value}}\"", //
-		Namespace.CAPABILITY_USES_DIRECTIVE + ":=\"${uniq;${replace;${#value};(.*)\\.[^.]+;$1}}\""
+		Namespace.CAPABILITY_USES_DIRECTIVE
+			+ ":=\"${if;${size;${#uses}};${#uses};${uniq;${replace;${#value};(.*)\\.[^.]+;$1}}}\"",
+		ATTRIBUTE_MACRO
 	})
 public @interface ServiceCapability {
-	Class<?>[] value() default Target.class;
+	/**
+	 * The service <em>type</em>.
+	 *
+	 * @return the service type
+	 */
+	Class<?> value() default Target.class;
+
+	/**
+	 * A list of classes whose packages are inspected to calculate the
+	 * {@code uses} directive for this capability.
+	 * <p>
+	 * If not specified, the {@code uses} directive is omitted from the
+	 * capability clause.
+	 */
+	Class<?>[] uses() default {};
+
+	/**
+	 * A list of attribute or directive names and values.
+	 * <p>
+	 * Each string should be specified in the form:
+	 * <ul>
+	 * <li>{@code "name=value"} for attributes.</li>
+	 * <li>{@code "name:type=value"} for typed attributes.</li>
+	 * <li>{@code "name:=value"} for directives.</li>
+	 * </ul>
+	 * These are added, separated by semicolons, to the
+	 * {@code osgi.serviceloader} capability. Non-standard
+	 * {@code osgi.serviceloader} attributes will be included as service
+	 * properties to the published service.
+	 */
+	String[] attribute() default {};
 }
---
+++ biz.aQute.bndlib.tests/test/test/servicecapability/misc2/Bar.java
@@ -0,0 +1,8 @@
+package test.servicecapability.misc2;
+
+import aQute.bnd.annotation.service.ServiceCapability;
+
+@ServiceCapability
+public class Bar {
+
+}
