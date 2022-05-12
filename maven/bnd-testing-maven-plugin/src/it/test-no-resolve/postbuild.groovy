9b7f8c6e14c8b73f87332b6fc685b2e08613ea14
BJ Hargrave <bj@hargrave.dev>
2022-05-12 14:38:19 -0400

    maven: Refactor testing integration tests into separate test projects
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

---
+++ maven/bnd-testing-maven-plugin/src/it/test-no-resolve/postbuild.groovy
@@ -0,0 +1,7 @@
+File testsuite_xml = new File(basedir, "/target/test-reports/test/TEST-test-no-resolve-0.0.1.xml")
+assert testsuite_xml.isFile();
+def testsuite = new XmlSlurper().parse(testsuite_xml)
+assert testsuite.@name == 'test.test-no-resolve'
+assert testsuite.@tests == 1
+assert testsuite.@errors == 0
+assert testsuite.@failures == 0
