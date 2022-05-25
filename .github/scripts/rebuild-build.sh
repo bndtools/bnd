ddb77c500d40897abbf5ba5f00a201a14d4696ea
BJ Hargrave <bj@hargrave.dev>
2022-05-25 09:20:03 -0400

    ci: Display build dependencies for rebuild-build
    This is useful for checking the Bnd version used to build Bnd.
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- .github/scripts/rebuild-build.sh
+++ .github/scripts/rebuild-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 ./gradlew --no-daemon --version
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :publish
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :buildscriptDependencies :publish
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --warning-mode=fail :gradle-plugins:build
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :gradle-plugins:publish
