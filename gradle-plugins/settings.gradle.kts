5c121ad381e469d49107f3bc5aa316a0b80dc28f
BJ Hargrave <bj@hargrave.dev>
2022-09-29 10:23:49 -0400

    ci: Only accept gradle build scan TOS on CI builds
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -1,7 +1,22 @@
 pluginManagement {
 	plugins {
-		id("com.gradle.plugin-publish") version "1.0.0"
-		id("dev.hargrave.addmavendescriptor") version "1.0.0"
+		id("com.gradle.plugin-publish") version("1.0.0")
+		id("dev.hargrave.addmavendescriptor") version("1.0.0")
+		id("com.gradle.enterprise") version("3.11.1")
+	}
+}
+
+plugins {
+	id("com.gradle.enterprise")
+}
+
+if (System.getenv("CI").toBoolean()) {
+	gradleEnterprise {
+		buildScan {
+			publishAlways()
+			termsOfServiceUrl = "https://gradle.com/terms-of-service"
+			termsOfServiceAgree = "yes"
+		}
 	}
 }
 
