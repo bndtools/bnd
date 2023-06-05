05d44aa3760ad3c0ad510f048be2e077759e04a5
Peter Kriens <Peter.Kriens@aQute.biz>
2023-06-05 08:54:31 +0200

    Update ci-build.sh
    Added retry and timeout to maven

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -4,4 +4,4 @@ set -ev
 ./mvnw --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-./mvnw -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
+./mvnw -Dmaven.repo.local=dist/m2  -Dhttp.connectionTimeout=100000 -Dhttp.retryCount=5 --batch-mode --no-transfer-progress install
