824e03b2a9ebb6900f33462012c5290898be936a
Peter Kriens <Peter.Kriens@aQute.biz>
2023-06-29 10:09:09 +0200

    5673 Merged properties for standalone. Fixes #5673
    ---
     Signed-off-by: Peter Kriens <Peter.Kriens@aQute.biz>
    
    Signed-off-by: Peter Kriens <Peter.Kriens@aQute.biz>

--- biz.aQute.resolve/test/biz/aQute/resolve/StandaloneTest.java
+++ biz.aQute.resolve/test/biz/aQute/resolve/StandaloneTest.java
@@ -121,6 +121,26 @@ public class StandaloneTest {
 	}
 
 	@Test
+	public void testMergedUrls() throws Exception {
+		File f = IO.getFile("testdata/standalone/merged.bndrun");
+		Run run = Run.createRun(null, f);
+
+		List<Repository> repositories = run.getWorkspace()
+			.getPlugins(Repository.class);
+		assertEquals(2, repositories.size());
+		assertTrue(repositories.get(0) instanceof OSGiRepository);
+		assertTrue(repositories.get(1) instanceof OSGiRepository);
+
+		OSGiRepository f0 = (OSGiRepository) repositories.get(0);
+		assertEquals("repo01", f0.getName());
+		assertEquals("https://example.org/index1.xml", f0.getLocation());
+
+		OSGiRepository f1 = (OSGiRepository) repositories.get(1);
+		assertEquals("second", f1.getName());
+		assertEquals("https://example.org/index2.xml", f1.getLocation());
+	}
+
+	@Test
 	public void testRelativeUrl() throws Exception {
 		File f = IO.getFile("testdata/standalone/relative_url.bndrun");
 		Run run = Run.createRun(null, f);
