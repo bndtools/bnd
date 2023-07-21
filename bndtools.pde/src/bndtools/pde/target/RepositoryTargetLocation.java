59d22cabf03a7c9853e73c94bc8a210312571c93
Juergen Albert <j.albert@data-in-motion.biz>
2023-07-21 14:23:41 +0200

    Replaces deprecated interfaces in BndTargetLocation
    fixes #5481
    
    Signed-off-by: Juergen Albert <j.albert@data-in-motion.biz>

--- bndtools.pde/src/bndtools/pde/target/RepositoryTargetLocation.java
+++ bndtools.pde/src/bndtools/pde/target/RepositoryTargetLocation.java
@@ -9,6 +9,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.pde.core.target.ITargetDefinition;
 import org.eclipse.pde.core.target.ITargetLocation;
@@ -61,8 +62,9 @@ public class RepositoryTargetLocation extends BndTargetLocation {
 		return repositoryName;
 	}
 
+
 	@Override
-	public IWizard getEditWizard(ITargetDefinition target, ITargetLocation targetLocation) {
+	public IWizard getEditWizard(ITargetDefinition target, TreePath treePath) {
 		RepositoryTargetLocationWizard wizard = new RepositoryTargetLocationWizard();
 		wizard.setTarget(target);
 		wizard.setTargetLocation(this);
