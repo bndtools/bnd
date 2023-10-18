b68ff83c310b91cc5a3f424403f45a23bb9dc429
Christoph Rueger <chrisrueger@gmail.com>
2023-10-18 22:11:46 +0200

    use VersionRange when adding bundle to Run Bundles
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- bndtools.core/src/bndtools/model/repo/RepositoryBundleUtils.java
+++ bndtools.core/src/bndtools/model/repo/RepositoryBundleUtils.java
@@ -5,6 +5,8 @@ import org.bndtools.utils.repos.RepoUtils;
 import aQute.bnd.build.model.clauses.VersionedClause;
 import aQute.bnd.header.Attrs;
 import aQute.bnd.osgi.Constants;
+import aQute.bnd.version.Version;
+import aQute.bnd.version.VersionRange;
 
 public class RepositoryBundleUtils {
 	private static final String VERSION_LATEST = "latest";
@@ -17,6 +19,14 @@ public class RepositoryBundleUtils {
 		return new VersionedClause(bundle.getBsn(), attribs);
 	}
 
+	/**
+	 * Converts a RepositoryBundleVersion into a version or version range. e.g.
+	 * version='latest' or version='[1.2.3,2.0.0)'.
+	 *
+	 * @param bundleVersion
+	 * @param phase
+	 * @return a version or version range.
+	 */
 	public static VersionedClause convertRepoBundleVersion(RepositoryBundleVersion bundleVersion,
 		DependencyPhase phase) {
 		Attrs attribs = new Attrs();
@@ -24,6 +34,7 @@ public class RepositoryBundleUtils {
 			.getRepo()))
 			attribs.put(Constants.VERSION_ATTRIBUTE, VERSION_LATEST);
 		else {
+
 			StringBuilder builder = new StringBuilder();
 
 			builder.append(bundleVersion.getVersion()
@@ -31,12 +42,32 @@ public class RepositoryBundleUtils {
 			builder.append('.')
 				.append(bundleVersion.getVersion()
 					.getMinor());
+
+			// TODO why is this check?
 			if (phase != DependencyPhase.Build)
 				builder.append('.')
 					.append(bundleVersion.getVersion()
 						.getMicro());
 
-			attribs.put(Constants.VERSION_ATTRIBUTE, builder.toString());
+
+			// #5816 create a range from the given version up to the next major
+			// as in
+			// version='[1.2.3,2.0.0)'
+			// instead of version='1.2.3' because the latter is actually an open
+			// range meaning "1.2.3 and everything above" (see
+			// https://docs.osgi.org/specification/osgi.core/7.0.0/framework.module.html#d0e2221).
+			// This could surprise developers, as version='1.2.3' looks more
+			// like a exact version match.
+			// Thus we create a more limited range from the given version 1.2.3
+			// `up to the next major version
+			// this behavior is similar to what the resolver is inserting into
+			// the run bundles list.
+			Version l = new Version(builder.toString());
+			Version h = l.bumpMajor();
+			VersionRange vr = new VersionRange(true, l, h, false);
+			String range = vr.toString();
+
+			attribs.put(Constants.VERSION_ATTRIBUTE, range);
 		}
 		return new VersionedClause(bundleVersion.getParentBundle()
 			.getBsn(), attribs);
