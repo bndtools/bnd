0839b3c4332a30a93e4d2eff45f75143ccac434e
Christoph Rueger <chrisrueger@gmail.com>
2023-10-28 22:40:00 +0200

    change to using SoftAssertions
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>
    
    use using field injection @InjectSoftAssertions
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>
    
    missed one method @InjectSoftAssertions
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- bndtools.core/test/bndtools/wizards/repo/RepoBundleVersionToRangeTest.java
+++ bndtools.core/test/bndtools/wizards/repo/RepoBundleVersionToRangeTest.java
@@ -8,10 +8,13 @@ import static bndtools.model.repo.RepositoryBundleUtils.convertRepoBundle;
 import static bndtools.model.repo.RepositoryBundleUtils.convertRepoBundleVersion;
 import static bndtools.model.repo.RepositoryBundleUtils.toVersionRangeUpToNextMajor;
 import static bndtools.model.repo.RepositoryBundleUtils.toVersionRangeUpToNextMicro;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import aQute.bnd.build.WorkspaceRepository;
 import aQute.bnd.service.RepositoryPlugin;
@@ -31,15 +34,25 @@ import bndtools.model.repo.RepositoryBundleVersion;
  * version increment)</li>
  * </ul>
  */
+@ExtendWith(SoftAssertionsExtension.class)
 public class RepoBundleVersionToRangeTest {
+
+	@InjectSoftAssertions
+	SoftAssertions softly;
 
 	@Test
 	public void testVersionToVersionRanges() {
-		assertEquals(parseVersionRange("[1.2.3,1.2.4)").toString(),
-			toVersionRangeUpToNextMicro(Version.valueOf("1.2.3")).toString());
+		softly.assertThat(toVersionRangeUpToNextMicro(Version.valueOf("1.2.3")).toString())
+			.isEqualTo("[1.2.3,1.2.4)");
 
-		assertEquals(parseVersionRange("[1.2.3,2.0.0)").toString(),
-			toVersionRangeUpToNextMajor(Version.valueOf("1.2.3")).toString());
+		softly.assertThat(toVersionRangeUpToNextMajor(Version.valueOf("1.2.3")).toString())
+			.isEqualTo("[1.2.3,2.0.0)");
+
+		softly.assertThat(toVersionRangeUpToNextMicro(Version.valueOf("1.2.3")).toString())
+			.isEqualTo(parseVersionRange("[1.2.3,1.2.4)").toString());
+
+		softly.assertThat(toVersionRangeUpToNextMajor(Version.valueOf("1.2.3")).toString())
+			.isEqualTo(parseVersionRange("[1.2.3,2.0.0)").toString());
 
 	}
 
@@ -52,19 +65,25 @@ public class RepoBundleVersionToRangeTest {
 
 		// test repobundle (root node in the repo browser... just the bsn,
 		// without version)
-		assertEquals("foo.bundle.bar", convertRepoBundle(rb, Req).toString());
-		assertEquals("foo.bundle.bar", convertRepoBundle(rb, Run).toString());
-		assertEquals("foo.bundle.bar", convertRepoBundle(rb, Build).toString());
+		softly.assertThat(convertRepoBundle(rb, Req).toString())
+			.isEqualTo("foo.bundle.bar");
+		softly.assertThat(convertRepoBundle(rb, Run).toString())
+			.isEqualTo("foo.bundle.bar");
+		softly.assertThat(convertRepoBundle(rb, Build).toString())
+			.isEqualTo("foo.bundle.bar");
 
 
 		// adding to -runrequires
-		assertEquals("foo.bundle.bar;version='[1.2.3,2.0.0)'", convertRepoBundleVersion(rbv, Req).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Req).toString())
+			.isEqualTo("foo.bundle.bar;version='[1.2.3,2.0.0)'");
 
 		// adding to -runbundles
-		assertEquals("foo.bundle.bar;version='[1.2.3,1.2.4)'", convertRepoBundleVersion(rbv, Run).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Run).toString())
+			.isEqualTo("foo.bundle.bar;version='[1.2.3,1.2.4)'");
 
 		// adding to -buildpath
-		assertEquals("foo.bundle.bar;version='1.2'", convertRepoBundleVersion(rbv, Build).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Build).toString())
+			.isEqualTo("foo.bundle.bar;version='1.2'");
 
 	}
 
@@ -75,18 +94,24 @@ public class RepoBundleVersionToRangeTest {
 		RepositoryBundle rb = new RepositoryBundle(wsrepo, "foo.bundle.bar");
 		RepositoryBundleVersion rbv = new RepositoryBundleVersion(rb, Version.valueOf("1.2.3"));
 
-		assertEquals("foo.bundle.bar", convertRepoBundle(rb, Req).toString());
-		assertEquals("foo.bundle.bar;version=snapshot", convertRepoBundle(rb, Run).toString());
-		assertEquals("foo.bundle.bar;version=snapshot", convertRepoBundle(rb, Build).toString());
+		softly.assertThat(convertRepoBundle(rb, Req).toString())
+			.isEqualTo("foo.bundle.bar");
+		softly.assertThat(convertRepoBundle(rb, Run).toString())
+			.isEqualTo("foo.bundle.bar;version=snapshot");
+		softly.assertThat(convertRepoBundle(rb, Build).toString())
+			.isEqualTo("foo.bundle.bar;version=snapshot");
 
 		// adding to -runrequires
-		assertEquals("foo.bundle.bar", convertRepoBundleVersion(rbv, Req).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Req).toString())
+			.isEqualTo("foo.bundle.bar");
 
 		// adding to -runbundles
-		assertEquals("foo.bundle.bar;version=snapshot", convertRepoBundleVersion(rbv, Run).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Run).toString())
+			.isEqualTo("foo.bundle.bar;version=snapshot");
 
 		// adding to -buildpath
-		assertEquals("foo.bundle.bar;version=snapshot", convertRepoBundleVersion(rbv, Build).toString());
+		softly.assertThat(convertRepoBundleVersion(rbv, Build).toString())
+			.isEqualTo("foo.bundle.bar;version=snapshot");
 
 	}
 
