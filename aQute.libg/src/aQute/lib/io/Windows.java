e5e9d3a6efb07b483c281d3dba43cbc382ac9477
Peter Kriens <Peter.Kriens@aQute.biz>
2024-04-03 14:49:20 +0200

    Minor update of some utils
    ---
     Signed-off-by: Peter Kriens <Peter.Kriens@aQute.biz>
    
    Signed-off-by: Peter Kriens <Peter.Kriens@aQute.biz>

--- aQute.libg/src/aQute/lib/io/Windows.java
+++ aQute.libg/src/aQute/lib/io/Windows.java
@@ -12,6 +12,7 @@ class Windows implements OS {
 	final static Pattern WINDOWS_BAD_FILE_NAME_P = Pattern.compile(
 		"(?:(:?.*[\u0000-\u001F<>:\"|/\\\\?*].*)|\\.\\.|CON|PRN|AUX|NUL|COM\\d|COM¹|COM²|COM³|LPT\\d|LPT¹|LPT²|LPT³)(?:\\.\\w+)?",
 		Pattern.CASE_INSENSITIVE);
+	final static Pattern	DRIVE_P					= Pattern.compile("/?(?<drive>[a-z]:)", Pattern.CASE_INSENSITIVE);
 
 	@Override
 	public File getBasedFile(File base, String subPath) throws IOException {
@@ -72,4 +73,13 @@ class Windows implements OS {
 		return sb.toString();
 	}
 
+	@Override
+	public File getFile(File base, String file) {
+		file = file.replace('\\', '/');
+		Matcher m = DRIVE_P.matcher(file);
+		if (m.lookingAt()) {
+			base = new File(m.group("drive"));
+		}
+		return Other.getFile0(base, file);
+	}
 }
