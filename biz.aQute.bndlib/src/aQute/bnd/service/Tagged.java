e990f0759e0fa020c43f2d6ed3f6a4a562baf794
Christoph Rueger <chrisrueger@gmail.com>
2024-05-17 23:18:49 +0200

    move matchesTags back to Tagged
    Somehow I think it belongs there.
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- biz.aQute.bndlib/src/aQute/bnd/service/Tagged.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/Tagged.java
@@ -13,4 +13,23 @@ public interface Tagged {
 		return Tags.NO_TAGS;
 	}
 
+	/**
+	 * @param <T>
+	 * @param obj
+	 * @param tags
+	 * @return <code>true</code> if the passed object matches any of the given
+	 *         tags, otherwise returns <code>false</code>
+	 */
+	static <T> boolean matchesTags(T obj, String... tags) {
+		if (obj instanceof Tagged tagged) {
+			Tags taggedTags = tagged.getTags();
+			for (String tag : tags) {
+				if (taggedTags.contains(tag)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 }
--- biz.aQute.bndlib/src/aQute/bnd/service/Tags.java
+++ biz.aQute.bndlib/src/aQute/bnd/service/Tags.java
@@ -43,23 +43,6 @@ public final class Tags extends LinkedHashSet<String> {
 			.collect(toCollection(LinkedHashSet::new)));
 	}
 
-	/**
-	 * @param <T>
-	 * @param obj
-	 * @param tags
-	 * @return <code>true</code> if the passed object matches any of the given
-	 *         tags, otherwise returns <code>false</code>
-	 */
-	public static <T> boolean matchesTags(T obj, String... tags) {
-		if (obj instanceof Tagged tagged) {
-			Tags taggedTags = tagged.getTags();
-			for (String tag : tags) {
-				if (taggedTags.contains(tag)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+
 
 }
