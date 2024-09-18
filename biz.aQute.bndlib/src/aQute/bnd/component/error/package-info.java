4c63652e6af9e04841beb009fd425799de6358c9
Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>
2024-09-18 14:34:22 +0930

    [ds] Place properties warning on class; extra warning for =
    Changed so that warnings about missing properties files are shown on the class itself
    rather than on the bnd.bnd file. Extra hint if filename contains an = sign.
    
    Fixes #6265
    
    Signed-off-by: Fr Jeremy Krieg <fr.jkrieg@greekwelfaresa.org.au>

--- biz.aQute.bndlib/src/aQute/bnd/component/error/package-info.java
+++ biz.aQute.bndlib/src/aQute/bnd/component/error/package-info.java
@@ -1,7 +1,7 @@
 /**
  * This package provides Declarative Service Annotation Error information.
  */
-@Version("1.8.0")
+@Version("1.9.0")
 package aQute.bnd.component.error;
 
 import org.osgi.annotation.versioning.Version;
