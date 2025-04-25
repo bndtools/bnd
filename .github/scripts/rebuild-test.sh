8bd0113a4c06f1c8a957a0f770d9190f881fdd65
Christoph Rueger <chrisrueger@gmail.com>
2025-04-25 23:42:10 +0200

    detect JDK for warning-mode in rebuild-test
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/rebuild-test.sh
+++ .github/scripts/rebuild-test.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 set -ev
 ./gradlew --no-daemon --version
-# Temporarily ignore warnings like "Deprecated Gradle features were used in this build, making it incompatible with Gradle 9.0."
+
+# JDK24: ignore warnings like "Deprecated Gradle features were used in this build, making it incompatible with Gradle 9.0."
 # so we use --warning-mode=all instead of --warning-mode=fail
-#./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_snapshots=./dist/bundles --warning-mode=fail :buildscriptDependencies :build :publish "$@"
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_snapshots=./dist/bundles --warning-mode=all :buildscriptDependencies :build :publish "$@"
+# < JDK24 use fail
+
+# Detect Java major version
+JAVA_VERSION=$(java -version 2>&1 | awk -F[\".] '/version/ {print $2}')
+echo "Detected Java version: $JAVA_VERSION"
+
+# Choose warning mode based on Java version
+if [ "$JAVA_VERSION" -ge 24 ]; then
+  WARNING_MODE="all"
+else
+  WARNING_MODE="fail"
+fi
+
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 -Pbnd_snapshots=./dist/bundles --warning-mode=$WARNING_MODE :buildscriptDependencies :build :publish "$@"
