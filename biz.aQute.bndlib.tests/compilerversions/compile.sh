9ad4f5e33bc128691be659c9af0441bf3450e014
Christoph Rueger <chrisrueger@gmail.com>
2025-07-12 16:45:08 +0200

    add JDK25 support
    used OpenJDK Early Access Build 31 (2025/7/11) https://jdk.java.net/25/
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- biz.aQute.bndlib.tests/compilerversions/compile.sh
+++ biz.aQute.bndlib.tests/compilerversions/compile.sh
@@ -7,6 +7,9 @@
 #
 # So for a new compiler, just create a new directory and compile it 
 # by hand. 
+# Attention: make sure you adjust the package 
+# when copying the ClassRef.java from an existing folder
+# otherwise you may encounter a "Classes found in the wrong directory" error.
 #
 # Then build the jar:
 #
@@ -14,6 +17,13 @@
 #
 # Make sure to add the class files and the compilerversions.jar to git! 
 #
+
+# Example: 
+# ~/jdks/jdk-25/Contents/Home/bin/javac --release 25 -cp src ~/git/bnd/biz.aQute.bndlib.tests/compilerversions/src/jdk_25/*.java
+# cd ~/git/bnd/biz.aQute.bndlib.tests/compilerversions
+# bnd buildx compilerversions.bnd
+# or
+# java -jar ../../biz.aQute.bnd/generated/biz.aQute.bnd.jar buildx compilerversions.bnd  
 
 # javac -target 1.1 -source 1.2 -cp src src/sun_1_1/*.java
 # javac -target 1.2 -source 1.2 -cp src src/sun_1_2/*.java
@@ -40,6 +50,7 @@
 # javac --release 22 -cp src src/jdk_22/*.java
 # javac --release 23 -cp src src/jdk_23/*.java
 # javac --release 24 -cp src src/jdk_24/*.java
+# javac --release 25 -cp src src/jdk_25/*.java
 
 # java -jar ../jar/ecj_3.2.2.jar -target 1.1 -source 1.3 -cp src src/eclipse_1_1/*.java
 # java -jar ../jar/ecj_3.2.2.jar -target 1.2 -source 1.3 -cp src src/eclipse_1_2/*.java
