13a1b154db5decc53b8a06613fed037f4549019b
Christoph Rueger <chrisrueger@gmail.com>
2025-09-17 01:04:23 +0200

    use old_pom.xml in CI Build
    we needed to rename /pom.xml to old_pom.xml because otherwise it will conflict / confuse tycho
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ev
 
-# this is importing the private key on the CI machine
+# gpg key handling for github action build
 if [[ -n "${GPG_PRIVATE_KEY}" && -n "${GPG_PASSPHRASE}" ]]; then
-    echo -e "#\n# GPG - importing private key on local machine\n#\n"
+    echo "GPG environment variables validated successfully"
     echo "${GPG_PRIVATE_KEY}" | \
     gpg --batch \
         --yes \
@@ -16,45 +16,9 @@ if [[ -n "${GPG_PRIVATE_KEY}" && -n "${GPG_PASSPHRASE}" ]]; then
     export GPG_TTY=$(tty)
 fi
 
-# verify that the GPG agent is working by signing and verifying a test message
-# and configure MAVEN_SIGNING_ARGS to use the GPG key and passphrase for signing, or skip signing if not configured
-if [[ -n "${GPG_KEY_ID}" && -n "${GPG_PASSPHRASE}" ]]; then
-    echo -e "#\n# GPG signing to configure and activate GPG agent\n#\n"
-    echo "test" | \
-    gpg --batch \
-        --yes \
-        --local-user "${GPG_KEY_ID}" \
-        --pinentry-mode loopback \
-        --passphrase "${GPG_PASSPHRASE}" \
-        --clearsign | \
-    gpg --verify
-    echo -e "#\n# GPG signing to configure and activate GPG agent\n#\n"
-    MAVEN_SIGNING_ARGS=" -Dgpg.keyname=${GPG_KEY_ID} -Dgpg.passphraseEnvName=GPG_PASSPHRASE"
-else 
-    echo -e "#\n# GPG signing environment variables not configured, SKIPPING GPG signing\n#\n"
-    MAVEN_SIGNING_ARGS=" -Dgpg.skip=true"
-fi
-
 # build
-echo -e "#\n# gradle and maven versions\n#\n"
 ./gradlew --no-daemon --version
 ./mvnw --version
-
-echo -e "#\n# build bnd and bndtools\n#\n"
-./gradlew \
-    --no-daemon \
-    -Dmaven.repo.local=dist/m2 \
-    -Dbnd.sonatype.release.description=${GITHUB_JOB}_${GITHUB_RUN_NUMBER} \
-    --continue \
-    build "$@"
-
-echo -e "#\n# build gradle plugins\n#\n"
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-
-echo -e "#\n# build maven plugins\n#\n"
-./mvnw \
-    -Dmaven.repo.local=dist/m2 \
-    --batch-mode \
-    --no-transfer-progress \
-    $MAVEN_SIGNING_ARGS \
-    install
+./mvnw -f old_pom.xml -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
