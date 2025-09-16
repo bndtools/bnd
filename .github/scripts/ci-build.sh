829400ae41e83e358d2f9e06bac908ea9e38f0cc
Christoph Rueger <chrisrueger@gmail.com>
2025-09-17 01:29:09 +0200

    set -Djdk.xml.maxGeneralEntitySizeLimit
    to fix `JAXP00010003: The length of entity "[xml]" is "100,001" that exceeds the "100,000" limit set by "jdk.xml.maxGeneralEntitySizeLimit"`
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -4,4 +4,4 @@ set -ev
 ./mvnw --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-./mvnw -f old_pom.xml -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
+./mvnw -f old_pom.xml -Djdk.xml.maxGeneralEntitySizeLimit=500000 -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
