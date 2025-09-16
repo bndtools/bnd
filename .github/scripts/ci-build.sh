fdf37d9eb8c8346352fbaa9eee70f46b5765eb29
Christoph Rueger <chrisrueger@gmail.com>
2025-09-17 01:04:23 +0200

    use old_pom.xml in CI Build
    we needed to rename /pom.xml to old_pom.xml because otherwise it will conflict / confuse tycho
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -21,4 +21,4 @@ fi
 ./mvnw --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-./mvnw -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
+./mvnw -f old_pom.xml -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
