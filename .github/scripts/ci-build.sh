4bf033607c6168849b9b156efd3a65f7a779d34c
Christoph Rueger <chrisrueger@gmail.com>
2025-09-17 05:31:06 +0200

    more xml limits
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/ci-build.sh
+++ .github/scripts/ci-build.sh
@@ -21,4 +21,4 @@ fi
 ./mvnw --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :build "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:build
-./mvnw -f old_pom.xml -Djdk.xml.maxGeneralEntitySizeLimit=500000 -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
+./mvnw -f old_pom.xml -Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 -Djdk.xml.entityExpansionLimit=0 -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress install
