e66b4545070d853005fd43df9339c7f7d9f3b61c
Christoph Rueger <chrisrueger@gmail.com>
2025-09-17 16:39:13 +0200

    build maven-plugins/pom.xml explicitly
    this ensures that tycho is only used when building in root with `mvn clean verify` but not when building with `mvn -f maven-plugins/pom.xml`
    adjust CI build scripts to call `mvn -f maven-plugins/pom.xml`
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/codeql-build.sh
+++ .github/scripts/codeql-build.sh
@@ -4,4 +4,4 @@ set -ev
 ./mvnw --version
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue testClasses :dist:jarDependencies "$@"
 ./gradlew --no-daemon -Dmaven.repo.local=dist/m2 --continue :gradle-plugins:testClasses
-./mvnw -f old_pom.xml -Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0 -Djdk.xml.entityExpansionLimit=0 -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress test-compile
+./mvnw -f maven-plugins/pom.xml -Dmaven.repo.local=dist/m2 --batch-mode --no-transfer-progress test-compile
