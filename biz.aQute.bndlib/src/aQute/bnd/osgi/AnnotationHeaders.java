6144325d22b72dc872e9d5040eece589fee8de7b
Christoph Rueger <chrisrueger@gmail.com>
2025-10-15 22:53:43 +0200

    do not concat BUNDLE_ACTIVATOR
    fixes a problem in Verifier.verifyActivator() ("The Bundle-Activator header only supports a single type. The following types were found") when the bnd instructions already define a Bundle-Activator but a processed jar also contributed a Bundle-Activator. Before this fix the Bundle-Activator header was concatenated to the existing Bundle-Activator header in bnd.bnd and thus contained multiple values (separated by comma). But for Bundle-Activator this does not make sense as it supports only a single value.
    
    Added Constants.BUNDLE_HEADERS_SINGLE_VALUE for a list of other headers which should also not be concatenated
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- biz.aQute.bndlib/src/aQute/bnd/osgi/AnnotationHeaders.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/AnnotationHeaders.java
@@ -146,6 +146,14 @@ class AnnotationHeaders extends ClassDataCollector implements Closeable {
 		PROVIDE_CAPABILITY, BUNDLE_CATEGORY, BUNDLE_DOC_URL, BUNDLE_DEVELOPERS, BUNDLE_CONTRIBUTORS, BUNDLE_COPYRIGHT,
 		STD_REQUIREMENT, STD_CAPABILITY, STD_HEADER);
 
+	/**
+	 * OSGi Bundle headers, which only allow a single value (i.e. not multiple
+	 * values concatenated by comma)
+	 */
+	private static final Set<String>	BUNDLE_HEADERS_SINGLE_VALUE	= Sets.of(Constants.BUNDLE_ACTIVATOR,
+		Constants.BUNDLE_ACTIVATIONPOLICY, Constants.BUNDLE_NAME, Constants.BUNDLE_SYMBOLICNAME,
+		Constants.BUNDLE_VERSION, Constants.BUNDLE_MANIFESTVERSION, Constants.MAIN_CLASS);
+
 	// Used to detect attributes and directives on Require-Capability and
 	// Provide-Capability
 	static final String					STD_ATTRIBUTE				= "org.osgi.annotation.bundle.Attribute";
@@ -1030,11 +1038,20 @@ class AnnotationHeaders extends ClassDataCollector implements Closeable {
 	/*
 	 * This method is a pass thru for the properties of the analyzer. If we have
 	 * such a header, we get the analyzer header and concatenate our values
-	 * after removing dups.
+	 * after removing dups. But there are exceptions: Some headers (e.g.
+	 * Bundle-Activator) only allow a single value and for them concatenating to
+	 * the analyzer header does not make sense.
 	 */
 
 	public String getHeader(String name) {
 		String value = analyzer.getProperty(name);
+
+		// for some OSGi headers we do not want to concatenate to existing
+		// analyzer value (analyzer value wins)
+		if (value != null && BUNDLE_HEADERS_SINGLE_VALUE.contains(name)) {
+			return value;
+		}
+
 		if (headers.containsKey(name)) {
 			//
 			// Remove duplicates and sort
