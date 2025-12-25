d05296b708c319875fe6560ed32dcbc6b389a8ea
Christoph Rueger <chrisrueger@gmail.com>
2025-12-25 10:10:13 +0100

    Update CI publish script for Sonatype release process
    Refactors the CI publish script to separate publishing of gradle-plugins and maven-plugins, adds steps to copy artifacts to cnf/cache/sonatype-release for Sonatype release, and updates the release process to sign and release from the new location. Includes debugging output for verification.
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/ci-publish.sh
+++ .github/scripts/ci-publish.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 set -ev
-./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :publish :gradle-plugins:publish "$@"
+# Old way pre new Sonatype release
+#./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :publish :gradle-plugins:publish "$@"
+
+# publish gradle-plugins to dist/bundles
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :gradle-plugins:publish "$@"
+
+# publish maven-plugins to dist/bundles
 ./mvnw -Dmaven.repo.local=dist/m2  --batch-mode -Pdist -Dreleaserepo=file:dist/bundles deploy
+
+# Hack for sonatype release: Copy all above to cnf/cache/sonatype-release
+# so that they get picked up by the workspace release process
+mkdir -p cnf/cache/sonatype-release && \
+cp -a \
+  dist/bundles/biz/aQute/bnd/bnd* \
+  dist/bundles/biz/aQute/bnd/biz* \
+  cnf/cache/sonatype-release/
+
+# Debugging: print cnf/cache/sonatype-release
+ls cnf/cache/sonatype-release
+
+# publish (release) bnd workspace, which signs and releases everything under cnf/cache/sonatype-release
+./gradlew --no-daemon -Dmaven.repo.local=dist/m2 :publish "$@"
