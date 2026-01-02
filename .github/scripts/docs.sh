7d43fd5f73ce6db9d59b512ebef4cc4ac021c603
Christoph Rueger <chrisrueger@gmail.com>
2026-01-02 12:29:06 +0100

    Exclude archived release docs from Pagefind index
    Update docs build scripts to remove 'data-pagefind-body' from HTML files in _site/releases, preventing archived release documentation from being indexed by Pagefind. This follows Pagefind's recommended approach for excluding pages from the search index.
    
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- .github/scripts/docs.sh
+++ .github/scripts/docs.sh
@@ -8,4 +8,8 @@ bundle exec jekyll build
 
 # create search index under _site/pagefind
 pwd
+# ensure the archived _site/releases docs from former release do not get indexed by pagefind
+# by removing the 'data-pagefind-body' from those pages
+# see https://pagefind.app/docs/indexing/#removing-pages-from-pagefinds-index
+find _site/releases -type f -name "*.html" -exec sed -i 's/data-pagefind-body//g' {} +
 ./pagefind --verbose --site _site --output-subdir pagefindindex
