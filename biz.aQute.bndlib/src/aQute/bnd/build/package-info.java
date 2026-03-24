1fd30f6c8185bd5269b78b1ed9254d6744d97652
Christoph Rueger <chrisrueger@gmail.com>
2026-03-24 17:22:17 +0100

    build: Build Release 7.2.3.RC1
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- biz.aQute.bndlib/src/aQute/bnd/build/package-info.java
+++ biz.aQute.bndlib/src/aQute/bnd/build/package-info.java
@@ -1,6 +1,6 @@
 /**
  */
-@Version("4.7.1")
+@Version("4.7.3")
 package aQute.bnd.build;
 
 import org.osgi.annotation.versioning.Version;
--- biz.aQute.bndlib/src/aQute/bnd/osgi/About.java
+++ biz.aQute.bndlib/src/aQute/bnd/osgi/About.java
@@ -76,7 +76,8 @@ public class About {
 	public static final Version					_7_2		= new Version(7, 2, 0);
 	public static final Version					_7_2_1			= new Version(7, 2, 1);
 	public static final Version					_7_2_2			= new Version(7, 2, 2);
-	public static final Version					CURRENT			= _7_2_2;
+	public static final Version					_7_2_3			= new Version(7, 2, 3);
+	public static final Version					CURRENT			= _7_2_3;
 
 	public static final String[]				CHANGES_7_2_1	= {
 		"See https://github.com/bndtools/bnd/wiki/Changes-in-7.2.1 for a list of changes."
