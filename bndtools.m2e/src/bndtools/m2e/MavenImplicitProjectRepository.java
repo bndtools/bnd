5315c912b7445c2b6681d4ffa0a4ba6c7cfec5a3
Christoph Rueger <chrisrueger@gmail.com>
2026-08-29 12:59:07 +0200

    fix warnings
    Signed-off-by: Christoph Rueger <chrisrueger@gmail.com>

--- bndtools.m2e/src/bndtools/m2e/MavenImplicitProjectRepository.java
+++ bndtools.m2e/src/bndtools/m2e/MavenImplicitProjectRepository.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.CompletableFuture;
 
 import org.apache.maven.project.MavenProject;
